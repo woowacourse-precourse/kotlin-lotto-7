ad80b918a185dc1be8b4815738a39a77b17f35b5
박지민 <pjm5489@naver.com>
2024-10-30 03:46:22 +0900

    feat(input): 사용자 입력화면 구현

---
+++ src/main/kotlin/lotto/view/Input.kt
@@ -0,0 +1,28 @@
+package lotto.view
+
+import lotto.constants.Constants.BONUS_NUMBER_MSG
+import lotto.constants.Constants.PURCHASED_QUANTITY_MSG
+import lotto.constants.Constants.PURCHASE_PRICE_MSG
+import lotto.constants.Constants.WINNING_NUMBERS_MSG
+import lotto.domain.Lotto
+
+class Input {
+    fun purchasePriceMsg() {
+        println(PURCHASE_PRICE_MSG)
+    }
+
+    fun winningNumbersMsg() {
+        println(WINNING_NUMBERS_MSG)
+    }
+
+    fun bonusNumberMsg() {
+        println(BONUS_NUMBER_MSG)
+    }
+
+    fun purchasedQuantityMsg(count: Int, lottos: List<String>) {
+        println("$count$PURCHASED_QUANTITY_MSG")
+        for (i in lottos.indices) {
+            println(lottos[i])
+        }
+    }
+}
