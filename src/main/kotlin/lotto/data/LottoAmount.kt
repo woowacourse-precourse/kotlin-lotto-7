dd9c64302669770d704094b666355207b91cd708
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-31 00:46:11 +0900

    refactor(LottoAmount.kt): 로또 구입 금액 유효성 검사 개선
    개선 내역
    - 금액 입력 값이 비어 있는 경우에 대한 검증 추가
    - 0으로 시작하지 않는 숫자로만 구성 여부에 대한 검증 분리

--- src/main/kotlin/lotto/data/LottoAmount.kt
+++ src/main/kotlin/lotto/data/LottoAmount.kt
@@ -2,14 +2,18 @@ package lotto.data
 
 class LottoAmount(private val amount: String) {
     init {
-        require(amount.toIntOrNull() != null) { AMOUNT_FORMAT_ERROR_MESSAGE }
+        require(amount.isNotEmpty()) { AMOUNT_EMPTY_ERROR_MESSAGE }
+        require(amount.all { it.isDigit() }) { AMOUNT_NOT_ALL_NUMBER_ERROR_MESSAGE }
+        require(amount.toIntOrNull() != null) { AMOUNT_RANGE_ERROR_MESSAGE }
         require(amount.first() in AMOUNT_START_MIN_VALUE..AMOUNT_START_MAX_VALUE) { AMOUNT_FORMAT_ERROR_MESSAGE }
         require(amount.toInt() % AMOUNT_UNIT == EXPECTED_RESULT) { AMOUNT_UNIT_ERROR_MESSAGE }
         require(amount.toInt() in AMOUNT_UNIT..AMOUNT_MAX_VALUE) { AMOUNT_RANGE_ERROR_MESSAGE }
     }
 
     companion object {
-        private const val AMOUNT_FORMAT_ERROR_MESSAGE = "[ERROR] 1000, 35000 과 같이 숫자로만 입력해주세요."
+        private const val AMOUNT_EMPTY_ERROR_MESSAGE = "[ERROR] 구입 금액이 입력되지 않았습니다. 구입 금액을 입력해주세요."
+        private const val AMOUNT_NOT_ALL_NUMBER_ERROR_MESSAGE = "[ERROR] 숫자로만 입력해주세요."
+        private const val AMOUNT_FORMAT_ERROR_MESSAGE = "[ERROR] 10000 과 같은 형태로 입력해주세요."
         private const val AMOUNT_UNIT_ERROR_MESSAGE = "[ERROR] 1,000원 단위로만 구매 가능합니다."
         private const val AMOUNT_RANGE_ERROR_MESSAGE = "[ERROR] 1,000원 ~ 100,000원 이내로만 구매 가능합니다."
         private const val AMOUNT_START_MIN_VALUE = '1'
