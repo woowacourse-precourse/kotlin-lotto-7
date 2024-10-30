dcc717d35c2c49e7b7baf137ef7ff6ca65443d40
chanho0908 <uni070@naver.com>
2024-10-31 02:37:06 +0900

    feat(Exception.kt) : 입력 범위를 초과할 때 상태 추가

--- src/main/kotlin/util/Exception.kt
+++ src/main/kotlin/util/Exception.kt
@@ -2,7 +2,8 @@ package util
 
 enum class Exception(private val msg: String) {
     EMPTY_INPUT("빈 값이 입력 되었어요."),
-    INVALID_INPUT("숫자만 입력해주세요.");
+    INVALID_INPUT("숫자만 입력해주세요."),
+    EXCEED_INPUT("번호의 숫자 범위는 1~45까지 입니다.");
 
     override fun toString(): String = "$ERROR $msg"
 
