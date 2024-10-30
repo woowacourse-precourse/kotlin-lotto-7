7948248073b3d8b1ccde2a6cbe435fb50571f374
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-30 16:04:17 +0900

    feat: 로또 범위 및 중복 유효성 검사
    생성자로 받은 숫자 배열의 각 원소에 대한 중복 및 범위에 대한 유효성 검사 기능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,21 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
+
     init {
+        val checkAlreadyUseNumber = mutableSetOf<Int>()
+        var isNotDuplicate = true
+
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.fold(true) { result, number ->
+            isNotDuplicate = isNotDuplicate && !checkAlreadyUseNumber.contains(number)
+            checkAlreadyUseNumber.add(number)
+
+            result && number in 1..45
+        }) { "[ERROR] 로또 번호는 1과 45 사이여야 합니다." }
+        require(isNotDuplicate) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
     }
 
     // TODO: 추가 기능 구현
