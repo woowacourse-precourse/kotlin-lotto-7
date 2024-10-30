2a722c500b0f487390d42e257776447d87086527
chanho0908 <uni070@naver.com>
2024-10-31 01:46:38 +0900

    test(InputTest.kt) : 구입 금액이 숫자가 아닐 때 테스트 추가

--- src/test/kotlin/lotto/InputTest.kt
+++ src/test/kotlin/lotto/InputTest.kt
@@ -6,22 +6,32 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
 import util.Exception
-import view.InputView
+import util.Process
 
 class InputTest {
     private lateinit var commonErrorDelegate: CommonErrorDelegate
 
     @BeforeEach
-    fun init(){
+    fun init() {
         commonErrorDelegate = CommonErrorDelegator()
     }
 
     @ParameterizedTest
     @EmptySource
-    fun `입력값이 비어잇을 때`(입력: String){
-        Assertions.assertThatThrownBy{ commonErrorDelegate.isEmpty(입력) }
+    fun `입력값이 비어잇을 때`(입력: String) {
+        Assertions.assertThatThrownBy { commonErrorDelegate.isEmpty(입력) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage(Exception.EMPTY_INPUT.toString())
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["abc", "ㄱㄴㄷ", "!@#$", "100,23,22"])
+    fun `구입 입력 입력값이 숫자가 아닐 때`(value: String) {
+        val type = Process.PAY
+        Assertions.assertThatThrownBy { commonErrorDelegate.isNumeric(value, type) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("${type}은(는) ${Exception.INVALID_INPUT}")
     }
 }
