35e28ed13d6d29f018686935f83b396dfc080c23
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-30 21:00:40 +0900

    test(CalculateLottoRankUseCaseTest): 로또의 등수를 반환하는 기능 테스트
    - 6개를 맞춰서 등수가 1등일 경우 테스트
    - 5개를 맞추고 보너스 번호를 맞춰서 등수가 2등일 경우 테스트
    - 2개를 맞춰서 등수가 6등일 경우 테스트

---
+++ src/test/kotlin/lotto/CalculateLottoRankUseCaseTest.kt
@@ -0,0 +1,35 @@
+package lotto
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CalculateLottoRankUseCaseTest {
+    private val calculateLottoRankUseCase = CalculateLottoRankUseCase()
+
+    @Test
+    fun `6개를 맞춰서 등수가 1등일 경우 테스트`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
+        val rank = calculateLottoRankUseCase.execute(lotto, winningNumbers)
+        val expect = 1
+        assertEquals(expect,rank)
+    }
+
+    @Test
+    fun `5개를 맞추고 보너스 번호를 맞춰서 등수가 2등일 경우 테스트`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
+        val rank = calculateLottoRankUseCase.execute(lotto, winningNumbers)
+        val expect = 2
+        assertEquals(expect,rank)
+    }
+
+    @Test
+    fun `2개를 맞춰서 등수가 6등일 경우 테스트`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
+        val rank = calculateLottoRankUseCase.execute(lotto, winningNumbers)
+        val expect = 2
+        assertEquals(expect,rank)
+    }
+}
