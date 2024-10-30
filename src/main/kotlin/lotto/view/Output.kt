93458a227bed119344e6709200cae2d8778d6419
박지민 <pjm5489@naver.com>
2024-10-30 19:51:16 +0900

    feat(ouput): 매개변수 추가

--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -4,8 +4,9 @@ import lotto.constants.Constants.PROFIT_RATE_MSG
 import lotto.constants.Constants.WINNING_RESULT_MSG
 
 class Output {
-    fun winningResultMsg() {
+    fun winningResultMsg(prizeMessages: List<String>) {
         println(WINNING_RESULT_MSG)
+        prizeMessages.forEach { println(it) }
     }
 
     fun profitRateMsg(profitRate: Float) {
