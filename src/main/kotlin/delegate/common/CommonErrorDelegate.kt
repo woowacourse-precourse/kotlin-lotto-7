0b57e036be91d5cfcb167bd8966a5f34e2b06037
chanho0908 <uni070@naver.com>
2024-10-31 01:51:28 +0900

    feat(CommonErrorDelegate.kt) : 입력값이 숫자인지 확인하는 함수 추가

--- src/main/kotlin/delegate/common/CommonErrorDelegate.kt
+++ src/main/kotlin/delegate/common/CommonErrorDelegate.kt
@@ -1,5 +1,8 @@
 package delegate.common
 
+import util.Process
+
 interface CommonErrorDelegate {
     fun isEmpty(input: String)
+    fun isNumeric(input: String, process: Process)
 }
--- src/main/kotlin/delegate/common/CommonErrorDelegator.kt
+++ src/main/kotlin/delegate/common/CommonErrorDelegator.kt
@@ -1,9 +1,16 @@
 package delegate.common
 
 import util.Exception
+import util.Process
+import util.isNumeric
 
 class CommonErrorDelegator: CommonErrorDelegate {
     override fun isEmpty(input: String) {
         require(input.isNotEmpty()){ Exception.EMPTY_INPUT }
+    }
+
+    override fun isNumeric(input: String, process: Process) {
+        val title = process.toString()
+        require(input.isNumeric()) { "${title}은(는) ${Exception.INVALID_INPUT}" }
     }
 }
