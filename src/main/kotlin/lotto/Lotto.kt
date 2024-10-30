6e0371462b93d1a7efdfa5935368cbe32372d1df
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-30 17:42:43 +0900

    feat(Lotto): 로또 번호의 예외 처리
    - 로또가 중복 될 경우 예외 발생
    - 로또의 번호가 45보다 클 경우 예외 발생
    - 로또의 번호가 1보다 작거나 경우 예외 발생

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,15 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        validateNumbers()
     }
 
-    // TODO: 추가 기능 구현
+    private fun validateNumbers() {
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == numbers.size) { "[ERROR] 로또 번호는 중복되면 안됩니다." }
+        numbers.forEach { number ->
+            require(number <= 45) { "[ERROR] 로또 번호는 45보다 작거나 같아야 합니다." }
+            require(number >= 1) { "[ERROR] 로또 번호는 1보다 크거나 같아야 합니다." }
+        }
+    }
 }
