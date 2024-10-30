c146c90f78b4b50fdec706e0f4c9de1eab32037c
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-30 19:41:03 +0900

    feat(User): 로또를 구매하는 기능 추가
    - 로또를 구매하는 기눙 추가
    - 로또를 생성하는 기능 추가

--- src/main/kotlin/lotto/User.kt
+++ src/main/kotlin/lotto/User.kt
@@ -1,10 +1,23 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class User(private val money: Int) {
     lateinit var lottoTickets: List<Lotto>
 
     init {
         validateMoney()
+        buyLottoTickets()
+    }
+
+    private fun buyLottoTickets() {
+        val lottoCount = money.div(1000)
+        lottoTickets = List(lottoCount) { createLotto() }
+    }
+
+    private fun createLotto(): Lotto {
+        val lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return Lotto(lottoNumbers)
     }
 
     private fun validateMoney() {
