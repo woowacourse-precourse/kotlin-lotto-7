163e99c4c0f94f40cfe43a6431d97719ba33ddb6
chanho0908 <uni070@naver.com>
2024-10-31 03:03:09 +0900

    feat(Exception.kt) : 구입 금액이 천원 단위가 아닐 때 상태 추가

--- src/main/kotlin/util/Exception.kt
+++ src/main/kotlin/util/Exception.kt
@@ -3,6 +3,7 @@ package util
 enum class Exception(private val msg: String) {
     EMPTY_INPUT("빈 값이 입력 되었어요."),
     INVALID_INPUT("숫자만 입력해주세요."),
+    INVALID_UNIT("구입 금액은 천원 단위로만 입력해주세요."),
     EXCEED_INPUT("번호의 숫자 범위는 1~45까지 입니다.");
 
     override fun toString(): String = "$ERROR $msg"
