0f3e386b5580b20c4cc01a55c9799d2f40a43709
박지민 <pjm5489@naver.com>
2024-10-30 20:01:19 +0900

    feat(lottocontroller): 로또게임 전체 흐름 구현

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,44 @@
+package lotto.controller
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.domain.InputNumbers
+import lotto.domain.LottoAmount
+import lotto.domain.Ranking
+import lotto.domain.WinningResult
+import lotto.view.Input
+import lotto.view.Output
+
+class LottoController {
+    private val input = Input()
+    private val output = Output()
+
+    fun run() {
+        input.purchasePriceMsg()
+        val purchasePrice = Console.readLine()
+
+        val lottoAmount = LottoAmount(purchasePrice)
+        input.purchasedQuantityMsg(lottoAmount.purchaseCount, lottoAmount.lottos)
+
+        input.winningNumbersMsg()
+        val winningNumbers = Console.readLine()
+
+        input.bonusNumberMsg()
+        val bonusNumber = Console.readLine()
+
+        val inputNumbers = InputNumbers(winningNumbers, bonusNumber)
+        val winningResult = WinningResult(lottoAmount.lottos)
+
+        totalResult(winningResult, inputNumbers, purchasePrice)
+    }
+
+    private fun getPrizeRankMsg(countByMatchCount: List<Int>): List<String> {
+        return Ranking.entries.mapIndexed { index, ranking ->
+            "${ranking.formattedMsg()}${countByMatchCount[index]}개"
+        }
+    }
+
+    private fun totalResult(winningResult: WinningResult, inputNumbers: InputNumbers, purchasedPrice: String) {
+        output.winningResultMsg(getPrizeRankMsg(winningResult.getMatchCount(inputNumbers)))
+        output.profitRateMsg(winningResult.rate(inputNumbers,purchasedPrice))
+    }
+}
