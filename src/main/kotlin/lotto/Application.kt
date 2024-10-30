51fbf7681151feddac7332569c89603c7b3eaa68
지원 <cheerfulboys@naver.com>
2024-10-30 15:40:09 +0900

    feat: 로또 번호를 추첨하는 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,10 +1,12 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
 
 fun main() {
     println("구입금액을 입력해 주세요.")
     val price = Console.readLine()
     val count = price.toInt() / 1000
     println("\n${count}개를 구매했습니다.")
+    val lottoNumbers = pickUniqueNumbersInRange(1, 45, 6)
 }
