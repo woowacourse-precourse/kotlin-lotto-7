a1cfaee6a2b5da6e012146aa08a78afa55fbdcfb
박지민 <pjm5489@naver.com>
2024-10-30 20:00:10 +0900

    feat(winningresult): 당첨 결과에 관한 로직 추가

---
+++ src/main/kotlin/lotto/domain/WinningResult.kt
@@ -0,0 +1,28 @@
+package lotto.domain
+
+class WinningResult(private val lottos: List<Lotto>) {
+    private val countByMatchCount = mutableListOf(0, 0, 0, 0, 0)
+
+    fun getMatchCount(inputNumbers: InputNumbers): List<Int> {
+        for (i in lottos.indices)
+            when (lottos[i].winningCount(inputNumbers)) {
+                3 -> countByMatchCount[0]++
+                4 -> countByMatchCount[1]++
+                5 -> bonusCount(inputNumbers, i)
+                6 -> countByMatchCount[4]++
+            }
+        return countByMatchCount
+    }
+
+    private fun bonusCount(inputNumbers: InputNumbers, i: Int): Int {
+        if (lottos[i].isBonus(inputNumbers)) return countByMatchCount[3]++
+        return countByMatchCount[2]++
+    }
+
+    fun rate(inputNumbers: InputNumbers, purchasedPrice: String): Float {
+        val totalPrize = Ranking.entries
+            .mapIndexed { index, ranking -> countByMatchCount[index] * ranking.prize }
+            .sum()
+        return totalPrize / purchasedPrice.toFloat() * 100
+    }
+}
