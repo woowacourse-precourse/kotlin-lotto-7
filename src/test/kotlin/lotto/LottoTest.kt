775683eab880091e1b3839a5187e53aae5968935
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-30 16:02:20 +0900

    test: 로또 범위 및 중복 테스트
    로또 숫자들의 중복 여부 및 범위 체크 테스트코드

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -16,6 +17,20 @@ class LottoTest {
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @Test
+    fun `로또 번호의 범위가 1이상 45이하가 아니면 예외가 발생한다`() {
+
+        assertRandomUniqueNumbersInRangeTest({
+            assertThrows<IllegalArgumentException> {
+                createLotto()
+            }
+        }, listOf(1, 2, 333, 4, 6,7))
+
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 22222, 3, 4, 5, 6))
         }
     }
 
