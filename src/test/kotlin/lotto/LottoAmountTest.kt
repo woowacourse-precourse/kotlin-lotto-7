00892ef3f9b946301c4bb530b49ed917ae020d50
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-31 00:35:00 +0900

    test(LottoAmountTest.kt): 로또 구입 금액 테스트 케이스 수정 및 추가
    - 숫자가 아닌 다른 문자를 포함한 경우에 대한 테스트 추가
    - 0으로 시작하는 경우에 대한 테스트를 별도로 분리
    - 구입 금액을 입력하지 않은 경우에 대한 테스트 추가

--- src/test/kotlin/lotto/LottoAmountTest.kt
+++ src/test/kotlin/lotto/LottoAmountTest.kt
@@ -9,11 +9,25 @@ import org.junit.jupiter.params.provider.ValueSource
 
 @DisplayName("구입 금액 테스트 케이스")
 class LottoAmountTest {
-    @DisplayName("0으로 시작하지 않는 숫자로만 이루어진 문자열이 아닌 경우")
+    @DisplayName("숫자가 아닌 다른 문자를 포함한 경우")
     @ParameterizedTest
+    @ValueSource(strings = ["abcd", "1,000", "+35000", "-45000", "10000.0", "천원", "일만", "십억?"])
+    fun amountContainsNonNumber(input: String) {
+        assertThrows<IllegalArgumentException> { LottoAmount(input) }
+    }
+
+    @DisplayName("0으로 시작하는 경우")
+    @ParameterizedTest
+    @ValueSource(strings = ["001000", "01000", "02500"])
+    fun amountStartZero(input: String) {
+        assertThrows<IllegalArgumentException> { LottoAmount(input) }
+    }
+
+    @DisplayName("구입 금액을 입력하지 않은 경우")
+    @ParameterizedTest
+    @ValueSource(strings = ["\n", "\t"])
     @EmptySource
-    @ValueSource(strings = ["abcd", "\n", "0123", "천원", "01000", "-10000", "+10000", "1,000", "99,000"])
-    fun amountFormat(input: String) {
+    fun amountEmpty(input: String) {
         assertThrows<IllegalArgumentException> { LottoAmount(input) }
     }
 
