7296e553823ebdca74aa867c38d8626cc10d2ccd
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-30 21:56:31 +0900

    feat(LottoRank): 로또 랭킹을 가지는 열거형 객체 추가

---
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -0,0 +1,10 @@
+package lotto
+
+enum class LottoRank(val price: Int) {
+    First(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    SIXTH(0)
+}
