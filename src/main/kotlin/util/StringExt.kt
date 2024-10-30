6398b6b645e7be8b0cba121cf8f16f70471c270c
chanho0908 <uni070@naver.com>
2024-10-31 03:11:50 +0900

    feat(String.kt) : 입력 금액이 천원 단위인지 판별하는 확장 함수 추가

--- src/main/kotlin/util/StringExt.kt
+++ src/main/kotlin/util/StringExt.kt
@@ -3,3 +3,7 @@ package util
 fun String.isNumeric(): Boolean{
     return this.toIntOrNull() != null
 }
+
+fun String.isThousandUnit(): Boolean{
+    return this != "0" && this.toInt() % 1000 == 0
+}
