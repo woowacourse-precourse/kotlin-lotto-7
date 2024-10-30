7d44f45ca6a401c322ee6a161b3e68af3ac3484a
chanho0908 <uni070@naver.com>
2024-10-31 01:07:48 +0900

    feat(Retry.kt) : 사용자가 잘못된 값을 입력할 경우 재시도를 위한 함수 생성

---
+++ src/main/kotlin/util/Retry.kt
@@ -0,0 +1,11 @@
+package util
+
+fun <T> retryWhenNoException(action: () -> T): T{
+    while (true){
+        try {
+            return action.invoke()
+        }catch (e: IllegalArgumentException){
+            println(e)
+        }
+    }
+}
