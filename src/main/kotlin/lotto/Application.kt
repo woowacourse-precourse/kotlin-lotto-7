4ec2bdfec71a02ff7ef4243c5ecec76f3856f0b3
지원 <cheerfulboys@naver.com>
2024-10-30 17:37:20 +0900

    feat: 로또 당첨을 확인하는 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
 
 fun main() {
+    var lotteys: MutableList<List<Int>> = mutableListOf()
+    val winningCount: MutableMap<String, Int> = mutableMapOf("1등" to 0, "2등" to 0,
+        "3등" to 0, "4등" to 0, "5등" to 0)
+
     println("구입금액을 입력해 주세요.")
     val price = Console.readLine()
     val count = price.toInt() / 1000
@@ -13,8 +17,10 @@ fun main() {
         val lottoNumbers = pickUniqueNumbersInRange(1, 45, 6)
         val sortedLottoNumbers = lottoNumbers.sorted()
         println(sortedLottoNumbers)
+        lotteys.add(sortedLottoNumbers)
     }
 
+    println(lotteys)
 
     println("당첨 번호를 입력해 주세요.")
     val winningNumber = Console.readLine()
@@ -23,6 +29,31 @@ fun main() {
     println("보너스 번호를 입력해 주세요.")
     val bonusNumber = Console.readLine()
 
+    for (lottey in lotteys) {
+        var win = 0
+        for (lotto in lottey) {
+            if (splitedWinningNumber.contains(lotto.toString())) {
+                win += 1
+            }
+        }
+        if (win == 5) {
+            for (lotto in lottey) {
+                if (bonusNumber.toInt() == lotto) {
+                    win = -1
+                }
+            }
+        }
+        when(win) {
+            6 -> winningCount["1등"] = winningCount["1등"]!! + 1
+            5 -> {
+                winningCount["3등"] = winningCount["3등"]!! + 1
+            }
+            4 -> winningCount["4등"] = winningCount["4등"]!! + 1
+            3 -> winningCount["5등"] = winningCount["5등"]!! + 1
+            -1 -> winningCount["2등"] = winningCount["2등"]!! + 1
+        }
+    }
+
     println("\n당첨 통계")
     println("---")
     println("3개 일치 (5,000원) - 1개\n" +
@@ -30,5 +61,4 @@ fun main() {
             "5개 일치 (1,500,000원) - 0개\n" +
             "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
             "6개 일치 (2,000,000,000원) - 0개")
-
 }
