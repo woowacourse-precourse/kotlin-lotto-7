9c59aec42aa5d8a669f558eaa317d6b36dfa363b
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-30 17:32:42 +0900

    test(Lotto): 로또 번호가 1~45까지가 아닐 경우 예외 발생 테스트
    - 로또 번호가 1보다 작을 경우 예외 발생 테스트 추가
    - 로또 번호가 45보다 클 경우 예외 발생 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -19,5 +19,17 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또 번호가 1보다 작을 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 45보다 클 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
 }
