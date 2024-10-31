18a3986526ec54c3bb8617d096fa02a9f6e93b91
박지민 <pjm5489@naver.com>
2024-10-31 15:14:24 +0900

    refactor(error): 숫자범위 에러 로직 간결화

--- src/main/kotlin/lotto/utils/Error.kt
+++ src/main/kotlin/lotto/utils/Error.kt
@@ -1,21 +1,18 @@
 package lotto.utils
 
-import lotto.constants.ErrorConstants.ERROR_BONUS_NUMBER_ONLY_NUMERIC
-import lotto.constants.ErrorConstants.ERROR_BONUS_NUMBER_RANGE
+import lotto.constants.ErrorConstants.ERROR_NUMBERS_COUNT
+import lotto.constants.ErrorConstants.ERROR_NUMBERS_DUPLICATE
+import lotto.constants.ErrorConstants.ERROR_NUMBER_ONLY_NUMERIC
+import lotto.constants.ErrorConstants.ERROR_NUMBER_RANGE
 import lotto.constants.ErrorConstants.ERROR_PRICE_NOT_MULTIPLE_OF_1000
-import lotto.constants.ErrorConstants.ERROR_PRICE_ONLY_NUMERIC
 import lotto.constants.ErrorConstants.ERROR_PURCHASE_PRICE_MINIMUM
-import lotto.constants.ErrorConstants.ERROR_WINNING_NUMBERS_COUNT
-import lotto.constants.ErrorConstants.ERROR_WINNING_NUMBERS_DUPLICATE
-import lotto.constants.ErrorConstants.ERROR_WINNING_NUMBERS_ONLY_NUMERIC
-import lotto.constants.ErrorConstants.ERROR_WINNING_NUMBERS_RANGE
 import lotto.constants.LottoConstants.COMMA
 
 object Error {
 
     fun priceError(purchasePrice: String) {
         val price = purchasePrice.toIntOrNull()
-            ?: throw IllegalArgumentException(ERROR_PRICE_ONLY_NUMERIC)
+            ?: throw IllegalArgumentException(ERROR_NUMBER_ONLY_NUMERIC)
         if (price % 1000 != 0)
             throw IllegalArgumentException(ERROR_PRICE_NOT_MULTIPLE_OF_1000)
         if (price < 1000)
@@ -26,21 +23,20 @@ object Error {
         val numbers = winningNumbers.split(COMMA)
             .map {
                 it.toIntOrNull()
-                    ?: throw IllegalArgumentException(ERROR_WINNING_NUMBERS_ONLY_NUMERIC)
+                    ?: throw IllegalArgumentException(ERROR_NUMBER_ONLY_NUMERIC)
             }
         if (numbers.size != 6)
-            throw IllegalArgumentException(ERROR_WINNING_NUMBERS_COUNT)
+            throw IllegalArgumentException(ERROR_NUMBERS_COUNT)
         if (numbers.toSet().size != 6)
-            throw IllegalArgumentException(ERROR_WINNING_NUMBERS_DUPLICATE)
-        numbers.forEach { number->
-            if (number !in 1..45) throw IllegalArgumentException(ERROR_WINNING_NUMBERS_RANGE)
-        }
+            throw IllegalArgumentException(ERROR_NUMBERS_DUPLICATE)
+        if (!numbers.all { it in 1..45 })
+            throw IllegalArgumentException(ERROR_NUMBER_RANGE)
     }
 
     fun bonusNumberError(bonusNumber: String) {
         val number = bonusNumber.toIntOrNull()
-            ?: throw IllegalArgumentException(ERROR_BONUS_NUMBER_ONLY_NUMERIC)
+            ?: throw IllegalArgumentException(ERROR_NUMBER_ONLY_NUMERIC)
         if (number !in 1..45)
-            throw IllegalArgumentException(ERROR_BONUS_NUMBER_RANGE)
+            throw IllegalArgumentException(ERROR_NUMBER_RANGE)
     }
 }
