5cad6b90bd84812a5786ad619ca2a1b62a7c5b1e
baekcii <bsy8470@gmail.com>
2024-10-31 09:37:18 +0900

    feat(lottoGenerator): 사용자가 구입한 만큼의 로또 생성

---
+++ src/main/kotlin/lotto/model/LottoGenerator.kt
@@ -0,0 +1,20 @@
+package lotto.model
+import camp.nextstep.edu.missionutils.*
+
+class LottoGenerator {
+
+    fun generateLotto(numberOfLotto : Int) : List<List<Int>>{
+        val randomLotto : MutableList<List<Int>> = mutableListOf()
+
+        for(i in 0 until numberOfLotto){
+            randomLotto.add(getRandomNumbers())
+        }
+        return randomLotto
+    }
+    fun getNumberOfLotto(money : String) : Int {
+        return money.toInt() / 1000
+    }
+    private fun getRandomNumbers():List<Int>{
+        return Randoms.pickUniqueNumbersInRange(1,45,6)
+    }
+}
