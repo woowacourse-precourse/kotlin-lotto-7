4df8220cc97f364ee19b32e85daed34189520e49
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-31 23:34:30 +0900

    feat(User):로또 티켓들의 복사본을 반환하는 기능 추가
    - 로또 티켓을 내부에서만 사용하도록 변경
    - 로또 티켓의 복사본을 반환하는 함수 추가

--- src/main/kotlin/lotto/User.kt
+++ src/main/kotlin/lotto/User.kt
@@ -3,7 +3,7 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class User(private val money: Int) {
-    lateinit var lottoTickets: List<Lotto>
+    private lateinit var lottoTickets: List<Lotto>
 
     init {
         validateMoney()
@@ -24,4 +24,6 @@ class User(private val money: Int) {
         require(money.rem(1000) == 0) { "[ERROR] 돈은 1000원으로 나누어 떨어져야 합니다." }
         require(money >= 0) { "[ERROR] 돈은 0보다 같거나 커야 합니다." }
     }
+
+    fun getLottoTickets() = lottoTickets.toList()
 }
