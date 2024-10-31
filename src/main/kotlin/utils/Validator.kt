7ebb40358ee2e2ddf954b953962736c2443f4a35
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-31 16:23:57 +0900

    feat(Validator): 당첨 번호의 유효성을 검증하는 로직 추가
    validateWinningNumbers
    - 당첨 번호는 쉼표로 나뉠 수 있어야 하고, 6개로 나뉘어져야 한다.
    - 당첨 번호는 1~45 사이의 숫자여야 한다.
    - 당첨 번호는 중복되어서는 안된다.

--- src/main/kotlin/utils/Validator.kt
+++ src/main/kotlin/utils/Validator.kt
@@ -13,5 +13,20 @@ object Validator {
         require(money % 1000 == 0) { "금액은 1,000원 단위로 입력해야 합니다." }
     }
 
+    fun validateWinningNumbers(inputWinningNumbers: String) {
+        val winningNumbers = inputWinningNumbers.split(",")
+
+        require(winningNumbers.size == 6) { "당첨 번호는 6개여야 합니다." }
+
+        val numbers = winningNumbers.map {
+            it.getInt()
+                ?: throw IllegalArgumentException("당첨 번호는 숫자로 입력해야 합니다.")
+        }
+
+        require(numbers.all { it in 1..45 }) { "당첨 번호는 1~45 사이의 숫자여야 합니다." }
+
+        require(numbers.toSet().size == 6) { "당첨 번호는 중복되어서는 안됩니다." }
+    }
+
 
 }
