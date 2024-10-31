9b644b3a97681291775f065485d8f6f358021ecb
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-31 18:02:55 +0900

    feat(BonusNumberValidator): enum class 로 보너스 번호의 유효성을 검증하는 로직 분리

---
+++ src/main/kotlin/utils/BonusNumberValidator.kt
@@ -0,0 +1,30 @@
+package utils
+
+import utils.ErrorMessages.BONUS_NUMBER_DUPLICATE_ERROR
+import utils.ErrorMessages.BONUS_NUMBER_NUMBER_ERROR
+import utils.ErrorMessages.BONUS_NUMBER_RANGE_ERROR
+import utils.ExtensionUtil.getInt
+
+enum class BonusNumberValidator {
+    SHOULD_BE_NUMBER {
+        override fun validate(bonusNumber: String, winningNumbers: List<Int>?) {
+            bonusNumber.getInt() ?: throw IllegalArgumentException(BONUS_NUMBER_NUMBER_ERROR)
+        }
+    },
+    SHOULD_BE_1_TO_45_NUMBER {
+        override fun validate(bonusNumber: String, winningNumbers: List<Int>?) {
+            require(bonusNumber.toInt() in 1..45) { BONUS_NUMBER_RANGE_ERROR }
+        }
+    },
+    SHOULD_NOT_BE_DUPLICATE {
+        override fun validate(bonusNumber: String, winningNumbers: List<Int>?) {
+            require(bonusNumber.toInt() !in winningNumbers!!) { BONUS_NUMBER_DUPLICATE_ERROR }
+        }
+    };
+
+    abstract fun validate(
+        bonusNumber: String = "",
+        winningNumbers: List<Int>? = null
+    )
+
+}
