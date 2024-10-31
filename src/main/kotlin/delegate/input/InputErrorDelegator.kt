e566b5c3b93bd64b0f595f06cc2bf0a5e19d1d6b
chanho0908 <uni070@naver.com>
2024-10-31 16:42:59 +0900

    feat(InputErrorDelegator.kt) : 당첨 번호의 개수가 6개가 아닐 때 예외 처리 추가

--- src/main/kotlin/delegate/input/InputErrorDelegator.kt
+++ src/main/kotlin/delegate/input/InputErrorDelegator.kt
@@ -10,8 +10,14 @@ class InputErrorDelegator : InputErrorDelegate {
         }
     }
 
+    override fun isInvalidLottoSize(input: List<String>) {
+        require(input.size == LOTTO_SIZE) { Exception.INVALID_SIZE }
+    }
+
+
     companion object {
         const val MIN_LOTTO_NUMBER = 1
         const val MAX_LOTTO_NUMBER = 45
+        const val LOTTO_SIZE = 6
     }
 }
