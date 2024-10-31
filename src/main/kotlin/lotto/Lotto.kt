a0a947a82f269ce8aebf7854470f3edb19c68aa3
지원 <cheerfulboys@naver.com>
2024-11-01 02:56:43 +0900

    feat(Lotto): 당첨 검증 check 메서드 및 보너스 번호 검증 isBonus 메서드 추가
    당첨 번호를 검증하여 당첨 개수를 반환하는 메서드와 보너스 번호 당첨 여부를 확인하는 메서드를 추가함.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -9,4 +9,18 @@ class Lotto(private val numbers: List<Int>) {
     fun sort(): List<Int> {
         return numbers.sorted()
     }
+
+    fun check(winningNumbers: List<Int>): Int {
+        var winningCount = 0
+        winningNumbers.forEach {
+            if (numbers.contains(it)) {
+                winningCount ++
+            }
+        }
+        return winningCount
+    }
+
+    fun isBonus(bonusNumber: Int): Boolean {
+        return numbers.contains(bonusNumber)
+    }
 }
