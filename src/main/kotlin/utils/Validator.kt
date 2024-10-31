a767b449df7246036b6eca9a0555acf80363f763
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-31 16:29:16 +0900

    feat(Validator): 보너스 번호의 유효성을 검증하는 로직 추가
    validateBonusNumber
    - 보너스 번호는 숫자이어야 한다.
    - 보너스 번호는 1~45 사이의 숫자이어야 한다.
    - 보너스 번호는 당첨 번호와 중복될 수 없다.

--- src/main/kotlin/utils/Validator.kt
+++ src/main/kotlin/utils/Validator.kt
@@ -28,5 +28,14 @@ object Validator {
         require(numbers.toSet().size == 6) { "당첨 번호는 중복되어서는 안됩니다." }
     }
 
+    fun validateBonusNumber(inputBonusNumber: String, winningNumbers: List<Int>) {
+        val bonusNumber =
+            inputBonusNumber.getInt() ?: throw IllegalArgumentException("보너스 번호는 숫자로 입력해야 합니다.")
+
+        require(bonusNumber in 1..45) { "보너스 번호는 1~45 사이의 숫자여야 합니다." }
+        require(bonusNumber in winningNumbers) { "보너스 번호는 당첨 번호와 중복될 수 없습니다." }
+
+    }
+
 
 }
