ef708967ef401b348ede3bcdb091e7835c2f2510
HamBeomJoon <hbj0802@naver.com>
2024-10-31 18:08:33 +0900

    style(OutputView): 로또 구입 수량 출력 띄어쓰기 수정

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,5 +1,5 @@
 package lotto.view
 
 class OutputView {
-    fun printLottoAmountMessage(amount: Int) = println("$amount 개를 구매했습니다.")
+    fun printLottoAmountMessage(amount: Int) = println("${amount}개를 구매했습니다.")
 }
