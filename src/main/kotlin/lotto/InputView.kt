8b63a6a9f90eea55c778af11ff8de8aae2cfcac2
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-31 21:59:45 +0900

    style(InputView): 'private'와 'const' 키워드 순서 변경
    - 'const val'선언 시 'private' 접근 제한자를 앞에 위치시켜 코드 스타일 권장 사항을 따랐습니다.

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -26,10 +26,10 @@ class InputView {
     }
 
     companion object InputMessage {
-        const private val INPUT_INFORMATION_MESSAGE = "안녕하세요!\n채채의 로또 판매점에 오신것을 환영합니다\n로또 번호 한줄당 1000원입니다\n오늘도 LUCKY한 하루되세요!"
-        const private val INPUT_PURCHASE_AMOUNT_MESSAGE = "아래의 사항을 준수해 로또 구입 금액을 입력하고 enter를 눌러주세요."
-        const private val INPUT_ONLY_NUMBER_MESSAGE = "• 숫자만 입력해 주세요."
-        const private val INPUT_OVER_1000_MESSAGE = "• 구입 금액은 1000원 이상 입력해 주세요."
-        const private val INPUT_1000_UNITS_MESSAGE = "• 구입 금액은 1000원 단위로 입력해 주세요."
+        private const val INPUT_INFORMATION_MESSAGE = "안녕하세요!\n채채의 로또 판매점에 오신것을 환영합니다\n로또 번호 한줄당 1000원입니다\n오늘도 LUCKY한 하루되세요!"
+        private const val INPUT_PURCHASE_AMOUNT_MESSAGE = "아래의 사항을 준수해 로또 구입 금액을 입력하고 enter를 눌러주세요."
+        private const val INPUT_ONLY_NUMBER_MESSAGE = "• 숫자만 입력해 주세요."
+        private const val INPUT_OVER_1000_MESSAGE = "• 구입 금액은 1000원 이상 입력해 주세요."
+        private const val INPUT_1000_UNITS_MESSAGE = "• 구입 금액은 1000원 단위로 입력해 주세요."
     }
 }
