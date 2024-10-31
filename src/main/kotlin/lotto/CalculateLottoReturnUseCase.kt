833f4a4d01aa3bcd3e4d9b6f0d740321fe9db7ae
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-31 22:07:10 +0900

    feat(CalculateLottoReturnUseCase): 로또의 수익률을 계산하는 기능
    - 수익률을 계산하는 기능 추가
    - 당첨금과 로또의 총 지출을 통해 수익률을 반환하는 기능 추가

---
+++ src/main/kotlin/lotto/CalculateLottoReturnUseCase.kt
@@ -0,0 +1,17 @@
+package lotto
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class CalculateLottoReturnUseCase {
+    fun execute(winningPrizes: List<Int>, totalLottoCost: Int): String {
+        val totalWinningPrizes = winningPrizes.sumOf { it.toBigDecimal() }
+        val lottoProfitRate = calculateProfitRate(totalWinningPrizes, totalLottoCost.toBigDecimal())
+        return "${lottoProfitRate.stripTrailingZeros()}%"
+    }
+
+    private fun calculateProfitRate(profit: BigDecimal, expenditure: BigDecimal) =
+        profit.divide(expenditure, 3, RoundingMode.HALF_UP).multiply(
+            BigDecimal(100)
+        )
+}
