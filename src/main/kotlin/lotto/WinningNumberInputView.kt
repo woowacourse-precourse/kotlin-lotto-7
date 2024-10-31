16500f4771a86c4c777add476f52aaed25d14749
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-31 22:39:14 +0900

    feat(WinningNumberInputView): 당첨에 필요한 번호를 입력 받는 화면 구현
    - 당첨 번호를 입력 받는다
    - 보너스 번호를 입력 받는다

---
+++ src/main/kotlin/lotto/WinningNumberInputView.kt
@@ -0,0 +1,9 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+
+class WinningNumberInputView {
+    fun inputWinningNumbers() = Console.readLine()
+
+    fun inputBonusNumber() = Console.readLine()
+}
