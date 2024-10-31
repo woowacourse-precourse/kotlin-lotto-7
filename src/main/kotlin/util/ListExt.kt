f70f4d098824576e11bea5a6562667d52c8dff00
chanho0908 <uni070@naver.com>
2024-11-01 00:18:12 +0900

    feat(ListExt.kt) : 로또 결과를 대괄로로 묶어서 출력하는 확장 함수 추가

--- src/main/kotlin/util/ListExt.kt
+++ src/main/kotlin/util/ListExt.kt
@@ -1,5 +1,15 @@
 package util
 
-fun<T> List<T>.toMapByEachCount(): Map<T, Int> {
+fun <T> List<T>.toMapByEachCount(): Map<T, Int> {
     return this.groupingBy { it }.eachCount()
+}
+
+fun <T> List<T>.printWithSquareBracket() {
+    println(
+        this.joinToString(
+            separator = ", ",
+            prefix = "[",
+            postfix = "]"
+        )
+    )
 }
