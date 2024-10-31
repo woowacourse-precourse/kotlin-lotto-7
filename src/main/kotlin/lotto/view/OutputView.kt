6b2c06a3eafdd2ab007792d788e5c3658d3859fc
DaeYoung Jeong <eodud1025@naver.com>
2024-10-31 15:46:41 +0900

    feat(OutPutView): 구입한 로또 출력 기능

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,10 +1,19 @@
 package lotto.view
 
+import lotto.model.Random
+
 class OutputView {
+    val randomGenerator = Random()
     fun printPurchaseLottoCount(money: Int) {
         val lottoCount = money / 1000
         println("${lottoCount}$PRINT_PURCHASE_LOTTO_COUNT_MESSAGE")
         println()
+    }
+
+    fun printPurchaseLotto(lottos: List<List<Int>>) {
+        for (lotto in lottos.indices) {
+            println(lotto)
+        }
     }
 
     companion object {
