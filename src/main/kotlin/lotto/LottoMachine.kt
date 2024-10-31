ae0b55c8ddf9c2c6cec61cdde5ea14de79e233be
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-31 13:44:21 +0900

    feat: 로또 등수 enum 생성
    로또 등수 정보를 갖고 있는 enum 생성
    가격과 설명 데이터가 있다.

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -7,4 +7,16 @@ class LottoMachine {
 
     private fun drawNumbers(): List<Int> =
         Randoms.pickUniqueNumbersInRange(1, 45, 6)
+
+    enum class Rank(
+        val winningPrice: Int,
+        val description: String
+    ) {
+        FIRST(2000000000, "6개 일치"),
+        SECOND(30000000, "5개 일치, 보너스 볼 일치"),
+        THIRD(1500000, "5개 일치"),
+        FORTH(50000, "4개 일치"),
+        FIFTH(5000, "3개 일치"),
+        LOSE(0, "낙첨")
+    }
 }
