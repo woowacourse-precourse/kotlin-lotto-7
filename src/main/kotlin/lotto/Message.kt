e26f614371c1c5c95d5b25084f2950d83b496cab
unknown <yangjk9203@naver.com>
2024-10-31 16:01:13 +0900

    feat(Message): 출력 메시지 모음

---
+++ src/main/kotlin/lotto/Message.kt
@@ -0,0 +1,19 @@
+package lotto
+
+class Message {
+    companion object{
+        const val ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+        const val NUMBER_OF_LOTTO = "개를 구매했습니다."
+        const val ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+        const val ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+        const val WINNING_STATISTICS = "당첨 통계"
+        const val FIFTH_PLACE_WINNER = "3개 일치 (5,000원) - "
+        const val FOURTH_PLACE_WINNER = "4개 일치 (50,000원) - "
+        const val THIRD_PLACE_WINNER = "5개 일치 (1,500,000원) - "
+        const val SECOND_PLACE_WINNER = "5개 일치 (30,000,000원) - "
+        const val FIRST_PLACE_WINNER = "6개 일치 (2.000.000,000원) - "
+        const val WINNER_COUNT_UNIT = "개"
+        const val TOTAL_RETURN_INTRO = "총 수익률은 "
+        const val TOTAL_RETURN_OUTRO = "%입니다."
+    }
+}
