941abc6c05456a4b10a7dec853ab5f55dd63d3c0
DoableTuple <doabletuple@gmail.com>
2024-10-31 22:45:19 +0900

    test: 구입 금액이 로또의 가격으로 나누어 떨어짐을 검증하는 기능 테스트

--- src/test/kotlin/lotto/InputValidatorTest.kt
+++ src/test/kotlin/lotto/InputValidatorTest.kt
@@ -6,11 +6,17 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 import lotto.util.InputValidator.validateInputIsNumeric
+import lotto.util.InputValidator.validateMoneyIsDivisible
 
 class InputValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = ["", "abc"])
     fun `숫자로 변환될 수 없는 문자열이 입력된 경우 예외가 발생한다`(input: String) {
         assertThrows<IllegalArgumentException> { validateInputIsNumeric(input) }
+    }
+
+    @Test
+    fun `구매 금액이 로또의 가격으로 나누어 떨어지지 않을 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { validateMoneyIsDivisible(1001) }
     }
 }
