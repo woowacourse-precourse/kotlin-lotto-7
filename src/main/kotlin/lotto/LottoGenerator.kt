13f311ca244923549a731340021f10f56b1ad876
DDangDin <Kcorelec7123@gmail.com>
2024-10-31 19:41:12 +0900

    fix: 로또 숫자 발행 시작 범위 수정
    - 로또 숫자 발행 시작 범위 0으로 잘못 명시 되어 있어서 1로 수정

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -26,7 +26,7 @@ class LottoGenerator(private val purchasePrice: Int) {
     }
 
     companion object {
-        private const val START_NUMBER = 0
+        private const val START_NUMBER = 1
         private const val END_NUMBER = 45
         private const val PICK_COUNT = 6
     }
