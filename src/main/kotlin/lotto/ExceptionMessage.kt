193c5684925f4e9248b806de5245309dd6daf7c3
unknown <yangjk9203@naver.com>
2024-10-31 20:03:09 +0900

    feat(ExceptionMessage): 오류 메시지 상수화

---
+++ src/main/kotlin/lotto/ExceptionMessage.kt
@@ -0,0 +1,9 @@
+package lotto
+
+class ExceptionMessage {
+    companion object {
+        const val BLANK = "[ERROR] 구입 금액을 입력해 주세요."
+        const val NOT_NUMBERS = "[ERROR] 숫자만 입력 가능합니다."
+        const val NOT_DIVIDE_THOUSAND = "[ERROR] 천원 단위로 입력해 주세요."
+    }
+}
