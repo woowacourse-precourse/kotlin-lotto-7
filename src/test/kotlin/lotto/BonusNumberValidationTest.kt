e70d1615408e03812ff45a355b2f17669157b929
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-31 18:32:56 +0900

    test(BonusNumberValidationTest): 보너스 번호 테스트 케이스 추가 및 성공
    - 1~45 사이의 숫자
    - 당첨 번호와 중복 불가능

---
+++ src/test/kotlin/lotto/BonusNumberValidationTest.kt
@@ -0,0 +1,34 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import utils.BonusNumberValidator
+
+class BonusNumberValidationTest {
+    @Test
+    fun `보너스 번호는 숫자이어야 한다`() {
+        val bonusNumber = "보너스"
+
+         assertThrows<IllegalArgumentException> {
+             BonusNumberValidator.SHOULD_BE_NUMBER.validate(bonusNumber)
+         }
+    }
+    @Test
+    fun `보너스 번호는 1~45 사이의 숫자이어야 한다`(){
+        val bonusNumber = "46"
+
+        assertThrows<IllegalArgumentException> {
+            BonusNumberValidator.SHOULD_BE_1_TO_45_NUMBER.validate(bonusNumber)
+        }
+    }
+    @Test
+    fun `보너스 번호는 당첨 번호와 중복되어서는 안된다`(){
+        val bonusNumber = "1"
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+        assertThrows<IllegalArgumentException> {
+            BonusNumberValidator.SHOULD_NOT_BE_DUPLICATE.validate(bonusNumber, winningNumbers)
+        }
+    }
+
+}
