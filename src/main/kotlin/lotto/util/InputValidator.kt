5c19d82bf21f711ccd62e1e63f33d30e77c1c86e
DoableTuple <doabletuple@gmail.com>
2024-10-31 22:41:59 +0900

    refactor: 에러 메시지 변수명 변경

--- src/main/kotlin/lotto/util/InputValidator.kt
+++ src/main/kotlin/lotto/util/InputValidator.kt
@@ -2,10 +2,10 @@ package lotto.util
 
 object InputValidator {
     fun validateInputIsNumeric(input: String) {
-        if (input.toIntOrNull() == null) throw IllegalArgumentException(ErrorMessage.ERROR_INPUT_NOT_NUMERIC)
+        if (input.toIntOrNull() == null) throw IllegalArgumentException(ErrorMessage.INPUT_NOT_NUMERIC)
     }
 
     fun validateMoneyIsDivisible(lottoMoney: Int) {
-        if (lottoMoney % Constant.LOTTO_PRICE != 0) throw IllegalArgumentException(ErrorMessage.ERROR_MONEY_NOT_DIVISIBLE)
+        if (lottoMoney % Constant.LOTTO_PRICE != 0) throw IllegalArgumentException(ErrorMessage.MONEY_NOT_DIVISIBLE)
     }
 }
