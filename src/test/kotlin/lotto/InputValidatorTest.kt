a37dcaaf7093e72dfabe1354f7058a2e007660ef
DoableTuple <doabletuple@gmail.com>
2024-10-31 22:59:57 +0900

    test: 구입 금액이 음수가 아님을 검증하는 기능 테스트

--- src/test/kotlin/lotto/InputValidatorTest.kt
+++ src/test/kotlin/lotto/InputValidatorTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 import lotto.util.InputValidator.validateInputIsNumeric
+import lotto.util.InputValidator.validateMoneyIsNotNegative
 import lotto.util.InputValidator.validateMoneyIsDivisible
 
 class InputValidatorTest {
@@ -13,6 +14,11 @@ class InputValidatorTest {
     @ValueSource(strings = ["", "abc"])
     fun `숫자로 변환될 수 없는 문자열이 입력된 경우 예외가 발생한다`(input: String) {
         assertThrows<IllegalArgumentException> { validateInputIsNumeric(input) }
+    }
+
+    @Test
+    fun `구매 금액이 음수일 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { validateMoneyIsNotNegative(-1000) }
     }
 
     @Test
