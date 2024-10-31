bee5f438b6bbbdf4bd06b390fe49a5f7bcca14fe
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-31 13:58:21 +0900

    feat: 로또 등수 계산 기능
    로또 번호 목록과 당첨 번호 데이터에 기반하여 등수를 산출한다.

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -5,6 +5,101 @@ import camp.nextstep.edu.missionutils.Randoms
 class LottoMachine {
     fun createLotto(): Lotto = Lotto(drawNumbers())
 
+    fun lottoRank(
+        numbers: List<Int>,
+        winningNumbers: List<Int>,
+        bonus: Int
+    ): Rank {
+        validateWinningInformation(winningNumbers, bonus)
+
+        val normalWinningNumberGroup = winningNumbers.toSet()
+        val winningNumberMatchCount = countNormalNumberMatch(numbers, normalWinningNumberGroup)
+        var bonusMatch = false
+
+        for (number in numbers) {
+            if (number == bonus) {
+                bonusMatch = true; break
+            }
+        }
+
+        return calculateRank(winningNumberMatchCount, bonusMatch)
+    }
+
+    private fun calculateRank(winningNumberMatchCount: Int, bonusMatch: Boolean): Rank {
+        return when (winningNumberMatchCount) {
+            6 -> {
+                Rank.FIRST
+            }
+
+            5 -> {
+                if (bonusMatch) {
+                    return Rank.SECOND
+                }
+                return Rank.THIRD
+            }
+
+            4 -> {
+                Rank.FORTH
+            }
+
+            3 -> {
+                Rank.FIFTH
+            }
+
+            else -> {
+                Rank.LOSE
+            }
+        }
+    }
+
+    private fun validateWinningInformation(
+        winningNumbers: List<Int>,
+        bonusNumber: Int
+    ) {
+        if (winningNumbers.size != 6) {
+            throw IllegalArgumentException("[ERROR] 로또 당첨 번호는 6개여야 합니다.")
+        }
+        val usedNumber = mutableSetOf<Int>()
+        val allNumbers = listOf(*winningNumbers.toTypedArray(), bonusNumber)
+
+        for (number in allNumbers) {
+            if (!numberRangeValidate(number)) {
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1 이상 45 이하여야 합니다.")
+            }
+            if (!numberDuplicateValidate(number, usedNumber)) {
+                throw IllegalArgumentException("[ERROR] 중복된 로또 당첨 번호입니다.")
+            }
+
+            usedNumber.add(number)
+        }
+    }
+
+    private fun countNormalNumberMatch(
+        numbers: List<Int>,
+        winningNumbers: Set<Int>
+    ): Int {
+        var count = 0
+
+        for (number in numbers) {
+            if (winningNumbers.contains(number)) {
+                count++
+            }
+        }
+
+        return count
+    }
+
+    private fun numberRangeValidate(number: Int): Boolean {
+        return number in 1..45
+    }
+
+    private fun numberDuplicateValidate(
+        number: Int,
+        usedNumber: Set<Int>
+    ): Boolean {
+        return !usedNumber.contains(number)
+    }
+
     private fun drawNumbers(): List<Int> =
         Randoms.pickUniqueNumbersInRange(1, 45, 6)
 
