edbc8afc16d759fd7495b6538c3ef1aa61b33ac6
DDangDin <Kcorelec7123@gmail.com>
2024-10-31 19:07:17 +0900

    feat: 로또 구입 금액 입력하는 기능 추가
    - 구입 금액을 입력할 때 출력되는 텍스트는 LottoOutputText에서 통합적으로 관리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,11 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import lotto.util.LottoOutputText
+
 fun main() {
-    // TODO: 프로그램 구현
+    // TODO: 예외 발생 후에도 항상 해당 부분 부터 다시 입력을 받는다.
+    println(LottoOutputText.INPUT_PURCHASE_PRICE_TEXT)
+    val purchasePrice = Console.readLine().toInt()  // TODO: 입력 예외 처리 필요
+    println()
 }
---
+++ src/main/kotlin/lotto/util/LottoOutputText.kt
@@ -0,0 +1,5 @@
+package lotto.util
+
+object LottoOutputText {
+    const val INPUT_PURCHASE_PRICE_TEXT = "구입금액을 입력해 주세요."
+}
