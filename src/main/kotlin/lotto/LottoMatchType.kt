4d496c34133ca2b0082b3b1f776058b08f8a01cb
이용인 <yongin0517@gmail.com>
2024-11-01 01:03:28 +0900

    feat: 프로퍼티 및 함수 추가

--- src/main/kotlin/lotto/LottoMatchType.kt
+++ src/main/kotlin/lotto/LottoMatchType.kt
@@ -1,11 +1,17 @@
 package lotto
 
-enum class LottoMatchType(val matchCount: Int, val reward: String, val description: String) {
-    THREE(3, "5,000원", "3개 일치"),
-    FOUR(4, "50,000원", "4개 일치"),
-    FIVE(5, "1,500,000원", "5개 일치"),
-    FIVE_BONUS(5, "30,000,000원", "5개 일치, 보너스 볼 일치"),
-    SIX(6, "2,000,000,000원", "6개 일치");
+enum class LottoMatchType(val matchCount: Int, val reward: String, val description: String, val hasBonus: Boolean) {
+    THREE(3, "5,000원", "3개 일치", false),
+    FOUR(4, "50,000원", "4개 일치", false),
+    FIVE(5, "1,500,000원", "5개 일치", false),
+    FIVE_BONUS(5, "30,000,000원", "5개 일치, 보너스 볼 일치", true),
+    SIX(6, "2,000,000,000원", "6개 일치", false);
 
     override fun toString(): String = "$description ($reward)"
+
+    companion object {
+        fun getMatchType(matchCount: Int, hasBonus: Boolean): LottoMatchType? {
+            return values().find { it.matchCount == matchCount && it.hasBonus == hasBonus }
+        }
+    }
 }
