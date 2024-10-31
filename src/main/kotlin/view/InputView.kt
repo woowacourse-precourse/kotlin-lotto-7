9fac18ff84e28573744ebb0683454b2f330ce6aa
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-31 18:44:37 +0900

    feat(InputView): 입력 메시지 상수화

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,25 +1,28 @@
 package view
 
 import camp.nextstep.edu.missionutils.Console
+import utils.InputMessages.BONUS_NUMBER_INPUT_MESSAGE
+import utils.InputMessages.MONEY_INPUT_MESSAGE
+import utils.InputMessages.WINNING_NUMBERS_INPUT_MESSAGE
 import utils.validator.Validator
 
 object InputView {
     fun getMoney(): Int {
-        println("구입금액을 입력해 주세요.")
+        println(MONEY_INPUT_MESSAGE)
         val inputMoney = Console.readLine()
 
         return Validator.validateMoney(inputMoney)
     }
 
     fun getWinningNumbers(): List<Int> {
-        println("당첨 번호를 입력해 주세요.")
+        println(WINNING_NUMBERS_INPUT_MESSAGE)
         val inputWinningNumbers = Console.readLine()
 
         return Validator.validateWinningNumbers(inputWinningNumbers)
     }
 
     fun getBonusNumber(winningNumbers: List<Int>): Int {
-        println("보너스 번호를 입력해 주세요.")
+        println(BONUS_NUMBER_INPUT_MESSAGE)
         val inputBonusNumber = Console.readLine()
 
         return Validator.validateBonusNumber(inputBonusNumber, winningNumbers)
