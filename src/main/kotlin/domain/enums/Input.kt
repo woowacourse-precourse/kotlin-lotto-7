82bb095ee977a6d87623ffdc96f6087793cd3948
chanho0908 <uni070@naver.com>
2024-10-31 20:44:39 +0900

    feat(Input.kt) : 입력 상태 추가

--- src/main/kotlin/domain/enums/Input.kt
+++ src/main/kotlin/domain/enums/Input.kt
@@ -1,7 +1,9 @@
 package domain.enums
 
 enum class Input(private val msg: String) {
-    INPUT_PAY("구입금액을 입력해 주세요.");
+    INPUT_PAY("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     override fun toString(): String = msg
 }
