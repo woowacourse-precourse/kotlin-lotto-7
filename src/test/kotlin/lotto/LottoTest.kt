10a38bed27ed413344ebe4de360cdfe2ce95575b
Jang Muyeong <jmy8876@gmail.com>
2024-10-31 15:37:44 +0900

    test: 당첨, 보너스 번호 입력 예외 처리 테스트 추가
    - 당첨 번호의 숫자가 1~45 사이가 아닐 때
    - 보너스 번호의 숫자가 1~45 사이가 아닐 때
    - 입력받은 로또 번호와 보너스 번호가 중복될 때

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.Lotto
+import lotto.domain.WinningLotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,5 +21,28 @@ class LottoTest {
         }
     }
 
+    @Test
+    fun `로또 번호의 숫자가 1부터 45 사이가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `보너스 번호의 숫자가 1부터 45 사이가 아니면 예외가 발생한다`() {
+         assertThrows<IllegalArgumentException> {
+             WinningLotto(listOf(1,2,3,4,5,6),46)
+         }
+    }
+
+    @Test
+    fun `입력받은 로또 번호와 보너스 번호가 중복되면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            WinningLotto(listOf(1,2,3,4,5,6),6)
+        }
+    }
+
+
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
 }
