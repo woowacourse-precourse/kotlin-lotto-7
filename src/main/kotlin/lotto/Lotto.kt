e5abe0dd311f86158605f6346fd6646d7c79e134
이용인 <yongin0517@gmail.com>
2024-11-01 00:53:49 +0900

    feat: Lotto 리스트 접근 메소드 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,16 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == 6) { ERROR_INVALID_NUMBER_COUNT }
     }
 
-    // TODO: 추가 기능 구현
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
+
+    companion object {
+        const val ERROR_INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다."
+    }
 }
