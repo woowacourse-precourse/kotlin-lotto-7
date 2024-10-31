d21051c4d55ff5f527b6d55ae3f40804f470e80f
yrsel <xxknu88@gmail.com>
2024-10-31 18:20:32 +0900

    fix(Purchase): 접근자 변경
    생성자의 접근자 수정,  메서드를 통해 데이터 전달

--- src/main/kotlin/lotto/domain/purchase/Purchase.kt
+++ src/main/kotlin/lotto/domain/purchase/Purchase.kt
@@ -2,13 +2,17 @@ package lotto.domain.purchase
 
 import lotto.validator.PurchaseAmountValidator
 
-class Purchase private constructor(val amount: Int) {
+class Purchase private constructor(private val amount: Int) {
 
     companion object {
         fun valueOf(input: String): Purchase {
             PurchaseAmountValidator.validate(input)
             return Purchase(input.toInt())
         }
+    }
+
+    fun getAmount(): Int {
+        return amount
     }
 
 }
--- src/test/kotlin/lotto/domain/purchase/PurchaseTest.kt
+++ src/test/kotlin/lotto/domain/purchase/PurchaseTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class PurchaseTest {
 
     @Test
-    fun `valueOf 메서드의 인자로 받은 값은 생성된 Purchase 인스턴스의 amount 필드값과 동일하다`() {
+    fun `valueOf 메서드의 인자로 받은 값은 getAmount() 메서드로 확인할 수 있다`() {
         // given
         val input = "10000"
         val expected = 10_000
@@ -15,6 +15,6 @@ class PurchaseTest {
         val purchase = Purchase.valueOf(input)
 
         // then
-        assertEquals(expected, purchase.amount)
+        assertEquals(expected, purchase.getAmount())
     }
 }
