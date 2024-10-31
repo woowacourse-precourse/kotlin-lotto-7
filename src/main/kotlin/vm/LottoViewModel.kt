bf534958085dab6f0767ef3d9f7f28a832a51f9a
chanho0908 <uni070@naver.com>
2024-10-31 20:51:01 +0900

    feat(LottoViewModel.kt) : 당첨 번호, 보너스 번호 상태 관리 추가

--- src/main/kotlin/vm/LottoViewModel.kt
+++ src/main/kotlin/vm/LottoViewModel.kt
@@ -9,4 +9,13 @@ class LottoViewModel(
     fun onCompleteInputPayment(pay: Int){
         state = state.copy(purchaseLottoCount = pay)
     }
+
+    fun onCompleteInputWinningNumber(winningNumber: List<Int>){
+        state = state.copy(winningNumber = winningNumber)
+    }
+
+    fun onCompleteInputBonusNumber(bonusNumber: Int){
+        state = state.copy(bonusNumber = bonusNumber)
+    }
+
 }
