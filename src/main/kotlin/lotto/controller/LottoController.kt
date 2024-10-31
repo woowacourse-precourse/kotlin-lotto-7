c0ebca5bfd41823086bce314b1b61c7c204d6305
UiHyeon <hyunkim6790@gmail.com>
2024-10-31 17:50:06 +0900

    feat(LottoController): inputView 실행
    LottoController 작성
    Application 컨트롤러 연결

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,13 +1,22 @@
 package lotto.controller
 
+import lotto.view.InputView
+import lotto.view.OutputView
+
 /**
  * 컨트롤러는 모델과 뷰에 의존해도 된다.
  * 컨트롤러 내부에 모델과 뷰의 코드가 있을 수 있다.
  */
 
 class LottoController {
+    val inputView = InputView()
+    val outputView = OutputView()
 
-    private fun start() {
+    fun start() {
+        val purchaseAmount = inputView.getPurchaseAmount()
 
+        val winningNumbers = inputView.getWinningNumbers()
+
+        val bonusNumber = inputView.getBonusNumber()
     }
 }
