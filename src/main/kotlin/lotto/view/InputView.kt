499dc149d71cdee27a63684a6e8863d43eda6758
HamBeomJoon <hbj0802@naver.com>
2024-10-31 17:09:41 +0900

    feat(InputView): 잘못된 입력 시 처리
    - 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,8 +5,17 @@ import lotto.Constants
 
 class InputView {
     fun getLottoAmount(): Int {
-        println(Constants.INPUT_BUY_AMOUNT_MESSAGE)
-        val input = Console.readLine().toInt()
-        return input
+        while (true) {
+            println(Constants.INPUT_BUY_AMOUNT_MESSAGE)
+            val input = Console.readLine()
+            try {
+                validateLottoAmount(input)
+                return input.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+    private fun validateLottoAmount(lottoAmount: String) {
     }
 }
