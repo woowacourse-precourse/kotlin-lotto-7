0bc868644d0a853e6eec4e0cadfdb32edc10dbb6
HamBeomJoon <hbj0802@naver.com>
2024-10-31 17:42:39 +0900

    feat(LottoController, OutputView): 로또 구입 수량 출력

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,14 +1,17 @@
 package lotto.controller
 
 import lotto.view.InputView
+import lotto.view.OutputView
 
 class LottoController {
     private val inputView = InputView()
+    private val outputView = OutputView()
 
     fun start() {
         val lottoAmount = inputView.getLottoAmount()
 
         val lottoCount = lottoAmount / 1000
+        outputView.printLottoAmountMessage(lottoCount)
     }
 
 }
---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,5 @@
+package lotto.view
+
+class OutputView {
+    fun printLottoAmountMessage(amount: Int) = println("$amount 개를 구매했습니다.")
+}
