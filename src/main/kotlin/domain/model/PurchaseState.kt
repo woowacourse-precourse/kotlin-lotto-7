28a74dcff5920a5201f7a2c2d99988e897174854
chanho0908 <uni070@naver.com>
2024-11-01 00:15:47 +0900

    refactor(PurchaseState.kt) : 구매 상태 관리 클래스에 구매한 로또 정보 추가

--- src/main/kotlin/domain/model/PurchaseState.kt
+++ src/main/kotlin/domain/model/PurchaseState.kt
@@ -3,5 +3,6 @@ package domain.model
 data class PurchaseState (
     val purchaseLottoCount: Int = 0,
     val winningNumber: List<Int> = emptyList(),
-    val bonusNumber: Int = 0
+    val bonusNumber: Int = 0,
+    val pickedLotto: List<List<Int>> = emptyList()
 )
