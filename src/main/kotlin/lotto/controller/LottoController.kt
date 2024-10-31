6a4f782ba9fb1468ecd07bdb4ded34a2a1541a4f
HamBeomJoon <hbj0802@naver.com>
2024-10-31 18:12:30 +0900

    feat(LottoController): 구매한 로또 번호 출력
    - 로또 번호는 오름차순 정렬하여 출력한다.
    - 로또 번호는 앞뒤로 대괄호로 감싸서 출력한다.
    - 로또 번호는 쉼표(,)로 구분하여 출력한다.

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,5 +1,6 @@
 package lotto.controller
 
+import camp.nextstep.edu.missionutils.Randoms
 import lotto.Constants
 import lotto.view.InputView
 import lotto.view.OutputView
@@ -13,6 +14,15 @@ class LottoController {
 
         val lottoCount = lottoAmount / Constants.LOTTO_PRICE
         outputView.printLottoAmountMessage(lottoCount)
+
+        printLottoNumber(lottoCount)
     }
 
+    private fun printLottoNumber(lottoCount: Int) {
+        repeat(lottoCount) {
+            val lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            println(lottoNumbers.sorted().joinToString(prefix = "[", postfix = "]"))
+        }
+        println()
+    }
 }
