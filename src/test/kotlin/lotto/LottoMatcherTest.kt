aaf8b6d7716c41383123e2627cb8b2bf1f680119
DDangDin <Kcorelec7123@gmail.com>
2024-11-01 19:36:41 +0900

    test: LottoMatcherTest 테스트 코드 추가

---
+++ src/test/kotlin/lotto/LottoMatcherTest.kt
@@ -0,0 +1,29 @@
+package lotto
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LottoMatcherTest {
+
+    private val lottoMatcher = LottoMatcher()
+
+    @Test
+    fun `로또 번호 당첨 번호와 비교했을 때 3개 일치`() {
+        val count = lottoMatcher.matchWithWinningNumbers(lottoTestData, winningNumbersTestData)
+        assertEquals(THREE_MATCHES, count)
+    }
+
+    @Test
+    fun `로또 번호에 보너스 번호 포함`() {
+        val hasBonusNumber = lottoMatcher.hasBonusNumber(lottoTestData, BONUS_NUMBER)
+        assertEquals(HAS_BONUS_NUMBER, hasBonusNumber)
+    }
+
+    companion object {
+        private val lottoTestData = Lotto(listOf(1,2,3,4,5,6))
+        private val winningNumbersTestData = listOf(1,2,3,10,11,12)
+        private const val THREE_MATCHES = 3
+        private const val BONUS_NUMBER = 6
+        private const val HAS_BONUS_NUMBER = true
+    }
+}
