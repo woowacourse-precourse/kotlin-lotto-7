19e07b09efc0c639a732d8c9c42d229771c99fbd
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-01 11:45:52 +0900

    feat(OutputMessages): 출력 메시지 문자열들을 상수화하는 object 추가

---
+++ src/main/kotlin/utils/OutputMessages.kt
@@ -0,0 +1,13 @@
+package utils
+
+object OutputMessages {
+    const val BUYING_OUTPUT_MESSAGE = "개를 구매했습니다."
+    const val WINNING_STATISTICS_OUTPUT_MESSAGE = "당첨 통계"
+    const val DASH_OUTPUT_MESSAGE = "---"
+
+    const val COUNT_MATCH_OUTPUT_MESSAGE = "개 일치 ("
+    const val BONUS_BALL_MATCH_OUTPUT_MESSAGE = "개 일치, 보너스 볼 일치 ("
+    const val MONEY_OUTPUT_MESSAGE = "원) - "
+    const val WINNING_COUNT_OUTPUT_MESSAGE = "개"
+
+}
