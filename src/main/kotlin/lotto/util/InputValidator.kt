9e51661e8d5adb8f0576619c06c066afb47b99bb
DoableTuple <doabletuple@gmail.com>
2024-11-01 16:40:41 +0900

    refactor: util의 validateInputIsNumeric을 view의 parseNumericInput으로 변경

--- src/main/kotlin/lotto/util/InputValidator.kt
+++ src/main/kotlin/lotto/util/InputValidator.kt
@@ -1,10 +1,6 @@
 package lotto.util
 
 object InputValidator {
-    fun validateInputIsNumeric(input: String) {
-        if (input.toIntOrNull() == null) throw IllegalArgumentException(ErrorMessage.INPUT_NOT_NUMERIC)
-    }
-
     fun validateMoneyIsNotNegative(money: Int) {
         if (money < 0) throw IllegalArgumentException(ErrorMessage.MONEY_IS_NEGATIVE)
     }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,17 +1,20 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
-import lotto.util.InputValidator.validateInputIsNumeric
+import lotto.util.ErrorMessage
 import lotto.util.InputValidator.validateMoneyIsNotNegative
 import lotto.util.InputValidator.validateMoneyIsEnough
 import lotto.util.InputValidator.validateMoneyIsDivisible
 
 class InputView {
+    private fun parseNumericInput(input: String): Int {
+        if (input.toIntOrNull() == null) throw IllegalArgumentException(ErrorMessage.INPUT_NOT_NUMERIC)
+        return input.toInt()
+    }
+
     fun readLottoMoney(): Int {
         println(PROMPT_LOTTO_MONEY)
-        val input = Console.readLine()
-        validateInputIsNumeric(input)
-        val money = input.toInt()
+        val money = parseNumericInput(Console.readLine())
         validateMoneyIsNotNegative(money)
         validateMoneyIsEnough(money)
         validateMoneyIsDivisible(money)
