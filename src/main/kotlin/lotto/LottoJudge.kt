d58f26e8271757f78c43cdaa630db7e222e063e3
joonhokil <kiljh@me.com>
2024-11-01 19:13:10 +0900

    feat: 1보다 작거나 45보다 큰 숫자를 입력하면 예외 처리

--- src/main/kotlin/lotto/LottoJudge.kt
+++ src/main/kotlin/lotto/LottoJudge.kt
@@ -7,6 +7,9 @@ class LottoJudge {
         if (numbers.size != 6) {
             throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
         }
+        if (numbers.any { it < 1 || it > 45 }) {
+            throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_COUNT_ERROR.getMessage())
+        }
         winnerNumbers = numbers
     }
 }
