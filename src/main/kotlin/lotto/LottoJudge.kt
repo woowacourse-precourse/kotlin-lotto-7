7411df5902e9653f58c612cc50e28eddd6f6136b
joonhokil <kiljh@me.com>
2024-11-01 18:00:42 +0900

    feat: 당첨 번호가 6개가 아닐 경우 예외 처리

--- src/main/kotlin/lotto/LottoJudge.kt
+++ src/main/kotlin/lotto/LottoJudge.kt
@@ -4,6 +4,9 @@ class LottoJudge {
     var winnerNumbers: List<Int> = emptyList()
 
     fun setLottoWinnerNumbers(numbers: List<Int>) {
+        if (numbers.size != 6) {
+            throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
+        }
         winnerNumbers = numbers
     }
 }
