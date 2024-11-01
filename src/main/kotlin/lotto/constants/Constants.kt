6998b98eb944f77c407aaa74566f65264971b93d
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-11-01 17:55:19 +0900

    feat: constants 생성 및 상수 정의

---
+++ src/main/kotlin/lotto/constants/Constants.kt
@@ -0,0 +1,8 @@
+package lotto.constants
+
+
+const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+const val INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+const val OUTPUT_RESULT_OF_WINNING = "당첨 통계\n" + "---\n"
+const val ERROR = "[ERROR]: %s"
