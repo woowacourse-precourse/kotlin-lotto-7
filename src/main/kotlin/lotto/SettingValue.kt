f54453e1c0c6cbb5296093ac37413c232025f947
unknown <yangjk9203@naver.com>
2024-11-01 15:58:56 +0900

    feat(SettingValue): 상수 추가

--- src/main/kotlin/lotto/SettingValue.kt
+++ src/main/kotlin/lotto/SettingValue.kt
@@ -3,5 +3,9 @@ package lotto
 class SettingValue {
     companion object {
         const val LOTTO_PRICE = 1000
+        const val LOTTO_NUMBER_MINIMUM = 1
+        const val LOTTO_NUMBER_MAXIMUM = 45
+        const val LOTTO_NUMBER_COUNT = 6
+        const val NUMBER_DELIMITER = ","
     }
 }
