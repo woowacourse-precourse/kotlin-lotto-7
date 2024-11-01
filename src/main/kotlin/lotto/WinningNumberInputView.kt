ac719a7118c6a15bed568c0b268420262e211ab9
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-01 22:16:45 +0900

    feat(WinningNumberInputView): 당첨 번호 화면에 가이드 출력 추가
    - 당첨 번호 입력 안내문 출력 기능 추가
    - 보너스 번호 입력 안내문 출력 기능 추가

--- src/main/kotlin/lotto/WinningNumberInputView.kt
+++ src/main/kotlin/lotto/WinningNumberInputView.kt
@@ -3,6 +3,11 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class WinningNumberInputView {
+
+    fun guideWinningNumbers() = println("당첨 번호를 입력해 주세요.")
+
+    fun guideBonusNumber() = println("보너스 번호를 입력해 주세요.")
+
     fun inputWinningNumbers() = Console.readLine()
 
     fun inputBonusNumber() = Console.readLine()
