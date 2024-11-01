b2d8a7868fe8b80153fb37b14ba9f0d0eff7cea8
HamBeomJoon <hbj0802@naver.com>
2024-11-02 01:10:43 +0900

    feat(Lotto): 발행한 로또 번호 출력 함수 생성
    - 발행한 로또 번호 출력하는 함수 생성

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,13 @@
 package lotto.model
 
+import lotto.Constants
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    fun getNumbers(): String {
+        return numbers.sorted().joinToString(prefix = "[", postfix = "]")
+    }
 }
