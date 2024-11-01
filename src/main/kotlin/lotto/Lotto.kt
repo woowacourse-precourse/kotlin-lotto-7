257899c0dd001d7a148f9cae6e6a6852a2173f1f
oungsi2000 <oungsi1000@gmail.com>
2024-11-01 17:11:30 +0900

    feat(Lotto.kt): 로또 발행 기능 추가
    로또 발행 기능을 추가하였습니다
    getLotto() 함수를 실행하면 1~45까지의 숫자 6개의 배열을 반환합니다
    
    추가된 함수 및 객체
    -getLotto()

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,21 @@
 package lotto
 
+const val LOTTO_COUNT:Int = 6
+const val STRAT_LOTTO_NUM:Int = 1
+const val END_LOTTO_NUM:Int = 45
+
+fun getLotto():MutableList<Int> {
+    return camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(STRAT_LOTTO_NUM, END_LOTTO_NUM, LOTTO_COUNT)
+}
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        for (number in numbers) {
+            require(number in 1..45) {"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." }
+        }
     }
+
 
     // TODO: 추가 기능 구현
 }
