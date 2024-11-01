3d5223c95dd464eb29f702a3e90d17cfadf07361
지원 <cheerfulboys@naver.com>
2024-11-01 16:19:57 +0900

    refactory(LottoGame): 로또를 발행하는 기능을 generateLottoTickets()으로 이동

--- src/main/kotlin/lotto/model/LottoGame.kt
+++ src/main/kotlin/lotto/model/LottoGame.kt
@@ -1,5 +1,7 @@
 package lotto.model
 
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
+
 class LottoGame {
     private var numbers: List<Int> = listOf()
 
@@ -18,6 +20,17 @@ class LottoGame {
         return price.toInt() / LOTTO_TICKET_PRICE
     }
 
+    fun generateLottoTickets(count: Int): List<List<Int>> {
+        val lottoTickets: MutableList<List<Int>> = mutableListOf()
+        repeat(count) {
+            val numbers = pickUniqueNumbersInRange(MIN_BALL_NUMBER, MAX_BALL_NUMBER, WINNING_BALL_COUNT)
+            val lottoTicket = Lotto(numbers).sort()
+            println(lottoTicket)
+            lottoTickets.add(lottoTicket)
+        }
+        return lottoTickets
+    }
+
     fun check(winningNumbers: List<Int>): Int {
         var winningCount = 0
         winningNumbers.forEach {
@@ -34,5 +47,9 @@ class LottoGame {
 
     companion object {
         const val LOTTO_TICKET_PRICE = 1000
+
+        const val MAX_BALL_NUMBER = 45
+        const val MIN_BALL_NUMBER = 1
+        const val WINNING_BALL_COUNT = 6
     }
 }
