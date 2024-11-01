97329a3923fda903a5e4fbf9f6a8ee1c5d9e0971
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 01:28:49 +0900

    feat: 구입금액, 당첨번호, 보너스번호 입력 받기

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,1 +1,23 @@
 package lotto
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+class LottoGame {
+    fun inputCost(): Int {
+        println("구입금액을 입력해 주세요.")
+        return Console.readLine().toInt()
+    }
+    fun inputWinningLotto(): Lotto {
+        println("당첨 번호를 입력해 주세요.")
+        val winningLotto = Console.readLine().split(',').map { it.toInt() }
+        return Lotto(winningLotto)
+    }
+    fun inputBonusNumber(): Int {
+        println("보너스 번호를 입력해 주세요.")
+        val bonusNumber = Console.readLine().toInt()
+        return bonusNumber
+    }
+
+
+
+}
