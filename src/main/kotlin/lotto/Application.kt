c46512a0e5451013ef7761b46d7e162140d5e492
yewon-yw <kimyewon0401@gmail.com>
2024-11-01 17:13:14 +0900

    feat(Application, Lotto): 발행한 로또 수량 및 번호 출력
    - 로또 수량, 로또 번호 출력
    - Lotto 클래스에 get 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,9 +1,22 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val price = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException()
     require(price > 0)
     require(price % 1000 == 0)
+
+    val lottoCount = price / 1000
+    val lotto = mutableListOf<Lotto>()
+    lotto.apply {
+        repeat(lottoCount) {
+            add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
+        }
+    }
+    println(lottoCount)
+    lotto.forEach {
+        println(it.getNumbers())
+    }
 }
