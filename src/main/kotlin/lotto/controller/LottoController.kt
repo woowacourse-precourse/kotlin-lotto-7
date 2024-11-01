aaf0171c4060ebb3e9553d5b0113285468cb8624
HamBeomJoon <hbj0802@naver.com>
2024-11-02 01:10:01 +0900

    refactor(LottoController): 구매한 로또 출력 로직 수정
    - printLottoNumber에서 로또를 생성하고 출력까지 하는 로직을 분리했다.
    - generateLotto 함수로 로또를 생성하고, printLottoNumber로 로또 번호를 출력함.

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -2,12 +2,14 @@ package lotto.controller
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.Constants
+import lotto.model.Lotto
 import lotto.view.InputView
 import lotto.view.OutputView
 
 class LottoController {
     private val inputView = InputView()
     private val outputView = OutputView()
+    private var lottoTickets = mutableListOf<Lotto>()
 
     fun start() {
         val lottoAmount = inputView.getLottoAmount()
@@ -15,22 +17,26 @@ class LottoController {
         val lottoCount = lottoAmount / Constants.LOTTO_PRICE
         outputView.printLottoAmountMessage(lottoCount)
 
-        printLottoNumber(lottoCount)
+        generateLotto(lottoCount)
+        printLottoNumber()
 
         val winningNumbers = inputView.getWinningNumbers()
 
         val bonusNumber = inputView.getBonusNumber(winningNumbers)
     }
 
-    private fun printLottoNumber(lottoCount: Int) {
+    private fun generateLotto(lottoCount: Int) {
         repeat(lottoCount) {
             val lottoNumbers = Randoms.pickUniqueNumbersInRange(
                 Constants.LOTTO_NUMBER_MIN,
                 Constants.LOTTO_NUMBER_MAX,
                 Constants.LOTTO_NUMBERS_SIZE
             )
-            println(lottoNumbers.sorted().joinToString(prefix = "[", postfix = "]"))
+            lottoTickets.add(Lotto(lottoNumbers))
         }
-        println()
+    }
+
+    private fun printLottoNumber() {
+        lottoTickets.forEach { lotto -> println(lotto.getNumbers()) }
     }
 }
