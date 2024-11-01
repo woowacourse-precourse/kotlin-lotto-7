d9ee51f0633efddb87df55843f5735a50420ae7f
이용인 <yongin0517@gmail.com>
2024-11-01 16:29:20 +0900

    feat: LottoController 생성 및 객체적용

---
+++ src/main/kotlin/lotto/LottoController.kt
@@ -0,0 +1,33 @@
+package lotto
+
+class LottoController(
+    private val lottoWinningResultCalculator: LottoWinningResultCalculator,
+    private val profitRateCalculator: ProfitRateCalculator,
+    private val inputController: InputController,
+    private val outputView: OutputView,
+    val lottoGenerator: LottoGenerator
+) {
+    fun run() {
+        // 유효한 구입 금액 입력 받기
+        val purchaseAmount = inputController.getValidPurchaseAmount()
+
+        // 구입 금액에 맞춰 로또를 생성
+        val purchasedLottos = generateLottos(purchaseAmount / 1000)
+        outputView.printLottoNumbers(purchasedLottos)
+
+        // 유효한 당첨 번호 및 보너스 번호 입력 받기
+        val winningNumbers = inputController.getValidWinningNumbers()
+        val bonusNumber = inputController.getValidBonusNumber()
+
+        // 로또 당첨 결과 및 수익률 계산
+        val results = lottoWinningResultCalculator.calculateResults(purchasedLottos, winningNumbers, bonusNumber)
+        val profitRate = profitRateCalculator.calculateProfitRate(results, purchaseAmount)
+
+        // 결과 출력
+        outputView.printResult(results, profitRate)
+    }
+
+    private fun generateLottos(count: Int): List<Lotto> {
+        return List(count) { Lotto(lottoGenerator.generateLotto()) }
+    }
+}
