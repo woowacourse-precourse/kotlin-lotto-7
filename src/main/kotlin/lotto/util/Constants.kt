82a986930c5758a6fde6334b10c3e40b9129be19
moondev03 <moondev03@gmail.com>
2024-11-01 19:35:02 +0900

    feat(Constants): 공통 상수 정의
    - 에외 메시지 접두사
    - 로또 번호 개수
    - 로또 범위
    - 로또 가격

---
+++ src/main/kotlin/lotto/util/Constants.kt
@@ -0,0 +1,11 @@
+package lotto.util
+
+object Constants {
+
+    const val EXCEPTION_PREFIX = "[ERROR] "
+
+    const val LOTTO_NUMBER_SIZE = 6
+    const val LOTTO_MIN_NUMBER = 1
+    const val LOTTO_MAX_NUMBER = 45
+    const val LOTTO_PRICE = 1_000
+}
