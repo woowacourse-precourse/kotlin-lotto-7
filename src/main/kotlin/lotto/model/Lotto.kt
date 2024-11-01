10e966dd8ee43b96d6d919c12038d497a507d7c4
baekcii <bsy8470@gmail.com>
2024-11-02 02:45:42 +0900

    feat: 결과값 출력

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,10 +1,46 @@
-package lotto
+package lotto.model
+
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
-    //
-    //생성된 로또와 사용자가 입력한 로또 번호를 비교하는 로직
 
+    fun get(): List<Int> {
+        return numbers
+    }
+
+    fun getResult(money: Int, bonusNum: Int, purchasedLotto: List<List<Int>>) {
+
+        purchasedLotto.forEach {
+            recordRank(it, bonusNum)
+        }
+        recordPrice(money)
+    }
+
+    fun recordRank(lotto: List<Int>, bonusNum: Int) {
+        val count = lotto.intersect(numbers).size
+        println(count)
+        if (count == 6) LottoResult.first += 1
+        else if (count == 5 && lotto.contains(bonusNum)) LottoResult.second += 1
+        else if (count == 5) LottoResult.third += 1
+        else if (count == 4) LottoResult.fourth += 1
+        else if (count == 3) LottoResult.fifth += 1
+
+    }
+
+    fun recordPrice(money: Int) {
+        var total = 0f
+        total = LottoResult.first * 2000000000f + LottoResult.second * 30000000f + LottoResult.third*1500000f+LottoResult.fourth*50000f+LottoResult.fifth*5000f
+        LottoResult.priceRatio = (total/money*100f)
+    }
+}
+
+object LottoResult {
+    var first: Int = 0
+    var second: Int = 0
+    var third: Int = 0
+    var fourth: Int = 0
+    var fifth: Int = 0
+    var priceRatio: Float = 0f
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,10 +1,22 @@
 package lotto.view
 
+import lotto.model.LottoResult
+
 class OutputView {
     fun displayLotto(lotto : List<List<Int>>){
         println("${lotto.size}개를 구매했습니다.")
         lotto.forEach{
             println(it)
         }
+    }
+    fun displayResult(){
+        println("3개 일치 - ${LottoResult.fifth}개")
+        println("4개 일치 - ${LottoResult.fourth}개")
+        println("5개 일치 - ${LottoResult.third}개")
+        println("5개 일치 - ${LottoResult.second}개")
+        println("6개 일치 - ${LottoResult.first}개")
+        println("총 수익률은 ${"%.1f".format(LottoResult.priceRatio)}")
+        println(LottoResult.priceRatio)
+
     }
 }
