fed6a6ae3041afc1b972aa7c3360f62d1c9413a0
이용인 <yongin0517@gmail.com>
2024-11-01 16:27:29 +0900

    feat: 당첨번호, 보너스번호 중복방지 로직 추가

--- src/main/kotlin/lotto/InputValidator.kt
+++ src/main/kotlin/lotto/InputValidator.kt
@@ -2,11 +2,11 @@ package lotto
 
 class InputValidator {
 
-    fun validatePurchaseAmount(input: String): String {
+    fun validatePurchaseAmount(input: String): Int {
         require(input.matches(Regex("\\d+"))) { ERROR_INVALID_PURCHASE_AMOUNT_FORMAT }
         val amount = input.toInt()
         require(amount % 1000 == 0) { ERROR_INVALID_PURCHASE_AMOUNT_UNIT }
-        return input
+        return amount
     }
 
     fun validateWinningNumbers(input: String): List<Int> {
@@ -15,21 +15,24 @@ class InputValidator {
         }
         require(numbers.size == 6) { ERROR_INVALID_WINNING_NUMBERS_COUNT }
         require(numbers.all { it in 1..45 }) { ERROR_INVALID_WINNING_NUMBER_RANGE }
+        require(numbers.distinct().size == numbers.size) { ERROR_DUPLICATE_WINNING_NUMBERS }
         return numbers
     }
 
-    fun validateBonusNumber(input: String): Int {
-        val bonus = input.toIntOrNull()
-        require(bonus != null) { ERROR_INVALID_BONUS_NUMBER_FORMAT }
+    fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int {
+        val bonus = input.toIntOrNull() ?: throw IllegalArgumentException(ERROR_INVALID_BONUS_NUMBER_FORMAT)
         require(bonus in 1..45) { ERROR_INVALID_WINNING_NUMBER_RANGE }
+        require(bonus !in winningNumbers) { ERROR_DUPLICATE_BONUS_NUMBER }
         return bonus
     }
 
     companion object {
-        const val ERROR_INVALID_PURCHASE_AMOUNT_FORMAT = "[ERROR] 유효하지 않은 구입 금액입니다. 숫자를 입력해 주세요."
+        const val ERROR_INVALID_PURCHASE_AMOUNT_FORMAT = "[ERROR] 구입 금액은 숫자로 입력해야 합니다."
         const val ERROR_INVALID_PURCHASE_AMOUNT_UNIT = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다."
         const val ERROR_INVALID_WINNING_NUMBERS_COUNT = "[ERROR] 당첨 번호는 6개의 숫자를 입력해야 합니다."
         const val ERROR_INVALID_WINNING_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
-        const val ERROR_INVALID_BONUS_NUMBER_FORMAT = "[ERROR] 유효하지 않은 보너스 번호입니다. 숫자를 입력해 주세요."
+        const val ERROR_INVALID_BONUS_NUMBER_FORMAT = "[ERROR] 보너스 번호는 숫자로 입력해야 합니다."
+        const val ERROR_DUPLICATE_WINNING_NUMBERS = "[ERROR] 당첨 번호에는 중복된 숫자가 없어야 합니다."
+        const val ERROR_DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
     }
 }
