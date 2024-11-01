c385352056b76477548c651520edb36a5607a54a
unknown <yangjk9203@naver.com>
2024-11-01 15:54:48 +0900

    feat(BonusNumber): 입력된 보너스번호 예외처리

---
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -0,0 +1,57 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+
+class BonusNumber {
+    init {
+        bonusNumberMessege()
+    }
+
+    private var validatorTest = false
+    private var bonusNumber: Int = 0
+
+    fun bonusNumber(winningNumber: List<String>): Int {
+        while (!validatorTest) {
+            bonusNumberException(winningNumber)
+        }
+        return bonusNumber
+    }
+
+    private fun bonusNumberException(winningNumber: List<String>): Any {
+        try {
+            val bonusNumber = bonusNumberInput()
+            checkBlank(bonusNumber)
+            checkNumber(bonusNumber)
+            checkRange(bonusNumber)
+            checkDuplicate(bonusNumber, winningNumber)
+            validatorTest = true
+            return bonusNumber
+        } catch (ex: Exception) {
+            return println(ex.message)
+        }
+    }
+
+    private fun bonusNumberInput(): String {
+        return Console.readLine()
+    }
+
+    private fun checkBlank(bonusNumber: String) {
+        require(bonusNumber.isNotBlank()) { ExceptionMessage.BLANK }
+    }
+
+    private fun checkNumber(bonusNumber: String) {
+        require(bonusNumber.contains(Regex("^[0-9]*$"))) { ExceptionMessage.NOT_NUMBERS }
+    }
+
+    private fun checkRange(bonusNumber: String) {
+        require(bonusNumber.toInt() in 1..45) { ExceptionMessage.NOT_LOTTO_NUMBER_RANGE }
+    }
+
+    private fun checkDuplicate(bonusNumber: String, winningNumber: List<String>) {
+        require(!winningNumber.contains(bonusNumber)) { ExceptionMessage.DUPLICATED_NUMBERS }
+    }
+
+    private fun bonusNumberMessege() {
+        return println(Message.ENTER_BONUS_NUMBER)
+    }
+}
