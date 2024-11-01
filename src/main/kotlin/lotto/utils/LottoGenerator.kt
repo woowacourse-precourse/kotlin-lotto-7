93cdc7827cfc1839a8fff0bbe493faff84e51e6d
yewon-yw <kimyewon0401@gmail.com>
2024-11-02 04:04:50 +0900

    refactor(LottoGenerator): 로또 생성 클래스 분리

---
+++ src/main/kotlin/lotto/utils/LottoGenerator.kt
@@ -0,0 +1,10 @@
+package lotto.utils
+
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.model.Lotto
+
+class LottoGenerator {
+    fun getLotto(): Lotto {
+        return Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+    }
+}
