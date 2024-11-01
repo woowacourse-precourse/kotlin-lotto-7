57f1eb1caf5a9ce73f8a59e8e138ca983abe7175
DoableTuple <doabletuple@gmail.com>
2024-11-01 11:03:42 +0900

    refactor: 로또 번호 개수 상수 처리

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,10 +1,11 @@
 package lotto.model
 
+import lotto.util.Constant
 import lotto.util.ErrorMessage
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { ErrorMessage.LOTTO_NUMBERS_COUNT_MISMATCH }
+        require(numbers.size == Constant.LOTTO_NUMBERS_COUNT) { ErrorMessage.LOTTO_NUMBERS_COUNT_MISMATCH }
     }
 
     // TODO: 추가 기능 구현
--- src/main/kotlin/lotto/util/Constant.kt
+++ src/main/kotlin/lotto/util/Constant.kt
@@ -2,4 +2,5 @@ package lotto.util
 
 object Constant {
     const val LOTTO_PRICE = 1000
+    const val LOTTO_NUMBERS_COUNT = 6
 }
