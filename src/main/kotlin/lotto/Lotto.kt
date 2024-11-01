57cc5fe79f5345dd10b76ad964dfdca6ada4be22
unknown <yangjk9203@naver.com>
2024-11-01 13:42:43 +0900

    feat(Lotto): 구매수만큼 무작위 로또번호 출력

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,79 @@
 package lotto
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(private val numbers: List<List<Int>>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        numbers.forEach { lotto -> println(lotto) }
     }
-
-    // TODO: 추가 기능 구현
 }
+//
+//    fun winningNumber(): List<Int> {
+//        while (!validatorTest) {
+//            winningNumberException()
+//        }
+//        return winningNumber
+//    }
+//
+//    private fun winningNumberException(): Any {
+//        try {
+//            winningNumber = validateWinningNumber(separateWinningNumber())
+//            validatorTest = true
+//            return winningNumber
+//        } catch (ex: Exception) {
+//            return println(ex.message)
+//        }
+//    }
+//
+//    private fun winningNumberInput(): String {
+//        return Console.readLine()
+//    }
+//
+//    private fun validateWinningNumber(userInput: List<String>): List<Int> {
+//
+//        if (userInput.size != 6) {
+//            throw IllegalArgumentException(ExceptionMessage.NOT_INPUT_SIX_NUMBER)
+//        }
+//        if (userInput.indexOf(Regex("^[0-9]*$"))) {
+//            throw IllegalArgumentException(ExceptionMessage.NOT_NUMBERS)
+//        }
+//        if (userInput.toInt()>SettingValue.LOTTO_NUMBER_MAXIMUM || userInput.toInt()<SettingValue.LOTTO_NUMBER_MINIMUM) {
+//            throw IllegalArgumentException(ExceptionMessage.NOT_LOTTO_NUMBER_RANGE)
+//        }
+////        if (userInput) {
+////            throw IllegalArgumentException(ExceptionMessage.WINNING_NUMBER_INPUT)
+////        }
+//        return userInput
+//    }
+//    private fun separateWinningNumber(): List<String> {
+//        return winningNumberInput().split(SettingValue.NUMBER_DELIMITER)
+//    }
+//    private fun winningNumberMessege() {
+//        return println(Message.ENTER_WINNING_NUMBER)
+//    }
+//}
+////    fun start(numbers: List<Int>) {
+////        if (numbers.size > 6) {
+////            throw IllegalArgumentException(ExceptionMessage.NOT_DIVIDE_THOUSAND)
+////        }
+////    }
+////
+////    fun amount(): Int {
+////        return PurchaseAmount().lottoPurchased()
+////    }
+////
+////    fun winningNumber() {
+////        return WinningNumber().winningNumber()
+////    }
+////
+////    fun bonusNumber() {
+////        return BonusNumber().bonusNumber()
+////    }
+////
+////
+////    fun random(numbers: List<Int>): MutableList<List<Int>> {
+////        val listlotto = MutableList<List<Int>>()
+////        repeat(amount()) {
+////            listlotto.add(numbers)
+////        }
+////        return listlotto
+////    }
+////}
