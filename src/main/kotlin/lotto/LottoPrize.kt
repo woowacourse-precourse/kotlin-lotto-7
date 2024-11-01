d05a5c4ecb84ace6976fe67199a4bc82d8af5baf
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-01 23:33:50 +0900

    feat: 로또 당첨 기준 추가
    - LottoPrize.kt: enum class를 사용해 로또 당첨 기준을 작성합니다.

---
+++ src/main/kotlin/lotto/LottoPrize.kt
@@ -0,0 +1,13 @@
+package lotto
+
+enum class LottoPrize(
+    private val matchingNumber: Int,
+    private val price: Int,
+    private val isMatchingBonusNumber: Boolean,
+) {
+    FIRST_PRIZE(6, 2_000_000_000, false),
+    SECOND_PRIZE(5, 30_000_000, true),
+    THIRD_PRIZE(5, 1_500_000, false),
+    FOURTH_PRIZE(4, 50_000, false),
+    FIFTH_PRIZE(3, 5_000, false),
+}
