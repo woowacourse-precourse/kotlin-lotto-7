9883da3551650889da6322e8e374b3513d59ab67
chanho0908 <uni070@naver.com>
2024-11-01 22:11:39 +0900

    feat(Output.kt) : 당첨 통계, 수익률을 출력 상태를 관리 추가

--- src/main/kotlin/domain/enums/Output.kt
+++ src/main/kotlin/domain/enums/Output.kt
@@ -1,16 +1,23 @@
 package domain.enums
 
-import util.toThousandUnit
+import util.ext.toThousandUnit
 
 enum class Output(private val msg: String) {
-    PURCHASE("%d개를 구매했습니다.");
-    
+    PURCHASE("%d개를 구매했습니다."),
+    WINNING_STATISTICS("당첨 통계"),
+    THREE_HYPHEN("---"),
+    TOTAL_RATE_OF_RETURN("총 수익률은 %s%%입니다.");
+
     override fun toString(): String = msg
 
     companion object {
         fun getPurchase(pay: String): Pair<String, Int> {
             val purchaseLottoCount = pay.toThousandUnit()
             return Pair(PURCHASE.toString().format(purchaseLottoCount), purchaseLottoCount)
+        }
+
+        fun getTotalRateOfReturn(price: String): String {
+            return TOTAL_RATE_OF_RETURN.toString().format(price)
         }
     }
 }
