29d415f94e5424e7b64523feb4ed92e1b9e0530f
yewon-yw <kimyewon0401@gmail.com>
2024-11-02 04:27:11 +0900

    feat(ErrorMessage): 에러 메시지 추가

--- src/main/kotlin/lotto/constants/ErrorMessage.kt
+++ src/main/kotlin/lotto/constants/ErrorMessage.kt
@@ -1,4 +1,10 @@
 package lotto.constants
 
 object ErrorMessage {
+    const val PARSE_NUMBER_ERROR = "[ERROR] 숫자를 입력해 주세요."
+    const val NON_POSITIVE_ERROR = "[ERROR] 자연수를 입력해 주세요."
+    const val RANGE_ERROR = "[ERROR] 1~45 범위의 자연수를 입력해 주세요."
+    const val MODULO_ERROR = "[ERROR] 1,000으로 나눌 수 있는 금액을 입력해 주세요."
+    const val LOTTO_COUNT_ERROR = "[ERROR] 1~45 범위의 중복되지 않은 자연수 6개를 입력해 주세요."
+    const val DUPLICATED_NUMBER = "[ERROR] 당첨된 숫자와 중복되지 않은 숫자를 입력해 주세요."
 }
--- src/main/kotlin/lotto/utils/Validation.kt
+++ src/main/kotlin/lotto/utils/Validation.kt
@@ -1,5 +1,7 @@
 package lotto.utils
 
+import lotto.constants.ErrorMessage
+
 object Validation {
     fun isValidRange(vararg numbers: Int): Boolean = numbers.map { it in 1..45 }.all { it }
 
@@ -8,9 +10,9 @@ object Validation {
 
 object Price {
     fun validate(inputPrice: String): Int {
-        val price = inputPrice.toIntOrNull() ?: throw IllegalArgumentException()
-        require(price > 0)
-        require(price % 1000 == 0)
+        val price = inputPrice.toIntOrNull() ?: throw IllegalArgumentException(ErrorMessage.PARSE_NUMBER_ERROR)
+        require(price > 0) { ErrorMessage.NON_POSITIVE_ERROR }
+        require(price % 1000 == 0) { ErrorMessage.MODULO_ERROR }
         return price
     }
 }
@@ -18,17 +20,18 @@ object Price {
 object WinningNumber {
     fun validate(inputWinningNumber: String): Set<Int> {
         val winningNumber = inputWinningNumber.split(",").mapNotNull { it.trim().toIntOrNull() }.toSet()
-        require(winningNumber.size == 6)
-        require(Validation.isValidRange(*winningNumber.toIntArray()))
+        require(winningNumber.size == 6) { ErrorMessage.LOTTO_COUNT_ERROR }
+        require(Validation.isValidRange(*winningNumber.toIntArray())) { ErrorMessage.RANGE_ERROR }
         return winningNumber
     }
 }
 
 object BonusNumber {
     fun validate(inputBonusNumber: String, winningNumber: Set<Int>): Int {
-        val bonusNumber = inputBonusNumber.toIntOrNull() ?: throw IllegalArgumentException()
-        require(Validation.isValidRange(bonusNumber))
-        require(Validation.isNotDuplicated(winningNumber, bonusNumber))
+        val bonusNumber =
+            inputBonusNumber.toIntOrNull() ?: throw IllegalArgumentException(ErrorMessage.PARSE_NUMBER_ERROR)
+        require(Validation.isValidRange(bonusNumber)) { ErrorMessage.RANGE_ERROR }
+        require(Validation.isNotDuplicated(winningNumber, bonusNumber)) { ErrorMessage.DUPLICATED_NUMBER }
         return bonusNumber
     }
 }
