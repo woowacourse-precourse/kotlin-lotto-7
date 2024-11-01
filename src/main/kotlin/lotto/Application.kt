f68fc389ce7232376c6822807be24c8777e4583f
Leeyerin0210 <lyl8661@naver.com>
2024-11-01 13:51:43 +0900

    style : 코틀린 코드 스타일 적용
    코틀린 코드 스타일에 맞지 않는 부분들을 수정하였다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,15 +1,17 @@
 package lotto
+
 import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
-    moneyInput()
-
+    val money = moneyInput()
+    //val numberList:List<Int>
 }
 
-fun moneyInput():Int?{
-    val moneyInput = Console.readLine().toIntOrNull()?:throw(IllegalArgumentException("[ERROR]숫자가 아닙니다."))
-    if(moneyInput%1000!=0){
-        throw(IllegalStateException("[ERROR]천원 단위의 금액이 아닙니다."))
+fun moneyInput(): Int? {
+    val moneyInput = Console.readLine().toIntOrNull() ?: throw (IllegalArgumentException("[ERROR]숫자가 아닙니다."))
+    if (moneyInput % 1000 != 0) {
+        throw (IllegalStateException("[ERROR]천원 단위의 금액이 아닙니다."))
     }
     return moneyInput
 }
