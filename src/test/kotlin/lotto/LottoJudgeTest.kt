3bd50c4c1a81aad5b67e9af43e569797e0d93250
joonhokil <kiljh@me.com>
2024-11-01 17:55:51 +0900

    test: 로또 당첨 번호가 7개일 경우 예외 테스트

--- src/test/kotlin/lotto/LottoJudgeTest.kt
+++ src/test/kotlin/lotto/LottoJudgeTest.kt
@@ -17,4 +17,13 @@ class LottoJudgeTest {
     fun `로또 당첨 번호를 입력할 수 있어야 한다`() {
         lottoJudge.setLottoWinnerNumbers(listOf(1,2,3,4,5,6))
     }
+
+    @Test
+    fun `로또 당첨 번호는 6개 입력되어야 한다`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            lottoJudge.setLottoWinnerNumbers(listOf(1,2,3,4,5,6,7))
+        }
+
+        assertThat(exception.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
+    }
 }
