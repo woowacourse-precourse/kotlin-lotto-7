4f369afb58a2c47d68d0ebf138ac3657abfb9f28
이용인 <yongin0517@gmail.com>
2024-11-01 16:45:40 +0900

    feat: 로또 번호 중복 방지 로직 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,7 @@ class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.size == 6) { ERROR_INVALID_NUMBER_COUNT }
+        require(numbers.distinct().size == 6) { ERROR_DUPLICATE_NUMBER }
     }
 
     fun getNumbers(): List<Int> {
@@ -12,5 +13,6 @@ class Lotto(private val numbers: List<Int>) {
 
     companion object {
         const val ERROR_INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다."
+        const val ERROR_DUPLICATE_NUMBER = "[ERROR] 로또 번호는 중복될 수 없습니다."
     }
 }
