8d074043dd7603e0f4f704e25d7b4e206f2b8847
DoableTuple <doabletuple@gmail.com>
2024-11-01 14:58:49 +0900

    feat(model): 난수로 로또 번호를 생성하는 기능 추가

---
+++ src/main/kotlin/lotto/model/LottoHandler.kt
@@ -0,0 +1,11 @@
+package lotto.model
+
+import lotto.util.Constant
+import camp.nextstep.edu.missionutils.Randoms
+
+class LottoHandler {
+    fun generateRandomNumbers(): List<Int> {
+        val numbers = Randoms.pickUniqueNumbersInRange(Constant.LOTTO_NUMBER_MIN, Constant.LOTTO_NUMBER_MAX, Constant.LOTTO_NUMBERS_COUNT)
+        return numbers
+    }
+}
