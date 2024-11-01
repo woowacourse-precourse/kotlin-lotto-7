812c4299c5d2f60e744b1b5ae6d2ba975e881cd2
ijh1298 <imjh0417@gmail.com>
2024-11-01 11:55:55 +0900

    feat: Lotto 클래스 List<Int>.toLottoNumbers 확장함수 추가

--- src/main/kotlin/lotto/domain/entity/Lotto.kt
+++ src/main/kotlin/lotto/domain/entity/Lotto.kt
@@ -5,5 +5,7 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        private fun List<Int>.toLottoNumbers(): Lotto = Lotto(this)
+    }
 }
