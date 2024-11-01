3ab66041261bf154672d3a612d1624cc19ba539c
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 02:03:54 +0900

    feat: 로또 결과 집계 및 상금 계산

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -1,2 +1,15 @@
 package lotto
 
+class LottoResult {
+    private val rankCounts = mutableMapOf<LottoRank, Int>().withDefault {0}
+
+    fun addRank(rank: LottoRank) {
+        rankCounts[rank] = rankCounts.getValue(rank) + 1
+    }
+    fun countRank(rank: LottoRank): Int {
+        return rankCounts.getValue(rank)
+    }
+    fun calculateTotalPrize(): Int {
+        return rankCounts.entries.sumBy { (rank, count) -> rank.prize * count }
+    }
+}
