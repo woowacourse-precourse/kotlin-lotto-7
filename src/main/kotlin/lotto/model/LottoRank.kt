b6ebefe7b4fb1711d5868f49532fdfd0ed3450ac
UiHyeon <hyunkim6790@gmail.com>
2024-11-01 22:46:42 +0900

    feat(LottoRank): 등급 비교 후 반환하는 함수 작성

--- src/main/kotlin/lotto/model/LottoRank.kt
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -1,18 +1,17 @@
 package lotto.model
 
-/**
- * 모델은 컨트롤러나 뷰에 의존하면 안된다.
- * 컨트롤러나 뷰의 코드가 있으면 안된다
- */
-
-// 프로퍼티를 받아 어떤 등급이랑 일치하는지 확인?
-enum class LottoRank(val matchCount: Int, val reward: Int) {
+enum class LottoRank(val matchCount: Int, val reward: Int, val bonusNumber: Boolean = false) {
     FIRST(6, 2000000000),
-    SECOND(5, 30000000),
+    SECOND(5, 30000000, true),
     THIRD(5, 1500000),
     FOURTH(4, 50000),
     FIFTH(3, 5000),
     NONE(0, 0);
 
-    fun getRank() {}
+    companion object {
+        fun getRank(matchCount: Int, reward: Int, bonusNumber: Boolean): LottoRank {
+            return LottoRank.entries
+                .find { it.matchCount == matchCount && it.reward == reward || it.bonusNumber == bonusNumber} ?: NONE
+        }
+    }
 }
---
+++ src/test/kotlin/lotto/model/LottoRank.kt
@@ -0,0 +1,15 @@
+package lotto.model
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottoRank {
+
+    /*@Test
+    fun `로또 당첨 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            LottoRank(6,2000000000)
+
+        }
+    }*/
+}
