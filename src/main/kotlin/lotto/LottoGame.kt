a9ff395a057861501959b6b32ddc219485adac46
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 01:31:29 +0900

    feat: 로또 구매후정렬기능

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Console
 class LottoGame {
     fun inputCost(): Int {
         println("구입금액을 입력해 주세요.")
-        return Console.readLine().toInt()
+        val cost = Console.readLine().toInt()
+        return cost
     }
     fun inputWinningLotto(): Lotto {
         println("당첨 번호를 입력해 주세요.")
@@ -16,6 +17,16 @@ class LottoGame {
         println("보너스 번호를 입력해 주세요.")
         val bonusNumber = Console.readLine().toInt()
         return bonusNumber
+    }
+    fun buyLottos(cost: Int): List<Lotto> {
+        val lottoCount = cost / 1000
+        println("${lottoCount}개를 구매했습니다.")
+        return List(lottoCount) {
+            val lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45,6).sorted()
+            val lotto = Lotto(lottoNumbers)
+            println(lotto)
+            lotto
+        }
     }
 
 
