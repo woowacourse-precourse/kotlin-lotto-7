95799ded4ead171722f437ca32b006d107112c10
unknown <yangjk9203@naver.com>
2024-11-01 16:00:35 +0900

    feat(ExceptionMessage): 오류항목 추가

--- src/main/kotlin/lotto/ExceptionMessage.kt
+++ src/main/kotlin/lotto/ExceptionMessage.kt
@@ -5,5 +5,8 @@ class ExceptionMessage {
         const val BLANK = "[ERROR] 구입 금액을 입력해 주세요."
         const val NOT_NUMBERS = "[ERROR] 숫자만 입력 가능합니다."
         const val NOT_DIVIDE_THOUSAND = "[ERROR] 천원 단위로 입력해 주세요."
+        const val NOT_LOTTO_NUMBER_RANGE = "[ERROR] 로또번호 범위를 벗어났습니다."
+        const val NOT_INPUT_SIX_NUMBER = "[ERROR] 6개의 번호가 아닙니다."
+        const val DUPLICATED_NUMBERS = "[ERROR] 같은 숫자를 입력했습니다."
     }
 }
