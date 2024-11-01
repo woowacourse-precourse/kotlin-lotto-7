60e3710eaba46b318f8d682f2e70aa8e8f769f19
unknown <yangjk9203@naver.com>
2024-11-01 13:41:08 +0900

    feat(RandomLottoGenerator): 무작위 로또 생성기 구현

---
+++ src/main/kotlin/lotto/RandomLottoGenerator.kt
@@ -0,0 +1,38 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Randoms
+
+
+class RandomLottoGenerator(amount: Int) {
+    init {
+        println()
+        println(lottoCountCalculate(amount).toString() + Message.NUMBER_OF_LOTTO)
+    }
+
+    fun lottoPurchase(amount: Int): MutableList<List<Int>> {
+        val lottoCount = lottoCountCalculate(amount)
+        return matching(lottoCount)
+
+    }
+
+    private fun matching(lottoGame: Int): MutableList<List<Int>> {
+        val randomLottoList = mutableListOf<List<Int>>()
+        repeat(lottoGame) {
+            val randomLottoNumber = randomLottoGenerate()
+            randomLottoList.add(randomLottoNumber)
+        }
+        return randomLottoList
+    }
+
+    private fun lottoCountCalculate(amount: Int): Int {
+        return (amount / 1000)
+    }
+
+    private fun randomLottoGenerate(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(
+            SettingValue.LOTTO_NUMBER_MINIMUM,
+            SettingValue.LOTTO_NUMBER_MAXIMUM,
+            SettingValue.LOTTO_NUMBER_COUNT
+        )
+    }
+}
