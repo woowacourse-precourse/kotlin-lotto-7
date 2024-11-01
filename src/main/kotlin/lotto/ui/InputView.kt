6d6bac4cd696598152443dfec46b3d7689ddf673
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-01 17:11:45 +0900

    refactor(InputView.kt): 로또 구매 금액 입력 방식 수정
    수정 내용
    - LottoAmount을 반환하지 않고 입력 금액을 반환하도록 수정
    - buildLottoAmount 대신 readLottoAmount 로 함수 명 변경

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -1,19 +1,13 @@
 package lotto.ui
 
 import camp.nextstep.edu.missionutils.Console
-import lotto.data.LottoAmount
 
 object InputView {
 
     private const val LOTTO_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요."
 
-    fun buildLottoAmount(): LottoAmount {
-        try {
-            println(LOTTO_AMOUNT_INPUT_MESSAGE)
-            return LottoAmount(Console.readLine())
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            return buildLottoAmount()
-        }
+    fun readLottoAmount(): String {
+        println(LOTTO_AMOUNT_INPUT_MESSAGE)
+        return Console.readLine()
     }
 }
