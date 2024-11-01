4cff439a2ea219799336ae13c533d3fb628d1e2b
bbangiee <yoojin1117@gmail.com>
2024-11-02 06:13:53 +0900

    feat(구입금액 입력): 예외 처리
    - 구입금액 입력시 조건에 부합하지 못할 때, 예외 처리 추가

---
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -0,0 +1,29 @@
+package lotto
+
+import kotlin.properties.Delegates
+
+class LottoMachine {
+    var payment by Delegates.notNull<Int>()
+
+    init {
+        val input = IOHandler().inputToUser(PAYMENRFIRSTINSTRUCTION)
+
+        payment = getCorrectPayment(input)
+    }
+
+    private fun getCorrectPayment(input: String): Int {
+        var tempInput = input
+
+        return try {
+            Validation().checkPayment(tempInput)
+        } catch (e: IllegalArgumentException) {
+            tempInput = IOHandler().inputToUser("${e.message} $PAYMENTRETRYINSTRUCTION")
+            getCorrectPayment(tempInput)
+        }
+    }
+
+    companion object {
+        const val PAYMENRFIRSTINSTRUCTION = "구입금액을 입력해 주세요."
+        const val PAYMENTRETRYINSTRUCTION = "구입금액을 다시 입력해주세요."
+    }
+}
--- src/main/kotlin/lotto/Validation.kt
+++ src/main/kotlin/lotto/Validation.kt
@@ -2,19 +2,13 @@ package lotto
 
 class Validation {
     fun checkPayment(payment: String): Int {
+        require(isDigit(payment)) { PAYMENTNOTDIGITINSTRUCTION }
 
-        if (isDigit(payment)) {
-            val convertedPayment = payment.toInt()
+        val convertedPayment = payment.toInt()
 
-            when {
-                !isPaymentUnit1000(convertedPayment) -> throw IllegalArgumentException()
-            }
-
-            if (isPaymentUnder100000(convertedPayment) && (isPaymentUnit1000(convertedPayment))) {
-                return convertedPayment
-            }
-        }
-        throw IllegalArgumentException()
+        require(isPaymentUnder100000(convertedPayment)) { PAYMENTOVERLIMITINSTRUCTION }
+        require(isPaymentUnit1000(convertedPayment)) { PAYMENTNOT1000INSTRUCTION }
+        return convertedPayment
     }
 
     fun isDigit(input: String): Boolean {
@@ -26,17 +20,19 @@ class Validation {
         return true
     }
 
-    fun isPaymentUnder100000(payment: Int): Boolean {
-        return payment in 0..100000
+    private fun isPaymentUnder100000(payment: Int): Boolean {
+        return payment in 0..PURCHASELIMIT
     }
 
-    fun isPaymentUnit1000(payment: Int): Boolean {
-        return payment % 1000 == 0
+    private fun isPaymentUnit1000(payment: Int): Boolean {
+        return payment % SALEUNIT == 0
     }
-}
 
-class Exception {
-    fun showError() {
-        throw IllegalArgumentException()
+    companion object {
+        const val PURCHASELIMIT = 100000
+        const val SALEUNIT = 1000
+        const val PAYMENTNOTDIGITINSTRUCTION = "[ERROR] 구입금액은 숫자로 입력해야 합니다."
+        const val PAYMENTOVERLIMITINSTRUCTION = "[ERROR] 10만원 구입 한도를 초과하였습니다."
+        const val PAYMENTNOT1000INSTRUCTION = "[ERROR] 구입금액은 1000원 단위로 입력해야 합니다. 로또 1장의 가격은 1000원입니다."
     }
 }
