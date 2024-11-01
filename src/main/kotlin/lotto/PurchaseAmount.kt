3c012a97314122f58e87ee6bedbdf9bf7b622d20
unknown <yangjk9203@naver.com>
2024-11-01 15:53:33 +0900

    refactor(PurchaseAmount): 예외처리 방식 변경

--- src/main/kotlin/lotto/PurchaseAmount.kt
+++ src/main/kotlin/lotto/PurchaseAmount.kt
@@ -7,24 +7,53 @@ class PurchaseAmount {
         purchaseAmountMessage()
     }
 
-    fun price() = validateAmount(purchaseAmountInput())
+    private var validatorTest = false
+    private var amount = ""
+
+    fun lottoPurchased(): Int {
+        while (!validatorTest) {
+            purchaseAmountException()
+        }
+        return amount.toInt()
+    }
+
+
+    private fun purchaseAmountException(): Any {
+        try {
+            amount = purchaseAmountInput()
+            checkBlank(amount)
+            checkNumber(amount)
+            checkDivide(amount)
+            validatorTest = true
+            return amount
+        } catch (ex: Exception) {
+            return println(ex.message)
+        }
+    }
 
     private fun purchaseAmountInput(): String {
         return Console.readLine()
     }
 
-    private fun validateAmount(userInput: String): Int {
-        if (userInput.isBlank()) {
-            throw IllegalArgumentException(ExceptionMessage.BLANK)
-        }
-        if (!userInput.contains(Regex("^[0-9]*$"))) {
-            throw IllegalArgumentException(ExceptionMessage.NOT_NUMBERS)
-        }
-        if (userInput.toInt() % 1000 != 0) {
-            throw IllegalArgumentException(ExceptionMessage.NOT_DIVIDE_THOUSAND)
-        }
-        return userInput.toInt()
+//    private fun validateAmount(amount: String): Int {
+//        checkBlank(amount)
+//        checkNumber(amount)
+//        checkDivide(amount)
+//        return amount.toInt()
+//    }
+
+    private fun checkBlank(amount: String) {
+        require(amount.isNotBlank()) { ExceptionMessage.BLANK }
     }
+
+    private fun checkNumber(amount: String) {
+        require(amount.contains(Regex("^[0-9]*$"))) { ExceptionMessage.NOT_NUMBERS }
+    }
+
+    private fun checkDivide(amount: String) {
+        require(amount.toInt() % 1000 == 0) { ExceptionMessage.NOT_DIVIDE_THOUSAND }
+    }
+
 
     private fun purchaseAmountMessage() {
         return println(Message.ENTER_PURCHASE_AMOUNT)
