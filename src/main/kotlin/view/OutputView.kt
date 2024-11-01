74f134bca7b4befcc4e091e410360d182814f909
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-01 13:00:30 +0900

    refactor(OutputView): 출력 문자열을 상수화

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -2,23 +2,39 @@ package view
 
 import lotto.Lotto
 import model.Prize
+import utils.OutputMessages
+import utils.OutputMessages.BONUS_BALL_MATCH_OUTPUT_MESSAGE
+import utils.OutputMessages.BUYING_OUTPUT_MESSAGE
+import utils.OutputMessages.COUNT_MATCH_OUTPUT_MESSAGE
+import utils.OutputMessages.DASH_OUTPUT_MESSAGE
+import utils.OutputMessages.MONEY_OUTPUT_MESSAGE
+import utils.OutputMessages.WINNING_COUNT_OUTPUT_MESSAGE
+import utils.OutputMessages.WINNING_STATISTICS_OUTPUT_MESSAGE
 
 object OutputView {
     fun printLotto(count: Int, lottoes: List<Lotto>) {
-        println("${count}개를 구매했습니다.")
+        println(
+            count.toString() + BUYING_OUTPUT_MESSAGE
+        )
         lottoes.forEach { println(it) }
     }
 
     fun printWinningStatistics() {
-        println("당첨 통계")
-        println("---")
-        Prize.entries.forEach{
-            when(it){
+        println(WINNING_STATISTICS_OUTPUT_MESSAGE)
+        println(DASH_OUTPUT_MESSAGE)
+        Prize.entries.forEach {
+
+            when (it) {
                 Prize.SECOND -> println(
-                    "${it.matchCount}개 일치, 보너스 볼 일치(${it.prize}${it.winningCount}개"
+                    it.matchCount.toString() + BONUS_BALL_MATCH_OUTPUT_MESSAGE +
+                            it.prize.toString() + MONEY_OUTPUT_MESSAGE +
+                            it.winningCount.toString() + WINNING_COUNT_OUTPUT_MESSAGE
                 )
+
                 else -> println(
-                    "${it.matchCount}개 일치 (${it.prize}원) - ${it.winningCount}개"
+                    it.matchCount.toString() + COUNT_MATCH_OUTPUT_MESSAGE +
+                            it.prize.toString() + MONEY_OUTPUT_MESSAGE +
+                            it.winningCount.toString() + WINNING_COUNT_OUTPUT_MESSAGE
                 )
                 // Prize 클래스에 toString 을 정의하는 것도 고민해봤지만, 이 방식이 더 간결해서 else 를 사용했습니다.
                 // else 를 사용하지 않고, 간결성을 챙길 수 있다면 편하게 제안해주시면 감사하겠습니다 !
