039949740b947ad4c118da1fa7bcd3ad19389941
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-11-01 21:07:56 +0900

    feat: 로또 당첨번호 입력 및 보너스 번호 입력 기능 구현
    1. 로또 당첨번호와 보너스 번호 입력 UI 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -17,7 +17,7 @@ class InputView {
 
     companion object {
         const val PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요."
-        const val WINNING_NUMBER_INPUT = "당첨 번호를 입력해 주세요."
-        const val BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요."
+        const val WINNING_NUMBER_INPUT = "\n당첨 번호를 입력해 주세요."
+        const val BONUS_NUMBER_INPUT = "\n보너스 번호를 입력해 주세요."
     }
 }
