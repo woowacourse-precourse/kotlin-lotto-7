a81db6e98f690035d5ab94e451f38715055aeb59
joonhokil <kiljh@me.com>
2024-11-02 08:40:30 +0900

    refactor: 함수의 줄 길이가 늘어서 기능에 따라 함수 추출

--- src/main/kotlin/lotto/LottoJudge.kt
+++ src/main/kotlin/lotto/LottoJudge.kt
@@ -4,12 +4,20 @@ class LottoJudge {
     var winnerNumbers: List<Int> = emptyList()
 
     fun setLottoWinnerNumbers(numbers: List<Int>) {
+        exceptWinnerNumberCount(numbers)
+        exceptWinnerNumberRange(numbers)
+        winnerNumbers = numbers
+    }
+
+    private fun exceptWinnerNumberCount(numbers: List<Int>) {
         if (numbers.size != 6) {
             throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
         }
+    }
+
+    private fun exceptWinnerNumberRange(numbers: List<Int>) {
         if (numbers.any { it < 1 || it > 45 }) {
             throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_COUNT_ERROR.getMessage())
         }
-        winnerNumbers = numbers
     }
 }
