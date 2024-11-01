6027102b636a3a46a43dddf5a8f00ec6b9840f21
chanho0908 <uni070@naver.com>
2024-11-01 22:13:29 +0900

    feat(Lotto.kt) : 유효성 검사 및 당첨 번호 일치 여부 반환 기능 추가

---
+++ src/main/kotlin/domain/lotto/Lotto.kt
@@ -0,0 +1,31 @@
+package domain.lotto
+
+import domain.enums.Exception
+import domain.enums.LottoSetting.LOTTO_MIN
+import domain.enums.LottoSetting.LOTTO_MAX
+import domain.enums.LottoSetting.LOTTO_SIZE
+import domain.enums.Rank
+import java.util.TreeSet
+
+class Lotto(
+    // 당첨 번호
+    private val numbers: List<Int>
+) {
+    init {
+        require(numbers.size == 6) { Exception.INVALID_LOTTO_SIZE }
+        require(numbers.distinct().size == LOTTO_SIZE.value()) { Exception.INVALID_DUPLICATED }
+        require(numbers.all { it in LOTTO_MIN.value()..LOTTO_MAX.value() }) {
+            Exception.EXCEED_INPUT
+        }
+    }
+
+    fun getMatches(lottoNumbers: TreeSet<Int>, bonusNumber: Int): Rank {
+        val matches = numbers.count { lottoNumbers.contains(it) }
+        val bonusMatched = lottoNumbers.contains(bonusNumber)
+        val rank = getRank(matches, bonusMatched)
+        return rank
+    }
+
+    private fun getRank(matches: Int, bonusMatched: Boolean): Rank =
+        Rank.getRank(matches, bonusMatched)
+}
