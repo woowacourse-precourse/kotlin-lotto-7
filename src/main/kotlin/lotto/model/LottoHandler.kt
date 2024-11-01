b44820d6aff4fa819b966eddb02218ee0b1e522a
DoableTuple <doabletuple@gmail.com>
2024-11-01 15:01:04 +0900

    refactor: 로또 번호 개수 관련 상수 이름 변경

--- src/main/kotlin/lotto/model/LottoHandler.kt
+++ src/main/kotlin/lotto/model/LottoHandler.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LottoHandler {
     fun generateRandomNumbers(): List<Int> {
-        val numbers = Randoms.pickUniqueNumbersInRange(Constant.LOTTO_NUMBER_MIN, Constant.LOTTO_NUMBER_MAX, Constant.LOTTO_NUMBERS_COUNT)
+        val numbers = Randoms.pickUniqueNumbersInRange(Constant.LOTTO_NUMBER_MIN, Constant.LOTTO_NUMBER_MAX, Constant.LOTTO_NUMBER_COUNT)
         return numbers
     }
 }
--- src/main/kotlin/lotto/util/Constant.kt
+++ src/main/kotlin/lotto/util/Constant.kt
@@ -2,7 +2,7 @@ package lotto.util
 
 object Constant {
     const val LOTTO_PRICE = 1000
-    const val LOTTO_NUMBERS_COUNT = 6
+    const val LOTTO_NUMBER_COUNT = 6
     const val LOTTO_NUMBER_MIN = 1
     const val LOTTO_NUMBER_MAX = 45
 }
