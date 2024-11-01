bb296d0a90625c2ebb2078a86afbdde48d7b9f30
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 01:16:28 +0900

    feat: 각 로또와 당첨 로또를 비교하는 기능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,4 +4,14 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
+    fun getMatchCount(winningLotto: Lotto): Int {
+        return numbers.intersect(winningLotto.numbers.toSet()).size
+    }
+    fun contains(number: Int): Boolean {
+        return number in numbers
+    }
+    override fun toString(): String {
+        return numbers.toString()
+    }
 }
+
