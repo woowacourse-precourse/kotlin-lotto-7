489ad099dad6cca812a2e64e1c6287913e284861
지원 <cheerfulboys@naver.com>
2024-11-01 15:38:00 +0900

    feat(LottoGame): validatePrice 메서드와 buy 메서드 추가
    입력된 가격이 숫자가 아니면 오류 메세지를 반환하고 아니면 null을 반환하는 validatePrice 메서드를 추가함.
    
    구매가격을 로또 가격으로 나누어 구매한 로또의 수량을 반환하는 buy 메서드를 추가함.

--- src/main/kotlin/lotto/model/LottoGame.kt
+++ src/main/kotlin/lotto/model/LottoGame.kt
@@ -1,8 +1,21 @@
 package lotto.model
 
-class LottoGame(private val numbers: List<Int>) {
-    init {
-        Lotto(numbers).sort()
+class LottoGame {
+    private var numbers: List<Int> = listOf()
+
+    fun set(lottoNumbers: List<Int>) {
+        numbers = lottoNumbers
+    }
+
+    fun validatePrice(price: String): String? {
+        if (price.toIntOrNull() == null) {
+            return "[ERROR] 구매 가격은 숫자여야 합니다."
+        }
+        return null
+    }
+
+    fun buy(price: String): Int {
+        return price.toInt() / LOTTO_TICKET_PRICE
     }
 
     fun check(winningNumbers: List<Int>): Int {
@@ -17,5 +30,9 @@ class LottoGame(private val numbers: List<Int>) {
 
     fun isBonus(bonusNumber: Int): Boolean {
         return numbers.contains(bonusNumber)
+    }
+
+    companion object {
+        const val LOTTO_TICKET_PRICE = 1000
     }
 }
