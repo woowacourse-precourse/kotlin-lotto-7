6c513d3669413334cee4e22665f72f0511ac1861
HamBeomJoon <hbj0802@naver.com>
2024-11-02 01:11:06 +0900

    fix(Constants): 로또 번호 구분자 수정
    - ","에서 ", "로 수정

--- src/main/kotlin/lotto/Constants.kt
+++ src/main/kotlin/lotto/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val INPUT_BUY_AMOUNT_MESSAGE = "구입금액을 입력해주세요."
     const val INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해주세요."
     const val INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요."
-    const val NUMBER_DELIMITER = ","
+    const val NUMBER_DELIMITER = ", "
     const val LOTTO_PRICE = 1000
     const val LOTTO_NUMBERS_SIZE = 6
     const val LOTTO_NUMBER_MIN = 1
