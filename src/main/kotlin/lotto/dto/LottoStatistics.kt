9bf3c4ff2e16699669bf29e38d01baa9db543826
Potato-Y <projectpotatoy@gmail.com>
2024-11-01 22:09:30 +0900

    refactor(LottoStatistics): DTO 클래스를 data class로 변경
    기존의 class를 data class로 변경하여 DTO 역할에 맞게 변경

--- src/main/kotlin/lotto/dto/LottoStatistics.kt
+++ src/main/kotlin/lotto/dto/LottoStatistics.kt
@@ -2,5 +2,4 @@ package lotto.dto
 
 import lotto.domain.LottoRank
 
-class LottoStatistics(val rankCount: Map<LottoRank, Int>, val roi: Double) {
-}
+data class LottoStatistics(val rankCount: Map<LottoRank, Int>, val roi: Double)
