7ed8d46e10463b8896d0fb5b932ff50934e26ee6
Junghs-cloud <rabbit4935@gmail.com>
2024-11-02 00:46:44 +0900

    test: 당첨 번호와 비교하여 등수 확인하는 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,8 @@
 package lotto
 
+import lotto.Model.LottoRank
+import lotto.Model.WinningLottoResult
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -39,5 +42,46 @@ class LottoTest {
         assertDoesNotThrow {
             Lotto(listOf(1, 2, 3, 4, 5, 6))
         }
+    }
+
+    @Test
+    fun testLottoFirstRank() {
+        val winningLottoResult = makeWinningLottoResult()
+        val releasedLotto = Lotto(listOf(1,2,3,4,5,6))
+        assertEquals(releasedLotto.getLottoResult(winningLottoResult), LottoRank.First)
+    }
+
+    @Test
+    fun testLottoSecondRank() {
+        val winningLottoResult = makeWinningLottoResult()
+        val releasedLotto = Lotto(listOf(1,2,3,4,5,7))
+        assertEquals(releasedLotto.getLottoResult(winningLottoResult), LottoRank.Second)
+    }
+
+    @Test
+    fun testLottoThirdRank() {
+        val winningLottoResult = makeWinningLottoResult()
+        val releasedLotto = Lotto(listOf(1,2,3,4,5,45))
+        assertEquals(releasedLotto.getLottoResult(winningLottoResult), LottoRank.Third)
+    }
+
+    @Test
+    fun testLottoFourthRank() {
+        val winningLottoResult = makeWinningLottoResult()
+        val releasedLotto = Lotto(listOf(1,2,3,4,44,45))
+        assertEquals(releasedLotto.getLottoResult(winningLottoResult), LottoRank.Fourth)
+    }
+
+    @Test
+    fun testLottoFifthRank() {
+        val winningLottoResult = makeWinningLottoResult()
+        val releasedLotto = Lotto(listOf(1,2,3,43,44,45))
+        assertEquals(releasedLotto.getLottoResult(winningLottoResult), LottoRank.Fifth)
+    }
+
+    private fun makeWinningLottoResult(): WinningLottoResult {
+        val winningLotto = Lotto(listOf(1,2,3,4,5,6))
+        val bonusNumber = 7
+        return WinningLottoResult(winningLotto, bonusNumber)
     }
 }
