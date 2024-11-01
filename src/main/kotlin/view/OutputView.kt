56deb548d86373e4ad8a7258f565606bba06b137
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-01 13:04:07 +0900

    refactor(OutputView): 당첨 통계 출력 시 중첩되는 부분 함수화

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -2,7 +2,6 @@ package view
 
 import lotto.Lotto
 import model.Prize
-import utils.OutputMessages
 import utils.OutputMessages.BONUS_BALL_MATCH_OUTPUT_MESSAGE
 import utils.OutputMessages.BUYING_OUTPUT_MESSAGE
 import utils.OutputMessages.COUNT_MATCH_OUTPUT_MESSAGE
@@ -23,23 +22,23 @@ object OutputView {
         println(WINNING_STATISTICS_OUTPUT_MESSAGE)
         println(DASH_OUTPUT_MESSAGE)
         Prize.entries.forEach {
+            println(
+                it.matchCount.toString() + matchOutputMessage(it)
+                        + it.prize.toBigDecimal().toString() + MONEY_OUTPUT_MESSAGE +
+                        it.winningCount.toString() + WINNING_COUNT_OUTPUT_MESSAGE
+            )
 
-            when (it) {
-                Prize.SECOND -> println(
-                    it.matchCount.toString() + BONUS_BALL_MATCH_OUTPUT_MESSAGE +
-                            it.prize.toString() + MONEY_OUTPUT_MESSAGE +
-                            it.winningCount.toString() + WINNING_COUNT_OUTPUT_MESSAGE
-                )
-
-                else -> println(
-                    it.matchCount.toString() + COUNT_MATCH_OUTPUT_MESSAGE +
-                            it.prize.toString() + MONEY_OUTPUT_MESSAGE +
-                            it.winningCount.toString() + WINNING_COUNT_OUTPUT_MESSAGE
-                )
-                // Prize 클래스에 toString 을 정의하는 것도 고민해봤지만, 이 방식이 더 간결해서 else 를 사용했습니다.
-                // else 를 사용하지 않고, 간결성을 챙길 수 있다면 편하게 제안해주시면 감사하겠습니다 !
-            }
+            // Prize 클래스에 toString 을 정의하는 것도 고민해봤지만, 이 방식이 더 간결해서 else 를 사용했습니다.
+            // else 를 사용하지 않고, 간결성을 챙길 수 있다면 편하게 제안해주시면 감사하겠습니다 !
         }
     }
 
+    private fun matchOutputMessage(prize: Prize): String {
+        return when (prize) {
+            Prize.SECOND -> BONUS_BALL_MATCH_OUTPUT_MESSAGE
+            else -> COUNT_MATCH_OUTPUT_MESSAGE
+
+        }
+    }
 }
+
