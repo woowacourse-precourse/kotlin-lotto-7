b1cda03f734adaaf353ccb4f51f3e40ed1de5f79
이용인 <yongin0517@gmail.com>
2024-11-01 16:28:49 +0900

    feat: 생성된 로또 오름차순 정렬 로직 추가

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -3,7 +3,7 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoGenerator {
-    fun generateLotto():List<Int>{
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    fun generateLotto(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
     }
 }
