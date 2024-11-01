9ef9805424bbd16ce61b8f9ae5fe3f6a644898b6
chanho0908 <uni070@naver.com>
2024-11-01 22:16:41 +0900

    feat(LottoViewModel.kt) : 로또 당첨 여부에 따라 당첨 금액과 수익률을 계산하는 기능 추가

--- src/main/kotlin/vm/LottoViewModel.kt
+++ src/main/kotlin/vm/LottoViewModel.kt
@@ -1,8 +1,10 @@
 package vm
 
+import domain.enums.Rank
 import domain.lotto.Lotto
 import domain.model.PurchaseState
 import sam.LottoFactory
+import util.convertRoundAtTwoDecimal
 import java.util.TreeSet
 
 class LottoViewModel(
@@ -12,20 +14,21 @@ class LottoViewModel(
     var state = PurchaseState()
         private set
 
-    fun onCompleteInputPayment(pay: Int){
+    fun onCompleteInputPayment(pay: Int) {
         state = state.copy(purchaseLottoCount = pay)
         pickLotto()
     }
 
-    fun onCompleteInputWinningNumber(winningNumber: List<Int>){
+    fun onCompleteInputWinningNumber(winningNumber: List<Int>) {
         state = state.copy(winningNumber = winningNumber)
     }
 
-    fun onCompleteInputBonusNumber(bonusNumber: Int){
+    fun onCompleteInputBonusNumber(bonusNumber: Int) {
         state = state.copy(bonusNumber = bonusNumber)
+        getLottoResult()
     }
 
-    private fun pickLotto(){
+    private fun pickLotto() {
         val purchaseLottoAmount = state.purchaseLottoCount
         val pickedLotto = mutableListOf<TreeSet<Int>>()
         repeat(purchaseLottoAmount) {
@@ -34,12 +37,35 @@ class LottoViewModel(
         state = state.copy(pickedLotto = pickedLotto)
     }
 
-    private fun getLottoResult(){
+    private fun getLottoResult() {
         val lotto = Lotto(state.winningNumber)
+        val updatedReward = state.reward.copy()
 
         state.pickedLotto.map {
-            //lotto.calculateReword(it)
+            val matches = lotto.getMatches(it, state.bonusNumber)
+            if (matches != Rank.NONE) {
+                updatedReward.winning[matches] = updatedReward
+                    .winning
+                    .getOrDefault(matches, 0) + 1
+            }
         }
 
+        state = state.copy(reward = updatedReward)
+        calculateRateOfReturn()
+    }
+
+    private fun calculateRateOfReturn() {
+        // 당첨 금액
+        var winningMoney = 0L
+        val totalPurchaseAmount = state.purchaseLottoCount * 1000
+        state.reward.winning.mapKeys {
+            winningMoney += it.key.getReword() * it.value
+        }
+
+        if (winningMoney != 0L) {
+            val rateOfReturn = (winningMoney.toDouble() / totalPurchaseAmount) * 100
+            val formattedRateOfReturn = rateOfReturn.convertRoundAtTwoDecimal()
+            state = state.copy(rateOfReturn = formattedRateOfReturn)
+        }
     }
 }
