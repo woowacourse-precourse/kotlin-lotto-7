c914de20a8ccfafd6a0482d567573b610f008fdf
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 20:37:19 +0900

    fix: 번호 일치 개수에 따른 등수 분배 실수 수정

--- src/test/kotlin/lotto/LottoRankTest.kt
+++ src/test/kotlin/lotto/LottoRankTest.kt
@@ -10,23 +10,23 @@ class LottoRankTest {
         assertThat(rank).isEqualTo(LottoRank.FIRST)
     }
     @Test
-    fun `5개 번호가 일치하면 SECOND`() {
-        val rank = LottoRank.getRank(matchCount = 5, matchBonus = false)
+    fun `5개 번호와 보너스번호 일치하면 SECOND`() {
+        val rank = LottoRank.getRank(matchCount = 5, matchBonus = true)
         assertThat(rank).isEqualTo(LottoRank.SECOND)
     }
     @Test
-    fun `5개 번호와 보너스번호 일치하면 THIRD`() {
-        val rank = LottoRank.getRank(matchCount = 6, matchBonus = true)
+    fun `5개 번호가 일치하면 THIRD`() {
+        val rank = LottoRank.getRank(matchCount = 5, matchBonus = false)
         assertThat(rank).isEqualTo(LottoRank.THIRD)
     }
     @Test
     fun `4개 번호가 일치하면 FOURTH`() {
-        val rank = LottoRank.getRank(matchCount = 6, matchBonus = false)
+        val rank = LottoRank.getRank(matchCount = 4, matchBonus = false)
         assertThat(rank).isEqualTo(LottoRank.FOURTH)
     }
     @Test
     fun `3개 번호가 일치하면 FIRST`() {
-        val rank = LottoRank.getRank(matchCount = 6, matchBonus = false)
+        val rank = LottoRank.getRank(matchCount = 3, matchBonus = false)
         assertThat(rank).isEqualTo(LottoRank.FIFTH)
     }
     @Test
