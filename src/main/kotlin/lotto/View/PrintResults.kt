e5f6fa0f50c91e48503666edc5f3c4a0de20f1a7
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-11-03 03:19:53 +0900

    ♻️ Refactor: PrintResult 리팩토링
    ResultPrinter -> PrintResult 파일 내 resultPrinter 함수로 변경

--- src/main/kotlin/lotto/View/PrintResults.kt
+++ src/main/kotlin/lotto/View/PrintResults.kt
@@ -1,15 +1,23 @@
 package lotto.View
 
-import lotto.Controller.getPrizesToPrint
 import lotto.Model.Prize
+import java.text.NumberFormat
+import java.util.Locale
 
 fun printResults(results: Map<Prize, Int>, profitRate: Double) {
     println("당첨 통계\n---")
 
-    val prizesToPrint = getPrizesToPrint()
+    val prizesToPrint = Prize.getPrizesToPrint()
     prizesToPrint.forEach { prize ->
-        printPrizeResult(prize, results[prize] ?: 0)
+        resultPrinter(prize, results[prize] ?: 0)
     }
 
     println("총 수익률은 %.1f%%입니다.".format(profitRate))
+}
+
+private fun resultPrinter(prize: Prize, count: Int) {
+    val formatter = NumberFormat.getNumberInstance(Locale.KOREA)
+    val formattedPrizeMoney = formatter.format(prize.prizeMoney)
+    val bonusText = if (prize.bonus) ", 보너스 볼 일치" else ""
+    println("${prize.matchCount}개 일치$bonusText (${formattedPrizeMoney}원) - ${count}개")
 }
