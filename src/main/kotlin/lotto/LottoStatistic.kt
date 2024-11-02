0ea9b86ab609f3ced618e941fe4956aa3eb79160
unknown <yangjk9203@naver.com>
2024-11-02 23:31:37 +0900

    feat(LottoStatistic): 당첨여부 확인

---
+++ src/main/kotlin/lotto/LottoStatistic.kt
@@ -0,0 +1,32 @@
+package lotto
+
+class LottoStatistic {
+
+    fun confirmWinning(purchasedLottos: List<List<Int>>, winningNumber: List<Int>, bonusNumber: Int) {
+        for (i in purchasedLottos) {
+            analyzeWinningCount(winningNumber, i, bonusNumber)
+        }
+    }
+
+    private fun analyzeWinningCount(winningNumber: List<Int>, randomLotto: List<Int>, bonusNumber: Int): Any {
+        val matchCount = checkWinning(winningNumber, randomLotto)
+        if (matchCount == 6) return LottoMatchingCount.FIRST.count++
+        if (matchCount == 5 && checkBonus(bonusNumber, randomLotto)) return LottoMatchingCount.SECOND.count++
+        if (matchCount == 5) return LottoMatchingCount.THIRD.count++
+        if (matchCount == 4) return LottoMatchingCount.FOURTH.count++
+        if (matchCount == 3) return LottoMatchingCount.FIFTH.count++
+        return LottoMatchingCount.NONE.count++
+    }
+
+    private fun checkWinning(winningNumber: List<Int>, randomLotto: List<Int>): Int {
+        return winningNumber.intersect(randomLotto.toSet()).size
+    }
+
+    private fun checkBonus(bonusNumber: Int, randomLotto: List<Int>): Boolean {
+        return bonusNumber in randomLotto
+    }
+
+    fun calculateRateOfReturn(amount: Int): Double {
+        return (LottoMatchingCount.entries.sumOf { it.count * it.prize } / amount) * 100
+    }
+}
