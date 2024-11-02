5348bdd1d24ebe45af6a4e0957ddbf7dec41f6c9
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-02 23:16:47 +0900

    feat(Winning.kt): 당첨 번호 유효성 검사 기능 구현
    유효성 검증 목록
    - 입력을 하지 않았을 경우
    - `,`쉼표를 기준으로 6개가 아닐 경우
    - `,`슁표를 기준으로 숫자로만 이루어져 있지 않은 경우
    - 0 으로 시작하는 숫자를 입력한 경우
    - 중복된 값이 존재할 경우
    - 당첨 번호 범위를 벗어난 번호가 있는 경우
    - 당첨 번호가 오름차순으로 정렬되어 있지 않은 경우

---
+++ src/main/kotlin/lotto/data/Winning.kt
@@ -0,0 +1,34 @@
+package lotto.data
+
+class Winning(private val input: String) {
+    val numbers: List<Int>
+
+    init {
+        require(input.isNotEmpty()) { INPUT_EMPTY_ERROR_MESSAGE }
+        require(input.split(DELIMITER).size == NUMBER_SIZE) { NUMBER_COUNT_IS_NOT_SIX_ERROR_MESSAGE }
+        require(input.replace(DELIMITER, EMPTY_STRING).all { it.isDigit() }) { NUMBER_IS_NOT_ALL_DIGIT_ERROR_MESSAGE }
+        require(!input.split(DELIMITER).any { it.startsWith(ZERO) }) { NUMBER_START_ZERO_ERROR_MESSAGE }
+        require(input.split(DELIMITER).distinct().size == NUMBER_SIZE) { NUMBER_IS_DUPLICATION_ERROR_MESSAGE }
+        require(input.split(DELIMITER).map { it.toIntOrNull() }
+            .all { it != null }) { NUMBER_IS_OUT_OF_RANGE_ERROR_MESSAGE }
+        numbers = input.split(DELIMITER).map { it.toInt() }
+        require(numbers.all { it in WINNING_MIN_NUMBER..WINNING_MAX_NUMBER }) { NUMBER_IS_OUT_OF_RANGE_ERROR_MESSAGE }
+        require(numbers == numbers.sorted()) { NUMBER_IS_NOT_OF_SORT_ERROR_MESSAGE }
+    }
+
+    companion object {
+        private const val DELIMITER = ","
+        private const val NUMBER_SIZE = 6
+        private const val EMPTY_STRING = ""
+        private const val ZERO = "0"
+        private const val WINNING_MIN_NUMBER = 1
+        private const val WINNING_MAX_NUMBER = 45
+        private const val INPUT_EMPTY_ERROR_MESSAGE = "[ERROR] 당첨 번호가 입력되지 않았습니다. 6개의 번호를 쉼표(,)를 기준으로 입력해주세요."
+        private const val NUMBER_COUNT_IS_NOT_SIX_ERROR_MESSAGE = "[ERROR] 6개의 당첨 번호를 입력해주세요."
+        private const val NUMBER_IS_NOT_ALL_DIGIT_ERROR_MESSAGE = "[ERROR] 6개의 번호를 쉼표(,)를 기준으로 숫자만 입력해주세요."
+        private const val NUMBER_START_ZERO_ERROR_MESSAGE = "[ERROR] 0으로 시작하지 않는 6개의 번호를 쉼표(,)를 기준으로 입력해주세요."
+        private const val NUMBER_IS_DUPLICATION_ERROR_MESSAGE = "[ERROR] 중복되지 않는 6개의 번호를 입력해주세요."
+        private const val NUMBER_IS_OUT_OF_RANGE_ERROR_MESSAGE = "[ERROR] 1 ~ 45 범위 내 6개의 번호를 입력해주세요."
+        private const val NUMBER_IS_NOT_OF_SORT_ERROR_MESSAGE = "[ERROR] 당첨 번호는 오름차순으로 입력해주세요."
+    }
+}
