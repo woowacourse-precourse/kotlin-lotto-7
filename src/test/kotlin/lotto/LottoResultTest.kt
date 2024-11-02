6f09c2653e6d96143baf1166ab5f44ba962fdd78
DDangDin <Kcorelec7123@gmail.com>
2024-11-02 23:17:50 +0900

    test: LottoResultTest 테스트 코드 수정
    - LottoResult 수정에 따른 테스트 코드 수정

--- src/test/kotlin/lotto/LottoResultTest.kt
+++ src/test/kotlin/lotto/LottoResultTest.kt
@@ -8,27 +8,44 @@ class LottoResultTest {
 
     private lateinit var lottoResult: LottoResult
     private lateinit var expectedLottoList: List<Lotto>
-    private lateinit var expectedWinningNumbers: List<Int>
+    private lateinit var expectedWinningNumbersWithThreeMatches: List<Int>
+    private lateinit var expectedWinningNumbersWithBonusNumber: List<Int>
     private lateinit var expectedLottoRankList: List<LottoRank>
     private var expectedRateOfReturn: Double = 0.0
 
     @BeforeEach
     fun setUp() {
-        expectedLottoList = listOf(Lotto(listOf(1,2,3,4,5,6)))
-        expectedWinningNumbers = listOf(1,2,3,30,31,32)
-        expectedLottoRankList = listOf(LottoRank.THREE_MATCHES)
-        expectedRateOfReturn = (LottoRank.THREE_MATCHES.price / 1000).toDouble() * 100
-        lottoResult = LottoResult(expectedLottoList, expectedWinningNumbers, BONUS_NUMBER)
+        expectedLottoList = listOf(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        expectedWinningNumbersWithThreeMatches = listOf(1, 2, 3, 30, 31, 32)
+        expectedWinningNumbersWithBonusNumber = listOf(1, 2, 3, 4, 5, 10)
     }
 
     @Test
-    fun `로또 당첨 통계 테스트`() {
+    fun `로또번호와 당첨번호 비교 후 당첨 통계 테스트`() {
+        lottoResult = LottoResult(expectedLottoList, expectedWinningNumbersWithThreeMatches, BONUS_NUMBER)
+        expectedRateOfReturn = (LottoRank.THREE_MATCHES.price / 1000).toDouble() * 100
+        expectedLottoRankList = listOf(LottoRank.THREE_MATCHES)
+
         val actual = lottoResult.getResult()
+
+        assertEquals(expectedRateOfReturn, actual.rateOfReturn)
+        assertEquals(expectedLottoRankList, actual.lottoRankList)
+    }
+
+    @Test
+    fun `로또번호와 보너스 번호 당첨 통계 테스트`() {
+        lottoResult =
+            LottoResult(expectedLottoList, expectedWinningNumbersWithBonusNumber, BONUS_NUMBER)
+        expectedRateOfReturn = (LottoRank.FIVE_AND_BONUS_MATCHES.price / 1000).toDouble() * 100
+        expectedLottoRankList = listOf(LottoRank.FIVE_AND_BONUS_MATCHES)
+
+        val actual = lottoResult.getResult()
+
         assertEquals(expectedRateOfReturn, actual.rateOfReturn)
         assertEquals(expectedLottoRankList, actual.lottoRankList)
     }
 
     companion object {
-        private const val BONUS_NUMBER = 35
+        private const val BONUS_NUMBER = 6
     }
 }
