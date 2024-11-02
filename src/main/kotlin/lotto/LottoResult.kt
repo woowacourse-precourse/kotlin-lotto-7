8e7e2b9eb79ed3655a9c0d4f89ea1e3e8a0fe302
rkqls4764 <rkqls4764@naver.com>
2024-11-02 22:01:57 +0900

    feat: 당첨자 수 추가 함수 생성

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -2,4 +2,9 @@ package lotto
 
 class LottoResult {
     val winnersCount: MutableMap<LottoRank, Int> = HashMap()
+
+    /* 당첨자 수 추가 함수 */
+    fun addWinners(rank: LottoRank) {
+        winnersCount[rank] = (winnersCount[rank] ?: 0) + 1
+    }
 }
