ca3ed2ce74dca600ec72c01efd95ba8b54fea044
조형석 <chs9902@naver.com>
2024-11-02 17:20:08 +0900

    :sparkles: feat: 구매 로또 생성 기능 추가

--- src/main/kotlin/lotto/model/LottoGenerator.kt
+++ src/main/kotlin/lotto/model/LottoGenerator.kt
@@ -1,12 +1,15 @@
 package lotto.model
 
 class LottoGenerator {
-    private val lottos =  mutableListOf <Lotto>()
-    fun makeLotto(){
-
+    private val lottoes =  mutableListOf <Lotto>()
+    fun makeLotto(count :Int){
+        val random = Random()
+        repeat(count){
+            lottoes.add(Lotto(random.generate()))
+        }
     }
 
-    fun getLottos(){
-
+    fun getLottoes():List<Lotto>{
+        return lottoes
     }
 }
