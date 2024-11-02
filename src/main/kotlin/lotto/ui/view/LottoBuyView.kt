512774367e2dda07812deacef2215737df4da03f
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-03 00:45:50 +0900

    refactor: 화면에 출력하는 메세지 상수화
    값에 따라 변하는 출력은 추출 할 경우 오히려 가독성이 떨어져서
    고정된 출력만 상수화로 추출

--- src/main/kotlin/lotto/ui/view/LottoBuyView.kt
+++ src/main/kotlin/lotto/ui/view/LottoBuyView.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 import lotto.domain.entity.Lotto
 
 class LottoBuyView {
-    fun guidePurchaseAmount() = println("구입금액을 입력해 주세요.")
+    fun guidePurchaseAmount() = println(INPUT_PURCHASE_AMOUNT_MESSAGE)
 
     fun inputPurchaseAmount() = Console.readLine()
 
@@ -14,5 +14,9 @@ class LottoBuyView {
         lottoTickets.forEach { lotto ->
             println(lotto.getNumbers())
         }
+    }
+
+    companion object {
+        private const val INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요."
     }
 }
--- src/main/kotlin/lotto/ui/view/WinningNumberInputView.kt
+++ src/main/kotlin/lotto/ui/view/WinningNumberInputView.kt
@@ -4,11 +4,16 @@ import camp.nextstep.edu.missionutils.Console
 
 class WinningNumberInputView {
 
-    fun guideWinningNumbers() = println("당첨 번호를 입력해 주세요.")
+    fun guideWinningNumbers() = println(INPUT_WINNING_NUMBERS_MESSAGE)
 
-    fun guideBonusNumber() = println("보너스 번호를 입력해 주세요.")
+    fun guideBonusNumber() = println(INPUT_BONUS_NUMBER_MESSAGE)
 
     fun inputWinningNumbers() = Console.readLine()
 
     fun inputBonusNumber() = Console.readLine()
+
+    companion object {
+        private const val INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요."
+        private const val INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요."
+    }
 }
