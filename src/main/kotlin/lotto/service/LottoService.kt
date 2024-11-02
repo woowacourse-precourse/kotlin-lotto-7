4d367c1f2b340a9ec837e172f316b2633f4f8961
ijh1298 <imjh0417@gmail.com>
2024-11-02 22:47:03 +0900

    feat: Service 내 matchAllLotto, winStatusUpdate 메소드 추가

--- src/main/kotlin/lotto/service/LottoService.kt
+++ src/main/kotlin/lotto/service/LottoService.kt
@@ -2,6 +2,8 @@ package lotto.service
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.domain.entity.Lotto
+import lotto.domain.entity.WinningLotto
+import lotto.domain.entity.increase
 
 class LottoService {
     private fun createRandomLotto() = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted())
@@ -10,6 +12,26 @@ class LottoService {
         val matchAmount = winLotto.nums.intersect(comparingLotto.nums.toSet()).size
         val hasBonus = comparingLotto.nums.contains(bonusNumber)
         return matchAmount to hasBonus
+    }
+
+    fun matchAllLotto(winLotto: Lotto, comparingLottos: List<Lotto>, bonusNumber: Int) {
+        comparingLottos.forEach {
+            val (matchAmount, hasBonus) = matchLotto(winLotto, it, bonusNumber)
+            winStatusUpdate(matchAmount, hasBonus)
+        }
+    }
+
+    private fun winStatusUpdate(matchAmount: Int, hasBonus: Boolean) {
+        if (matchAmount == 5 && hasBonus) {
+            WinningLotto.FiveBonus.increase()
+            return
+        }
+        when (matchAmount) {
+            3 -> WinningLotto.Three.increase()
+            4 -> WinningLotto.Four.increase()
+            5 -> WinningLotto.Five.increase()
+            6 -> WinningLotto.Six.increase()
+        }
     }
 
     fun purchaseLottos(money: Int): List<Lotto> {
