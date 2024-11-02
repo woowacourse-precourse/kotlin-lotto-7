adddf13d4e66ad03af86269364ef6f12c1805231
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-02 19:28:48 +0900

    feat: 로또 번호 중복 예외 처리
    - Lotto.kt: 로또 번호 중복이 있을 경우 예외를 발생합니다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,6 +7,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복이 없어야 합니다." }
     }
 
     // TODO: 추가 기능 구현
