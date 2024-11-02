66fa76b191b0bb5f022f3cdbb249ab148f701238
chanho0908 <uni070@naver.com>
2024-11-03 01:17:00 +0900

    feat : String Type 리스트를 Int 형 리스트로 변환하는 확장함수 추가

--- src/main/kotlin/domain/util/ext/CollectionExt.kt
+++ src/main/kotlin/domain/util/ext/CollectionExt.kt
@@ -6,6 +6,11 @@ fun <T> List<T>.toMapByEachCount(): Map<T, Int> {
     return this.groupingBy { it }.eachCount()
 }
 
+fun <T> List<T>.mapToInt(): List<Int> {
+    return this.map { it.toString().toInt() }
+}
+
 fun <T> TreeSet<T>.joinToStringWithSquareBracket(): String {
     return this.joinToString(separator = ", ", prefix = "[", postfix = "]")
 }
+
