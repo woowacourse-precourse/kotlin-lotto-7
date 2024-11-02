adf5be9388fcbe9607718a80da651bcc43adc5b2
rkqls4764 <rkqls4764@naver.com>
2024-11-02 20:04:51 +0900

    feat: 당첨 번호 입력 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -17,6 +17,8 @@ fun startLotto() {
     }
 
     showLottoInfo(lottoList)
+
+    val winningNumbers = inputWinningNumbers()
 }
 
 /* 로또 구입 금액 입력 함수 */
@@ -47,4 +49,28 @@ fun pickLottoNumbers(lottoList: MutableList<Lotto>) {
 fun showLottoInfo(lottoList: MutableList<Lotto>) {
     println("\n${lottoList.size}개를 구매했습니다.")
     lottoList.forEach { println(it.toString()) }
+}
+
+/* 당첨 번호 입력 함수 */
+fun inputWinningNumbers(): List<Int> {
+    while (true) {
+        try {
+            println("\n당첨 번호를 입력해 주세요.")
+            val input = readLine() ?: throw IllegalArgumentException("[ERROR] 입력 값이 없습니다.")
+
+            val numbers = input.split(",")
+                .map { it.trim().toIntOrNull() } //숫자로 변환
+                .filterNotNull() //null 제거
+
+            if (numbers.size != 6) {
+                throw IllegalArgumentException("[ERROR] 6개를 입력해 주세요.")
+            }
+
+            return numbers
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 숫자를 입력해 주세요.")
+        }
+    }
 }
