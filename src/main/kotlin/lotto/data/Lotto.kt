cd7e18abf04ce440eed17d97296066d0ff57a617
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-02 21:03:42 +0900

    feat(Lotto.kt): 로또 번호 유효성 검사 기능 구현
    유효성 검증 목록
    - 갯수 여부
    - 범위 여부
    - 중복 여부
    - 정렬 여부

--- src/main/kotlin/lotto/data/Lotto.kt
+++ src/main/kotlin/lotto/data/Lotto.kt
@@ -2,8 +2,20 @@ package lotto.data
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_NUMBER_COUNT) { NUMBER_COUNT_NOT_SIX_ERROR_MESSAGE }
+        require(numbers.all { it in LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER }) { NUMBER_RANGE_OUT_ERROR_MESSAGE }
+        require(numbers.distinct() == numbers) { NUMBER_NOT_UNIQUE_ERROR_MESSAGE }
+        require(numbers.sorted() == numbers) { NUMBER_NOT_SORT_ERROR_MESSAGE }
     }
 
     // TODO: 추가 기능 구현
+    companion object {
+        private const val NUMBER_COUNT_NOT_SIX_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
+        private const val NUMBER_RANGE_OUT_ERROR_MESSAGE = "[ERROR] 로또 번호는 1 부터 45 까지 지정 가능합니다."
+        private const val NUMBER_NOT_UNIQUE_ERROR_MESSAGE = "[ERROR] 중복된 로또 번호가 있습니다."
+        private const val NUMBER_NOT_SORT_ERROR_MESSAGE = "[ERROR] 오름차순으로 정렬되어 있지 않습니다."
+        private const val LOTTO_NUMBER_COUNT = 6
+        private const val LOTTO_MAX_NUMBER = 45
+        private const val LOTTO_MIN_NUMBER = 1
+    }
 }
