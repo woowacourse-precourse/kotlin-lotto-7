d7088823f72ff05bbc5c7b1664233f66bea802a5
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-02 20:46:27 +0900

    feat: 당첨금 상수를 선언한다

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,16 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Console
+
+const val PRIZE_MONEY_3 = 5000
+const val PRIZE_MONEY_4 = 50000
+const val PRIZE_MONEY_5 = 1500000
+const val PRIZE_MONEY_5_BONUS = 30000000
+const val PRIZE_MONEY_6 = 2000000000
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
-
-
 
 
 }
