2fc86b0b3af692c8eb1692776283ed3ad3f997f1
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-02 15:04:43 +0900

    feat: 로또 구입 금액이 양의 정수가 아닐 경우 예외 처리
    - LottoAmount.kt: 0원 이상인지 판별합니다.
    - LottoController.kt: 로또 입력이 정수인지 판별합니다.

--- src/main/kotlin/lotto/LottoAmount.kt
+++ src/main/kotlin/lotto/LottoAmount.kt
@@ -1,8 +1,11 @@
 package lotto
 
 class LottoAmount(private val amount: Int) {
+    var lottoAmount = amount
+        private set
 
     init {
+        require(amount > 0) { "[ERROR] 로또 구매 금액은 0원 이상이어야 합니다." }
         require(amount % 1_000 == 0) { "[ERROR] 로또 구매 금액은 천 원 단위로 입력 가능합니다." }
     }
 }
--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,12 +1,22 @@
 package lotto.controller
 
 import camp.nextstep.edu.missionutils.Console.readLine
+import lotto.LottoAmount
+import lotto.LottoService
 
 class LottoController {
+    private val lottoService = LottoService()
 
     fun start() {
         println("구입금액을 입력해 주세요.")
-        val price = readLine()
-        require(price.isNotBlank()) { "[ERROR] 공백은 입력할 수 없습니다." }
+        val inputAmount = readLine()
+        require(inputAmount.isNotBlank()) { "[ERROR] 공백은 입력할 수 없습니다." }
+        val amount =
+            LottoAmount(
+                inputAmount.toIntOrNull()
+                    ?: throw IllegalArgumentException("[ERROR] 정수만 입력할 수 있습니다.")
+            )
+
+
     }
 }
