93316bc9058a3c64b256a4113bb3ebdecb1bc640
ijh1298 <imjh0417@gmail.com>
2024-11-02 21:00:31 +0900

    feat: purchaseLottos 메소드 추가

--- src/main/kotlin/lotto/service/LottoService.kt
+++ src/main/kotlin/lotto/service/LottoService.kt
@@ -4,11 +4,22 @@ import camp.nextstep.edu.missionutils.Randoms
 import lotto.domain.entity.Lotto
 
 class LottoService {
-    fun createRandomLotto() = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+    private fun createRandomLotto() = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
 
     fun matchLotto(winLotto: Lotto, comparingLotto: Lotto, bonusNumber: Int): Pair<Int, Boolean> {
         val matchAmount = winLotto.nums.intersect(comparingLotto.nums.toSet()).size
         val hasBonus = comparingLotto.nums.contains(bonusNumber)
         return matchAmount to hasBonus
+    }
+
+    fun purchaseLottos(money: Int): List<Lotto> {
+        val amount = money / LOTTO_PRICE
+        val lottos = mutableListOf<Lotto>()
+        repeat(amount) { lottos += createRandomLotto() }
+        return lottos
+    }
+
+    companion object {
+        const val LOTTO_PRICE = 1000
     }
 }
