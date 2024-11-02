3024895160face23fe9f9af0bd9aa56ebd2679c8
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 20:57:54 +0900

    test: Lotto 클래스의 기능 테스트 케이스들 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -28,7 +28,36 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
-    
+    @Test
+    fun `5개의 번호가 일치하면 일치 개수가 5여야 한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val matchCount = lotto.getMatchCount(winningLotto)
 
+        assertThat(matchCount).isEqualTo(5)
+    }
 
+    @Test
+    fun `0개의 번호가 일치하면 일치 개수가 0여야 한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = Lotto(listOf(10, 11, 12, 13, 14, 15))
+        val matchCount = lotto.getMatchCount(winningLotto)
+
+        assertThat(matchCount).isEqualTo(0)
+    }
+
+    @Test
+    fun `로또가 보너스 번호를 포함하면 true를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val containBonus = lotto.contains(4)
+
+        assertThat(containBonus).isTrue
+    }
+    @Test
+    fun `로또가 보너스 번호를 포함하지 않으면 false를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val containBonus = lotto.contains(9)
+
+        assertThat(containBonus).isFalse
+    }
 }
