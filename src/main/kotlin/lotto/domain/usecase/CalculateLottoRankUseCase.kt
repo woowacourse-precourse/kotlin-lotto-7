a2fb9fc0a75483c16cde22399243422e343ef6b2
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-02 21:32:07 +0900

    refactor(CalculateLottoRankUseCase): 보너스 넘버를 매개변수로 받아서 사용
    - WinningNumbers의 bonusNumber를 bonusNumber 매개변수로 변경
    
    당첨 번호 객체에서 보너스 번호를 분할했기 때문에

--- src/main/kotlin/lotto/domain/usecase/CalculateLottoRankUseCase.kt
+++ src/main/kotlin/lotto/domain/usecase/CalculateLottoRankUseCase.kt
@@ -2,16 +2,17 @@ package lotto.domain.usecase
 
 import lotto.domain.entity.Lotto
 import lotto.common.LottoRank
+import lotto.domain.entity.BonusNumber
 import lotto.domain.entity.WinningNumbers
 
 class CalculateLottoRankUseCase {
-    fun execute(lotto: Lotto, winningNumbers: WinningNumbers): LottoRank {
+    fun execute(lotto: Lotto, winningNumbers: WinningNumbers, bonusNumber: BonusNumber): LottoRank {
         val matchCount = lotto.getNumbers().count { number ->
             winningNumbers.getNumbers().contains(number)
         }
         return when (matchCount) {
             6 -> LottoRank.FIRST
-            5 -> if (lotto.getNumbers().contains(winningNumbers.bonusNumber)) LottoRank.SECOND else LottoRank.FOURTH
+            5 -> if (lotto.getNumbers().contains(bonusNumber.number)) LottoRank.SECOND else LottoRank.FOURTH
             4 -> LottoRank.THIRD
             3 -> LottoRank.FIFTH
             else -> LottoRank.SIXTH
