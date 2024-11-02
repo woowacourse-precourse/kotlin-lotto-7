d682531c9bee154e28843af18b7ebee01ee930f6
Tnalxmsk <alstnwkd990@naver.com>
2024-11-03 02:38:34 +0900

    test: 로또 기계 테스트 코드 작성
    6개의 번호를 가진 로또를 생성하는지 확인하는 테스트

---
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -0,0 +1,11 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+
+class LottoMachineTest {
+    @Test
+    fun `6개의 번호를 가진 로또를 생성한다`() {
+        val lotto = LottoMachine.generate()
+        assert(lotto.getNumbers().size == 6)
+    }
+}
