d44893a8ee8a4e94073a1b868e4030973ec76217
yeobeomhwi <yeobh0828@naver.com>
2024-11-03 01:48:34 +0900

    feat(Lotto):Add Error Message

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,8 @@
-package lotto
+package lotto.model
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.all {it in 1..45}){"[ERROR} 로또 번호는 1부터 45 사이의 숫자여야 합니다."}
     }
-
-    // TODO: 추가 기능 구현
 }
