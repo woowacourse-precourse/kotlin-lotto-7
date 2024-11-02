7a79808d796fdf610d3ed6efcc87cdf162fce534
jiwon <pjw99032986@gmail.com>
2024-11-02 16:12:09 +0900

    Feat(Output.kt) : 구입 금액 입력 문구 출력
    구입금액을 입력해 주세요. 출력

---
+++ src/main/kotlin/lotto/Output.kt
@@ -0,0 +1,11 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+import java.lang.NumberFormatException
+
+class Output {
+    fun outPutMoney(){
+        println("구입금액을 입력해 주세요.\n")
+    }
+
+}
