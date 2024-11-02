4895f1d6856cde9438cdd47e4c53d1f47e51054b
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-02 22:02:47 +0900

    feat: 로또 맞은 개수와 보너스 번호 맞춤 여부로 상 반환하는 메서드 추가
    - LottoPrize.kt: 로또 당첨 기준에 맞게 로또 맞은 개수와 보너스 번호 맞춤 여부로 각 로또의 당첨 기준에 해당하는 값 반환하는 메서드를 추가합니다.

--- src/main/kotlin/lotto/LottoPrize.kt
+++ src/main/kotlin/lotto/LottoPrize.kt
@@ -10,5 +10,22 @@ enum class LottoPrize(
     THIRD_PRIZE(5, 1_500_000, false),
     FOURTH_PRIZE(4, 50_000, false),
     FIFTH_PRIZE(3, 5_000, false),
-    NONE_PRIZE(0, 0, false),
+    NONE_PRIZE(0, 0, false);
+
+    companion object {
+
+        fun fromMatchingCount(matchingCount: Int, isMatchingBonusNumber: Boolean): LottoPrize {
+            if (matchingCount in 0..2) {
+                return NONE_PRIZE
+            }
+
+            if (matchingCount == 5) {
+                return entries.find { it.matchingCount == matchingCount && it.isMatchingBonusNumber == isMatchingBonusNumber }
+                    ?: throw IllegalArgumentException("예상치 못한 값입니다.")
+            }
+
+            return entries.find { it.matchingCount == matchingCount }
+                ?: throw IllegalArgumentException("예상치 못한 값입니다.")
+        }
+    }
 }
