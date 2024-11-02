e559a79901a10c0e4da1657884e88472dc895cec
rkqls4764 <rkqls4764@naver.com>
2024-11-03 00:50:38 +0900

    feat: 로또 번호 중복 예외 처리 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == numbers.size) { "[ERROR] 로또 번호는 중복될 수 없습니다."}
     }
 
     fun getNumbers(): List<Int> {
