b0e2fa0282e71b105c0c1a8e72c3d99c6a5607e7
HamBeomJoon <hbj0802@naver.com>
2024-11-02 15:27:34 +0900

    feat(Constants, Lotto): 발행한 로또 번호 유효성 검사

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,7 +4,11 @@ import lotto.Constants
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == Constants.LOTTO_NUMBERS_SIZE) { Constants.ERROR_LOTTO_NUMBERS_INCORRECT_SIZE }
+        require(numbers.all { it in Constants.LOTTO_NUMBER_MIN..Constants.LOTTO_NUMBER_MAX }) {
+            Constants.ERROR_LOTTO_NUMBERS_OUT_OF_RANGE
+        }
+        require(numbers.size == numbers.distinct().size) { Constants.ERROR_LOTTO_NUMBERS_DUPLICATE }
     }
 
     fun getNumbers(): List<Int> {
