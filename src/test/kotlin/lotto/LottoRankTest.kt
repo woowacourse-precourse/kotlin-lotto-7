0dd8f2ea3e57b9302c1f8ad0ab45fcec5c421877
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-02 17:20:03 +0900

    test: LottoRank에 대한 기능 테스트 케이스 생성

---
+++ src/test/kotlin/lotto/LottoRankTest.kt
@@ -0,0 +1,36 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoRankTest {
+    @Test
+    fun `6개 번호가 일치하면 FIRST`() {
+        val rank = LottoRank.getRank(matchCount = 6, matchBonus = false)
+        assertThat(rank).isEqualTo(LottoRank.FIRST)
+    }
+    @Test
+    fun `5개 번호가 일치하면 SECOND`() {
+        val rank = LottoRank.getRank(matchCount = 5, matchBonus = false)
+        assertThat(rank).isEqualTo(LottoRank.SECOND)
+    }
+    @Test
+    fun `5개 번호와 보너스번호 일치하면 THIRD`() {
+        val rank = LottoRank.getRank(matchCount = 6, matchBonus = true)
+        assertThat(rank).isEqualTo(LottoRank.THIRD)
+    }
+    @Test
+    fun `4개 번호가 일치하면 FOURTH`() {
+        val rank = LottoRank.getRank(matchCount = 6, matchBonus = false)
+        assertThat(rank).isEqualTo(LottoRank.FOURTH)
+    }
+    @Test
+    fun `3개 번호가 일치하면 FIRST`() {
+        val rank = LottoRank.getRank(matchCount = 6, matchBonus = false)
+        assertThat(rank).isEqualTo(LottoRank.FIFTH)
+    }
+
+}
