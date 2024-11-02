2c1e6435d0568892e281c300c6a021a93c3a83f5
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-02 22:07:55 +0900

    feat(LottoRank): 로또 순위에 매치된 숫자 개수 변수 추가

--- src/main/kotlin/lotto/common/LottoRank.kt
+++ src/main/kotlin/lotto/common/LottoRank.kt
@@ -1,10 +1,11 @@
 package lotto.common
 
-enum class LottoRank(val price: Int) {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    SIXTH(0)
+enum class LottoRank(val price: Int, val matchCount: Int) {
+    FIRST(2_000_000_000, 6),
+    SECOND(30_000_000, 5),
+    THIRD(1_500_000, 5),
+    FOURTH(50_000, 4),
+    FIFTH(5_000, 3),
+    // 6등은 matchCount가 의미가 없어서 0으로 구현
+    SIXTH(0, 0);
 }
