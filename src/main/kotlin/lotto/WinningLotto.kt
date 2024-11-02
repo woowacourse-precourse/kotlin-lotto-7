9b9c72080c63b9c9bde8ad8a2d2a59c4fbd98b20
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-02 19:34:45 +0900

    feat: 보너스 번호 중복 예외 처리
    - WinningLotto.kt: 보너스 번호가 로또 번호와 중복일 경우 예외를 발생합니다.

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -3,6 +3,6 @@ package lotto
 class WinningLotto(private val lotto: Lotto, private val bonusNumber: Int) {
 
     init {
-        require(!lotto.lottoNumber.contains(bonusNumber)) {}
+        require(!lotto.lottoNumber.contains(bonusNumber)) { "[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다." }
     }
 }
