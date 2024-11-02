07afb6c2f8a12cdab77069c1be9bc275c8bca195
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-02 12:18:28 +0900

    feat: 1~45 중 랜덤 6개를 뽑는다

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,15 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
     // TODO: 추가 기능 구현
+    fun RandomLotto(){
+        Randoms.pickUniqueNumbersInRange(1,45,6)
+    }
+
 }
