0b1fb69e2b72421a549eb646f5c5c348a718f193
chanho0908 <uni070@naver.com>
2024-11-02 19:06:30 +0900

    test : 당첨 금액, 수익률 테스트 코드 추가

---
+++ src/test/kotlin/lotto/CalculatorTest.kt
@@ -0,0 +1,62 @@
+package lotto
+
+import domain.calculator.Calculate
+import domain.calculator.Calculator
+import domain.enums.Rank
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class CalculatorTest {
+    private lateinit var calculate: Calculate
+
+    @BeforeEach
+    fun setUp() {
+        calculate = Calculator()
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또_당첨_금액_계산_테스트_데이터")
+    fun `로또 당첨 금액 계산 테스트`(입력값: Map<Rank, Int>, 예측값: String) {
+        // when
+        val actual = calculate.calculateWinningMoney(입력값)
+
+        // then
+        assertEquals(예측값, actual.toString())
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또_수익률_계산_테스트_데이터")
+    fun `로또 수익률 계산 테스트`(당첨금: Long, 구매한_로또_수: Int, 예측값: String) {
+        // when
+        val actual = calculate.calculateRateOfReturn(
+            winningMoney = 당첨금, purchaseLottoCount = 구매한_로또_수
+        )
+
+        // then
+        assertEquals(예측값, actual)
+    }
+
+    companion object {
+        @JvmStatic
+        fun `로또_당첨_금액_계산_테스트_데이터`() = listOf(
+            Arguments.of(
+                mapOf(
+                    Rank.FIRST to 1,
+                    Rank.SECOND to 1,
+                    Rank.THIRD to 1,
+                    Rank.FOURTH to 1,
+                    Rank.FIFTH to 1,
+                ),
+                "2031555000"
+            )
+        )
+
+        @JvmStatic
+        fun `로또_수익률_계산_테스트_데이터`() = listOf(
+            Arguments.of(5000L, 8, "62.5")
+        )
+    }
+}
