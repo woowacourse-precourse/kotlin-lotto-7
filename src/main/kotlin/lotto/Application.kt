a3bc4de805027b33af442f51fefa790f2a736ae5
Leeyerin0210 <lyl8661@naver.com>
2024-11-02 20:51:02 +0900

    feat : 숫자 입력 기능 추가
    기본 로또 6개 숫자를 입력받는 기능을 추가하였다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,13 +5,25 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // TODO: 프로그램 구현
     val money = moneyInput()
-    //val numberList:List<Int>
+    val numberList: List<Int> = numberInput()
+
+    val lotto = Lotto(numberList)
 }
 
-fun moneyInput(): Int? {
+fun moneyInput(): Int {
     val moneyInput = Console.readLine().toIntOrNull() ?: throw (IllegalArgumentException("[ERROR]숫자가 아닙니다."))
     if (moneyInput % 1000 != 0) {
         throw (IllegalStateException("[ERROR]천원 단위의 금액이 아닙니다."))
     }
     return moneyInput
+}
+
+fun numberInput(): List<Int> {
+    val numberList: MutableList<Int> = mutableListOf()
+    val tmpList: List<String> = Console.readLine().split(",")
+    for (tmp in tmpList) {
+        val number = tmp.toIntOrNull() ?: throw (IllegalArgumentException("[ERROR]숫자가 아닙니다."))
+        numberList.add(number)
+    }
+    return numberList
 }
