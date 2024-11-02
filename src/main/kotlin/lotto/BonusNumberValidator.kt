278b477e1d1fbe6764d5cfaf4cb9185282845805
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-02 14:18:02 +0900

    docs(README): 보너스 번호 예외 상황 -  2개 이상 들어오는 경우 삭제

---
+++ src/main/kotlin/lotto/BonusNumberValidator.kt
@@ -0,0 +1,59 @@
+package lotto
+
+import lotto.BonusNumberErrorType.*
+
+class BonusNumberValidatort(
+    private val lotto: Lotto,
+    private val bonusNumber: String
+) {
+    private val parsedInteger: Int? = bonusNumber.toIntOrNull()
+    private val parsedDecimal: Double? = bonusNumber.toDoubleOrNull()
+
+    fun validateBonusNumber() {
+        checkEmpty()
+        checkDecimal()
+        checkInteger()
+        checkNumberBetween1And45()
+        checkDuplicateWithLuckyNumbers()
+    }
+
+    private fun checkEmpty() {
+        if (bonusNumber.isEmpty()) throw IllegalArgumentException(EMPTY_INPUT.errorMessage)
+    }
+
+    private fun checkBonusNumberCount() {
+        val bonusNumbers: List<Int?> = bonusNumber.split(DELIMITER).map { it.trim().toIntOrNull() }
+        if (bonusNumbers.size != 1 && null !in bonusNumbers) throw IllegalArgumentException(ONLY_ONE.errorMessage)
+    }
+
+    private fun checkDecimal() {
+        if (parsedDecimal != null && parsedDecimal % ONE != ZERO_EXPRESSED_IN_DECIMAL) throw IllegalArgumentException(
+            NOT_DECIMAL.errorMessage
+        )
+    }
+
+    private fun checkInteger() {
+        if (parsedInteger == null) throw IllegalArgumentException(
+            NOT_INTEGER.errorMessage
+        )
+    }
+
+    private fun checkNumberBetween1And45() {
+        val isValidBonusNumber: Boolean = parsedInteger in START_NUMBER..END_NUMBER
+        if (!isValidBonusNumber) throw IllegalArgumentException(BONUS_NUMBER_RANGE.errorMessage)
+    }
+
+    private fun checkDuplicateWithLuckyNumbers() {
+        parsedInteger?.let {
+            if (it in lotto.luckyNumbers) throw IllegalArgumentException(NO_DUPLICATE_LUCKY_NUMBERS.errorMessage)
+        }
+    }
+
+    companion object {
+        private const val ZERO_EXPRESSED_IN_DECIMAL: Double = 0.0
+        private const val ONE: Int = 1
+        private const val START_NUMBER: Int = 1
+        private const val END_NUMBER: Int = 45
+        private const val DELIMITER = ","
+    }
+}
