fb73fce8e88084ebf812753bed53ab0da1f073ec
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-11-03 03:13:49 +0900

    ♻️ Refactor: Controller 리팩토링
    PrizeUtils -> Mode.Prize의 companion.object 내 getPrizesToPrint 함수로 변경

--- src/main/kotlin/lotto/Controller/PrizeUtils.kt
+++
@@ -1,9 +0,0 @@
-package lotto.Controller
-
-import lotto.Model.Prize
-
-fun getPrizesToPrint(): List<Prize> {
-    return Prize.values()
-        .filter { it.matchCount >= 3 }
-        .sortedBy { it.matchCount }
-}
--- src/main/kotlin/lotto/Model/Prize.kt
+++ src/main/kotlin/lotto/Model/Prize.kt
@@ -8,8 +8,10 @@ enum class Prize(val matchCount: Int, val bonus: Boolean, val prizeMoney: Int) {
     FIFTH(3, false, 5_000);
 
     companion object {
-        fun getPrize(matchCount: Int, matchBonus: Boolean): Prize? {
-            return values().find { it.matchCount == matchCount && it.bonus == matchBonus }
-        }
+        fun getPrize(matchCount: Int, matchBonus: Boolean): Prize? =
+            values().firstOrNull { it.matchCount == matchCount && it.bonus == matchBonus }
+
+        fun getPrizesToPrint(): List<Prize> =
+            values().filter { it.matchCount >= 3 }.sortedBy { it.matchCount }
     }
 }
