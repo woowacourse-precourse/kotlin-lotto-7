6527e6508a5a37651183b583862a61c541ba3f14
rkqls4764 <rkqls4764@naver.com>
2024-11-02 18:51:05 +0900

    feat: 로또 구입 금액 입력 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,24 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+}
+
+/* 로또 구입 금액 입력 함수 */
+fun inputMoney(): Int {
+    while (true) {
+        try {
+            println("구입금액을 입력해 주세요.")
+            val money = readLine()?.toInt() ?: throw IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.")
+
+            if (money % 1000 != 0) {
+                throw IllegalArgumentException("[ERROR] 금액이 1000으로 나누어 떨어지지 않습니다.")
+            }
+
+            return money / 1000
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 숫자를 입력해 주세요.")
+        }
+    }
 }
