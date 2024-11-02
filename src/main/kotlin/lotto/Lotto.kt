62f455c0f1d47c7ce11f569442bf7657509729d7
rkqls4764 <rkqls4764@naver.com>
2024-11-02 22:37:12 +0900

    feat: 로또 번호 리턴 함수 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,6 +5,10 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
+
     override fun toString(): String {
         return numbers.sorted().toString() //오름차순 정렬 후 문자열 반환
     }
