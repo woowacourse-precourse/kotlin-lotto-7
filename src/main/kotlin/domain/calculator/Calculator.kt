6ce08bb11c75fe5eeed282b424dde6bb8ab54b3b
chanho0908 <uni070@naver.com>
2024-11-02 19:07:01 +0900

    feat : 당첨 금액, 수익률 계산을 위한 클래스 추가

---
+++ src/main/kotlin/domain/calculator/Calculator.kt
@@ -0,0 +1,18 @@
+package domain.calculator
+
+import domain.enums.Rank
+import util.convertRoundAtTwoDecimal
+
+class Calculator: Calculate {
+    override fun calculateWinningMoney(winning: Map<Rank, Int>): Long {
+        return winning.entries.sumOf { (rank, count) ->
+            rank.getReword() * count
+        }.toLong()
+    }
+
+    override fun calculateRateOfReturn(winningMoney: Long, purchaseLottoCount: Int): String {
+        val totalPurchaseAmount = purchaseLottoCount * 1000
+        val rateOfReturn = (winningMoney.toDouble() / totalPurchaseAmount) * 100
+        return rateOfReturn.convertRoundAtTwoDecimal()
+    }
+}
