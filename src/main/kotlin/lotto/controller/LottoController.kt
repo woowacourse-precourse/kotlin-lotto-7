0f89e2e114c05078197a01aef385978138d19e2d
이지윤 <02leejiyun@naver.com>
2024-11-02 18:33:35 +0900

    feat: PurchaseMoney 객체 구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,10 +1,11 @@
 package lotto.controller
 
+import lotto.domain.PurchaseMoney
 import lotto.view.InputView
 
 class LottoController {
     fun start() {
-        val purchaseMoney = InputView.inputPurchaseMoney()
+        val purchaseMoney = PurchaseMoney(InputView.inputPurchaseMoney())
         val winningNumbers = InputView.inputWinningNumber()
         val bonusNumber = InputView.inputBonusNumber()
     }
---
+++ src/main/kotlin/lotto/domain/PurchaseMoney.kt
@@ -0,0 +1,7 @@
+package lotto.domain
+
+class PurchaseMoney(private val money: Int) {
+    init {}
+
+    fun getMoney(): Int = money
+}
