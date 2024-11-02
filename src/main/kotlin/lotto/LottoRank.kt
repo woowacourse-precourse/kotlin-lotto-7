1a759aa592b4d647c587958be22361cd2ede1270
rkqls4764 <rkqls4764@naver.com>
2024-11-02 23:55:16 +0900

    style: 순위명 변경

--- src/main/kotlin/lotto/LottoRank.kt
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -9,7 +9,7 @@ enum class LottoRank(
     FIRST(6, false, 2_000_000_000, "6개 일치"),
     SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
     THIRD(5, false, 1_500_000, "5개 일치"),
-    FORTH(4, false, 50_000, "4개 일치"),
+    FOUTH(4, false, 50_000, "4개 일치"),
     FIFTH(3, false, 5_000, "3개 일치");
 
     companion object {
