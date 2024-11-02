c0aa1d92a2eeb9ab66d04165f7d3fc5e73e5989e
jiwon <pjw99032986@gmail.com>
2024-11-02 16:19:06 +0900

    Feat(Application.kt) : 랜덤 로또 생성
    로또 생성, 중복 되지 않는 1-45 사이의 6개의 정수,

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,12 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    val input = Input()
+    val output = Output()
+    val lotto = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+    output.outPutMoney()
+    input.inputMoney()
 }
+
