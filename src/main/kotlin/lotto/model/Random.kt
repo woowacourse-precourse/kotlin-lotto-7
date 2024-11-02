0d1d0f0467ab701b4c30a773c0888c7cab90f0d4
조형석 <chs9902@naver.com>
2024-11-02 17:05:01 +0900

    :sparkles: feat: 랜덤 1~45중 6개 추출 기능 추가

--- src/main/kotlin/lotto/model/Random.kt
+++ src/main/kotlin/lotto/model/Random.kt
@@ -1,7 +1,9 @@
 package lotto.model
 
-class Random {
-    fun generate(){
+import camp.nextstep.edu.missionutils.Randoms
 
+class Random {
+    fun generate():List<Int>{
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
     }
 }
