7a7813f947ad2ba2b098cd00cb244d82e03fedd6
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-02 18:22:18 +0900

    feat(User): 로또 티켓을 반활할때 상태 에러 처리
    - 로또 티켓이 널 일때 호출할 경우에 stateException을 호출하는 기능 구현
    
    로또 티켓이 초기화 되지 않은 상태에 관한 처리가 있어야 한다고 생각해서 구현

--- src/main/kotlin/lotto/domain/entity/User.kt
+++ src/main/kotlin/lotto/domain/entity/User.kt
@@ -1,12 +1,14 @@
 package lotto.domain.entity
 
 class User(private val money: Int) {
-    private lateinit var lottoTickets: List<Lotto>
+    private var lottoTickets: List<Lotto>? = null
 
     fun buyLottoTickets(lottoTickets: (Int) -> List<Lotto>) {
         val lottoCount = money.div(1000)
         this.lottoTickets = lottoTickets(lottoCount)
     }
 
-    fun getLottoTickets() = lottoTickets.toList()
+    fun getLottoTickets(): List<Lotto> {
+        return checkNotNull(lottoTickets) { println("[ERROR] 사용자의 로또 티켓들이 아직 초기화 되지 않았습니다.") }
+    }
 }
