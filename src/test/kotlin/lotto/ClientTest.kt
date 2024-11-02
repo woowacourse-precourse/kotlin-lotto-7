58e46f37551e28a63d084ad555ac29c417db7776
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-02 19:48:07 +0900

    refactor(ClientTest.kt): 클래스 이름 변경에 따른 테스트 클래스 이름 수정
    이전: LottoAmountTest
    이후: ClientTest

--- src/test/kotlin/lotto/ClientTest.kt
+++ src/test/kotlin/lotto/ClientTest.kt
@@ -1,6 +1,6 @@
 package lotto
 
-import lotto.data.LottoAmount
+import lotto.data.Client
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
@@ -9,12 +9,12 @@ import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 @DisplayName("구입 금액 테스트 케이스")
-class LottoAmountTest {
+class ClientTest {
     @DisplayName("숫자가 아닌 다른 문자를 포함한 경우")
     @ParameterizedTest
     @ValueSource(strings = ["\n", "\t", "abcd", "1,000", "+35000", "-45000", "10000.0", "천원", "일만", "십억?"])
     fun amountContainsNonNumber(input: String) {
-        val result = assertThrows<IllegalArgumentException> { LottoAmount(input) }
+        val result = assertThrows<IllegalArgumentException> { Client(input) }
         assertEquals(result.message, "[ERROR] 숫자로만 입력해주세요.")
     }
 
@@ -22,7 +22,7 @@ class LottoAmountTest {
     @ParameterizedTest
     @ValueSource(strings = ["001000", "01000", "025000"])
     fun amountStartZero(input: String) {
-        val result = assertThrows<IllegalArgumentException> { LottoAmount(input) }
+        val result = assertThrows<IllegalArgumentException> { Client(input) }
         assertEquals(result.message, "[ERROR] 10000 과 같은 형태로 입력해주세요.")
     }
 
@@ -30,7 +30,7 @@ class LottoAmountTest {
     @ParameterizedTest
     @EmptySource
     fun amountEmpty(input: String) {
-        val result = assertThrows<IllegalArgumentException> { LottoAmount(input) }
+        val result = assertThrows<IllegalArgumentException> { Client(input) }
         assertEquals(result.message, "[ERROR] 구입 금액이 입력되지 않았습니다. 구입 금액을 입력해주세요.")
     }
 
@@ -38,7 +38,7 @@ class LottoAmountTest {
     @ParameterizedTest
     @ValueSource(strings = ["1500", "3500", "12100", "99001"])
     fun amountUnit(input: String) {
-        val result = assertThrows<IllegalArgumentException> { LottoAmount(input) }
+        val result = assertThrows<IllegalArgumentException> { Client(input) }
         assertEquals(result.message, "[ERROR] 1,000원 단위로만 구매 가능합니다.")
     }
 
@@ -46,7 +46,7 @@ class LottoAmountTest {
     @ParameterizedTest
     @ValueSource(strings = ["0", "101000", "10000000000000000000000"])
     fun amountRange(input: String) {
-        val result = assertThrows<IllegalArgumentException> { LottoAmount(input) }
+        val result = assertThrows<IllegalArgumentException> { Client(input) }
         assertEquals(result.message, "[ERROR] 1,000원 ~ 100,000원 이내로만 구매 가능합니다.")
     }
 }
