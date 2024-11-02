e32fe4d0212b63b58291ec7bce60fe9239ac6869
rkqls4764 <rkqls4764@naver.com>
2024-11-02 22:25:00 +0900

    feat: 당첨 등급 판별 함수 생성

--- src/main/kotlin/lotto/LottoRank.kt
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -10,5 +10,14 @@ enum class LottoRank(
     SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
     THIRD(5, false, 1_500_000, "5개 일치"),
     FORTH(4, false, 50_000, "4개 일치"),
-    FIFTH(3, false, 5_000, "3개 일치")
+    FIFTH(3, false, 5_000, "3개 일치");
+
+    companion object {
+        /* 당첨 등급 판별 함수 */
+        fun from(matchingCount: Int, hasBonus: Boolean): LottoRank? {
+            return values().find {
+                it.matchingCount == matchingCount && (!it.bonus || hasBonus == it.bonus)
+            }
+        }
+    }
 }
