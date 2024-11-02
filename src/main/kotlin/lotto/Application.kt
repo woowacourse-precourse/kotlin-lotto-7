3e2158c5cfd471a53f7610d6707770dca216418f
yeobeomhwi <yeobh0828@naver.com>
2024-11-03 01:45:13 +0900

    feat(LottoIssue):Create Class LottoIssue

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,23 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    val lottoIssue = LottoIssue()
+    val issuedLottos = lottoIssue.issueLottos(5)
+
+    println("${issuedLottos.size}개를 구매했습니다.")
+    issuedLottos.forEach { println(it) }
+}
+
+class LottoIssue {
+    // 로또 번호 한 세트를 생성
+    fun issueLotto(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+    }
+
+    // 여러 개의 로또 발행
+    fun issueLottos(count: Int): List<List<Int>> {
+        return List(count) { issueLotto() }
+    }
 }
