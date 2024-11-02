1a530e24ce4f5f37e3f4d893ca459a0cf9c3e6b4
yewon-yw <kimyewon0401@gmail.com>
2024-11-02 19:27:28 +0900

    feat(Rank): 인덱스 필드 추가

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -1,10 +1,14 @@
 package lotto.model
 
-enum class Rank(val matchedNumber: Int, val winningPrice: Int) {
-    NONE(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000),
+enum class Rank(
+    val index: Int,
+    val matchedNumber: Int,
+    val winningPrice: Int
+) {
+    NONE(0, 0, 0),
+    FIFTH(1, 3, 5_000),
+    FOURTH(2, 4, 50_000),
+    THIRD(3, 5, 1_500_000),
+    SECOND(4, 5, 30_000_000),
+    FIRST(5, 6, 2_000_000_000),
 }
