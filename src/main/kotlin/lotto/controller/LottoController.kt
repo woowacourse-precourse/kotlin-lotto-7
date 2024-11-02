176aa3fa556bf2ea092ec3d4dfc5bca207296a92
이지윤 <02leejiyun@naver.com>
2024-11-02 18:34:43 +0900

    feat: Lotto 객체 적용

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,12 +1,13 @@
 package lotto.controller
 
+import lotto.domain.Lotto
 import lotto.domain.PurchaseMoney
 import lotto.view.InputView
 
 class LottoController {
     fun start() {
         val purchaseMoney = PurchaseMoney(InputView.inputPurchaseMoney())
-        val winningNumbers = InputView.inputWinningNumber()
+        val winningNumbers = Lotto(InputView.inputWinningNumber())
         val bonusNumber = InputView.inputBonusNumber()
     }
 }
