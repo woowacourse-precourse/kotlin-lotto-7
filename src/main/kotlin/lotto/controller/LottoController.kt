cde2ec17af221b66fcc14c7b76440e0209191ea7
Jinwoo Choi <cjw1234577@gmail.com>
2024-11-02 23:57:08 +0900

    feat(InputView): 당첨 번호 입력 기능 구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,10 +1,18 @@
 package lotto.controller
 
+import lotto.model.Lottos
 import lotto.view.InputView
 
 class LottoController {
 
     fun run() {
         val lottoPrice = InputView.inputLottoPrice()
+        //구매한 로또들 생성
+        val lottos = Lottos()
+        lottos.buyLottos(lottoPrice)
+
+        //당첨 번호를 입력 받는다. 번호는 쉼표(,)를 기준으로 구분한다.
+        val winningNumbers = InputView.inputWinningNumbers()
+
     }
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console.readLine
+import lotto.utils.Constants
 import lotto.utils.ErrorMessage
 import lotto.utils.InputMessage
 
@@ -11,6 +12,19 @@ object InputView {
                 println(InputMessage.INPUT_PRICE.getMessage())
                 val price = validateLottoPrice(readLine())
                 return price
+            } catch (e: IllegalArgumentException) {
+                println(e.message ?: ErrorMessage.UNKNOWN_ERROR.getMessage())
+                println()
+            }
+        }
+    }
+
+    fun inputWinningNumbers(): List<Int> {
+        while (true) {
+            try {
+                println(InputMessage.INPUT_WINNING_NUMBER.getMessage())
+                val numbers = validateWinningNumbers(readLine())
+                return numbers
             } catch (e: IllegalArgumentException) {
                 println(e.message ?: ErrorMessage.UNKNOWN_ERROR.getMessage())
                 println()
@@ -29,5 +43,16 @@ object InputView {
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage())
         }
+    }
+
+    private fun validateWinningNumbers(inputNumbers: String): List<Int> {
+        val numbers = inputNumbers.split(",").map { it.trim().toInt() }
+        if (numbers.size != 6) {
+            throw IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBERS.getMessage())
+        }
+        if (numbers.any { it !in Constants.RANDOM_MIN..Constants.RANDOM_MAX }) {
+            throw IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage())
+        }
+        return numbers
     }
 }
