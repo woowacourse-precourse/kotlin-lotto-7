b1a94ef65108e5186d17559ca8bb6f9b8faaa76f
Tnalxmsk <alstnwkd990@naver.com>
2024-11-03 01:29:47 +0900

    feat: 번호 비교 기능 구현
    특정 번호가 존재하는지 확인하는 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,10 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == 6) { "[ERROR] 로또 번호에 중복된 숫자가 있습니다." }
     }
 
-    // TODO: 추가 기능 구현
+    fun contain(number: Int): Boolean {
+        return numbers.contains(number)
+    }
 }
