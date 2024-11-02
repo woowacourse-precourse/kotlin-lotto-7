f994e904f0217cc72ee9225b50f85169e1e91f6e
ijh1298 <imjh0417@gmail.com>
2024-11-02 23:07:17 +0900

    feat: OutputView 내 showStatus 함수 추가

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,8 @@
 package lotto.view
 
 import lotto.domain.entity.Lotto
+import lotto.domain.entity.WinningLotto
+import lotto.domain.entity.toPriceString
 
 class OutputView {
     fun showRandomLottos(lottos: List<Lotto>) {
@@ -9,7 +11,29 @@ class OutputView {
         println()
     }
 
+    fun showStatus() {
+        println(WIN_STATUS_MSG)
+        println("$THREE$SAME_NUM_MSG (${WinningLotto.Three.toPriceString()}$WON) $DELIMIETER ${WinningLotto.Three.amount}$AMOUNT")
+        println("$FOUR$SAME_NUM_MSG (${WinningLotto.Four.toPriceString()}$WON) $DELIMIETER ${WinningLotto.Four.amount}$AMOUNT")
+        println("$FIVE$SAME_NUM_MSG (${WinningLotto.Five.toPriceString()}$WON) $DELIMIETER ${WinningLotto.Five.amount}$AMOUNT")
+        println("$FIVE$SAME_NUM_MSG$BONUS_SAME_MSG (${WinningLotto.FiveBonus.toPriceString()}$WON) $DELIMIETER ${WinningLotto.FiveBonus.amount}$AMOUNT")
+        println("$SIX$SAME_NUM_MSG (${WinningLotto.Six.toPriceString()}$WON) $DELIMIETER ${WinningLotto.Six.amount}$AMOUNT")
+    }
+
     companion object {
         const val LOTTO_PURCHASE_MSG = "개를 구매했습니다."
+
+        const val WIN_STATUS_MSG = "\n당첨 통계\n---"
+        const val SAME_NUM_MSG = "개 일치"
+
+        const val DELIMIETER = "-"
+        const val WON = "원"
+        const val AMOUNT = "개"
+
+        const val THREE = 3
+        const val FOUR = 4
+        const val FIVE = 5
+        const val BONUS_SAME_MSG = ", 보너스 볼 일치"
+        const val SIX = 6
     }
 }
