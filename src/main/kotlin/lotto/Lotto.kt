474385a32bcb3c8ab7e9204ab930189957fc8eb3
jiwon <pjw99032986@gmail.com>
2024-11-02 17:00:17 +0900

    Feat(Lotto.kt) : 보너스 로또 번호 추가
    1부터45사이의 중복되지않는 번호, 일치하지 않을 경우 에러 발생

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,24 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
+    private var bonusNumber = 0
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
+    fun drawLotto() {
+        while (true) {
+            try {
+                val randomNumber = Randoms.pickNumberInRange(1, 45)
+                require(!numbers.contains(bonusNumber)) { "[ERROR] 보너스 번호는 기존 번호와 중복될 수 없습니다." }
+                bonusNumber = randomNumber
+                break
+            } catch (e: IllegalArgumentException) {
+                continue
+            }
+        }
+    }
 
-    // TODO: 추가 기능 구현
+
 }
