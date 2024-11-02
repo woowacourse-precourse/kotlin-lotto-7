8bed70e676a18a42ffbbeef5b2b1c38e1175b516
rkqls4764 <rkqls4764@naver.com>
2024-11-02 18:53:31 +0900

    feat: 로또 발행 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console.readLine
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
 
 fun main() {
     startLotto()
@@ -9,6 +10,11 @@ fun main() {
 /* 로또 실행 함수 */
 fun startLotto() {
     val lottoCnt = inputMoney() //구입한 로또 개수
+    val lottoList = mutableListOf<Lotto>() //발행한 로또 리스트
+
+    repeat(lottoCnt) {
+        pickLottoNumbers(lottoList)
+    }
 }
 
 /* 로또 구입 금액 입력 함수 */
@@ -27,4 +33,10 @@ fun inputMoney(): Int {
             println("[ERROR] 숫자를 입력해 주세요.")
         }
     }
+}
+
+/* 로또 발행 함수 */
+fun pickLottoNumbers(lottoList: MutableList<Lotto>) {
+    val lottoNumbers = pickUniqueNumbersInRange(1, 45, 6)
+    lottoList.add(Lotto(lottoNumbers))
 }
