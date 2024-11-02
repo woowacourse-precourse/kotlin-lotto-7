b89ffff82f80c4d0f8a9ad07d32fef5c18fe4c63
ijh1298 <imjh0417@gmail.com>
2024-11-02 20:44:44 +0900

    chore: OVER_RANGE 예외 메세지 수정

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -74,6 +74,6 @@ class LottoController(val view: InputView) {
         private const val INVALID_INT_NUMBER_EXCEPTION_MSG = "[ERROR] 유효하지 않은 정수입니다."
         private const val INVALID_NUMBERS_EXCEPTION_MSG = "[ERROR] 유효하지 않은 당첨 번호 리스트입니다."
         private const val NOT_ZERO_EXCEPTION_MSG = "[ERROR] 구입 금액은 0원보다 커야 합니다."
-        private const val OVER_RANGE_EXCEPTION_MSG = "[ERROR] 로또 번호는 45 이하여야 합니다."
+        private const val OVER_RANGE_EXCEPTION_MSG = "[ERROR] 로또 번호는 1이상 45 이하여야 합니다."
     }
 }
