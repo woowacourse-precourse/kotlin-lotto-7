f4d217a4834d468326df9b9b10acfca0c01740e1
지원 <cheerfulboys@naver.com>
2024-11-02 18:02:00 +0900

    feat(Lotto): 로또 번호의 범위를 확인하고 예외를 던지는 로직 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,6 +4,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.size == numbers.distinct().size) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
+        numbers.forEach { require(it in 1..45) { "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." } }
     }
 
     fun sort(): List<Int> {
