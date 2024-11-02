3cb69caf2f5deef65252f8fa1b22c5a59d2c0993
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-02 19:36:57 +0900

    feat: 보너스 번호 범위 예외 처리
    - WinningLotto.kt: 보너스 번호가 1~45 사이의 숫자가 아닐 경우 예외를 발생합니다.

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -4,5 +4,6 @@ class WinningLotto(private val lotto: Lotto, private val bonusNumber: Int) {
 
     init {
         require(!lotto.lottoNumber.contains(bonusNumber)) { "[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다." }
+        require(bonusNumber in 1..45) { "[ERROR] 보너스 번호는 1~45 사이의 숫자만 가능합니다." }
     }
 }
