db9a2f05a5252b4f43ae76083561ac19bd9a6a0a
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-02 18:08:57 +0900

    feat(User): 사용자가 티켓을 구매하는 기능 추가
    - 사용자 초기화시에 돈을 통해 로또를 생성하는 로직에서
    로또를 구매해서 lottoTickets를 초기화하는 기능 구현
    
    사용자가 로또를 생성하지는 않기 때문에 책임 분리가 필요하다고 생각하고
    로또를 구매하는게 적절하다고 생각해서 변경

--- src/main/kotlin/lotto/domain/entity/User.kt
+++ src/main/kotlin/lotto/domain/entity/User.kt
@@ -1,22 +1,11 @@
 package lotto.domain.entity
 
-import camp.nextstep.edu.missionutils.Randoms
-
 class User(private val money: Int) {
     private lateinit var lottoTickets: List<Lotto>
 
-    init {
-        buyLottoTickets()
-    }
-
-    private fun buyLottoTickets() {
+    fun buyLottoTickets(lottoTickets: (Int) -> List<Lotto>) {
         val lottoCount = money.div(1000)
-        lottoTickets = List(lottoCount) { createLotto() }
-    }
-
-    private fun createLotto(): Lotto {
-        val lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-        return Lotto(lottoNumbers)
+        this.lottoTickets = lottoTickets(lottoCount)
     }
 
     fun getLottoTickets() = lottoTickets.toList()
