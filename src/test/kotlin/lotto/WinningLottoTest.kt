45722b29c9bb029732782740eea5597b81ac6f7c
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-02 18:41:23 +0900

    test: 보너스 번호 테스트 > 당첨 번호 테스트로 수정

--- src/test/kotlin/lotto/WinningLottoTest.kt
+++ src/test/kotlin/lotto/WinningLottoTest.kt
@@ -3,14 +3,14 @@ package lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class BonusNumberTest {
+class WinningLottoTest {
 
     @Test
     fun `보너스 번호가 1~45 사이의 수가 아닐 경우 예외가 발생한다`() {
         val bonusNumber = 46
 
         assertThrows<IllegalArgumentException> {
-            BonusNumber(bonusNumber)
+            WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)), bonusNumber)
         }
     }
 }
