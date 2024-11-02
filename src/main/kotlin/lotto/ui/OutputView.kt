b962429951e06183d459ced4b26e2249bfc541d1
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-03 01:06:22 +0900

    refactor(OutputView.kt): 지정하지 못한 예외 발생 시 출력 예외 메세지 수정

--- src/main/kotlin/lotto/ui/OutputView.kt
+++ src/main/kotlin/lotto/ui/OutputView.kt
@@ -2,7 +2,7 @@ package lotto.ui
 
 object OutputView {
 
-    private const val OTHER_ERROR_MESSAGE = "다시 입력해주세요."
+    private const val OTHER_ERROR_MESSAGE = "[ERROR] 다시 입력해주세요."
     private const val LOTTO_QUANTITY_MESSAGE = "%d개를 구매했습니다."
 
     fun printErrorCauseMessage(message: String) = println(message)
