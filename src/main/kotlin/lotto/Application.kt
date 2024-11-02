c30b15c32e7fe2ebad2591f38654fdd9de81abfd
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-02 19:18:23 +0900

    feat: 당첨번호와 보너스 번호를 입력받는다

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -15,9 +15,13 @@ fun main() {
 
     val inputAmount = input.getAmount()
 
-    val lottoCount = inputAmount!!.toInt().div(LOTTO_AMOUNT)
-
+    val lottoCount = inputAmount.toInt().div(LOTTO_AMOUNT)
     val lottoNumber = randomNumber.randomLottos(lottoCount)
     output.printLottoNumbers(lottoNumber)
+
+
+    val inputNumbers = input.getLottoNumbers()
+    val inputBonusNumber = input.getLottoBonusNumbers()
+
 
 }
--- src/main/kotlin/lotto/Input.kt
+++ src/main/kotlin/lotto/Input.kt
@@ -3,14 +3,26 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class Input {
-    private fun input(): String? = Console.readLine()
+    private fun input(): String = Console.readLine()
 
-    fun getAmount(): String? {
+    fun getAmount(): String {
         println(REQUEST_AMOUNT_MESSAGE);
         return input()
     }
 
+    fun getLottoNumbers(): List<Int> {
+        println(REQUEST_NUMBERS_MESSAGE)
+        return input().split(',').map { it.toInt() }
+    }
+
+    fun getLottoBonusNumbers() : Int {
+        println(REQUEST_BONUS_NUMBERS_MESSAGE)
+        return input().toInt()
+    }
+
     companion object {
         private const val REQUEST_AMOUNT_MESSAGE = "구입금액을 입력해 주세요."
+        private const val REQUEST_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요."
+        private const val REQUEST_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요."
     }
 }
