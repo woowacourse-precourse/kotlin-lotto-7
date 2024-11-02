ec78678e6187d550254b2915902659017fde397a
ijh1298 <imjh0417@gmail.com>
2024-11-02 22:36:03 +0900

    chore: WinningLotto amount를 val에서 var로 변경

--- src/main/kotlin/lotto/domain/entity/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/entity/WinningLotto.kt
@@ -1,6 +1,6 @@
 package lotto.domain.entity
 
-enum class WinningLotto(val rewardPrice: Int, val amount: Int) {
+enum class WinningLotto(val rewardPrice: Int, var amount: Int) {
     Three(RewardPrice.PRICE_OF_THREE_WIN, 0),
     Four(RewardPrice.PRICE_OF_FOUR_WIN, 0),
     Five(RewardPrice.PRICE_OF_FIVE_WIN, 0),
