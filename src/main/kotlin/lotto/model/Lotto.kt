8d32d3aa6e2f799dea7fcf5c7c9ef8f2101ba082
eunyoung kim <kermit.eea@gmail.com>
2024-11-04 02:08:24 +0900

    test: 중복된 번호 입력 시 예외 발생하도록 require 문 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,7 +4,12 @@ import lotto.utils.WinningNumbers
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == WinningNumbers.WINNING_NUMBER_COUNT.value) { "[ERROR] 로또 번호는 ${WinningNumbers.WINNING_NUMBER_COUNT.value}개여야 합니다." }
+        require(numbers.size == WinningNumbers.WINNING_NUMBER_COUNT.value) {
+            "[ERROR] 로또 번호는 ${WinningNumbers.WINNING_NUMBER_COUNT.value}개여야 합니다."
+        }
+        require(numbers.distinct().size == WinningNumbers.WINNING_NUMBER_COUNT.value) {
+            "[ERROR] 로또 번호는 중복되지 않아야 합니다."
+        }
     }
 
     // TODO: 추가 기능 구현
