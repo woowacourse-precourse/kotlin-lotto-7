16bf5ddf9a3a7ccda2b038c48e002aefe6af4f2b
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-03 21:43:24 +0900

    feat(LottoPrizeCalculator): 로또 수익률 계산

--- src/main/kotlin/lotto/domain/usecase/LottoPrizeCalculator.kt
+++ src/main/kotlin/lotto/domain/usecase/LottoPrizeCalculator.kt
@@ -3,9 +3,14 @@ package lotto.domain.usecase
 import lotto.domain.model.Prize
 import lotto.domain.model.PurchaseInfo
 
-class LottoPrizeCalcuator(
+class LottoPrizeCalculator(
     private val matchResult: List<Prize>,
     private val PurchaseInfo: PurchaseInfo,
 ) {
     private fun calculateTotalPrize(): Int = matchResult.sumOf { it.prizeAmountValue }
+
+    fun calculateProfitRate(): Double {
+        val totalPrize = calculateTotalPrize()
+        return totalPrize.toDouble() / PurchaseInfo.purchaseAmount * 100
+    }
 }
