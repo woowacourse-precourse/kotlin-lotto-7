4d3dd61a33671c12c420f2eed686e9bdc6c2f942
joonhokil <kiljh@me.com>
2024-11-04 06:02:52 +0900

    refactor: 기능 별로 함수 분리

--- src/main/kotlin/lotto/LottoJudge.kt
+++ src/main/kotlin/lotto/LottoJudge.kt
@@ -44,12 +44,26 @@ class LottoJudge {
     }
 
     fun printWinningStatistics(lottos: List<Lotto>) {
-        val ranks = lottos.map { it.checkRank(winnerNumbers, bonusNumber) }
         println("\n당첨 통계\n---")
-        println("3개 일치 (5,000원) - ${ranks.filter { it == LottoRank.FIFTH }.size}개")
-        println("4개 일치 (50,000원) - ${ranks.filter { it == LottoRank.FOURTH }.size}개")
-        println("5개 일치 (1,500,000원) - ${ranks.filter { it == LottoRank.THIRD }.size}개")
-        println("5개 일치, 보너스 볼 일 (30,000,000원) - ${ranks.filter { it == LottoRank.SECOND }.size}개")
-        println("6개 일치 (2,000,000,000원) - ${ranks.filter { it == LottoRank.FIRST }.size}개")
+        val ranksCount = getRanksCount(lottos.map { it.checkRank(winnerNumbers, bonusNumber) })
+        printRanks(ranksCount)
+    }
+
+    private fun printRanks(ranksCount: List<Int>) {
+        println("3개 일치 (5,000원) - ${ranksCount[4]}개")
+        println("4개 일치 (50,000원) - ${ranksCount[3]}개")
+        println("5개 일치 (1,500,000원) - ${ranksCount[2]}개")
+        println("5개 일치, 보너스 볼 일 (30,000,000원) - ${ranksCount[1]}개")
+        println("6개 일치 (2,000,000,000원) - ${ranksCount[0]}개")
+    }
+
+    private fun getRanksCount(ranks: List<LottoRank>): List<Int> {
+        return listOf(
+            ranks.filter { it == LottoRank.FIRST }.size,
+            ranks.filter { it == LottoRank.SECOND }.size,
+            ranks.filter { it == LottoRank.THIRD }.size,
+            ranks.filter { it == LottoRank.FOURTH }.size,
+            ranks.filter { it == LottoRank.FIFTH }.size
+        )
     }
 }
