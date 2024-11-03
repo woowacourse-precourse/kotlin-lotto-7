270ef533266d33fa1acdc2a5abdc5894d4689436
jiwon <pjw99032986@gmail.com>
2024-11-04 01:22:24 +0900

    Feat(CustomErrorHandler.kt) : main 함수 최소화
    클래스 별 관심사 분리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,12 +1,8 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Randoms
-
 fun main() {
     val input = Input()
-    val output = Output()
-    val lotto = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
-    output.outPutMoney()
-    input.inputMoney()
+    input.start()
 }
+
 
