9ddb90072a0a11264b7555d902548c7877aa6674
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-03 22:16:55 +0900

    test(Lotto): 로또가 우승번호와 일치하는 숫자의 개수를 반환하는 지 테스트 추가 및 성공

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,10 @@
 package lotto
 
+import controller.LottoController
 import model.Lotto
+import model.Prize
+import model.WinningLotto
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -21,4 +25,12 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또가 우승번호와 일치하는 숫자의 개수를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 7)), 8)
+        val matchCount = lotto.matchCount(winningLotto)
+        assertEquals(5, matchCount)
+    }
+
 }
