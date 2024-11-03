6979990d2ca7956f408a0f0a7af19c5484e06792
yeonjeen <jeendev2019@gmail.com>
2024-11-04 06:56:03 +0900

    refactor(LottoController.kt): 로또 게임 로직을 LottoController 클래스로 분리 - 유효성 검증 로직을 분리하여 입력 값들을 검증합니다. - model들과 상호작용하며 게임 시작부터 결과 출력까지의 흐름을 제어합니다.

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,55 @@
+package lotto.controller
+
+import lotto.model.LottoCalculator
+import lotto.model.LottoMachine
+import lotto.util.Validator
+import lotto.view.LottoInput
+import lotto.view.LottoOutput
+
+class LottoController {
+    private val lottoInput = LottoInput()
+    private val lottoOutput = LottoOutput()
+    private val lottoMachine = LottoMachine()
+    private val validator = Validator()
+    private val lottoCalculator = LottoCalculator()
+
+    fun startLotto() {
+        val purchasePrice = getValidatedPurchasePrice()
+
+        val lottoTicketCount = purchasePrice / 1000
+        lottoOutput.printTicketCount(lottoTicketCount)
+
+        val lottoTickets = lottoMachine.generateLottoTickets(lottoTicketCount)
+        lottoOutput.printLottoTickets(lottoTickets)
+
+        val winningNumbers = getValidatedWinningNumbers()
+        val bonusNumber = getValidatedBonusNumber(winningNumbers)
+
+        val results = lottoCalculator.getLottoResults(lottoTickets, winningNumbers, bonusNumber)
+        lottoOutput.printResult(results)
+
+        val profitRate = lottoCalculator.calculateProfitRate(results, purchasePrice)
+        lottoOutput.printProfitRate(profitRate)
+    }
+
+    private fun getValidatedPurchasePrice(): Int {
+        return generateSequence {
+            val input = lottoInput.getPurchasePrice()
+            validator.validatePurchasePrice(input)
+        }.firstNotNullOfOrNull { it } ?: getValidatedPurchasePrice()
+    }
+
+    private fun getValidatedWinningNumbers(): List<Int> {
+        return generateSequence {
+            val input = lottoInput.getWinningNumbers()
+            validator.validateWinningNumbers(input)
+        }.firstNotNullOfOrNull { it } ?: getValidatedWinningNumbers()
+    }
+
+    private fun getValidatedBonusNumber(winningNumbers: List<Int>): Int {
+        return generateSequence {
+            val input = lottoInput.getBonusNumber()
+            validator.validateBonusNumber(input, winningNumbers)
+        }.firstNotNullOfOrNull { it } ?: getValidatedBonusNumber(winningNumbers)
+    }
+}
