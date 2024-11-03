7430d8746f48323887dcda1ae2d863bb9794e942
yeonjeen <jeendev2019@gmail.com>
2024-11-04 07:20:17 +0900

    refactor(LottoCalculator.kt): 불필요한 코드 삭제

--- src/main/kotlin/lotto/model/LottoCalculator.kt
+++ src/main/kotlin/lotto/model/LottoCalculator.kt
@@ -1,19 +1,6 @@
 package lotto.model
 
 class LottoCalculator {
-    fun getLottoResults(tickets: List<Lotto>, winningNumbers: List<Int>, bonusNumber: Int): Map<Rank, Int> {
-        val lottoResult = LottoResult()
-
-        tickets.forEach { ticket ->
-            val rank = Rank.from(
-                matchCount = ticket.countMatchingNumbers(winningNumbers),
-                matchBonus = ticket.containsBonusNumber(bonusNumber)
-            )
-            lottoResult.addResult(rank)
-        }
-        return lottoResult.getResults()
-    }
-
     fun calculateProfitRate(results: Map<Rank, Int>, purchasePrice: Int): String {
         val totalPrize = calculateTotalPrize(results)
         val profitRate = (totalPrize.toDouble() / purchasePrice) * 100
