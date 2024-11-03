36c65a6dc1b8b1b333f4aa1ea9c0320b64eaf16e
gahyunkim <mapledt001@naver.com>
2024-11-03 17:34:41 +0900

    feat: Add input and validation for lotto winning numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,7 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms
 
 enum class LottoError(val message: String) {
     INVALID_AMOUNT("[ERROR] 유효한 금액을 입력해주세요."),
-    INVALID_UNIT("[ERROR] 로또 금액은 1000원 단위로 입력해야 합니다.");
+    INVALID_UNIT("[ERROR] 로또 금액은 1000원 단위로 입력해야 합니다."),
+    INVALID_NUM("[ERROR] 1~45 사이의 중복되지 않는 6개의 숫자를 입력해 주세요."),
+    INVALID_SPLIT("[ERROR] 유효한 구분자와 숫자를 사용해 주세요.");
 
     fun throwException(): IllegalArgumentException {
         return IllegalArgumentException(this.message)
@@ -17,6 +19,8 @@ fun main() {
     val numberOfLottos = calculateNumberOfLottos(amount)
     println("${numberOfLottos}개를 구매했습니다.")
     generateLottos(numberOfLottos)
+
+    readLottoNumber()
 }
 
 fun readAndValidateAmount(): Int {
@@ -37,12 +41,12 @@ fun promptUserForAmount(): Int {
     return Validation.parseAmount(input)
 }
 
-fun calculateNumberOfLottos(amount:Int):Int{
+fun calculateNumberOfLottos(amount: Int): Int {
     println()
-    return amount /1000
+    return amount / 1000
 }
 
-fun generateLottos(count:Int){
+fun generateLottos(count: Int) {
     repeat(count) {
         val lottoNumbers = generateRandomLottoNumbers()
         println(lottoNumbers)
@@ -51,4 +55,22 @@ fun generateLottos(count:Int){
 
 fun generateRandomLottoNumbers(): List<Int> {
     return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+}
+
+fun readLottoNumber(): List<Int> {
+    while (true) {
+        try {
+            val lottoNum = promptUserForLotto()
+            Validation.validateLottoNumbers(lottoNum) // 번호 유효성 검증
+            return lottoNum
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
+fun promptUserForLotto(): List<Int> {
+    println("\n당첨 번호를 입력해 주세요.")
+    val input = Console.readLine()?.trim()
+    return Validation.parseLottoNumbers(input)
 }
--- src/main/kotlin/lotto/Validation.kt
+++ src/main/kotlin/lotto/Validation.kt
@@ -10,4 +10,17 @@ object Validation {
         if (amount <= 0) return throw LottoError.INVALID_AMOUNT.throwException()
         if (amount % 1000 != 0) return throw LottoError.INVALID_UNIT.throwException()
     }
+
+    fun validateLottoNumbers(numbers: List<Int>) {
+        if (numbers.size != 6 || numbers.any { it < 1 || it > 45 } || numbers.toSet().size != numbers.size)
+            throw LottoError.INVALID_NUM.throwException()
+    }
+
+    fun parseLottoNumbers(input: String?): List<Int> {
+        if (input.isNullOrBlank()) throw LottoError.INVALID_AMOUNT.throwException()
+
+        val numbers = input.split(',').map { it.trim().toIntOrNull() }
+        if (numbers.any { it == null }) throw LottoError.INVALID_SPLIT.throwException()
+        return numbers.filterNotNull()
+    }
 }
