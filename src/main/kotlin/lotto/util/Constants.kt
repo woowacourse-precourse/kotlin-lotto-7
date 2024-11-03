019280fd66e4fad719a5f096884d1cbe033e9a40
Jang Muyeong <jmy8876@gmail.com>
2024-11-04 00:51:33 +0900

    refactor(Constants): 상수화 내용 추가
    - 에러 메세지 분리
    - 로또 MIN/MAX도 상수화

--- src/main/kotlin/lotto/util/Constants.kt
+++ src/main/kotlin/lotto/util/Constants.kt
@@ -1,30 +1,27 @@
 package lotto.util
 
 object Constants {
-    // 에러 메세지
-    const val PURCHASE_AMOUNT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다."
-    const val PURCHASE_AMOUNT_NUMBER_ERROR = "[ERROR] 구입 금액은 숫자여야 합니다."
-    const val WINNING_NUMBERS_SIZE_ERROR = "[ERROR] 당첨 번호는 6개여야 합니다."
-    const val WINNING_NUMBERS_RANGE_ERROR = "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다."
-    const val WINNING_NUMBERS_DUPLICATE_ERROR = "[ERROR] 당첨 번호는 중복될 수 없습니다."
-    const val BONUS_NUMBER_RANGE_ERROR = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
-    const val BONUS_NUMBER_DUPLICATE_ERROR = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
-    const val BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 숫자여야 합니다."
-    const val WINNING_NUMBERS_ERROR = "[ERROR] 당첨 번호는 숫자여야 합니다."
+    // 에러 메시지
+    const val ERROR_PREFIX = "[ERROR]"
+    const val ERROR_INVALID_PURCHASE_AMOUNT = "$ERROR_PREFIX 구입 금액은 1,000원 단위의 숫자여야 합니다."
+    const val ERROR_INVALID_NUMBER_INPUT = "$ERROR_PREFIX 숫자만 입력 가능합니다."
+    const val ERROR_INVALID_WINNING_NUMBERS_SIZE = "$ERROR_PREFIX 당첨 번호는 6개여야 합니다."
+    const val ERROR_INVALID_LOTTO_NUMBERS_SIZE = "$ERROR_PREFIX 로또 번호는 6개여야 합니다."
+    const val ERROR_NUMBER_OUT_OF_RANGE = "$ERROR_PREFIX 번호는 1부터 45 사이의 숫자여야 합니다."
+    const val ERROR_DUPLICATE_NUMBER = "$ERROR_PREFIX 번호는 중복될 수 없습니다."
+    const val ERROR_BONUS_NUMBER_DUPLICATE = "$ERROR_PREFIX 보너스 번호는 당첨 번호와 중복될 수 없습니다."
 
-    // 메세지
-    const val PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요."
-    const val WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요."
-    const val BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요."
-    const val PURCHASED_LOTTOS_MESSAGE = "%d개를 구매했습니다."
-    const val LOTTO_NUMBERS_SEPARATOR = ", "
-    const val RESULT_STATISTICS = "당첨 통계"
-    const val RESULT_SEPARATOR = "---"
-    const val YIELD_MESSAGE = "총 수익률은 %.1f%%입니다."
+    // 안내 메시지
+    const val MESSAGE_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+    const val MESSAGE_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요."
+    const val MESSAGE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val MESSAGE_PURCHASED_LOTTOS = "%d개를 구매했습니다."
+    const val MESSAGE_WINNING_STATISTICS = "당첨 통계\n---"
+    const val MESSAGE_TOTAL_YIELD = "총 수익률은 %.1f%%입니다."
 
-    // 기본 조건
+    // 로또 설정
     const val LOTTO_PRICE = 1_000
     const val LOTTO_NUMBER_COUNT = 6
-    const val LOTTO_NUMBER_RANGE_START = 1
-    const val LOTTO_NUMBER_RANGE_END = 45
+    const val LOTTO_NUMBER_MIN = 1
+    const val LOTTO_NUMBER_MAX = 45
 }
