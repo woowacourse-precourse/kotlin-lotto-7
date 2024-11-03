81691a07be10491ef7ba28cdf9e85d101acb6314
Jang Muyeong <jmy8876@gmail.com>
2024-11-04 00:54:58 +0900

    refactor: Lotto 클래스 상수 적용 및 메서드 분리

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,17 +1,37 @@
 package lotto.domain
 
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.exception.InvalidInputException
+import lotto.util.Constants
 
+// 로또 번호를 관리
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
-        require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." }
-        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
+        validateNumbers(numbers)
     }
 
+    // 로또 번호 자동 생성
     constructor() : this(
-        Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        Randoms.pickUniqueNumbersInRange(
+            Constants.LOTTO_NUMBER_MIN,
+            Constants.LOTTO_NUMBER_MAX,
+            Constants.LOTTO_NUMBER_COUNT
+        ).sorted()
     )
 
+    // 로또 번호 유효성 검증
+    private fun validateNumbers(numbers: List<Int>) {
+        if (numbers.size != Constants.LOTTO_NUMBER_COUNT) {
+            throw InvalidInputException(Constants.ERROR_INVALID_LOTTO_NUMBERS_SIZE)
+        }
+        if (numbers.distinct().size != Constants.LOTTO_NUMBER_COUNT) {
+            throw InvalidInputException(Constants.ERROR_DUPLICATE_NUMBER)
+        }
+        if (!numbers.all { it in Constants.LOTTO_NUMBER_MIN..Constants.LOTTO_NUMBER_MAX }) {
+            throw InvalidInputException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
+        }
+    }
+
+    // 로또 번호 반환
     fun getNumbers(): List<Int> = numbers
 }
