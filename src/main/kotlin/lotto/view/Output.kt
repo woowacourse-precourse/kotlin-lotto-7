ec76c14845f512f0a6d795413f0430126065b659
rungeun <wjdrmsdl5@gmail.com>
2024-11-04 03:10:55 +0900

    feat(Output): 입력 가이드 라인 출력 기능 추가
    - 출력 기능만 하는 오브젝트 추가

---
+++ src/main/kotlin/lotto/view/Output.kt
@@ -0,0 +1,18 @@
+package lotto.view
+
+object GuideOutput {
+    fun amount(){
+        println(InputUserGuide.MONEY)
+    }
+    fun winNumber(){
+        newline()
+        println(InputUserGuide.NUMBER)
+    }
+
+    fun bonusNumber(){
+        newline()
+        println(InputUserGuide.BONUS)
+    }
+
+    fun newline() = println()
+}
