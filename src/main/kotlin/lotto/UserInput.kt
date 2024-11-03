61aa212951b428d0bfab9b5b128392d3718f903e
dioo1461 <dioo1461@gmail.com>
2024-11-03 15:41:54 +0900

    feat: 당첨 번호 및 보너스 번호 입력받기 기능 구현

--- src/main/kotlin/lotto/UserInput.kt
+++ src/main/kotlin/lotto/UserInput.kt
@@ -9,12 +9,19 @@ object UserInput {
         getInput()
     }
 
+    fun requestWinningNumbers() {
+        println("당첨번호를 입력해 주세요.")
+        getInput()
+    }
+
+    fun requestBonusNumber() {
+        println("보너스 번호를 입력해 주세요.")
+        getInput()
+    }
 
     private fun getInput(): String {
         return readLine()
     }
-
-
 
 }
 
