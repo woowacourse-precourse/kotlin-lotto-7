adc28676420ebed609518fe15cd46a5a1affdc0b
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-11-03 21:28:27 +0900

    feat(InputTest) : inputTest 클래스 생성 및 메서드 생성

--- src/test/kotlin/lotto/InputTest.kt
+++ src/test/kotlin/lotto/InputTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
 
 class InputTest : NsTest() {
     @Test
-    fun `숫자 외의 것이 들어왔을 경우 예외 상황`() {
+    fun `입력된 값이 숫자 외의 것이 들어왔을 경우 예외 상황`() {
         assertSimpleTest {
             runException("1000j")
             assertThat(output()).contains("숫자만 입력할 수 있습니다")
@@ -21,6 +21,15 @@ class InputTest : NsTest() {
             runException("  ")
             assertThat(output()).contains("값을 입력해 주세요")
         }
+    }
+
+    @Test
+    fun `입력된 값이 1000의 배수가 아닌 경우 예외 상황`() {
+        assertSimpleTest {
+            runException("1500")
+            assertThat(output()).contains("1000원 단위로 입려해주십시오")
+        }
+
     }
 
 
