7d639b1d37d1e2dc2ed5d502d2fa1c38c16a218c
jklom0326 <jklom0326@gmail.com>
2024-11-04 00:28:20 +0900

    test: 로또 번호 테스트 추가 (1부터 45사이의 숫자가 아닐 때)

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -11,7 +11,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +18,10 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또 번호에 1부터 45사이의 숫자가 아니면 예외가 발생한다 `() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 55, 66))
+        }
+    }
 }
