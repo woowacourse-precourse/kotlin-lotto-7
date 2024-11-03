74c38c91d76d1d9b53505efc2474b619624b9247
eunmyeungjun <audwns419@gmail.com>
2024-11-03 21:12:39 +0900

    feat: 당첨 결과 계산 및 등수별 당첨 내역 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -22,3 +22,21 @@ fun calculateTicket(money: Int): Int {
 fun generateLotto(ticketsCount : Int) : List<Lotto> {
     return List(ticketsCount) { Lotto(Lotto.generateLotto()) }
 }
+
+fun calculateWin(tickets: List<Lotto>, winNumber: List<Int> , bonusNum: Int) : Map<String,Int> {
+    val statistics = mutableMapOf("1등" to 0, "2등" to 0, "3등" to 0, "4등" to 0, "5등" to 0)
+
+    tickets.forEach { lottoTicket ->
+        val matchCount = lottoTicket.lottoMatches(winNumber)
+        val bonusMatch = lottoTicket.isBonusMatches(bonusNum)
+
+        when (matchCount) {
+            6 -> statistics["1등"] = statistics.getValue("1등") + 1
+            5 -> if (bonusMatch) statistics["2등"] = statistics.getValue("2등") + 1
+            else statistics["3등"] = statistics.getValue("3등") + 1
+            4 -> statistics["4등"] = statistics.getValue("4등") + 1
+            3 -> statistics["5등"] = statistics.getValue("5등") + 1
+        }
+    }
+    return statistics
+}
