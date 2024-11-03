cd46bebc2274bfc32715277c71002e4d7efaa7ea
DongjooSEO <readyjoo24@gmail.com>
2024-11-04 03:32:57 +0900

    feat: Rank Enum 클래스 추가

---
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+enum class Rank(
+    val matchCount: Int,
+    val isBonusMatch: Boolean,
+    val prize: Int,
+) {
+    FIRST(6, false, 2_000_000_000 ),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+}
