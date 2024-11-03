c79efbee157d085b1c3fe70c7a7b2b30381167be
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-04 02:58:20 +0900

    test: BonusNumber 테스트 생성

--- src/test/kotlin/lotto/domain/model/BonusNumberTest.kt
+++ src/test/kotlin/lotto/domain/model/BonusNumberTest.kt
@@ -1,4 +1,16 @@
 package lotto.domain.model
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class BonusNumberTest {
+    @Test
+    @DisplayName("보너스 모델 생성")
+    fun `BonusNumber는 주어진 보너스 번호로 생성되어야 한다`() {
+        val bonusNumberValue = 7
+        val bonusNumber = BonusNumber(bonusNumberValue)
+
+        assertEquals(bonusNumberValue, bonusNumber.bonusNumber)
+    }
 }
