820f5c871b2fcb2b5f820f8132c21a68145f8d8d
DongjooSEO <readyjoo24@gmail.com>
2024-11-04 06:02:40 +0900

    feat: 당첨 통계 내용 출력

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,9 @@
 package lotto.view
 
 import lotto.domain.Lotto
+import lotto.domain.Rank
+import lotto.domain.WinningStatus
+import java.text.DecimalFormat
 
 class OutputView {
     fun printPurchaseAmount(amount: Int) {
@@ -10,6 +13,20 @@ class OutputView {
     fun printLottoStatus(lottos: List<Lotto>) {
         lottos.forEach {lotto ->
             println(lotto.getNumbers().joinToString(", ", "[", "]"))
+                }
+            }
+
+        fun printStatistics(result: WinningStatus) {
+            val decimal = DecimalFormat("#,###")
+
+            println("\n당첨 통계")
+            println("---")
+            Rank.entries.filterNot { it == Rank.NONE }
+                .sortedBy { it.prize }
+                .forEach { rank ->
+                    println("${rank.description} (${decimal.format(rank.prize)}원) - ${result.rankCount[rank] ?: 0}개")
+            }
+        println("총 수익률은 ${"%.1f".format(result.profitRate)}%입니다.")
+
         }
-    }
 }
