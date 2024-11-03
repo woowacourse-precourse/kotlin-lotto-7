7275931c5f315da65e9513e8dba20c5fbc4e7876
yeonjeen <jeendev2019@gmail.com>
2024-11-04 03:33:11 +0900

    feat(Validator.kt): 보너스 번호 검증 로직 구현

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -21,8 +21,20 @@ class Validator {
         }.getOrNull()
     }
 
+    fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int? {
+        return runCatching {
+            val bonusNumber = input.trim().toInt()
+            require(NumberValidation.RANGE.isValid(listOf(bonusNumber))){NumberValidation.RANGE.errorMessage}
+            require(bonusNumber !in winningNumbers) { ERROR_BONUS_DUPLICATE }
+            bonusNumber
+        }.onFailure {
+            println(it.message ?: ERROR_UNVALIDATED_NUMBER)
+        }.getOrNull()
+    }
+
     companion object {
         private const val ERROR_INCORRECT_UNIT = "[ERROR] 구입 금액은 1,000원 단위여야 합니다."
         private const val ERROR_UNVALIDATED_NUMBER = "[ERROR] 숫자를 올바르게 입력해 주세요."
+        private const val ERROR_BONUS_DUPLICATE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
     }
 }
