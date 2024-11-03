b21642587235c6da6720b8ecb116858679d2210f
wjdrjs00 <kimjk3409@gmail.com>
2024-11-03 16:47:12 +0900

    feat(domain): 로또 번호를 처리하는 Lotto 클래스 구현
    로또 번호 리스트에 대한 유효성 검사 구현:
    - 빈 리스트 검증
    - 사이즈 검증
    - 중복요소값 검증
    로또 번호 반환 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,28 @@
 package lotto.domain
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(
+    private val numbers: List<Int>
+) {
+
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        validateLotto(numbers)
     }
 
-    // TODO: 추가 기능 구현
+    fun getLotto(): List<Int> = numbers.toList()
+
+    private fun validateLotto(numbers: List<Int>) {
+        requireNotEmpty(numbers)
+        requireValidSize(numbers)
+        requireUnique(numbers)
+    }
+
+    private fun requireNotEmpty(numbers: List<Int>) =
+        require(numbers.isNotEmpty()) { "[ERROR] 로또 번호는 비어있을 수 없습니다." }
+
+    private fun requireValidSize(numbers: List<Int>) =
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+
+    private fun requireUnique(numbers: List<Int>) =
+        require(numbers.distinct().size == numbers.size) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
+
 }
