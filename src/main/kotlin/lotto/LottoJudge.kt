133c6fcc27e54e03dd80afebe47243aa3d266c2d
joonhokil <kiljh@me.com>
2024-11-03 10:03:58 +0900

    refactor: 에러메시지를 많이 사용하므로 임포트하여 코드 줄이기

--- src/main/kotlin/lotto/LottoJudge.kt
+++ src/main/kotlin/lotto/LottoJudge.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.ErrorMessage.*
+
 class LottoJudge {
     var winnerNumbers: List<Int> = emptyList()
     var bonusNumber: Int = 0
@@ -18,25 +20,25 @@ class LottoJudge {
 
     private fun exceptWinnerNumberCount(numbers: List<Int>) {
         if (numbers.size != LottoMaker.NUMBERS_COUNT) {
-            throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
+            throw IllegalArgumentException(INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
         }
     }
 
     private fun exceptWinnerNumberRange(numbers: List<Int>) {
         if (numbers.any { it < LottoMaker.FIRST_NUMBER || it > LottoMaker.LAST_NUMBER }) {
-            throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
+            throw IllegalArgumentException(INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
         }
     }
 
     private fun exceptBonusNumberRange(number: Int) {
         if (number < LottoMaker.FIRST_NUMBER || number > LottoMaker.LAST_NUMBER ) {
-            throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
+            throw IllegalArgumentException(INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
         }
     }
 
     private fun exceptDuplicates(numbers: List<Int>) {
         if (numbers.toSet().size != numbers.size) {
-            throw IllegalArgumentException(ErrorMessage.INPUT_WINNER_NUMBER_NO_DUPLICATE.getMessage())
+            throw IllegalArgumentException(INPUT_WINNER_NUMBER_NO_DUPLICATE.getMessage())
         }
     }
 }
--- src/test/kotlin/lotto/LottoJudgeTest.kt
+++ src/test/kotlin/lotto/LottoJudgeTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import lotto.ErrorMessage.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
@@ -21,10 +22,10 @@ class LottoJudgeTest {
     @Test
     fun `로또 당첨 번호는 6개 입력되어야 한다`() {
         val exceptionForNumbersCountIsSeven = `로또 당첨 번호가 7개인 경우 예외가 발생해야 한다`()
-        assertThat(exceptionForNumbersCountIsSeven.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
+        assertThat(exceptionForNumbersCountIsSeven.message).contains(INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
 
         val exceptionForNumbersCountIsFive = `로또 당첨 번호가 5개인 경우 예외가 발생해야 한다`()
-        assertThat(exceptionForNumbersCountIsFive.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
+        assertThat(exceptionForNumbersCountIsFive.message).contains(INPUT_WINNER_NUMBER_COUNT_ERROR.getMessage())
     }
 
     private fun `로또 당첨 번호가 7개인 경우 예외가 발생해야 한다`(): Exception {
@@ -46,10 +47,10 @@ class LottoJudgeTest {
     @Test
     fun `로또 당첨 번호는 1 이상, 45 이하로 입력되어야 한다`() {
         val exceptionForNumbersMinus = `로또 당첨 번호가 1미만인 경우 예외가 발생해야 한다`()
-        assertThat(exceptionForNumbersMinus.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
+        assertThat(exceptionForNumbersMinus.message).contains(INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
 
         val exceptionForNumbersAboveFortyFive = `로또 당첨 번호가 45초과인 경우 예외가 발생해야 한다`()
-        assertThat(exceptionForNumbersAboveFortyFive.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
+        assertThat(exceptionForNumbersAboveFortyFive.message).contains(INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
     }
 
     private fun `로또 당첨 번호가 1미만인 경우 예외가 발생해야 한다`(): Exception {
@@ -74,7 +75,7 @@ class LottoJudgeTest {
             lottoJudge.setLottoWinnerNumbers(listOf(1,1,2,3,4,5))
         }
 
-        assertThat(exception.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_NO_DUPLICATE.getMessage())
+        assertThat(exception.message).contains(INPUT_WINNER_NUMBER_NO_DUPLICATE.getMessage())
     }
 
     @Test
@@ -85,10 +86,10 @@ class LottoJudgeTest {
     @Test
     fun `보너스 번호는 1 이상, 45 이하로 입력되어야 한다`() {
         val exceptionForBonusNumberLessThanOne = `보너스 번호가 1미만인 경우 예외가 발생해야 한다`()
-        assertThat(exceptionForBonusNumberLessThanOne.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
+        assertThat(exceptionForBonusNumberLessThanOne.message).contains(INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
 
         val exceptionForBonusNumberGreaterThanFortyFive = `보너스 번호가 45초과인 경우 예외가 발생해야 한다`()
-        assertThat(exceptionForBonusNumberGreaterThanFortyFive.message).contains(ErrorMessage.INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
+        assertThat(exceptionForBonusNumberGreaterThanFortyFive.message).contains(INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR.getMessage())
     }
 
     private fun `보너스 번호가 1미만인 경우 예외가 발생해야 한다`(): Exception {
