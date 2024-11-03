53a3875fb9afb9df9e4f137d55cd469a1bff629e
yeonjeen <jeendev2019@gmail.com>
2024-11-04 03:29:13 +0900

    feat(LottoGame): 검증로직을 이용해 유요한 당첨번호 구현

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,5 +1,8 @@
 package lotto
 
+import lotto.view.LottoInput
+import lotto.view.LottoOutput
+
 class LottoGame {
     private val lottoInput = LottoInput()
     private val lottoOutput = LottoOutput()
@@ -8,12 +11,14 @@ class LottoGame {
 
     fun startLotto() {
         val purchasePrice = checkValidatedPurchaseAmount()
-        val lottoTicketCount = purchasePrice / 1000
 
+        val lottoTicketCount = purchasePrice / 1000
         lottoOutput.printTicketCount(lottoTicketCount)
 
         val lottoTickets = lottoMachine.generateLottoTickets(lottoTicketCount)
         lottoOutput.printLottoTickets(lottoTickets)
+
+        val winningNumbers = checkValidatedWinningNumbers()
     }
 
     private fun checkValidatedPurchaseAmount(): Int {
@@ -21,5 +26,12 @@ class LottoGame {
             val input = lottoInput.getPurchasePrice()
             validator.validatePurchasePrice(input)
         }.firstNotNullOfOrNull { it } ?: checkValidatedPurchaseAmount()
+    }
+
+    private fun checkValidatedWinningNumbers(): List<Int> {
+        return generateSequence {
+            val input = lottoInput.getWinningNumbers()
+            validator.validateWinningNumbers(input)
+        }.firstNotNullOfOrNull { it } ?: checkValidatedWinningNumbers()
     }
 }
