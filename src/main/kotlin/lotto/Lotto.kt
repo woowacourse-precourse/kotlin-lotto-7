e6ffbb4df92ec8ec5fbbed1fca30aa2e57d3d97d
limch37 <limch37@naver.com>
2024-11-04 02:54:10 +0900

    feat(Lotto): 로또 번호 중복 검사 추가
    - 로또 번호 리스트에 중복된 숫자가 있는 경우 예외가 발생하도록 require 조건을 추가했습니다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,12 +3,14 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { ERROR_LOTTO_COUNT_MESSAGE }
+        require(numbers.distinct().size == 6) { ERROR_LOTTO_DUPLICATE_MESSAGE }
     }
 
     fun getNumbers(): List<Int> = numbers.sorted()
 
     companion object {
         const val ERROR_LOTTO_COUNT_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
+        const val ERROR_LOTTO_DUPLICATE_MESSAGE = "[ERROR] 로또 번호에 중복이 없어야 합니다."
     }
 }
 
