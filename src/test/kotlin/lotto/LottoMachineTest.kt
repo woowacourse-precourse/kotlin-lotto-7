6eb0b5848917158bb63d65cb22da3e7840e6b10a
jklom0326 <jklom0326@gmail.com>
2024-11-04 04:29:39 +0900

    test: 당첨 결과 확인 테스트 구현

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test
 
 class LottoMachineTest {
 
+    private val lottoMachine = LottoMachine("3000")
+
     @Test
     fun `로또 번호와 당첨번호 + 보너스 번호를 비교`() {
-        val lottoMachine = LottoMachine("3000")
 
         val lottos = listOf(
             Lotto(listOf(1, 3, 10, 26, 41, 45)),
@@ -24,6 +25,24 @@ class LottoMachineTest {
             Pair(4, false),
             Pair(2, false),
         )
+
+        assertEquals(expectResult, result)
+    }
+
+    @Test
+    fun `당첨 결과 확인 테스트`() {
+        val winningResult = listOf(
+            Pair(4, true),
+            Pair(4, false),
+            Pair(2, false),
+        )
+
+        val expectResult = mapOf(
+            Rank.THIRD_RANK to 2,
+            Rank.MISS to 1,
+        )
+
+        val result = lottoMachine.countPrize(winningResult)
 
         assertEquals(expectResult, result)
     }
