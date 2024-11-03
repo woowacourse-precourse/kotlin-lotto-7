4fba89c66f4c7faaa6e89c8f3006ca1c6c91592b
hwsg0912@naver.com <hwsg0912@naver.com>
2024-11-03 22:21:13 +0900

    chore: 구매 단위를 상수형 변경 및 require 조건 변경

--- src/main/kotlin/lotto/model/PurchaseAmount.kt
+++ src/main/kotlin/lotto/model/PurchaseAmount.kt
@@ -2,9 +2,13 @@ package lotto.model
 
 class PurchaseAmount(private val amount: Int) {
     init {
-        require (amount % 1000 == 0) { "[ERROR] 구매금액은 1000원 단위로 입력해야 합니다." }
-        require (amount <= 0) { "[ERROR] 구매금액은 양수로 입력해야 합니다."}
+        require (amount % PURCHASE_UNIT == 0) { "[ERROR] 구매 금액은 ${PURCHASE_UNIT}원 단위로 입력 해야 합니다." }
+        require (amount > 0) { "[ERROR] 구매 금액은 양수로 입력 해야 합니다."}
     }
 
-    fun calculatePurchaseQuantity() = amount / 1000
+    fun calculatePurchaseQuantity() = amount / PURCHASE_UNIT
+
+    companion object {
+        private const val PURCHASE_UNIT = 1000
+    }
 }
