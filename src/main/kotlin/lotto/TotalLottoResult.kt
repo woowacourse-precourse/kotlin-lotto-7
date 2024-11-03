869ae67d58f317e9f5aed8086c88eac39fc7e38d
bbangiee <yoojin1117@gmail.com>
2024-11-03 21:42:51 +0900

    feat(수익률 계산): 수익률 구하기
    - (당첨금 / 구입 금액 * 100)%

--- src/main/kotlin/lotto/TotalLottoResult.kt
+++ src/main/kotlin/lotto/TotalLottoResult.kt
@@ -1,14 +1,18 @@
 package lotto
 
+import kotlin.math.round
+
 class TotalLottoResult(
     private val purchasedLotto: PurchasedLotto,
     private val winningNumbers: WinningNumbers,
     private val bonusNumber: BonusNumber
 ) {
     val winningStatus = mutableMapOf(1 to 0, 2 to 0, 3 to 0, 4 to 0, 5 to 0)
+    val rank = mapOf(1 to WinnerRank.FIRST, 2 to WinnerRank.SECOND, 3 to WinnerRank.THIRD, 4 to WinnerRank.FOURTH, 5 to WinnerRank.FIFTH)
 
     init {
         checkAllLotto()
+        calculateRateOfReturn()
     }
 
     fun checkAllLotto() {
@@ -19,5 +23,15 @@ class TotalLottoResult(
                 winningStatus[rank] = winningStatus[rank]!! + 1
             }
         }
+    }
+
+    fun calculateRateOfReturn(): Float {
+        val payment = purchasedLotto.amountOfLotto * 1000
+        var totalPrize = 0
+
+        winningStatus.forEach {
+            totalPrize += rank[it.key]!!.prize * it.value
+        }
+        return totalPrize.toFloat() / payment * 100
     }
 }
