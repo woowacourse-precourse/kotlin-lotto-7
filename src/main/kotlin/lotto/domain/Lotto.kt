0d6b583a3f0e6bd7094bd65b9ea38f9da8e21454
Jang Muyeong <jmy8876@gmail.com>
2024-11-04 01:17:48 +0900

    fix: Lotto, WinningLotto 클래스 오류 수정
    - 예외 처리 클래스 부분 삭제(문제의 조건에 부합하지 않음)

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,7 +1,6 @@
 package lotto.domain
 
 import camp.nextstep.edu.missionutils.Randoms
-import lotto.exception.InvalidInputException
 import lotto.util.Constants
 
 // 로또 번호를 관리
@@ -22,13 +21,13 @@ class Lotto(private val numbers: List<Int>) {
     // 로또 번호 유효성 검증
     private fun validateNumbers(numbers: List<Int>) {
         if (numbers.size != Constants.LOTTO_NUMBER_COUNT) {
-            throw InvalidInputException(Constants.ERROR_INVALID_LOTTO_NUMBERS_SIZE)
+            throw IllegalArgumentException(Constants.ERROR_INVALID_LOTTO_NUMBERS_SIZE)
         }
         if (numbers.distinct().size != Constants.LOTTO_NUMBER_COUNT) {
-            throw InvalidInputException(Constants.ERROR_DUPLICATE_NUMBER)
+            throw IllegalArgumentException(Constants.ERROR_DUPLICATE_NUMBER)
         }
         if (!numbers.all { it in Constants.LOTTO_NUMBER_MIN..Constants.LOTTO_NUMBER_MAX }) {
-            throw InvalidInputException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
+            throw IllegalArgumentException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
         }
     }
 
--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,6 +1,5 @@
 package lotto.domain
 
-import lotto.exception.InvalidInputException
 import lotto.util.Constants
 
 // 당첨 번호와 보너스 번호 관리
@@ -13,23 +12,23 @@ class WinningLotto(private val winningNumbers: List<Int>, private val bonusNumbe
     // 당첨 번호 유효성 검증
     private fun validateWinningNumbers() {
         if (winningNumbers.size != Constants.LOTTO_NUMBER_COUNT) {
-            throw InvalidInputException(Constants.ERROR_INVALID_WINNING_NUMBERS_SIZE)
+            throw IllegalArgumentException(Constants.ERROR_INVALID_WINNING_NUMBERS_SIZE)
         }
         if (winningNumbers.distinct().size != Constants.LOTTO_NUMBER_COUNT) {
-            throw InvalidInputException(Constants.ERROR_DUPLICATE_NUMBER)
+            throw IllegalArgumentException(Constants.ERROR_DUPLICATE_NUMBER)
         }
         if (!winningNumbers.all { it in Constants.LOTTO_NUMBER_MIN..Constants.LOTTO_NUMBER_MAX }) {
-            throw InvalidInputException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
+            throw IllegalArgumentException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
         }
     }
 
     // 보너스 번호 유효성 검증
     private fun validateBonusNumber() {
         if (bonusNumber !in Constants.LOTTO_NUMBER_MIN..Constants.LOTTO_NUMBER_MAX) {
-            throw InvalidInputException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
+            throw IllegalArgumentException(Constants.ERROR_NUMBER_OUT_OF_RANGE)
         }
         if (winningNumbers.contains(bonusNumber)) {
-            throw InvalidInputException(Constants.ERROR_BONUS_NUMBER_DUPLICATE)
+            throw IllegalArgumentException(Constants.ERROR_BONUS_NUMBER_DUPLICATE)
         }
     }
 
