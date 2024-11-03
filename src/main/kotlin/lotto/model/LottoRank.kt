2034bb2bde8a8723139c5bcee42d414d7c1d63de
DoableTuple <doabletuple@gmail.com>
2024-11-04 00:26:49 +0900

    feat(model): LottoRank enum class 생성

---
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -0,0 +1,10 @@
+package lotto.model
+
+enum class LottoRank(val matchCount: Int, val hasBonus: Boolean, val prize: Int) {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_00_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    FAIL(0, false, 0)
+}
