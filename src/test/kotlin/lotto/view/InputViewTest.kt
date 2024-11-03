fb2c4887a179ea0d596094bc63effa88d39c22c5
DongjooSEO <readyjoo24@gmail.com>
2024-11-04 06:01:26 +0900

    feat: 예외 처리 테스트 코드 수정

--- src/test/kotlin/lotto/view/InputViewTest.kt
+++ src/test/kotlin/lotto/view/InputViewTest.kt
@@ -7,13 +7,13 @@ class InputViewTest {
     @Test
     fun `구매 금액은 0보다 큰 수이여야 한다`(){
         assertThrows<IllegalArgumentException> {
-            InputView().validatePurchaseAmount(-1)
+            InputView().validatePurchaseAmount("-1")
         }
     }
     @Test
     fun `구매 금액은 1000원 단위로 입력되어야한다`(){
         assertThrows<IllegalArgumentException> {
-            InputView().validatePurchaseAmount(1234)
+            InputView().validatePurchaseAmount("1234")
         }
     }
     @Test
@@ -39,9 +39,10 @@ class InputViewTest {
     }
     @Test
     fun `보너스 번호는 1에서 45 사이의 숫자여야 한다`(){
-        val number = 46
+        val numbers = listOf(1,2,3,4,5,6,7)
+        val number = "46"
         assertThrows<IllegalArgumentException> {
-            InputView().validateBonusNumber(number)
+            InputView().validateBonusNumber(number, numbers)
         }
     }
 }
