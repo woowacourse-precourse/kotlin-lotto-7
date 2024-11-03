54039945802d0c0085f1d28f1c30d55f24600383
jklom0326 <jklom0326@gmail.com>
2024-11-03 17:20:01 +0900

    feat: 구입 금액 유효성 검사 (0보다 작은 수를 받았을 때)

--- src/main/kotlin/lotto/Payment.kt
+++ src/main/kotlin/lotto/Payment.kt
@@ -4,13 +4,18 @@ class Payment(private val payment: String) {
 
     init {
         require(isNumber()) { "$ERROR $NOT_NUMBER_MESSAGE" }
-
+        require(isNegativeAndZeroNumber()) {"$ERROR $MORE_THAN_ZERO"}
     }
+
+    private fun isNegativeAndZeroNumber() = payment.toInt() <= NEGATIVE_AND_ZERO
 
     private fun isNumber() = payment.toIntOrNull() != null
 
     companion object {
+        const val NEGATIVE_AND_ZERO = 0
+
         const val ERROR = "[ERROR]"
         const val NOT_NUMBER_MESSAGE = "숫자만 입력해주세요"
+        const val MORE_THAN_ZERO = "구입 금액이 0보다 작을 수 없습니다."
     }
 }
