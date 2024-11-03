88f1319e5ae6e92bb68c4b83554091a368ae11f3
yeonjeen <jeendev2019@gmail.com>
2024-11-04 03:22:36 +0900

    refactor(Lotto.kt): NumberValidation.kt을 사용해 번호 유효성 검증 적용

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,8 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
-    }
+        NumberValidation.validate(numbers)
+         }
 
     override fun toString(): String {
         return numbers.toString()
