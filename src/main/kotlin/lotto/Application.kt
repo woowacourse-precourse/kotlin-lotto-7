50c043f53ff02ed8dbd45573b2248ae9de06189f
ezhoon <david0275@naver.com>
2024-11-04 02:27:06 +0900

    style(main): 로또 관련 전역 변수 추가, 메인 함수 로직 변경

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,53 +3,64 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
-// # 메인 함수
+val LOTTO_PRICE = 1000
+val LOTTO_MIN = 1
+val LOTTO_MAX = 45
+
 fun main() {
     // TODO: 프로그램 구현
 
-    val price = Input.purchasePrice()
-    println("price: $price")
+    // [1] 구입 금액 입력 및 로또 구매
+    val purchaseAmount = Input.purchasePrice()
+    val lottoCount = purchaseAmount / LOTTO_PRICE
+    println("\n" + lottoCount.toString() + "개를 구매했습니다.")
 
-    val list = Input.setWinningNumber()
-    println("list = $list")
+    // [2] 로또 발행 후 시각화
+    val lottoManager = LottoManager(lottoCount)
+    lottoManager.showLottoTickets()
 
-    val bonus = Input.setBonusNumber()
-    println("bonus = $bonus")
+    // [3] 당첨 번호 및 보너스 번호 입력
+    val winningNumber = Input.setWinningNumber()
+    val bonusNumber = Input.setBonusNumber()
+
+    // [4] 당첨 통계화 및 시각화
+
+
+    // [5] 수익률 계산
 
 }
 
-// [#] 입력 기능
+// [#] 입력 기능 싱글톤 객체
 object Input {
-    // [1] 구매 금액 입력 기능
+    // [*] 구매 금액 입력 기능
     fun purchasePrice(): Int {
         println("구입금액을 입력해 주세요.")
         val input = Console.readLine()
             ?.takeIf { it.isNotBlank() }
             ?: throw IllegalArgumentException("[ERROR] 입력 값을 다시 확인해주세요.")
         val price = input.toInt()
-        require(price % 1000 == 0){"[ERROR] 구매 금액을 올바르게 입력해주세요."}
+        require(price % LOTTO_PRICE == 0){"[ERROR] 구매 금액을 올바르게 입력해주세요."}
         return price
     }
 
-    // [2] 당첨 번호 입력 기능, 공백 허용
+    // [*] 당첨 번호 입력 기능, 공백 허용
     fun setWinningNumber() : List<Int> {
         println("당첨 번호를 입력해 주세요.")
         val winningNumber = Console.readLine().split(",").map { it.trim().toInt() }
         require(winningNumber.size == 6){"[ERROR] 당첨 번호는 6개여야 합니다."}
-        require(winningNumber.all{ it > 0 && it < 46 }){"[ERROR] 1에서 45 사이의 숫자만 입력 가능합니다."}
+        require(winningNumber.all{ it in LOTTO_MIN..LOTTO_MAX }){"[ERROR] 1에서 45 사이의 숫자만 입력 가능합니다."}
         require(winningNumber.toSet().size == 6){"[ERROR] 중복 없는 6개 번호를 입력해주세요."}
         return winningNumber
     }
 
-    // [3] 보너스 번호 입력 기능
+    // [*] 보너스 번호 입력 기능
     fun setBonusNumber() : Int{
         println("보너스 번호를 입력해 주세요.")
         val input = Console.readLine()
             ?.takeIf { it.isNotBlank() }
             ?: throw IllegalArgumentException("[ERROR] 입력 값을 다시 확인해주세요.")
         val bonusNumber = input.toInt()
-        require( bonusNumber > 0 && bonusNumber < 46) {"[ERROR] 보너스 번호 값을 다시 확인해주세요."}
+        require( bonusNumber in LOTTO_MIN..LOTTO_MAX) {"[ERROR] 보너스 번호 값을 다시 확인해주세요."}
         return bonusNumber
     }
-
 }
