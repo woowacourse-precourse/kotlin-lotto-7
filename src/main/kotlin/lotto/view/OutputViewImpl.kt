20c5897334837a0941556937cb4529ff06cc05c0
Tnalxmsk <alstnwkd990@naver.com>
2024-11-03 21:04:53 +0900

    refactor: OutputView 개선
    - 소수 반올림 및 첫째 자리 출력하도록 변경
    - 단위 별 , 추가

--- src/main/kotlin/lotto/view/OutputViewImpl.kt
+++ src/main/kotlin/lotto/view/OutputViewImpl.kt
@@ -8,7 +8,7 @@ class OutputViewImpl : OutputView {
     override fun printNewLine() = println()
 
     override fun printPurchaseSummary(lottoBundle: List<Lotto>) {
-        println("${lottoBundle.size}개를 구매했습니다.")
+        println(PURCHASE_SUMMARY_MESSAGE.format(lottoBundle.size))
         for (lotto in lottoBundle) {
             println(lotto.getNumbers().joinToString(", ", "[", "]"))
         }
@@ -16,23 +16,32 @@ class OutputViewImpl : OutputView {
     }
 
     override fun printWinningStatistics(winningStatistics: Map<LottoWinning, Int>) {
+        println("당첨 통계\n" +
+                "---")
         LottoWinning.entries.reversed().forEach {
             when (it) {
-                LottoWinning.NONE -> return
-                LottoWinning.SECOND -> println("${it.count}개 일치, 보너스 볼 일치(${it.prize}원) - ${winningStatistics[it]}개")
-                else -> println("${it.count}개 일치 (${it.prize}원) - ${winningStatistics[it]}개")
+                LottoWinning.NONE -> return@forEach
+                LottoWinning.SECOND -> println(
+                    WINNING_STATISTICS_WITH_BONUS_MESSAGE.format(
+                        it.count,
+                        it.prize,
+                        winningStatistics[it]
+                    )
+                )
+
+                else -> println(WINNING_STATISTICS_MESSAGE.format(it.count, it.prize, winningStatistics[it]))
             }
         }
     }
 
     override fun printProfitRate(profitRate: Double) {
-        println("총 수익률은 $profitRate%입니다.")
+        println(PROFIT_RATE_MESSAGE.format(profitRate))
     }
 
     companion object {
-        private const val PURCHASE_SUMMARY_MESSAGE = "%d개를 구매했습니다."
-        private const val WINNING_STATISTICS_MESSAGE = "%d개 일치 (%d원) - %d개"
-        private const val WINNING_STATISTICS_WITH_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치(%d원) - %d개"
-        private const val PROFIT_RATE_MESSAGE = "총 수익률은 %.2f%%입니다."
+        private const val PURCHASE_SUMMARY_MESSAGE = "%,d개를 구매했습니다."
+        private const val WINNING_STATISTICS_MESSAGE = "%d개 일치 (%,d원) - %,d개"
+        private const val WINNING_STATISTICS_WITH_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %,d개"
+        private const val PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다."
     }
 }
