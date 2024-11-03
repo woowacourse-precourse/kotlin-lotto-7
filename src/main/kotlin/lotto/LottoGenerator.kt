33b30800c92c49c9b1440985bc17d2ba3b06bc99
yoon <cbj0010@naver.com>
2024-11-04 01:36:03 +0900

    feat : 로또 중복 숫자 검사

---
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -0,0 +1,14 @@
+package lotto
+
+class LottoGenerator : NumbersValidator {
+
+    override fun validate(value: List<Int>) {
+        require(value.distinct().size == value.size) {
+            GENERATOR_ERROR_FORMAT.format(value)
+        }
+    }
+
+    companion object {
+        private const val GENERATOR_ERROR_FORMAT = "로또 번호는 중복될 수 없습니다."
+    }
+}
