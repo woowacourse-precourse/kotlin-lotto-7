49f23818bf03201c8731daeaf3bf6f2ab8a3000a
조형석 <chs9902@naver.com>
2024-11-04 05:27:58 +0900

    :sparkles: feat: 수익률 계산 기능 추가

--- src/main/kotlin/lotto/model/WinningCalculator.kt
+++ src/main/kotlin/lotto/model/WinningCalculator.kt
@@ -1,15 +1,23 @@
 package lotto.model
 
+import lotto.util.Prize
+
+
 class WinningCalculator {
     fun calculateDetails(winningLotto: Lotto, bonus: Int, lottoes: List<Lotto>): List<Int> {
         var winningDetails = mutableListOf(0, 0, 0, 0, 0)
         for (purchasedLotto in lottoes) {
-            winningDetails = calculateDetail(purchasedLotto,winningLotto,bonus,winningDetails)
+            winningDetails = calculateDetail(purchasedLotto, winningLotto, bonus, winningDetails)
         }
         return winningDetails
     }
 
-    fun calculateDetail(purchasedLotto:Lotto,winningLotto:Lotto,bonus: Int,winningDetails:MutableList<Int>) : MutableList<Int>{
+    fun calculateDetail(
+        purchasedLotto: Lotto,
+        winningLotto: Lotto,
+        bonus: Int,
+        winningDetails: MutableList<Int>
+    ): MutableList<Int> {
         var count = 0
         purchasedLotto.getNumbers().forEach {
             if (winningLotto.getNumbers().contains(it)) {
@@ -26,7 +34,16 @@ class WinningCalculator {
         return winningDetails
     }
 
-    fun calculateReturnRate() {
+    fun calculateReturnRate(money:Int,winningDetails: MutableList<Int>) : Double {
+        return (calculatePrize(winningDetails) / money * 100).toDouble()
+    }
 
+    fun calculatePrize(winningDetails: MutableList<Int>):Int{
+        var total = 0
+        val prize = Prize.entries
+        for ((index,count) in winningDetails.withIndex()){
+            total += prize.find { it.rank == index+1 }!!.prize * count
+        }
+        return total
     }
 }
---
+++ src/main/kotlin/lotto/util/Prize.kt
@@ -0,0 +1,9 @@
+package lotto.util
+
+enum class Prize (val rank:Int,val prize:Int){
+    FIRST(1,2000000000),
+    SECOND(2,30000000),
+    THIRD(3,15000000),
+    FOURTH(4,50000),
+    FIFTH(5,5000)
+}
