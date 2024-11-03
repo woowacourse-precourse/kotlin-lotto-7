4dfe87edc8a21b7e8b572cfeb40e324f4f0077a2
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-11-03 16:40:37 +0900

    feat(Controller): 로또 구입 개수 변수 생성

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -6,10 +6,12 @@ import lotto.viewm.InputView
 
 object Controller {
     val validate = Validator()
+    private var lottoBuyNumber = 0
     fun run() {
         while (true) {
             try {
                 validate.validateInputAmount(InputView.inputPurchaseAmount())
+                lottoBuyNumber = InputView.inputPurchaseAmount().toInt() / 1000
                 break
             } catch (e: IllegalArgumentException) {
                 println(e)
