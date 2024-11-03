61495731b5696e9a82971f95dd31e1dac659d9da
DongjooSEO <readyjoo24@gmail.com>
2024-11-04 04:13:16 +0900

    feat: Lotto 상수로 역할 분리

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,12 +2,19 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_NUMBER_SIZE) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.size == numbers.toSet().size) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
-        require(numbers.all { it in 1..45 })
+        require(numbers.all { it in MINIMUM_NUMBER..MAXIMUM_NUMBER })
     }
 
     fun getNumbers(): List<Int> = numbers
+
+    companion object {
+        const val PRICE = 1000
+        const val LOTTO_NUMBER_SIZE = 6
+        const val MINIMUM_NUMBER = 1
+        const val MAXIMUM_NUMBER = 45
+    }
 }
 
 
