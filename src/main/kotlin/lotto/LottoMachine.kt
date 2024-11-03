f08ca62da3433e82c3ce3a6f68bf58074d78251a
jklom0326 <jklom0326@gmail.com>
2024-11-03 23:50:34 +0900

    feat: 로또 생성 기능 구현

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,13 +1,26 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoMachine(private val payment: String) {
 
     fun calculateTotalLottoCount(): Int {
         return payment.toInt() / LOTTO_PRICE
     }
 
+    fun generateLotto(): Lotto {
+        val number = lottoGenerator()
+        return Lotto(number)
+    }
+
+    private fun lottoGenerator(): MutableList<Int> =
+        Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUM, LOTTO_MAX_NUM, LOTTO_COUNT)
+
 
     companion object {
         const val LOTTO_PRICE = 1000
+        const val LOTTO_MIN_NUM = 1
+        const val LOTTO_MAX_NUM = 45
+        const val LOTTO_COUNT = 6
     }
 }
