3774f58f63799f06baa825de3a29dcc0bfda980a
joonhokil <kiljh@me.com>
2024-11-03 09:40:58 +0900

    feat: 로또 번호는 중복되지 않도록 예외 처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == LottoMaker.NUMBERS_COUNT) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == numbers.size) { ErrorMessage.LOTTO_NUMBER_NO_DUPLICATE }
     }
 
     // TODO: 추가 기능 구현
