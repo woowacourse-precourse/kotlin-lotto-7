8737f32518d13ffc8443c05ab4d637ffd9eeb2fb
hwsg0912@naver.com <hwsg0912@naver.com>
2024-11-03 21:04:28 +0900

    feat: OutputView 클래스 생성 및 출력문 구현

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,31 @@
+package lotto.view
+
+import lotto.model.Lotto
+
+class OutputView {
+    fun printPurchaseAmountRequest() {
+        println("구매금액을 입력해 주세요.")
+    }
+
+    fun printPurchaseQuantity(quantity: Int) {
+        println("${quantity}개를 구매했습니다.")
+    }
+
+    fun printPurchaseLotto(lottos: List<Lotto>) {
+        lottos.forEach {
+            println(it.getLotto())
+        }
+    }
+
+    fun printWinningLottoRequest() {
+        println("당첨 번호를 입력해 주세요.")
+    }
+
+    fun printBonusNumberRequest() {
+        println("보너스 번호를 입력해 주세요.")
+    }
+
+    fun printTotalRateOfReturn(rateOfReturn: Double) {
+        println("총 수익률은 ${rateOfReturn}%입니다.")
+    }
+}
