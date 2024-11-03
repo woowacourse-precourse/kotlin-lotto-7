159f630009c497fdec10cb8ed701414c43625a5b
46112 <duf325@naver.com>
2024-11-03 13:38:09 +0900

    feat(Application): 유효한 사용자 입력을 받을 때까지 반복하는 로직 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,14 +6,18 @@ import java.lang.NumberFormatException
 
 fun main() {
     println("구입금액을 입력해 주세요.")
-    try {
-        val purchaseAmount = Console.readLine().toInt()
-        require(purchaseAmount % 1000 == 0) { "[ERROR] 로또는 1,000원 단위로만 구매할 수 있습니다." }
-    } catch (e: NumberFormatException) {
-        println("[ERROR] 유효한 숫자를 입력해 주세요.")
-    } catch (e: IllegalArgumentException) {
-        println(e.message)
-    } catch (e: Exception) {
-        println("[ERROR] " + e.message)
+    while (true) {
+        try {
+            val purchaseAmount = Console.readLine().toInt()
+            require(purchaseAmount % 1000 == 0) { "[ERROR] 로또는 1,000원 단위로만 구매할 수 있습니다." }
+            break
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 유효한 숫자를 입력해 주세요.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        } catch (e: Exception) {
+            println("[ERROR] " + e.message)
+        }
     }
+
 }
