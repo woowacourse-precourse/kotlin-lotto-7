04dc7fa7ee1aa93a07ba81b416b72394c2362dfa
wjdrjs00 <kimjk3409@gmail.com>
2024-11-03 15:11:06 +0900

    test(domain): Purchase 클래스에 대한 단위 테스트 구현
    구매 금액 유효성 검사 테스트 구현:
      - 숫자가 아닌 입력값 검증
      - 빈 문자열 입력값 검증
      - 음수 입력값 검증
      - 1,000원 단위가 아닌 입력값 검증
    정상 케이스 테스트 구현:
      - 올바른 구매 가격 반환 검증
      - 올바른 구매 수량 반환 검증

---
+++ src/test/kotlin/lotto/domain/PurchaseTest.kt
@@ -0,0 +1,90 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class PurchaseTest {
+
+    @Test
+    fun `구매 금액이 숫자가 아닌 경우 예외를 발생시킨다`() {
+        // Given
+        val inputAmount = "삼천원"
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Purchase(inputAmount)
+        }
+
+        //Then
+        assertEquals("[ERROR] 구매금액은 숫자를 입력해야 합니다.", exception.message)
+    }
+
+    @Test
+    fun `구매 금액이 빈 문자열인 경우 예외를 발생시킨다`() {
+        // Given
+        val inputAmount = ""
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Purchase(inputAmount)
+        }
+
+        //Then
+        assertEquals("[ERROR] 구매금액은 숫자를 입력해야 합니다.", exception.message)
+    }
+
+    @Test
+    fun `구매 금액이 음수인 경우 예외를 발생시킨다`() {
+        // Given
+        val inputAmount = "-1000"
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Purchase(inputAmount)
+        }
+
+        //Then
+        assertEquals("[ERROR] 구매금액은 양수를 입력해야 합니다.", exception.message)
+    }
+
+    @Test
+    fun `구매 금액이 1,000원 단위가 아닌 경우 예외를 발생시킨다`() {
+        // Given
+        val inputAmount = "11500"
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Purchase(inputAmount)
+        }
+
+        //Then
+        assertEquals("[ERROR] 구매금액은 1,000원 단위로 입력해야 합니다.", exception.message)
+    }
+
+    @Test
+    fun `올바른 구매 금액이 주어진 경우 올바른 구매 금액을 반환한다`() {
+        // Given
+        val inputAmount = "11000"
+
+        // When
+        val lottoCount = Purchase(inputAmount).amount
+
+        // Then
+        assertThat(lottoCount).isEqualTo(11000)
+    }
+
+    @Test
+    fun `올바른 구매 금액이 주어진 경우 구입 금액에 따른 구매 횟수를 반환한다`() {
+        // Given
+        val inputAmount = "11000"
+
+        // When
+        val lottoCount = Purchase(inputAmount).lottoCount
+
+        // Then
+        assertThat(lottoCount).isEqualTo(11)
+    }
+
+}
