d84d909d171a87c72725384b8a2a686cb6306665
46112 <duf325@naver.com>
2024-11-03 12:15:22 +0900

    feat(lotto): 보너스 번호 중복 체크 함수 추가
    - 번호가 이전에 추첨한 번호와 일치하는지 판별하는 containsBonusNumber메서드 추가
    - 정상 작동 확인하는 테스트 메서드 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,5 +6,7 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.distinct().size == numbers.size) { "[ERROR] 로또 번호에는 중복된 숫자가 있을 수 없습니다." }
     }
 
-    // TODO: 추가 기능 구현
+    fun containsBonusNumber(bonusNumer: Int): Boolean {
+        return bonusNumer in numbers
+    }
 }
