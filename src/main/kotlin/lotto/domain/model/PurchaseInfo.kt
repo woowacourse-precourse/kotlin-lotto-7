f84fd4deedc72c1956f1ff6a76f5d85a4815a6f4
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-03 19:35:10 +0900

    refactor(PurchaseInfo): init 삭제 - 중복으로 타당성 검사 로직 실행 - numberOfTickets 간단한 계산 로직이라서 변수에 바로 할당

--- src/main/kotlin/lotto/domain/model/PurchaseInfo.kt
+++ src/main/kotlin/lotto/domain/model/PurchaseInfo.kt
@@ -1,21 +1,11 @@
 package lotto.domain.model
 
-import lotto.util.validator.purchaseamount.PurchaseAmountErrorType
-
-data class PurchaseInfo(private val purchaseAmount: Int) {
-    private val numberOfTickets: Int
-
-    init {
-        require(purchaseAmount >= ZERO) { PurchaseAmountErrorType.ZERO_AMOUNT.errorMessage }
-        numberOfTickets = purchaseAmount / PRICE_PER_SHEET
-    }
-
-    fun getNumberOfTickets(): Int {
-        return numberOfTickets
-    }
+data class PurchaseInfo(
+    val purchaseAmount: Int
+) {
+    val numberOfTickets: Int = purchaseAmount / PRICE_PER_SHEET
 
     companion object {
-        private const val ZERO = 0
         private const val PRICE_PER_SHEET = 1000
     }
 }
