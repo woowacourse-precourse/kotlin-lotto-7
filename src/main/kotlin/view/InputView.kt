f55cd16ec4ab9c24541b07179ddc48ef8a87d4a3
chanho0908 <uni070@naver.com>
2024-11-03 18:41:54 +0900

    refactor : 입력 받는 View의  사용자 입력 함수 수정

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -3,8 +3,5 @@ package view
 import camp.nextstep.edu.missionutils.Console.readLine
 
 class InputView {
-    fun userInput(): String {
-        val input = readLine()
-        return input
-    }
+    fun userInput(): String = readLine()
 }
