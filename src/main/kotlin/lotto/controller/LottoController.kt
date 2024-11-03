12866dccea2b0ef31bc978196c284943ed93fc82
ijh1298 <imjh0417@gmail.com>
2024-11-03 21:48:46 +0900

    refactor: 중복 로또 번호 검사 로직을 Controller에서 Lotto 클래스로 이동

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -39,7 +39,6 @@ class LottoController(
 
     private fun getValidWinningLotto(input: String): Lotto {
         require(input.isValidNumbers()) { INVALID_NUMBERS_EXCEPTION_MSG }
-        require(input.isNoDuplicateNumbers()) { HAS_DUPLICATE_NUMBER_EXCEPTION_MSG }
         val inputNumbers = input.split(DELIMITER).map { it.toInt() }
         return inputNumbers.toLottoNumbers()
     }
@@ -88,7 +87,6 @@ class LottoController(
         private const val INVALID_NUMBERS_EXCEPTION_MSG = "[ERROR] 유효하지 않은 당첨 번호 리스트입니다."
         private const val OVER_RANGE_EXCEPTION_MSG = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
         private const val INVALID_PRICE_RANGE_EXCEPTION_MSG = "[ERROR] 구입 금액은 1000원 이상의 1000으로 나누어지는 금액이어야 합니다."
-        private const val HAS_DUPLICATE_NUMBER_EXCEPTION_MSG = "[ERROR] 중복된 로또 번호가 없어야 합니다."
         private const val DUPLICATE_BONUS_NUMBER_EXCEPTION_MSG = "[ERROR] 보너스 번호가 당첨 번호에 있습니다."
     }
 }
--- src/main/kotlin/lotto/domain/entity/Lotto.kt
+++ src/main/kotlin/lotto/domain/entity/Lotto.kt
@@ -5,6 +5,7 @@ class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == 6) { HAS_DUPLICATE_NUMBER_EXCEPTION_MSG }
     }
 
     override fun toString(): String {
@@ -13,5 +14,7 @@ class Lotto(private val numbers: List<Int>) {
 
     companion object {
         fun List<Int>.toLottoNumbers(): Lotto = Lotto(this)
+
+        private const val HAS_DUPLICATE_NUMBER_EXCEPTION_MSG = "[ERROR] 중복된 로또 번호가 없어야 합니다."
     }
 }
