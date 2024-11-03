50a2f0c0a1af9224855e03a7c4aaa6043100782a
joonhokil <kiljh@me.com>
2024-11-03 15:44:37 +0900

    feat: 로또 등수를 확인하는 함수 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,7 +6,28 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.toSet().size == numbers.size) { ErrorMessage.LOTTO_NUMBER_NO_DUPLICATE }
     }
 
-    fun checkRank(numbers: List<Int>, bonus: Int): LottoRank {
+    fun checkRank(winnerNumbers: List<Int>, bonusNumber: Int): LottoRank {
+        val winNumbersCount = countCommonNumbers(winnerNumbers, numbers)
+        if (winNumbersCount == 6) {
+            return LottoRank.FIRST
+        }
+        if (winNumbersCount == 5) {
+            if (numbers.contains(bonusNumber)) {
+                return LottoRank.SECOND
+            }
+            return LottoRank.THIRD
+        }
+        if (winNumbersCount == 4) {
+            return LottoRank.FOURTH
+        }
+        if (winNumbersCount == 3) {
+            return LottoRank.FIFTH
+        }
+
         return LottoRank.FOURTH
+    }
+
+    private fun countCommonNumbers(list1: List<Int>, list2: List<Int>): Int {
+        return list1.intersect(list2).size
     }
 }
--- src/main/kotlin/lotto/LottoRank.kt
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -1,9 +1,10 @@
 package lotto
 
-enum class LottoRank(val rank: Int) {
-    FIRST(1),
-    SECOND(2),
-    THIRD(3),
+enum class LottoRank(val winNumbersCount: Int) {
+    FIRST(6),
+    SECOND(5),
+    THIRD(5),
     FOURTH(4),
-    FIFTH(5)
+    FIFTH(3),
+    LOSE(2)
 }
