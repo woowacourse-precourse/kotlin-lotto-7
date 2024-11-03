49ab08544782d2e5214cc7b92d1d837b7bdcb5c4
Grove1212 <stella223@korea.ac.kr>
2024-11-03 12:57:23 +0900

    feat: add exception handling for lotto validation

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,17 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == 6) { LottoErrorMessages.INVALID_SIZE }
+        require(numbers.distinct().size == numbers.size) { LottoErrorMessages.DUPLICATE_NUMBERS }
+        require(numbers.all { it in 1..45 }) { LottoErrorMessages.OUT_OF_RANGE }
+        require(numbers == numbers.sorted()) { LottoErrorMessages.NOT_SORTED }
     }
 
-    // TODO: 추가 기능 구현
+    object LottoErrorMessages {
+        const val INVALID_SIZE = "[ERROR] 로또 번호는 6개여야 합니다."
+        const val DUPLICATE_NUMBERS = "[ERROR] 로또 번호는 중복되지 않아야 합니다."
+        const val OUT_OF_RANGE = "[ERROR] 모든 로또 번호는 1부터 45 사이여야 합니다."
+        const val NOT_SORTED = "[ERROR] 로또 번호는 오름차순으로 정렬되어 있어야 합니다."
+    }
+
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -5,9 +5,26 @@ import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
+    fun `로또 안의 숫자가 1~45사이의 숫자가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(-1, 2, 3, 4, 5, 6))
+        }
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 4, 5, 6, 333))
+        }
+    }
+
+    @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    @Test
+    fun `로또 번호의 개수가 6개 미만이면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
         }
     }
 
@@ -16,6 +33,13 @@ class LottoTest {
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 오름차순이 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 3, 2, 4, 5, 6))
         }
     }
 
