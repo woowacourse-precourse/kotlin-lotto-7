7382bf1f462403f347eb0e294e0517630ab02a73
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-03 21:09:24 +0900

    feat(Controller): 컨트롤러 클래스 생성

---
+++ src/main/kotlin/controller/LottoController.kt
@@ -0,0 +1,20 @@
+package controller
+
+/* LottoController 설계
+* 1. 구입 금액 입력
+* 2. 구매 개수 출력
+* 3. 로또 번호 생성
+* 4. 로또 번호 출력
+* 5. 당첨 번호 입력
+* 6. 보너스 번호 입력
+* 7. 당첨 통계 계산
+* 8. 당첨 결과 출력
+*
+* */
+class LottoController {
+
+    fun playLottoGame(){
+
+    }
+
+}
