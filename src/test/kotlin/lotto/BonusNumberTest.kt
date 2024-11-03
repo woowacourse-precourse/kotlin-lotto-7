541494be23be98d4fb81f5d13c0b8463a8900b11
jklom0326 <jklom0326@gmail.com>
2024-11-04 03:27:17 +0900

    test: 보너스 번호 입력 테스트 구현 (당첨 번호와 중복)

--- src/test/kotlin/lotto/BonusNumberTest.kt
+++ src/test/kotlin/lotto/BonusNumberTest.kt
@@ -5,13 +5,20 @@ import org.junit.jupiter.api.assertThrows
 
 class BonusNumberTest {
 
-    private val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+    private val winningNumber = WinningNumber("1,2,3,4,5,6")
 
 
     @Test
     fun `보너스 번호에 숫자가 아닌 문자가 입력했을 경우 오류를 출력한다`() {
         assertThrows<IllegalArgumentException> {
-            BonusNumber("문자",lotto)
+            BonusNumber("문자", winningNumber)
+        }
+    }
+
+    @Test
+    fun `보너스 번호가 당첨 번호와 중복이 될 경우`() {
+        assertThrows<IllegalArgumentException> {
+            BonusNumber("1", winningNumber)
         }
     }
 }
