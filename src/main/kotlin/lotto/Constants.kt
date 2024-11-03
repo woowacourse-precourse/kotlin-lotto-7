a91a87a583d877adc949a62d82f8883fd99e50c4
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2024-11-03 16:54:51 +0900

    feat: 에러 메시지 상수 추가
    - 상수 값을 관리하기 위해 Constants.kt로 이동

---
+++ src/main/kotlin/lotto/Constants.kt
@@ -0,0 +1,10 @@
+package lotto
+
+object Constants {
+    const val ERROR_INVALID_NUMBER_COUNT_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
+    const val  ERROR_INVALID_NUMBER_RANGE_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+    const val ERROR_DUPLICATED_NUMBER_MESSAGE = "[ERROR] 로또 번호에는 중복된 숫자가 포함될 수 없습니다."
+    const val ERROR_EMPTY_LOTTO_PURCHASE_MESSAGE = "[ERROR] 구입 금액을 입력해야 합니다."
+    const val ERROR_NEGATIVE_LOTTO_PURCHASE_MESSAGE = "[ERROR] 구입 금액은 0보다 커야 합니다."
+    const val ERROR_INVALID_LOTTO_PURCHASE_MESSAGE = "[ERROR] 숫자만 입력할 수 있습니다."
+}
