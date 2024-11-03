c794b9a616e801170a959e86dcf3cec26c5ef732
jklom0326 <jklom0326@gmail.com>
2024-11-04 00:08:49 +0900

    feat: 로또 번호 유효성 검사 (숫자가 중복일 때)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,10 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "$ERROR $LOTTO_SIZE_MESSAGE" }
+        require(numbers.distinct().size == numbers.size) { "$ERROR $LOTTO_NUMBER_DUPLICATE_MESSAGE" }
     }
 
     companion object {
         const val ERROR = "[ERROR]"
         const val LOTTO_SIZE_MESSAGE = "로또 번호는 6개여야 합니다."
+        const val LOTTO_NUMBER_DUPLICATE_MESSAGE = "로또 번호는 중복될 수 없습니다."
     }
 }
