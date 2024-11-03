84b3b5962c0785a81538c3592b3edc46cf7545ce
yeonjeen <jeendev2019@gmail.com>
2024-11-04 06:49:17 +0900

    refactor(Rank.kt): 포멧팅 및 출력순서대로 구현

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -1,12 +1,16 @@
 package lotto.model
 
-enum class Rank(val matchCount: Int, val matchBonus: Boolean, val prize: Int) {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
+enum class Rank(
+    val matchCount: Int,
+    val matchBonus: Boolean,
+    val prize: Int,
+) {
+    NONE(0, false, 0),
     FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
 
     companion object {
         fun from(matchCount: Int, matchBonus: Boolean): Rank {
