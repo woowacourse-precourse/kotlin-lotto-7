d1d8aabcc19f1462bde66137aabbc43888303a93
jiwon <pjw99032986@gmail.com>
2024-11-04 01:21:47 +0900

    Feat(CustomErrorHandler.kt) : 에러 처리 및 재입력을 위한 클래스 작성
    에러 처리 및 재 입력을 위한 커스텀 에러 클래스 적용

--- src/main/kotlin/lotto/CustomErrorHandler.kt
+++ src/main/kotlin/lotto/CustomErrorHandler.kt
@@ -5,8 +5,9 @@ import java.lang.RuntimeException
 
 class CustomErrorHandler(override val message: String, private val exception: CustomException) : RuntimeException(){
     private lateinit var customException : CustomException
-    fun invoke(){
+    private val output = Output()
+    fun inputError(){
         this.customException = exception
+        output.printCustomMessage(exception)
     }
-
 }
