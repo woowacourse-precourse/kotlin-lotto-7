819f5625b55f07c8e3725086eaadec5606afcf4e
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-11-04 06:08:48 +0900

    feat(WinningResult.kt): 수익률 계산 기능 구현

--- src/main/kotlin/lotto/data/WinningResult.kt
+++ src/main/kotlin/lotto/data/WinningResult.kt
@@ -6,11 +6,14 @@ class WinningResult(
     private val bonusNumber: Int
 ) {
     val details: Map<Rank, Int>
+    val profitRate: Double
 
     init {
         details = Rank.entries
             .filter { it != Rank.MISS }
             .associateWith { rank -> calculateRanks().count { it == rank } }
+
+        profitRate = calculateProfitRate()
     }
 
     private fun calculateRanks() =
@@ -19,4 +22,14 @@ class WinningResult(
     private fun countMatches(numbers: List<Int>) = numbers.count { winningNumbers.contains(it) }
 
     private fun containsBonus(numbers: List<Int>) = numbers.contains(bonusNumber)
+
+    private fun calculateProfitRate(): Double {
+        val totalWinnings = details.entries.sumOf { it.value * it.key.winningPrize }
+        val totalSpent = lottoNumbers.size * PRICE_PER_LOTTO
+        return totalWinnings.toDouble() / totalSpent * 100.0
+    }
+
+    companion object {
+        private const val PRICE_PER_LOTTO = 1_000
+    }
 }
