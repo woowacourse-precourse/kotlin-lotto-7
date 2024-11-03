c8071312dd637abe8a962f6c8d122af497e2723b
joonhokil <kiljh@me.com>
2024-11-03 09:45:54 +0900

    refactor: 에러메시지 하드코딩 제거

--- src/main/kotlin/lotto/ErrorMessage.kt
+++ src/main/kotlin/lotto/ErrorMessage.kt
@@ -5,7 +5,8 @@ enum class ErrorMessage(private val message: String) {
     INPUT_WINNER_NUMBER_COUNT_ERROR("당첨 번호는 6개의 숫자여야 합니다."),
     INPUT_WINNER_NUMBER_OUT_OF_RANGE_ERROR("당첨 번호는 1부터 45사이에 숫자여야 합니다."),
     INPUT_WINNER_NUMBER_NO_DUPLICATE("당첨 번호는 중복되지 않은 숫자여야 합니다."),
-    LOTTO_NUMBER_NO_DUPLICATE("로또 번호는 중복되지 않은 숫자여야 합니다.");
+    LOTTO_NUMBER_NO_DUPLICATE("로또 번호는 중복되지 않은 숫자여야 합니다."),
+    LOTTO_NUMBER_COUNT_ERROR("로또 번호는 6개여야 합니다.");
 
     fun getMessage(): String = "[ERROR] $message"
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,7 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == LottoMaker.NUMBERS_COUNT) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LottoMaker.NUMBERS_COUNT) { ErrorMessage.LOTTO_NUMBER_COUNT_ERROR }
         require(numbers.toSet().size == numbers.size) { ErrorMessage.LOTTO_NUMBER_NO_DUPLICATE }
     }
 
