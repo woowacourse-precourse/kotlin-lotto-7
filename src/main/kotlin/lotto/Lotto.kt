5a984977f626ebf1d493323c99e05013d64265e4
dioo1461 <dioo1461@gmail.com>
2024-11-03 18:30:28 +0900

    feat(Lotto.kt): 로또 당첨 등수를 Enum 클래스로 관리하도록 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,16 +2,25 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
 
+enum class LottoRank {
+    NONE,
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH
+}
+
 class Lotto(private val numbers: List<Int>) {
-    fun getLottoResultAsRank(winningNumbers: List<Int>, bonusNumber: Int): Int {
+    fun getLottoResultAsRank(winningNumbers: List<Int>, bonusNumber: Int): LottoRank {
         val matchCount = numbers.count { it in winningNumbers }
 
         return when (matchCount) {
-            6 -> 1
-            5 -> if (bonusNumber in numbers) 2 else 3
-            4 -> 4
-            3 -> 5
-            else -> 6
+            6 -> LottoRank.FIRST
+            5 -> if (bonusNumber in numbers) LottoRank.SECOND else LottoRank.THIRD
+            4 -> LottoRank.FOURTH
+            3 -> LottoRank.FIFTH
+            else -> LottoRank.NONE
         }
     }
 
