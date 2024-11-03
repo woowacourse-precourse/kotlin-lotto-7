f6707bc2ec1c3dc371228f7df22a5ee9a499aeb5
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-04 00:15:33 +0900

    feat(OutputView): 로또 당첨 내역을 출력

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -7,8 +7,11 @@ import lotto.domain.model.PurchaseInfo
 
 object OutputView {
     private const val ISSUE_QUICK_PICK_LOTTO = "개를 구매했습니다."
+    private const val LOTTO_PRIZE = "\n당첨 통계"
+    private const val DASH = "---"
     private const val PROFIT_RATE = "총 수익률은 %.2f%%입니다."
-
+    private const val PROFIT_RATE_SIMPLE = "총 수익률은 %.1f%%입니다."
+    private const val ZERO = 0
 
 
     fun showPurchaseInfo(purchaseInfo: PurchaseInfo, quickPickLottoTickets: List<Lotto>) {
@@ -19,8 +22,26 @@ object OutputView {
         println()
     }
 
+    fun showCalculateInfo(matchResult: List<Prize>) {
+        println(LOTTO_PRIZE)
+        println(DASH)
+        val prizeCount = matchResult.groupingBy { it }.eachCount()
+        Prize.entries.filter { it != Prize.NONE }.forEach { prize ->
+            val count = prizeCount[prize] ?: ZERO
+            val result = when (prize) {
+                Prize.SECOND -> "5개 일치, 보너스 볼 일치 (${prize.prizeAmount}원) - ${count}개"
+                else -> "${prize.matchingNumberCount}개 일치 (${prize.prizeAmount}원) - ${count}개"
+            }
+            println(result)
+        }
+    }
+
     fun showProfit(profitRate: Double) {
-        val message = String.format(PROFIT_RATE, profitRate)
+        val message = if (profitRate * 10 % 1 == 0.0) {
+            String.format(PROFIT_RATE_SIMPLE, profitRate)
+        } else {
+            String.format(PROFIT_RATE, profitRate)
+        }
         println(message)
     }
 }
