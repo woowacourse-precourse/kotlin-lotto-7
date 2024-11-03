e740a54f11ace55dfbbc11877cd028809f71edb2
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-04 00:51:25 +0900

    chore(Controller): 상단 로직 주석 제거

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -7,17 +7,6 @@ import model.WinningLotto
 import view.InputView
 import view.OutputView
 
-/* LottoController 설계
-* 1. 구입 금액 입력
-* 2. 구매 개수 출력
-* 3. 로또 번호 생성
-* 4. 로또 번호 출력
-* 5. 당첨 번호 입력
-* 6. 보너스 번호 입력
-* 7. 당첨 통계 계산
-* 8. 당첨 결과 출력
-*
-* */
 class LottoController {
 
     fun playLottoGame() {
@@ -25,20 +14,15 @@ class LottoController {
         val lottoCount = money / DECIMAL
 
         val lottoes = LottoMachine.issueLottoes(lottoCount)
-
         OutputView.printLotto(lottoCount, lottoes)
 
         val winningLotto = getWinningLotto()
         calculatePrize(lottoes, winningLotto)
-
         OutputView.printWinningStatistics()
 
         val earningMoney = calculateEarningMoney()
-
         val earningRate = getEarningRate(earningMoney, money)
-
         OutputView.printEarningRate(earningRate)
-
     }
 
     private fun getWinningLotto(): WinningLotto {
@@ -71,6 +55,7 @@ class LottoController {
                 }
                 Prize.THIRD.plusCount()
             }
+
             4 -> Prize.FOURTH.plusCount()
             3 -> Prize.FIFTH.plusCount()
         }
