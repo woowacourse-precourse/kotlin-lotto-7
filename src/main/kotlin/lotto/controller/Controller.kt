42216d199864d8f943168860f2fadab6206536f1
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-11-03 16:29:51 +0900

    feat(Controller): 사용자의 입력 및 예외 상황 시 재 입력 구현 - Valitdator 메서드 수정

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -1,7 +1,22 @@
 package lotto.controller
 
+import lotto.utils.Validator
+import lotto.viewm.InputView
+
+
 object Controller {
+    val validate = Validator()
     fun run() {
+        while (true) {
+            try {
+                validate.validateInputAmount(InputView.inputPurchaseAmount())
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+
+        }
+
     }
 
 }
--- src/main/kotlin/lotto/utils/Validator.kt
+++ src/main/kotlin/lotto/utils/Validator.kt
@@ -6,7 +6,9 @@ class Validator {
     fun validateInputAmount(input: String) {
         require(input.isNotEmpty()) { INPUT_EMPTY.message }
 
-        val amount = input.toIntOrNull() ?: throw error(ONLY_NUMBER.message)
+        require(input.all { it.isDigit() }) { ONLY_NUMBER.message }
+
+        val amount = input.toInt()
 
         require(amount % 1000 == 0) { NOT_MULTIPLE_OF_1000.message }
     }
