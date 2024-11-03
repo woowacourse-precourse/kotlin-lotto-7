a0be84451907006c6bd9122d0d36dd23fe90b0c0
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-03 20:31:18 +0900

    refactor(BonusNumberErrorType): 한국어 문법 수정과 Error -> ERROR 수정

--- src/main/kotlin/lotto/util/validator/bonusnumber/BonusNumberErrorType.kt
+++ src/main/kotlin/lotto/util/validator/bonusnumber/BonusNumberErrorType.kt
@@ -3,9 +3,9 @@ package lotto.util.validator.bonusnumber
 enum class BonusNumberErrorType(
     private val _errorMessage: String
 ){
-    EMPTY_INPUT("보너스 번호는 빈 문자를 입력하면 안됩니다."),
-    NOT_DECIMAL("보너스 번호는 소수를 입력하면 안됩니다."),
-    NOT_INTEGER("보너스 번호는 정수 외 다른것을 입력하면 안됩니다."),
+    EMPTY_INPUT("보너스 번호는 빈 문자를 입력하면 안 됩니다."),
+    NOT_DECIMAL("보너스 번호는 소수를 입력하면 안 됩니다."),
+    NOT_INTEGER("보너스 번호는 정수 외 다른 것을 입력하면 안 됩니다."),
     BONUS_NUMBER_RANGE("보너스 번호는 1부터 45까지 가능합니다."),
     NO_DUPLICATE_LUCKY_NUMBERS("당첨 번호와 중복된 숫자가 있습니다.");
 
@@ -13,6 +13,6 @@ enum class BonusNumberErrorType(
         get() = ERROR_MESSAGE + _errorMessage
 
     companion object {
-        private const val ERROR_MESSAGE = "[Error] "
+        private const val ERROR_MESSAGE = "[ERROR] "
     }
 }
