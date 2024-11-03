48f6fbc3090eaf16d5d360992a296c6554146849
yeonjeen <jeendev2019@gmail.com>
2024-11-03 23:52:52 +0900

    refactor(Validator.kt): 구매금액 단위에러 메세지 수정 - '[' 추가했습니다.

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -12,7 +12,7 @@ class Validator {
     }
 
     companion object {
-        private const val ERROR_INCORRECT_UNIT = "ERROR] 구입 금액은 1,000원 단위여야 합니다."
+        private const val ERROR_INCORRECT_UNIT = "[ERROR] 구입 금액은 1,000원 단위여야 합니다."
         private const val ERROR_UNVALIDATED_NUMBER = "[ERROR] 숫자를 올바르게 입력해 주세요."
     }
 }
