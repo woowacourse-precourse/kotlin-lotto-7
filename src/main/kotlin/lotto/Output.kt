42704a6fec87e140c5a7d487001a921a1bf681a8
jiwon <pjw99032986@gmail.com>
2024-11-04 00:52:34 +0900

    Feat(Input.kt) : 당첨번호 입력
    당첨 번호를 입력 받는다. 번호는 쉼표(,)를 기준으로 구분한다.

--- src/main/kotlin/lotto/Output.kt
+++ src/main/kotlin/lotto/Output.kt
@@ -1,21 +1,40 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Console
-import java.lang.NumberFormatException
+import lotto.CustomException.*
+import lotto.LottoType.BONUS
+import lotto.LottoType.LOTTO
 
 class Output {
 
-    fun output(message : String) = println(message)
-    fun outPutMoney(){
-        println("구입금액을 입력해 주세요.\n")
+    companion object {
+        const val BONUSBALL = 3
+        const val PLUSCORRECTNUM = 3
+        const val BONUSCORRECT = 5
+
     }
 
-    fun outPutTicket(ticket : Int, lottoList : List<List<Int>>){
-        println("${ticket}개를 구매했습니다.\n")
-        for(list in lottoList){
+    fun output(message: String) = println(message)
+    fun outPutMoney() {
+        println("구입금액을 입력해 주세요.")
+    }
+
+    fun outPutTicket(ticket: Int, lottoList: List<List<Int>>) {
+        println("\n${ticket}개를 구매했습니다.")
+        for (list in lottoList) {
             println(list)
         }
-        println()
     }
+
+    fun printCustomMessage(customException: CustomException) {
+        when (customException) {
+            DIVIDE -> throw IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.")
+            NUMBER -> throw IllegalArgumentException("[ERROR] 티켓은 양의 정수만 입력할 수 있습니다.")
+            INTEGER -> throw IllegalArgumentException("[ERROR] 티켓은 1000원 단위로만 입력할 수 있습니다.")
+            BLANK -> throw IllegalArgumentException("[ERROR] 로또 번호가 비어있습니다.")
+            BOUNDARY -> throw IllegalArgumentException("[ERROR] 로또 번호는 1이상 45이하의 정수여야 합니다.")
+        }
+    }
+
+    fun outPutCorrectNumber() = println("\n당첨 번호를 입력해 주세요.")
 
 }
