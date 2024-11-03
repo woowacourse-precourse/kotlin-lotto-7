8169e3595ff7841d5f4a2869aa0b7d5d0d6f423d
jklom0326 <jklom0326@gmail.com>
2024-11-04 00:53:54 +0900

    refactor: 로또 번호 유효 범위 검사 및 메시지 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,17 +4,17 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(isLottoNumberSize()) { "$ERROR $LOTTO_SIZE_MESSAGE" }
         require(isDuplicate()) { "$ERROR $LOTTO_NUMBER_DUPLICATE_MESSAGE" }
-        numbers.forEach {
-            require(isNumberInRange(it)) { "$ERROR $LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE" }
-        }
-        require(numbers == numbers.sorted()) {"$ERROR $LOTTO_NUMBER_NOT_ASCENDING"}
+        require(isNumberInRange()) { "$ERROR $LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE" }
+        require(isSorted()) { "$ERROR $LOTTO_NUMBER_NOT_ASCENDING" }
     }
 
     private fun isLottoNumberSize() = numbers.size == LOTTO_SIZE
 
-    private fun isNumberInRange(it: Int) = it in MIN_NUM..MAX_NUM
-
     private fun isDuplicate() = numbers.distinct().size == numbers.size
+
+    private fun isNumberInRange() = numbers.all { it in MIN_NUM..MAX_NUM }
+
+    private fun isSorted() = numbers == numbers.sorted()
 
     companion object {
         const val LOTTO_SIZE = 6
@@ -24,7 +24,7 @@ class Lotto(private val numbers: List<Int>) {
         const val ERROR = "[ERROR]"
         const val LOTTO_SIZE_MESSAGE = "로또 번호는 6개여야 합니다."
         const val LOTTO_NUMBER_DUPLICATE_MESSAGE = "로또 번호는 중복될 수 없습니다."
-        const val LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE = "로또 번호는 중복될 수 없습니다."
+        const val LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE = "로또 번호는 1부터 45사이의 숫자여야 합니다."
         const val LOTTO_NUMBER_NOT_ASCENDING = "로또 번호가 오름차순 정렬되지 않았습니다."
     }
 }
