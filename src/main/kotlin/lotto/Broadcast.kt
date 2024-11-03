4a94823381856a1ec6ed5fcc1fbfa549c842c87f
jklom0326 <jklom0326@gmail.com>
2024-11-04 02:46:06 +0900

    feat: 당첨 번호 입력 메시지 출력 기능 구현

--- src/main/kotlin/lotto/Broadcast.kt
+++ src/main/kotlin/lotto/Broadcast.kt
@@ -16,8 +16,13 @@ class Broadcast {
         println("\n$count$PURCHASE_LOTTO_COUNT_MESSAGE")
     }
 
+    fun printWinningNumberMessage() {
+        println("\n$WINNING_NUMBER_MESSAGE")
+    }
+
     companion object {
         const val PURCHASE_PAYMENT_MESSAGE = "구입금액을 입력해 주세요."
         const val PURCHASE_LOTTO_COUNT_MESSAGE = "개를 구매했습니다."
+        const val WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요."
     }
 }
