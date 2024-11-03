a2420ad69638c4130070097533634adbd98f2864
46112 <duf325@naver.com>
2024-11-03 13:35:37 +0900

    feat(Application): 구입 금액 입력값 검증 및 예외 처리 추가
    - 구입 금액이 1,000원 단위로 입력되지 않을 경우 예외를 발생시키도록 require 함수를 사용한 검증 로직 추가
    - NumberFormatException catch

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import java.lang.Exception
+import java.lang.NumberFormatException
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("구입금액을 입력해 주세요.")
+    try {
+        val purchaseAmount = Console.readLine().toInt()
+        require(purchaseAmount % 1000 == 0) { "[ERROR] 로또는 1,000원 단위로만 구매할 수 있습니다." }
+    } catch (e: NumberFormatException) {
+        println("[ERROR] 유효한 숫자를 입력해 주세요.")
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    } catch (e: Exception) {
+        println("[ERROR] " + e.message)
+    }
 }
