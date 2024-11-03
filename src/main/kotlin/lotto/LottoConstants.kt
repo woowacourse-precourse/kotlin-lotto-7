92e473b6504b70d73d517315feb74f2206e6aaa0
yoon <cbj0010@naver.com>
2024-11-04 01:44:58 +0900

    feat : 로또 상수 구현

---
+++ src/main/kotlin/lotto/LottoConstants.kt
@@ -0,0 +1,9 @@
+package lotto
+
+object LottoConstants {
+    const val LOTTO_PRICE = 1000
+    const val MINIMUM_PRICE = 0
+    const val LOTTO_NUMBER_SIZE = 6
+    const val LOTTO_NUMBER_MIN = 1
+    const val LOTTO_NUMBER_MAX = 45
+}
