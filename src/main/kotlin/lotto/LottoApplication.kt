f8edd631d9fe4704a6fd59a90e5c65c604b20447
Tnalxmsk <alstnwkd990@naver.com>
2024-11-03 20:33:26 +0900

    feat: 로또 app 전체 실행 구현

--- src/main/kotlin/lotto/LottoApplication.kt
+++ src/main/kotlin/lotto/LottoApplication.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.model.Lotto
+import lotto.model.LottoResultCalculator
 import lotto.model.LottoSeller
 import lotto.presenter.InputPresenter
 import lotto.view.InputViewImpl
@@ -11,9 +13,23 @@ class LottoApplication {
         val inputView = InputViewImpl()
         val outputView = OutputViewImpl()
         val inputPresenter = InputPresenter(inputView, outputView)
+
         val amount = inputPresenter.onPurchaseAmountInput()
+
         val lottoSeller = LottoSeller(amount)
         val lottoBundle = lottoSeller.sell()
         outputView.printPurchaseSummary(lottoBundle)
+
+        val winningNumbers = inputPresenter.onWinningNumbersInput()
+        val winningLotto = Lotto(winningNumbers)
+
+        val bonusNumber = inputPresenter.onBonusNumberInput()
+
+        val lottoResultCalculator = LottoResultCalculator(winningLotto, bonusNumber)
+        val result = lottoResultCalculator.countMatchingNumber(lottoBundle)
+        outputView.printWinningStatistics(result)
+        val totalReward = lottoResultCalculator.calculateTotalReward()
+        val profitRate = lottoResultCalculator.calculateProfitRate(lottoBundle.size, totalReward)
+        outputView.printProfitRate(profitRate)
     }
 }
