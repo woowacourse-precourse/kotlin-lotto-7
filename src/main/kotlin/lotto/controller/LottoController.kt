2a474f718cea22577c5d22bd763c965e076b93bc
eunyoung kim <kermit.eea@gmail.com>
2024-11-03 17:00:25 +0900

    feat: 당첨 번호, 보너스 번호 입력 및 유효성 검사 로직 구현
    - 사용자로부터 당첨 번호를 입력받고 유효성을 검사받는 getWinningNumbers() 함수 구현
    - 당첨 번호가 1~45 사이의 숫자 6개인지 확인하는 validateWinningNumbers() 함수 구현
    - 보너스 번호를 입력받고 유효성을 검사받는 getBonusNumber() 함수 구현
    - 보너스 번호가 당첨 번호와 중복되지 않으며 1~45 사이의 숫자인지 확인하는 validateBonusNumber() 함수 구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -9,6 +9,10 @@ class LottoController {
         val purchaseAmount = getPurchaseAmount()
         val lottos = purchaseLottos(purchaseAmount)
         printLottos(lottos)
+        val winningNumbers = getWinningNumbers()
+        val bonusNumber = getBonusNumber(winningNumbers)
+        println(winningNumbers)
+        println(bonusNumber)
     }
 }
 
@@ -26,17 +30,55 @@ fun getPurchaseAmount(): Int {
 }
 
 fun validatePurchaseAmount(amount: Int) {
-    if (amount % 1000 != 0 || amount <= 0 || amount > 1_000_000) {
+    if (amount % 1000 != 0 || amount < 1000 || amount > 1_000_000) {
         throw IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 1,000,000원 이하이어야 합니다.")
     }
 }
 
 fun purchaseLottos(amount: Int): List<Lotto> {
-    val numberOfTickets = amount / 1000
-    return List(numberOfTickets) { Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)) }
+    val numberOfLottos = amount / 1000
+    return List(numberOfLottos) { Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)) }
 }
 
 fun printLottos(lottos: List<Lotto>) {
     println("${lottos.size}개를 구매했습니다.")
     lottos.forEach { println(it.getNumbers().sorted()) }
+}
+
+fun getWinningNumbers(): Set<Int> {
+    println("당첨 번호를 입력해 주세요.")
+    val input = Console.readLine()
+    return try {
+        val numbers = input.split(",").map { it.trim().toInt() }.toSet()
+        validateWinningNumbers(numbers)
+        numbers
+    } catch (e: Exception) {
+        println("[ERROR] 당첨 번호는 1~45 사이의 숫자 6개를 중복 없이 입력해야 합니다.")
+        getWinningNumbers()
+    }
+}
+
+fun validateWinningNumbers(numbers: Set<Int>) {
+    if (numbers.size != 6 || numbers.any { it !in 1..45 }) {
+        throw IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이의 숫자 6개여야 합니다.")
+    }
+}
+
+fun getBonusNumber(winningNumbers: Set<Int>): Int {
+    println("보너스 번호를 입력해 주세요.")
+    val input = Console.readLine()
+    return try {
+        val bonusNumber = input.toInt()
+        validateBonusNumber(bonusNumber, winningNumbers)
+        bonusNumber
+    } catch (e: Exception) {
+        println("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 숫자여야 합니다.")
+        getBonusNumber(winningNumbers)
+    }
+}
+
+fun validateBonusNumber(bonusNumber: Int, winningNumbers: Set<Int>) {
+    if (bonusNumber !in 1..45 || winningNumbers.contains(bonusNumber)) {
+        throw IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 숫자여야 합니다.")
+    }
 }
