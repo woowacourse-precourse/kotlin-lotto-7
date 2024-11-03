e196bfba3b0f9d4841cfa33ef079eda73cb7ff75
wjdrjs00 <kimjk3409@gmail.com>
2024-11-03 16:55:19 +0900

    test(domain): Lotto 클래스에 대한 단위 테스트 구현
    로또 숫자 리스트에 대한 유효성 검사 테스트 구현:
    - 빈 리스트 검증
    - 리스트 사이즈가 6초과 검사
    - 리스트 사지즈가 6미만 검사
    - 중복 요소값 검사
    정상 케이스 테스트 구현:
    - 올바른 숫자 리스트 반환 검증

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,23 +1,78 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
+
+    @Test
+    fun `로또 번호가 비어있는 경우 예외가 발생한다`() {
+        // Given
+        val numbers = emptyList<Int>()
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+
+        // Then
+        assertEquals("[ERROR] 로또 번호는 비어있을 수 없습니다.", exception.message)
+    }
+
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 6, 7)
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
         }
+
+        // Then
+        assertEquals("[ERROR] 로또 번호는 6개여야 합니다.", exception.message)
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+    @Test
+    fun `로또 번호의 개수가 6개 보다 작으면 예외가 발생한다`() {
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5)
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+
+        // Then
+        assertEquals("[ERROR] 로또 번호는 6개여야 합니다.", exception.message)
+    }
+
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 5)
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
         }
+
+        // Then
+        assertEquals("[ERROR] 로또 번호는 중복될 수 없습니다.", exception.message)
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `올바른 숫자 리스트가 주어진 경우 올바른 값을 반환한다`() {
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+
+        // When
+        val lotto = Lotto(numbers).getLotto()
+
+        // Then
+        assertThat(lotto).isEqualTo(numbers)
+    }
+
 }
