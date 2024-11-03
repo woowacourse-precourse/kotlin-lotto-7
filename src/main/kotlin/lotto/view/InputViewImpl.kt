f22285acdc464cebe927fdaf9cfa4866dd22c75d
Tnalxmsk <alstnwkd990@naver.com>
2024-11-03 20:45:16 +0900

    refactor: InputView 개선
    문자열 상수화

--- src/main/kotlin/lotto/view/InputViewImpl.kt
+++ src/main/kotlin/lotto/view/InputViewImpl.kt
@@ -5,17 +5,23 @@ import camp.nextstep.edu.missionutils.Console
 class InputViewImpl : InputView {
 
     override fun readPurchaseAmount(): String {
-        println("구입금액을 입력해 주세요.")
+        println(PURCHASE_AMOUNT_PROMPT)
         return Console.readLine()
     }
 
     override fun readWinningNumbers(): String {
-        println("당첨 번호를 입력해 주세요.")
+        println(WINNING_NUMBERS_PROMPT)
         return Console.readLine()
     }
 
     override fun readBonusNumber(): String {
-        println("보너스 번호를 입력해 주세요.")
+        println(BONUS_NUMBER_PROMPT)
         return Console.readLine()
+    }
+
+    companion object {
+        const val PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요."
+        const val WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요."
+        const val BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요."
     }
 }
