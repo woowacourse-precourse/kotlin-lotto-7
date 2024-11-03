ca27f0fa1eac44042562a8ff99aa40094508f09f
Grove1212 <stella223@korea.ac.kr>
2024-11-03 21:27:41 +0900

    feat: impliment lotto generation based on purchase amount

--- src/main/kotlin/lotto/Store.kt
+++ src/main/kotlin/lotto/Store.kt
@@ -3,7 +3,14 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 object Store {
-    //fun buyLotto(money: Int)
+    fun buyLotto(money: Int): List<Lotto> {
+        val numberOfLottoPurchased = money/1000
+        val purchasedLottos = mutableListOf<Lotto>()
+        repeat(numberOfLottoPurchased){
+            purchasedLottos.add(generateLotto())
+        }
+        return purchasedLottos
+    }
 
     fun generateLotto(): Lotto {
         var numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
--- src/test/kotlin/lotto/StoreTest.kt
+++ src/test/kotlin/lotto/StoreTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.assertj.core.api.Assertions.assertThat
 
 class StoreTest {
     val lotto = Store.generateLotto()
@@ -10,6 +11,11 @@ class StoreTest {
         assertDoesNotThrow {
             lotto
         }
+    }
+
+    @Test
+    fun `2000원 냈을 때 로또 2개를 반환하는지 검사한다`() {
+        assertThat(Store.buyLotto(2000)).hasSize(2)
     }
 
 }
