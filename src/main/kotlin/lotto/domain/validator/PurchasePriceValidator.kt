3caa8347208a89e6d94da92bbde60afec2572d50
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-03 18:05:00 +0900

    feat(PurchasePriceValidator): 구매 금액 최소값 수정
    - 구매 최소값을 0원에서 1000원으로 수정
    
    게임을 진행하기 위해서 최소 1개 이상에 로또를 구매해야 하기 때문에

--- src/main/kotlin/lotto/domain/validator/PurchasePriceValidator.kt
+++ src/main/kotlin/lotto/domain/validator/PurchasePriceValidator.kt
@@ -5,21 +5,21 @@ import lotto.common.LOTTO_PRICE
 class PurchasePriceValidator : NumberValidator() {
     fun validateMoney(money: Int) {
         validateMultipleOfThousand(money)
-        validateNonNegative(money)
+        validateUnderMinMoney(money)
     }
 
     private fun validateMultipleOfThousand(money: Int) {
         require(money.rem(LOTTO_PRICE) == REMAINDER_ZERO) { ERROR_MULTIPLE_OF_THOUSAND }
     }
 
-    private fun validateNonNegative(money: Int) {
+    private fun validateUnderMinMoney(money: Int) {
         require(money >= MIN_MONEY) { ERROR_NON_NEGATIVE }
     }
 
     companion object {
         private const val REMAINDER_ZERO = 0
         private const val ERROR_MULTIPLE_OF_THOUSAND = "[ERROR] 돈은 1000원에 배수여야 합니다."
-        private const val MIN_MONEY = 0
-        private const val ERROR_NON_NEGATIVE = "[ERROR] 돈은 0보다 같거나 커야 합니다."
+        private const val MIN_MONEY = 1000
+        private const val ERROR_NON_NEGATIVE = "[ERROR] 돈은 1000보다 같거나 커야 합니다."
     }
 }
