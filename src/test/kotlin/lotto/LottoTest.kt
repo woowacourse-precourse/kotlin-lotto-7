e507163c6613c5be4f7fa1c0bf3e06f9172004b2
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-03 23:36:45 +0900

    test(Lotto): 각 당첨내역 테스트 및 총 수입금액 계산 테스트 추가 및 성공

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -42,6 +42,18 @@ class LottoTest {
     }
 
     @Test
+    fun `로또 1등 당첨 여부를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)), 7)
+        val matchCount = lotto.matchCount(winningLotto)
+        val isMatchBonus = lotto.isMatchBonus(winningLotto)
+
+        LottoController().updatePrize(matchCount, isMatchBonus)
+
+        assertEquals(1, Prize.FIRST.winningCount)
+    }
+
+    @Test
     fun `로또 2등 당첨 여부를 반환한다`() {
         val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
         val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 7)), 6)
@@ -50,5 +62,60 @@ class LottoTest {
         LottoController().updatePrize(5, isMatchBonus)
 
         assertEquals(1, Prize.SECOND.winningCount)
+    }
+
+    @Test
+    fun `로또 3등 당첨 여부를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 7)), 8)
+        val matchCount = lotto.matchCount(winningLotto)
+        val isMatchBonus = lotto.isMatchBonus(winningLotto)
+
+        LottoController().updatePrize(matchCount, isMatchBonus)
+
+        assertEquals(1, Prize.THIRD.winningCount)
+    }
+
+    @Test
+    fun `로또 4등 당첨 여부를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 8, 9)), 8)
+        val matchCount = lotto.matchCount(winningLotto)
+        val isMatchBonus = lotto.isMatchBonus(winningLotto)
+
+        LottoController().updatePrize(matchCount, isMatchBonus)
+
+        assertEquals(1, Prize.FOURTH.winningCount)
+    }
+
+    @Test
+    fun `로또 5등 당첨 여부를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 8, 9, 11)), 10)
+        val matchCount = lotto.matchCount(winningLotto)
+        val isMatchBonus = lotto.isMatchBonus(winningLotto)
+
+        LottoController().updatePrize(matchCount, isMatchBonus)
+
+        assertEquals(1, Prize.FIFTH.winningCount)
+    }
+
+
+
+    @Test
+    fun `총 수익금액을 계산한다`(){
+        val lottoes = listOf(
+//            Lotto(listOf(1, 2, 3, 4, 5, 8)), // 3등
+            Lotto(listOf(1, 2, 3, 41, 45, 36)), // 5등
+            Lotto(listOf(1, 2, 3, 14, 35, 46)), // 5등
+            Lotto(listOf(1, 2, 3, 4, 15, 36)), // 4등
+        )
+
+        val winningLotto = WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)), 7)
+        LottoController().calculatePrize(lottoes, winningLotto)
+        val earningMoney = LottoController().calculateEarningMoney()
+
+        assertEquals(60000, earningMoney)
+
     }
 }
