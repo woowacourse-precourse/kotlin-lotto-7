d61c653edf4ec38b0934b84d6328ea4c3266aaab
jklom0326 <jklom0326@gmail.com>
2024-11-04 00:42:16 +0900

    refactor: 유효성 검사 조건 함수로 분리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,14 +2,21 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "$ERROR $LOTTO_SIZE_MESSAGE" }
-        require(numbers.distinct().size == numbers.size) { "$ERROR $LOTTO_NUMBER_DUPLICATE_MESSAGE" }
+        require(isLottoNumberSize()) { "$ERROR $LOTTO_SIZE_MESSAGE" }
+        require(isDuplicate()) { "$ERROR $LOTTO_NUMBER_DUPLICATE_MESSAGE" }
         numbers.forEach {
-            require(it in MIN_NUM..MAX_NUM) { "$ERROR $LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE" }
+            require(isNumberInRange(it)) { "$ERROR $LOTTO_NUMBER_NOT_IN_RANGE_MESSAGE" }
         }
     }
 
+    private fun isLottoNumberSize() = numbers.size == LOTTO_SIZE
+
+    private fun isNumberInRange(it: Int) = it in MIN_NUM..MAX_NUM
+
+    private fun isDuplicate() = numbers.distinct().size == numbers.size
+
     companion object {
+        const val LOTTO_SIZE = 6
         const val MIN_NUM = 1
         const val MAX_NUM = 45
 
