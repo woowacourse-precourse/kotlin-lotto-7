6406b66ae78d538762402d14e9f0b4e4fcf64671
jklom0326 <jklom0326@gmail.com>
2024-11-04 03:02:59 +0900

    feat: 보너스 번호 입력 메시지 출력 기능 구현

--- src/main/kotlin/lotto/Broadcast.kt
+++ src/main/kotlin/lotto/Broadcast.kt
@@ -20,9 +20,14 @@ class Broadcast {
         println("\n$WINNING_NUMBER_MESSAGE")
     }
 
+    fun printInputBonusNumberMessage() {
+        println("\n$INPUT_BONUS_NUMBER_MESSAGE")
+    }
+
     companion object {
         const val PURCHASE_PAYMENT_MESSAGE = "구입금액을 입력해 주세요."
         const val PURCHASE_LOTTO_COUNT_MESSAGE = "개를 구매했습니다."
         const val WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요."
+        const val INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요."
     }
 }
