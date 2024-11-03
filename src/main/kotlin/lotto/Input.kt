1b84c6a0d07acd06b3ba142a72de7665bf37efab
jiwon <pjw99032986@gmail.com>
2024-11-03 14:42:11 +0900

    Feat(Input.kt) : 발행한 티켓만큼 로또 번호 생성
    발행한 티켓의 수 만큼 로또 번호를 발행해주는 함수

--- src/main/kotlin/lotto/Input.kt
+++ src/main/kotlin/lotto/Input.kt
@@ -1,26 +1,31 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 class Input {
     var ticket = 0
-    lateinit var lotto: List<List<Int>>
     val output = Output()
+    lateinit var lottos: MutableList<MutableList<Int>>
+    lateinit var correctLotto: List<String>
+    lateinit var correctIntLotto: List<Int>
+    lateinit var lotto: Lotto
 
-    private fun input(): String {
+    fun input(): String {
         return Console.readLine()
     }
 
 
-    fun inputMoney() {
+    fun inputMoney(input: String) {
         while (true) {
             try {
-                val input = input()
                 val money = confirmInteger(input)
+                confirmNullOrBlank(input)
                 confirmNegativeOrZero(money)
                 ticket = confirmDivider(money)
                 return
-            } catch (e: IllegalArgumentException) {
+            } catch (e: CustomErrorHandler) {
+                output.output("\n" + e.message + "\n")
                 output.outPutMoney()
                 continue
             }
@@ -33,7 +38,7 @@ class Input {
         try {
             intMoney = money.toInt()
         } catch (e: NumberFormatException) {
-            throw CustomErrorHandler("[ERROR] 숫자만 입력할 수 있습니다.",CustomException.NUMBER)
+            throw CustomErrorHandler("[ERROR] 숫자만 입력할 수 있습니다.", CustomException.NUMBER)
         }
         return intMoney
     }
@@ -41,16 +46,35 @@ class Input {
     //금액이 0이거나 음수인 경우
     private fun confirmNegativeOrZero(money: Int) {
         if (money <= 0) {
-            throw CustomErrorHandler("[ERROR] 티켓은 양의 정수만 입력할 수 있습니다.",CustomException.INTEGER)
+            throw CustomErrorHandler("[ERROR] 티켓은 양의 정수만 입력할 수 있습니다.", CustomException.INTEGER)
         }
     }
 
     //티켓이 1000원 단위로 나누어 떨어 지지 않는 경우
     private fun confirmDivider(ticket: Int): Int {
         if (ticket % 1000 != 0) {
-            throw CustomErrorHandler("[ERROR] 티켓은 1000원 단위로만 입력할 수 있습니다.",CustomException.DIVIDE)
+            throw CustomErrorHandler("[ERROR] 티켓은 1000원 단위로만 입력할 수 있습니다.", CustomException.DIVIDE)
         }
         return ticket / 1000
     }
+
+    //빈 값
+    private fun confirmNullOrBlank(inputNumber: String) {
+        if (inputNumber.isBlank()) {
+            throw CustomErrorHandler("[ERROR] 로또 번호가 비어있습니다.", CustomException.BLANK)
+        }
+    }
+
+    //구입한 만큼 로또 번호생성
+    fun buyTicket() {
+        lottos = mutableListOf()
+        for (i in 0 until ticket) {
+            lottos.add(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+        }
+        output.outPutTicket(ticket, lottos)
+    }
+
+
+
 
 }
