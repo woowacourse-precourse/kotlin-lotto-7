a38c932a9407c17f88b5604b5f77f87d4b75d8b1
jklom0326 <jklom0326@gmail.com>
2024-11-04 04:50:05 +0900

    refactor: 당첨 번호 정보 조건 변경
    - MISS 삭제 (사용하지 않음)
    - 2등 조건만 세줘서 나머지 등급이 카운팅 안되는 현상 수정

--- src/main/kotlin/lotto/Rank.kt
+++ src/main/kotlin/lotto/Rank.kt
@@ -10,11 +10,14 @@ enum class Rank(
     THIRD_RANK(5, 1_500_000.0),
     FOURTH_RANK(4, 50_000.0),
     FIFTH_RANK(3, 5_000.0),
-    MISS(0, 0.0);
+    ;
 
     companion object {
         fun matchRankInfo(count: Int, bonus: Boolean): Rank? {
-            return entries.find { it.winningCount == count && it.bonusChance == bonus }
+            return entries.find {
+                it.winningCount == count
+                        && (it.bonusChance == bonus || !it.bonusChance)
+            }
         }
     }
 }
