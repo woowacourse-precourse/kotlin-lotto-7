8eebe596d02ad1a2a382124cac120ceadffbdd43
gahyunkim <mapledt001@naver.com>
2024-11-03 16:56:59 +0900

    feat: implement lotto issuance and display

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 enum class LottoError(val message: String) {
     INVALID_AMOUNT("[ERROR] 유효한 금액을 입력해주세요."),
@@ -12,19 +13,21 @@ enum class LottoError(val message: String) {
 }
 
 fun main() {
-    readAndValidateAmount()
+    val amount = readAndValidateAmount()
+    val numberOfLottos = calculateNumberOfLottos(amount)
+    println("${numberOfLottos}개를 구매했습니다.")
+    generateLottos(numberOfLottos)
 }
 
-fun readAndValidateAmount() {
+fun readAndValidateAmount(): Int {
     while (true) {
-        val amount = try {
-            promptUserForAmount()
+        try {
+            val amount = promptUserForAmount()
+            Validation.validateAmount(amount) // 이 부분도 try 블록 안에 포함됨
+            return amount
         } catch (e: IllegalArgumentException) {
             println(e.message)
-            continue
         }
-        Validation.validateAmount(amount)
-        return
     }
 }
 
@@ -32,4 +35,20 @@ fun promptUserForAmount(): Int {
     println("구입금액을 입력해 주세요.")
     val input = Console.readLine()?.trim()
     return Validation.parseAmount(input)
+}
+
+fun calculateNumberOfLottos(amount:Int):Int{
+    println()
+    return amount /1000
+}
+
+fun generateLottos(count:Int){
+    repeat(count) {
+        val lottoNumbers = generateRandomLottoNumbers()
+        println(lottoNumbers)
+    }
+}
+
+fun generateRandomLottoNumbers(): List<Int> {
+    return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
 }
