b331bdee95a2bdb3e8afaeb968a1dcadcf07dabd
ijh1298 <imjh0417@gmail.com>
2024-11-03 23:44:55 +0900

    feat: Lotto 번호 범위 검사 로직 추가

--- src/main/kotlin/lotto/domain/entity/Lotto.kt
+++ src/main/kotlin/lotto/domain/entity/Lotto.kt
@@ -4,6 +4,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.toSet().size == 6) { HAS_DUPLICATE_NUMBER_EXCEPTION_MSG }
+        require(numbers.all { it in 1..45 }) { SHOULD_BE_IN_RANGE_EXCEPTION_MSG }
     }
 
     fun getNumbers() = numbers
@@ -16,5 +17,6 @@ class Lotto(private val numbers: List<Int>) {
         fun List<Int>.toLottoNumbers(): Lotto = Lotto(this)
 
         private const val HAS_DUPLICATE_NUMBER_EXCEPTION_MSG = "[ERROR] 중복된 로또 번호가 없어야 합니다."
+        private const val SHOULD_BE_IN_RANGE_EXCEPTION_MSG = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
     }
 }
