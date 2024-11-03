3943eb32e180d4fc70e294d6e3efb29f2a1ff9ca
yeonjeen <jeendev2019@gmail.com>
2024-11-04 07:32:29 +0900

    test(LottoTest.kt): 기능 단위 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.model.Lotto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -11,7 +13,13 @@ class LottoTest {
         }
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+    @Test
+    fun `로또 번호의 개수가 6개 미만이면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +27,44 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또 번호가 1부터 45 범위를 벗어나면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 2, 3, 4, 5, 6))
+        }
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `로또 번호 중 당첨 번호와 일치하는 개수를 올바르게 계산한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = listOf(3, 4, 5, 6, 7, 8)
+        val matchCount = lotto.countMatchingNumbers(winningNumbers)
+
+        assertThat(matchCount).isEqualTo(4)
+    }
+
+    @Test
+    fun `로또 번호에 보너스 번호가 포함되어 있으면 true를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 5
+
+        assertThat(lotto.containsBonusNumber(bonusNumber)).isTrue
+    }
+
+    @Test
+    fun `로또 번호에 보너스 번호가 포함되어 있지 않으면 false를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 7
+
+        assertThat(lotto.containsBonusNumber(bonusNumber)).isFalse
+    }
+
+    @Test
+    fun `로또 번호를 문자열로 변환하여 출력 형식을 확인한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]")
+    }
 }
