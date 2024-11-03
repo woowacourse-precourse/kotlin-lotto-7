a70a7cef83ba6bb85923fa7d476645b6854ec752
dioo1461 <dioo1461@gmail.com>
2024-11-03 18:54:21 +0900

    feat(Lotto.kt): LottoRank enum 클래스에 prize 필드를 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,13 +2,13 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
 
-enum class LottoRank {
-    NONE,
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH
+enum class LottoRank(val prize: Int) {
+    NONE(0),
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000)
 }
 
 class Lotto(private val numbers: List<Int>) {
@@ -23,5 +23,4 @@ class Lotto(private val numbers: List<Int>) {
             else -> LottoRank.NONE
         }
     }
-
 }
