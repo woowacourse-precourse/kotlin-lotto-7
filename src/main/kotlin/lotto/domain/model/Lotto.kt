b13aa1bd63c1f0c6eb0fe0a3b201331bac490c56
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-04 00:18:13 +0900

    fix(Lotto): 중복된 숫자가 들어오는 경우 check

--- src/main/kotlin/lotto/domain/model/Lotto.kt
+++ src/main/kotlin/lotto/domain/model/Lotto.kt
@@ -1,10 +1,13 @@
 package lotto.domain.model
 
+import lotto.util.validator.winningnumbers.WinningNumbersErrorType
+
 data class Lotto(
     private val numbers: List<Int>
 ) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == numbers.toSet().size) { WinningNumbersErrorType.NO_DUPLICATE_LUCKY_NUMBERS}
     }
 
     val lottoNumbers: List<Int>
@@ -17,6 +20,6 @@ data class Lotto(
 
     companion object {
         private const val PREFIX = "["
-        private const val POSTFIX = "["
+        private const val POSTFIX = "]"
     }
 }
