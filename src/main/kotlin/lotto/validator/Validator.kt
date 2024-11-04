bbd7b6adce9735ad81dde43e26aadb05b3713bf7
Jieun <lje2000lje@naver.com>
2024-11-04 21:44:14 +0900

    feat(Validator.kt): 보너스 번호 유효성 검사
    빈값을 입력했는지, 1~45 범위 내에 있는지,
    로또 번호와 중복되는지 유효성 검사

--- src/main/kotlin/lotto/validator/Validator.kt
+++ src/main/kotlin/lotto/validator/Validator.kt
@@ -22,4 +22,10 @@ class Validator {
         require(numbers.all { it in Constants.MIN_NUMBER..Constants.MAX_NUMBER }) { ExceptionConstants.ERROR_INVALID_WINNING_NUMBERS_RANGE }
         require(numbers.distinct().size == Constants.WINNING_NUMBERS_COUNT) { ExceptionConstants.ERROR_DUPLICATE_WINNING_NUMBERS }
     }
+
+    fun validateBonusNumber(bonusNumber: Int, winningNumbers: List<Int>) {
+        require(winningNumbers.isNotEmpty()) { ExceptionConstants.ERROR_EMPTY_BONUS_NUMBERS }
+        require(bonusNumber in Constants.MIN_NUMBER..Constants.MAX_NUMBER) { ExceptionConstants.ERROR_INVALID_BONUS_NUMBER }
+        require(bonusNumber !in winningNumbers) { ExceptionConstants.ERROR_DUPLICATE_BONUS_NUMBER }
+    }
 }
