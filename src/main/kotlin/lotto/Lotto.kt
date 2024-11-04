70476b4eab0635d5c2115da82e42d9320fc086b7
Yujin Choe <dev.yujinchoe@gmail.com>
2024-11-04 23:14:42 +0900

    feat(lotto): 로또 번호 발행 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,20 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        fun generateNumbers(): List<Int> {
+            return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        }
+    }
+
+    override fun toString(): String {
+        return numbers.toString()
+    }
 }
