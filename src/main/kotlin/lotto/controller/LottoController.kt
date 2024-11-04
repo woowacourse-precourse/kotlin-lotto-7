b32ab3d3edb8cedd47ab79bbfd684fce6424da73
Jinwoo Choi <cjw1234577@gmail.com>
2024-11-04 23:23:15 +0900

    fix(LottoController): 당첨번호와 보너스 번호 입력 이전에 구매한 로또부터 출력하게 수정

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -12,10 +12,10 @@ class LottoController {
         val lottos = Lottos()
         lottos.buyLottos(lottoPrice) //구매한 로또들 생성
 
+        OutputView.printLottoNumbers(lottos.getLottos().map { it.get() })  // 구매한 로또들 출력
+
         val winningNumbers = InputView.inputWinningNumbers() // 당첨 번호를 입력
         val bonusNumber = InputView.inputBonusNumber(winningNumbers) // 보너스 번호를 입력
-
-        OutputView.printLottoNumbers(lottos.getLottos().map { it.get() })  // 구매한 로또들 출력
 
         val result = LottoCalculator.calculate(lottos, winningNumbers, bonusNumber) //로또 시뮬레이션
         OutputView.printLottoResult(result)   // 당첨 통계 출력
