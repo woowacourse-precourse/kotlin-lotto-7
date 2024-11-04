6afe84a4cff35f95e8522a35e514b23783657249
minseok0416 <day5274567@gmail.com>
2024-11-04 23:53:35 +0900

    refactor(LottoMachine): main메서드 내용 LottoMachine으로 이동

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,22 +1,5 @@
 package lotto
 
 fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
-
-    val purchaseAmount = inputView.getPurchaseAmount()
-    val lottoCount = purchaseAmount / 1000
-
-    val myLotto = Lotto.issueLotto(lottoCount)
-    outputView.printLottoCount(lottoCount, myLotto)
-
-    val winningNumbers = inputView.getLottoNumbers()
-    val bonusNumber = inputView.getBonusNumber()
-
-    val lotto = Lotto(winningNumbers)
-    lotto.getLottoRank(myLotto, bonusNumber)
-    outputView.printLottoRank(lotto)
-
-    val profitRate = lotto.calculateProfitRate(purchaseAmount)
-    outputView.printProfitRate(profitRate)
+    LottoMachine().run()
 }
