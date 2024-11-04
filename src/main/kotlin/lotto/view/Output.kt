5f81f9317d97fa037042a4d4e897129886812814
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-11-04 23:09:41 +0900

    feat(Output): Add printLottoNumbers() to show lotto numbers

--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -1,5 +1,13 @@
 package lotto.view
 
+import lotto.model.Lotto
+
 class Output {
-    fun printLottoNumbers(numbers: List<Int>) = println(numbers)
+    fun printLottoNumbers(lottos: List<Lotto>) {
+        println("${lottos.size}개를 구매했습니다.")
+        repeat(lottos.size) { idx ->
+            println(lottos[idx].getLottoNumbers())
+        }
+        println()
+    }
 }
