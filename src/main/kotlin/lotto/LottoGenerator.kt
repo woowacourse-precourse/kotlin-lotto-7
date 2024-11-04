6b2714e01ee5cc8d7bd94b95dc17c75a5534d7eb
DDangDin <Kcorelec7123@gmail.com>
2024-11-04 17:40:36 +0900

    refactor: 로또 최소, 최대 번호 상수 값을 LottoConstants에 작성
    - 유효성 검사를 위해서 로또 최소, 최대 번호 상수 값을 전역적으로 관리

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -21,13 +21,15 @@ class LottoGenerator(private val purchasePrice: Int) {
     private fun getLottoCount(): Int = purchasePrice / LottoConstants.PRICE
 
     private fun generateOneLotto(): Lotto {
-        val randomUniqueNumbers = pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT)
+        val randomUniqueNumbers = pickUniqueNumbersInRange(
+            LottoConstants.MIN_NUMBER,
+            LottoConstants.MAX_NUMBER,
+            PICK_COUNT
+        )
         return Lotto(randomUniqueNumbers)
     }
 
     companion object {
-        private const val START_NUMBER = 1
-        private const val END_NUMBER = 45
         private const val PICK_COUNT = 6
     }
 }
--- src/main/kotlin/lotto/util/LottoConstants.kt
+++ src/main/kotlin/lotto/util/LottoConstants.kt
@@ -3,4 +3,6 @@ package lotto.util
 object LottoConstants {
     const val PRICE = 1000
     const val WINNING_NUMBERS_DELIMITER = ","
+    const val MIN_NUMBER = 1
+    const val MAX_NUMBER = 45
 }
