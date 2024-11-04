a77f743e29d0cfbeb821658802225b36fb6dfb27
hi2242 <doodukly99@gmail.com>
2024-11-04 18:25:46 +0900

    로또 발매기 인터페이스 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,22 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("구입금액을 입력해 주세요.")
+    val moneyForLotto = readLine().toInt()
+    val countLotto = moneyForLotto / 1000
+
+    println("${countLotto}개를 구매했습니다.")
+    for (i in 0 until countLotto) {
+        val lottoNumberList = listOf(readLine())
+    }
+    println("당첨 번호를 입력해 주세요.")
+    val winNumber = readLine().split(",").map {it.toInt()}
+    println("보너스 번호를 입력해 주세요.")
+    val bonusNumber = readLine().toInt()
+    println("당첨 통계")
+    println("---")
+
 }
