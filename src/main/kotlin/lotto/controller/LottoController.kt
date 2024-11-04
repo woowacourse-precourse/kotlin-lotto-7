c47df53ccf1574ee1837a4550275fbcddfd603e1
이지윤 <02leejiyun@naver.com>
2024-11-04 17:42:49 +0900

    refactor: LottoController의 start 함수 분리

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,28 +1,47 @@
 package lotto.controller
 
 import lotto.domain.GameResult
+import lotto.domain.Lotto
 import lotto.domain.LottoGenerator
+import lotto.domain.PurchaseAmount
 import lotto.domain.WinningNumber
 import lotto.view.InputView
 import lotto.view.OutputView
 
 class LottoController {
     fun start() {
+        val purchaseAmount = getPurchaseAmount()
+        val lottos = generateLottos(purchaseAmount.getCount())
+        val winningNumber = getWinningNumber()
+
+        val gameResult = calculateGameResult(lottos, winningNumber)
+        OutputView.printGameResult(gameResult)
+    }
+
+    private fun getPurchaseAmount(): PurchaseAmount {
         val purchaseAmount = InputView.inputPurchaseMoney()
         OutputView.printPurchaseAmount(purchaseAmount.getCount())
+        return purchaseAmount
+    }
 
-        val lottos = List(purchaseAmount.getCount()) { LottoGenerator().generate() }
+    private fun generateLottos(count: Int): List<Lotto> {
+        val lottos = List(count) { LottoGenerator().generate() }
         lottos.forEach { OutputView.printLottoNumbers(it) }
+        return lottos
+    }
 
-        val winningNumbers = InputView.inputWinningNumbers()
-        val bonusNumber = InputView.inputBonusNumber(winningNumbers.getNumbers())
-        val winningNumber = WinningNumber(winningNumbers.getNumbers(), bonusNumber)
+    private fun getWinningNumber(): WinningNumber {
+        val lottoNumbers = InputView.inputLottoNumbers()
+        val bonusNumber = InputView.inputBonusNumber(lottoNumbers.getNumbers())
+        return WinningNumber(lottoNumbers.getNumbers(), bonusNumber)
+    }
 
+    private fun calculateGameResult(lottos: List<Lotto>, winningNumber: WinningNumber): GameResult {
         val gameResult = GameResult()
-        for (lotto in lottos) {
+        lottos.forEach { lotto ->
             val rank = winningNumber.getRank(lotto)
             gameResult.addWinningDetail(rank)
         }
-        OutputView.printGameResult(gameResult)
+        return gameResult
     }
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -19,8 +19,8 @@ object InputView {
         }
     }
 
-    fun inputWinningNumbers(): Lotto {
-        println(INPUT_WINNING_NUMBER_MESSAGE)
+    fun inputLottoNumbers(): Lotto {
+        println(INPUT_LOTTO_NUMBER_MESSAGE)
         return try {
             val winningNumbers = Console.readLine().split(NUMBER_DELIMITER).map {
                 it.toIntOrNull() ?: throw IllegalArgumentException(ERROR_INVALID_INPUT_MESSAGE)
@@ -28,7 +28,7 @@ object InputView {
             Lotto(winningNumbers)
         } catch (e: IllegalArgumentException) {
             println(e.message)
-            inputWinningNumbers()
+            inputLottoNumbers()
         }
     }
 
@@ -48,7 +48,7 @@ object InputView {
     }
 
     private const val INPUT_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요."
-    private const val INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요."
+    private const val INPUT_LOTTO_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요."
     private const val INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요."
     private const val ERROR_INVALID_INPUT_MESSAGE = "[ERROR] 올바른 숫자를 입력해야 합니다."
     private const val ERROR_DUPLICATE_NUMBER_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다."
