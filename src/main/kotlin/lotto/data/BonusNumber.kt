fa963f4d5af47bf03b0822918353f47c70e9c062
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-11-04 22:53:30 +0900

    refactor(): `init` 블록의 검증 함수화

--- src/main/kotlin/lotto/data/BonusNumber.kt
+++ src/main/kotlin/lotto/data/BonusNumber.kt
@@ -4,11 +4,15 @@ import lotto.constants.Error.ERROR_BONUS_NUMBER_DUPLICATE
 import lotto.constants.Error.ERROR_BONUS_NUMBER_NOT_INTEGER
 import lotto.constants.Error.ERROR_BONUS_NUMBER_OUT_OF_RANGE
 
-class BonusNumber(private val bonusNumber: String, winningNumbers: List<Int>) {
+class BonusNumber(private val bonusNumber: String, private val winningNumbers: List<Int>) {
     init {
-        bonusNumber.toIntOrNull() ?: throw NumberFormatException(ERROR_BONUS_NUMBER_NOT_INTEGER)
-        require(bonusNumber.toInt() in 1..45) { ERROR_BONUS_NUMBER_OUT_OF_RANGE }
-        require(winningNumbers.all { it != bonusNumber.toInt() }) { ERROR_BONUS_NUMBER_DUPLICATE }
+        validateBonusNumber()
+    }
+
+    private fun validateBonusNumber() {
+        val number = bonusNumber.toIntOrNull() ?: throw NumberFormatException(ERROR_BONUS_NUMBER_NOT_INTEGER)
+        require(number in 1..45) { ERROR_BONUS_NUMBER_OUT_OF_RANGE }
+        require(winningNumbers.all { it != number }) { ERROR_BONUS_NUMBER_DUPLICATE }
     }
 
     fun getNumber() = bonusNumber.toInt()
--- src/main/kotlin/lotto/data/PurchaseAmount.kt
+++ src/main/kotlin/lotto/data/PurchaseAmount.kt
@@ -6,6 +6,10 @@ import lotto.constants.Error.ERROR_AMOUNT_NOT_MULTIPLE_OF_TICKET_PRICE
 
 class PurchaseAmount(private val amount: String, private val ticketPrice: Int) {
     init {
+        validateAmount()
+    }
+
+    private fun validateAmount() {
         amount.toIntOrNull() ?: throw NumberFormatException(ERROR_AMOUNT_NOT_INTEGER)
         require(amount.toInt() > 0) { ERROR_AMOUNT_NEGATIVE_OR_ZERO }
         require(amount.toInt() % ticketPrice == 0) { ERROR_AMOUNT_NOT_MULTIPLE_OF_TICKET_PRICE.format(ticketPrice) }
