491d0d342094c208d03d7e3cd249ba701c2f0570
이지윤 <02leejiyun@naver.com>
2024-11-04 16:36:59 +0900

    feat: 당첨 통계 출력 형식 수정

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -15,26 +15,33 @@ object OutputView {
         println(numbers.sorted())
     }
 
-    fun printWinningDetail(winningDetail: Map<Rank, Int>) {
-        Rank.entries.reversed().forEach {
-            val result = String.format(
-                OUTPUT_MESSAGE,
-                it.matchCount,
-                DecimalFormat("#,###").format(it.prizeMoney),
-                winningDetail[it]
-            )
-            println(result)
-        }
-    }
-
     fun printGameResult(gameResult: GameResult) {
         println(OUTPUT_WINNING_STATISTICS_MESSAGE)
         printWinningDetail(gameResult.winningDetail)
         println(String.format(OUTPUT_EARNING_RATE_MESSAGE, gameResult.getEarningRate() * 100))
     }
 
+    private fun printWinningDetail(winningDetail: Map<Rank, Int>) {
+        Rank.entries.reversed().forEach {
+            val result = when (it) {
+                Rank.SECOND -> OUTPUT_RANK_BONUS_MESSAGE.format(
+                    it.matchCount,
+                    DecimalFormat("#,###").format(it.prizeMoney),
+                    winningDetail[it]
+                )
+                Rank.FIRST, Rank.THIRD, Rank.FOURTH, Rank.FIFTH -> OUTPUT_RANK_MESSAGE.format(
+                    it.matchCount,
+                    DecimalFormat("#,###").format(it.prizeMoney),
+                    winningDetail[it]
+                )
+            }
+            println(result)
+        }
+    }
+
     private const val OUTPUT_PURCHASE_AMOUNT_MESSAGE = "개를 구매했습니다."
     private const val OUTPUT_WINNING_STATISTICS_MESSAGE = "당첨 통계\n---"
-    private const val OUTPUT_MESSAGE = "%d개 일치 (%s원) - %d개"
+    private const val OUTPUT_RANK_MESSAGE = "%d개 일치 (%s원) - %d개"
+    private const val OUTPUT_RANK_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개"
     private const val OUTPUT_EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다."
 }
