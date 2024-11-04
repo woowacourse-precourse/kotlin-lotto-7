f95ece59fcdedab8fc9602cfb1b859b165742fb3
김민지 <gimminji@gimminjiui-MacBookAir.local>
2024-11-04 15:40:57 +0900

    chore: 테스트 역할 설명 주석 추가

--- src/test/kotlin/lotto/LottoGameTest.kt
+++ src/test/kotlin/lotto/LottoGameTest.kt
@@ -6,14 +6,16 @@ import org.assertj.core.api.Assertions.assertThat
 
 
 class LottoGameTest {
-    
+
+    // 기능 테스트
     @Test
     fun `유효한 구입 금액을 입력하면 해당 금액을 반환한다`() {
         val game = LottoGame(readLine = { "3000" })
         val cost = game.inputCost()
         assertThat(cost).isEqualTo(3000)
     }
-
+    
+    // 예외 처리 테스트
     @Test
     fun `금액이 1000원 미만이면 예외가 발생후 재입력 받아 정상 종료된다`() {
         val game = LottoGame(readLine = { "900" })
@@ -23,6 +25,7 @@ class LottoGameTest {
         assertThat(exception.message).contains("[ERROR] 금액은 1,000원 단위로 입력해야 합니다.")
     }
 
+    // 예외 처리 테스트
     @Test
     fun `금액이 1000 단위가 아니면 예외가 발생한다`() {
         val game = LottoGame(readLine = { "1500" })
@@ -32,6 +35,7 @@ class LottoGameTest {
         assertThat(exception.message).contains("[ERROR] 금액은 1,000원 단위로 입력해야 합니다.")
     }
 
+    // 기능 테스트
     @Test
     fun `유효한 당첨 번호를 입력하면 해당 로또를 반환한다`() {
         val game = LottoGame(readLine = { "1, 2, 3, 4, 5, 6" })
@@ -39,6 +43,7 @@ class LottoGameTest {
         assertThat(winningLotto.getNumbers()).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
     }
 
+    // 예외 처리 테스트
     @Test
     fun `당첨 번호가 유효하지 않으면 예외가 발생한다`() {
         val game = LottoGame(readLine = { "" })
@@ -49,6 +54,7 @@ class LottoGameTest {
         assertThat(exception.message).contains("[ERROR] 로또 번호는 6개여야 합니다.")
     }
 
+    // 기능 테스트
     @Test
     fun `유효한 보너스 번호를 입력하면 해당 번호를 반환한다`() {
         val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
@@ -57,6 +63,7 @@ class LottoGameTest {
         assertThat(bonusNumber).isEqualTo(8)
     }
 
+    // 예외 처리 테스트
     @Test
     fun `보너스 번호가 숫자 형식이 아니면 예외가 발생한다`() {
         val game = LottoGame(readLine = { "" })
@@ -68,6 +75,7 @@ class LottoGameTest {
         assertThat(exception.message).contains("[ERROR] 보너스 번호는 숫자로 입력해야 합니다.")
     }
 
+    // 예외 처리 테스트
     @Test
     fun `보너스 번호가 1~45 사이의 숫자가 아니면 예외가 발생한다`() {
         val game = LottoGame(readLine = { "" })
@@ -79,6 +87,7 @@ class LottoGameTest {
         assertThat(exception.message).contains("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.")
     }
 
+    // 예외 처리 테스트
     @Test
     fun `보너스 번호가 당첨 번호와 중복되면 예외가 발생한다`() {
         val game = LottoGame(readLine = { "" })
@@ -90,6 +99,7 @@ class LottoGameTest {
         assertThat(exception.message).contains("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.")
     }
 
+    // 기능 테스트
     @Test
     fun `구입 금액에 따른 로또 목록이 생성된다`() {
         val cost = 3000
@@ -104,6 +114,7 @@ class LottoGameTest {
         }
     }
 
+    // 기능 테스트
     @Test
     fun `로또들과 당첨 로또를 비교하여 각 등수 개수 정확하게 계산`() {
         val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
