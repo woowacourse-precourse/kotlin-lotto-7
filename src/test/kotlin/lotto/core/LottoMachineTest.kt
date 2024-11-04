0aa568915e2b68d6b436d0ab795b7ee7591c45f5
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-11-04 16:38:29 +0900

    test(LottoMachine): 기능 테스트 추가

---
+++ src/test/kotlin/lotto/core/LottoMachineTest.kt
@@ -0,0 +1,40 @@
+package lotto.core
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoMachineTest : NsTest() {
+    @Test
+    fun `기능 테스트`() {
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("8000", "1,2,3,4,5,6")
+                assertThat(output()).contains(
+                    "8개를 구매했습니다.",
+                    "[8, 21, 23, 41, 42, 43]",
+                    "[3, 5, 11, 16, 32, 38]",
+                    "[7, 11, 16, 35, 36, 44]",
+                    "[1, 8, 11, 31, 41, 42]",
+                    "[13, 14, 16, 38, 42, 45]",
+                    "[7, 11, 30, 40, 42, 43]",
+                    "[2, 13, 22, 32, 38, 45]",
+                    "[1, 3, 5, 14, 22, 45]"
+                )
+            },
+            listOf(8, 21, 23, 41, 42, 43),
+            listOf(3, 5, 11, 16, 32, 38),
+            listOf(7, 11, 16, 35, 36, 44),
+            listOf(1, 8, 11, 31, 41, 42),
+            listOf(13, 14, 16, 38, 42, 45),
+            listOf(7, 11, 30, 40, 42, 43),
+            listOf(2, 13, 22, 32, 38, 45),
+            listOf(1, 3, 5, 14, 22, 45)
+        )
+    }
+
+    override fun runMain() {
+        LottoMachine().run()
+    }
+}
