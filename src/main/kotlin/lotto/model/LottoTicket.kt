68182a6cbd7c377ad2375f0c9a65e314255c57b6
kangyein9892 <inwon9892@naver.com>
2024-11-04 17:38:25 +0900

    feat(LottoTicket.kt): 로또 각 티켓 당첨 등수 계산 함수 구현

--- src/main/kotlin/lotto/model/LottoTicket.kt
+++ src/main/kotlin/lotto/model/LottoTicket.kt
@@ -1,8 +1,14 @@
+package lotto.model
+
 import lotto.util.ConstantsUtil.TICKET_PRICE
 
 class LottoTicket {
     fun generateTickets(ticketsPrice: Int): List<Lotto> {
         val ticketCount = ticketsPrice / TICKET_PRICE
         return List(ticketCount) {Lotto.generate()}
+    }
+
+    fun calculateTickets(tickets: List<Lotto>, winningNumbers: Set<Int>, bonusNumber: Int): Map<LottoRank, Int> {
+        return tickets.groupingBy { it.match(winningNumbers, bonusNumber) }.eachCount()
     }
 }
