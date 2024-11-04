a77f55cb5846201691689f1291aa9cb907a2d8d3
PIYUJIN <yjyj020201@gmail.com>
2024-11-04 23:11:55 +0900

    feat($Lotto): 로또 당첨 번호 및 보너스 번호 비교 및 당첨 결과 추출 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,6 @@
 package lotto
 
+
 class Lotto(private val numbers: List<Int>) {
 
     init {
@@ -8,7 +9,29 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.all { it in 1..45}) {"[ERROR] 로또 당첨 번호는 1~45까지의 숫자로만 입력해야 합니다."}
         require(numbers.isNotEmpty()) {"[ERROR] 로또 당첨 번호는 중복이 아닌 6개의 숫자여야 합니다."}
     }
+
+    fun checkMatch(outputLottoNumbers: MutableList<List<Int>>, bonusLottoNum: Int): MutableList<Int> {
+        val results = MutableList(5) { 0 }
+
+        outputLottoNumbers.forEach { outputNum ->
+            val count = outputNum.count { it in numbers }
+            val isBonusMatch = bonusLottoNum in outputNum
+            val rank = checkWinning(count, isBonusMatch)
+
+            rank?.let { results[it] += 1 }
+        }
+
+        return results
     }
 
-    // TODO: 추가 기능 구현
+    fun checkWinning(count: Int, bonusMatch: Boolean): Int? {
+        return when {
+            count == 6 -> 4 // 1등
+            count == 5 && bonusMatch -> 3 // 2등
+            count == 5 -> 2 // 3등
+            count == 4 -> 1 // 4등
+            count == 3 -> 0 // 5등
+            else -> null
+        }
+    }
 }
