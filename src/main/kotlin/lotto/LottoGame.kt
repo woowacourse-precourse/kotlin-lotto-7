520ecac4c769dc0068ecba351cf8708247b9399d
jklom0326 <jklom0326@gmail.com>
2024-11-04 16:42:03 +0900

    feat: 로또 게임 구현 완료

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import java.lang.IllegalArgumentException
+
 class LottoGame {
     private val view = View()
     private val broadcast = Broadcast()
@@ -7,13 +9,50 @@ class LottoGame {
     private lateinit var inputPayment: Payment
 
     fun start() {
-        inputPayment = view.inputPayment()
+        inputPayment = tryInputPayment()
+
         val lottoMachine = LottoMachine(inputPayment.getPayment())
+        val lottoList = lottoMachine.lottoList
+        broadcast.printLottoNumbers(lottoList)
 
-        broadcast.printLottoNumbers(lottoMachine.lottoList)
-        val winningNumber = view.inputWinningNumber()
-        val bonusNumber = view.inputBonusNumber(winningNumber)
+        val winningNumber = tryInputWinningNumber()
+        val bonusNumber = tryInputBonusNumber(winningNumber)
 
-        broadcast.printWinningStatistics()
+        val winningResult = lottoMachine.winningLotteryResult(lottoList, bonusNumber)
+        val prizeCount = lottoMachine.countPrize(winningResult)
+        val winningMessage = lottoMachine.getWinningMessage(prizeCount)
+
+        broadcast.printWinningStatistics(winningMessage)
+        broadcast.printProfit(lottoMachine.getPrize(), inputPayment.getPayment())
+    }
+
+    private fun tryInputPayment(): Payment {
+        while (true) {
+            try {
+                return view.inputPayment()
+            } catch (e: IllegalArgumentException) {
+                println("\n${e.message}")
+            }
+        }
+    }
+
+    private fun tryInputWinningNumber(): WinningNumber {
+        while (true) {
+            try {
+                return view.inputWinningNumber()
+            } catch (e: IllegalArgumentException) {
+                println("\n${e.message}")
+            }
+        }
+    }
+
+    private fun tryInputBonusNumber(winningNumber: WinningNumber): BonusNumber {
+        while (true) {
+            try {
+                return view.inputBonusNumber(winningNumber)
+            } catch (e: IllegalArgumentException) {
+                println("\n${e.message}")
+            }
+        }
     }
 }
