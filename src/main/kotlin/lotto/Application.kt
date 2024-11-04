7298644f29835a6d0cedf0f6124c723f895971af
gixxla <gigixquisite@gmail.com>
2024-11-04 14:56:42 +0900

    feat: 로또 구입 기능 추가
    - 안내 문구 출력
    - 구입 금액이 1,000으로 나누어 떨어지지 않으면 IllegalArgumentException 발생
    - 올바른 금액이 입력될 때까지 위 과정 무한 루프

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,29 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("구입 금액을 입력해 주세요.")
+    do {
+        try {
+            val number = getAmount()
+            println(number)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            continue
+        }
+        break
+    } while (true)
+}
+
+fun getAmount(): Int {
+    val amount = Console.readLine().toInt()
+    return checkAmount(amount)
+}
+
+fun checkAmount(amount: Int): Int {
+    if (amount % 1000 != 0) {
+        throw IllegalArgumentException("[ERROR] 1,000원 단위로만 구매 가능합니다. 구입 금액을 다시 입력해주세요.")
+    }
+    return amount / 1000
 }
