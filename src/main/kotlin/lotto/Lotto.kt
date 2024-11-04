85011520aa5a5a0c8e598299c206531c79041aa5
junseo511 <prime54@g.skku.edu>
2024-11-04 14:58:22 +0900

    feat: 로또 클래스 구현 및 유효성 검증 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,11 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == 6) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
+        require(numbers.all { number ->
+            number in 1..45
+        }) { "[ERROR] 로또 번호는 1부터 45 사이여야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    fun getNumbers(): List<Int> = numbers.sorted()
 }
