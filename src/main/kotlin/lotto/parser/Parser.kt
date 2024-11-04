032f116d5bced8a541efc12e2b3ab30506f9beca
Jieun <lje2000lje@naver.com>
2024-11-04 23:45:13 +0900

    feat(Parser.kt): 구분자가 쉼표가 아닌 경우 에러 처리

--- src/main/kotlin/lotto/parser/Parser.kt
+++ src/main/kotlin/lotto/parser/Parser.kt
@@ -1,12 +1,17 @@
 package lotto.parser
 
+import lotto.util.ExceptionConstants
+
 class Parser {
     fun parsePurchaseAmount(input: String): Int {
         return input.toInt()
     }
 
     fun parseWinningNumbers(input: String): List<Int> {
-        return input.split(",").map { it.trim().toInt() }
+        val delimiter = ","
+        require(input.contains(delimiter)) { ExceptionConstants.ERROR_INVALID_DELIMITER }
+
+        return input.split(delimiter).map { it.trim().toInt() }
     }
 
     fun parseBonusNumber(input: String): Int {
--- src/main/kotlin/lotto/util/ExceptionConstants.kt
+++ src/main/kotlin/lotto/util/ExceptionConstants.kt
@@ -6,6 +6,7 @@ class ExceptionConstants {
         const val ERROR_INVALID_WINNING_NUMBERS_RANGE = "[ERROR] 로또 번호는 1부터 45 사이여야 합니다."
         const val ERROR_DUPLICATE_WINNING_NUMBERS = "[ERROR] 로또 번호는 중복될 수 없습니다."
         const val ERROR_EMPTY_WINNING_NUMBERS = "[ERROR] 당첨 번호가 비어 있습니다."
+        const val ERROR_INVALID_DELIMITER = "[ERROR] 당첨 번호는 쉼표(,)로 구분해야 합니다."
 
         const val ERROR_INVALID_PURCHASE_AMOUNT = "[ERROR] 로또는 1,000원 단위로 입력해야 합니다."
         const val ERROR_EMPTY_PURCHASE_AMOUNT = "[ERROR] 구매 금액이 비어 있습니다."
