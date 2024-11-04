66d5cc3bacc640fa79c39cc0faeab21e9d50d81d
park-hwaan <parkhwan1129@gmail.com>
2024-11-04 15:34:17 +0900

    rename: 로또번호 출력 함수 이름 변경

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -4,7 +4,7 @@ import lotto.model.lotto.Lotto
 import lotto.model.lotto.Prize
 
 class OutputView {
-    fun displayPurchaseLottoNumbers(lottoNumbers: List<Lotto>) {
+    fun printPurchaseLottoNumbers(lottoNumbers: List<Lotto>) {
         println()
         println("${lottoNumbers.size}개를 구매했습니다.")
         lottoNumbers.forEach { println(it.getNumbers()) }
