f1535198c9591db191b9d8066061ba60891b26c0
이지윤 <02leejiyun@naver.com>
2024-11-04 16:01:29 +0900

    feat: 로또 번호가 6개의 숫자가 아닌 경우 예외 처리

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,8 +2,14 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_SIZE) { ERROR_INVALID_SIZE_MESSAGE }
     }
 
     fun getNumbers(): List<Int> = numbers
+
+    companion object {
+        private const val ERROR_INVALID_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
+
+        private const val LOTTO_SIZE = 6
+    }
 }
