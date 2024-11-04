a708b23ee84f6b61a0fb3e16cfd41ec4092921b9
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-11-04 20:59:04 +0900

    refactor(`printLottoStats()`): 문자열 상수화

--- src/main/kotlin/lotto/constants/Message.kt
+++ src/main/kotlin/lotto/constants/Message.kt
@@ -8,6 +8,8 @@ object Message {
 
     const val MESSAGE_LOTTO_TICKETS_PURCHASED = "%d개를 구매했습니다."
     const val MESSAGE_LOTTO_TICKET_FORMAT = "[%s]"
+    const val MESSAGE_MATCH_WITH_BONUS_FORMAT = "%d개 일치, 보너스 볼 일치 (%d원) - %d개"
+    const val MESSAGE_MATCH_FORMAT = "%d개 일치 (%d원) - %d개"
     const val MESSAGE_FORMATTED_PROFIT_RATE = "총 수익률은 %.1f%%입니다."
 
     const val MESSAGE_INVALID_INPUT = "잘못된 입력입니다. 다시 입력해 주세요. %s"
