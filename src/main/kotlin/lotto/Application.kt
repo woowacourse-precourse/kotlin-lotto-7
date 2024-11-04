841ea466f8aaa646c91d640f79bb0b309482885e
kangyein9892 <inwon9892@naver.com>
2024-11-04 19:20:51 +0900

    feat(Application.kt): 프로그램 시작점 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
 
+import lotto.model.LottoTicket
+import lotto.presenter.LottoPresenter
+import lotto.view.LottoView
+import lotto.view.LottoViewImpl
+
 fun main() {
-    // TODO: 프로그램 구현
+    val view: LottoView = LottoViewImpl()
+    val lottoTicket = LottoTicket()
+    val presenter = LottoPresenter(view, lottoTicket)
+
+    val price = view.getTicketsPrice()
+    presenter.processLottoTickets(price)
+
+    val winningNumbers = view.getWinningNumbers()
+    val bonusNumbers = view.getBonusNumber()
+    presenter.processWinningNumbers(winningNumbers, bonusNumbers)
 }
