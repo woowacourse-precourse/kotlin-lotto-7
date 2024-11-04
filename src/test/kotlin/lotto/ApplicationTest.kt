ab2b35ed1c917d2f1de6725f626b10b425853def
joo <yjy010222gmail.com>
2024-11-04 23:17:22 +0900

    ApplicationTest: 구입금액 단위 테스트 추가

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -50,6 +50,30 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `1000의 배수가 아닌 경우 예외 테스트`() {
+        assertSimpleTest {
+            runException("1500")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `음수인 경우 예외 테스트`() {
+        assertSimpleTest {
+            runException("-5000")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `공백인 경우 예외 테스트`() {
+        assertSimpleTest {
+            runException(" ")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
     override fun runMain() {
         main()
     }
