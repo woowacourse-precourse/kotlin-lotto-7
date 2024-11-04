686e002065a34f19409f8a733cc1b9b8d92b340d
unifolio0 <boho82ha@gmail.com>
2024-11-04 16:35:24 +0900

    refactor: 상수화

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -3,12 +3,16 @@ package lotto.model
 import camp.nextstep.edu.missionutils.Randoms
 
 object LottoMachine {
+    private const val MIN_NUMBER = 1
+    private const val MAX_NUMBER = 45
+    private const val LOTTO_SIZE = 6;
+
     fun issueLotto(purchaseAmount: PurchaseAmount): List<Lotto> {
         val lottoCount = purchaseAmount.getLottoCount()
         val lottos = mutableListOf<Lotto>()
         repeat(lottoCount) {
             lottos.add(
-                Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)),
+                Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE))
             )
         }
         return lottos
