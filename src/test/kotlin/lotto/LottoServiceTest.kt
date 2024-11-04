123b4b01aee3f97e8ac2374ebe0aa1d1b8b64820
ijh1298 <imjh0417@gmail.com>
2024-11-04 13:12:55 +0900

    chore: 미사용 import문 제거

--- src/test/kotlin/lotto/LottoServiceTest.kt
+++ src/test/kotlin/lotto/LottoServiceTest.kt
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
-import kotlin.math.exp
 
 class LottoServiceTest {
     private val service = LottoService()
