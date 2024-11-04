fe60b650b47ef3c1e8bcbfa130f9dd0ce607baf7
Jieun <lje2000lje@naver.com>
2024-11-04 22:27:50 +0900

    feat(LottoController.kt): 생성된 랜덤 로또 번호 출력

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -5,16 +5,20 @@ import lotto.parser.Parser
 import lotto.util.Constants
 import lotto.validator.Validator
 import lotto.view.InputView
+import lotto.view.ResultView
 
 class LottoController {
     private val validator = Validator()
     private val parser = Parser()
     private val inputView = InputView(validator, parser)
+    private val resultView = ResultView()
     private val lottoGame = LottoGame()
 
     fun start() {
         val purchaseAmount = inputView.inputPurchaseAmount()
         val lottoCount = purchaseAmount / Constants.LOTTO_UNIT_PRICE
         val lottos = lottoGame.generateLotto(lottoCount)
+
+        resultView.displayPurchasedLotto(lottoCount, lottos)
     }
 }
