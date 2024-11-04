2ee4470b2a542dc05f8725a8e55cf0bcc2bb873b
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-04 16:21:03 +0900

    refactor: 로또 등수 enum 클래스에 맞추어 숫자를 enum으로 수정한다

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,28 +1,22 @@
 package lotto
 
-const val FIRST_PRIZE = 2000000000
-const val SECOND_PRIZE = 30000000
-const val THIRD_PRIZE = 1500000
-const val FOURTH_PRIZE = 50000
-const val FIFTH_PRIZE = 5000
-
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
-    private fun switchRank(matchNumberSize: Int, isBonusNumberMatch: Boolean): Int {
-        if (matchNumberSize == 6) return 1
-        if (matchNumberSize == 5 && !isBonusNumberMatch) return 2
-        if (matchNumberSize == 5) return 3
-        if (matchNumberSize == 4) return 4
-        if (matchNumberSize == 3) return 5
-        else throw IllegalArgumentException()
+    private fun switchRank(matchNumberSize: Int, isBonusNumberMatch: Boolean): RANK {
+        if (matchNumberSize == 6) return RANK.FIRST
+        if (matchNumberSize == 5 && isBonusNumberMatch) return RANK.SECOND
+        if (matchNumberSize == 5) return RANK.THIRD
+        if (matchNumberSize == 4) return RANK.FOURTH
+        if (matchNumberSize == 3) return RANK.FIFTH
+        return RANK.OTHERS
     }
 
-    fun getRank(lottoNumbers: List<Int>, bonusNumber: Int): Int {
-        var count = numbers.filter { number -> lottoNumbers.any { it == number } }
+    fun getRank(lottoNumbers: List<Int>, bonusNumber: Int): RANK {
+        val count = numbers.filter { number -> lottoNumbers.any { it == number } }
         val isBonusNumberMatch = numbers.any { it == bonusNumber }
 
         return switchRank(count.size, isBonusNumberMatch)
