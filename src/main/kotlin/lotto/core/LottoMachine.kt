6686433637651dc6fa44054b1d6aae89f17d509a
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-11-04 16:36:28 +0900

    feat: 구매한 모든 로또 수량 및 번호 출력하는 기능 추가

--- src/main/kotlin/lotto/core/LottoMachine.kt
+++ src/main/kotlin/lotto/core/LottoMachine.kt
@@ -1,15 +1,18 @@
 package lotto.core
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 import lotto.data.PurchaseAmount
 
 const val TICKET_PRICE = 1000
 
 class LottoMachine {
     private var purchaseAmount: Int = 0
+    private val lottoTickets = mutableListOf<List<Int>>()
 
     fun run() {
         requestPurchaseAmount()
+        printPurchasedLottoTickets()
     }
 
     private fun requestPurchaseAmount() {
@@ -23,6 +26,20 @@ class LottoMachine {
                 println("잘못된 입력입니다. 다시 입력해 주세요. ${e.message}")
             }
         }
+    }
+
+    private fun printPurchasedLottoTickets() {
+        val purchasedLottoCount = purchaseAmount / TICKET_PRICE
+
+        println("${purchasedLottoCount}개를 구매했습니다.")
+
+        repeat(purchasedLottoCount) {
+            val userLottoTicket = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+            lottoTickets.add(userLottoTicket)
+
+            println("[${userLottoTicket.joinToString(", ")}]")
+        }
+        println()
     }
 
 }
