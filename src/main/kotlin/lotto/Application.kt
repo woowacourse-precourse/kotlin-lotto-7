4d5943fc9326b594437da40d16eb4a74a90d68e2
ezhoon <david0275@naver.com>
2024-11-04 19:47:32 +0900

    feat(input): 잘못된 입력으로 예외 발생 시 다시 입력받는 loop 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -32,33 +32,51 @@ fun main() {
 object Input {
     // [*] 구매 금액 입력 기능
     fun purchasePrice(): Int {
-        println("구입금액을 입력해 주세요.")
-        val input = Console.readLine()
-            ?.takeIf { it.isNotBlank() }
-            ?: throw IllegalArgumentException("[ERROR] 입력 값을 다시 확인해주세요.")
-        val price = input.toInt()
-        require(price % LOTTO_PRICE == 0){"[ERROR] 구매 금액을 올바르게 입력해주세요."}
-        return price
+        while (true) {
+            try {
+                println("구입금액을 입력해 주세요.")
+                val input = Console.readLine()
+                    ?.takeIf { it.isNotBlank() }
+                    ?: throw IllegalArgumentException("[ERROR] 입력 값을 다시 확인해주세요.")
+                val price = input.toInt()
+                require(price % LOTTO_PRICE == 0 && price > 0) { "[ERROR] 구매 금액을 올바르게 입력해주세요." }
+                return price
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
     }
 
     // [*] 당첨 번호 입력 기능, 공백 허용
-    fun setWinningNumber() : List<Int> {
-        println("당첨 번호를 입력해 주세요.")
-        val winningNumber = Console.readLine().split(",").map { it.trim().toInt() }
-        require(winningNumber.size == 6){"[ERROR] 당첨 번호는 6개여야 합니다."}
-        require(winningNumber.all{ it in LOTTO_MIN..LOTTO_MAX }){"[ERROR] 1에서 45 사이의 숫자만 입력 가능합니다."}
-        require(winningNumber.toSet().size == 6){"[ERROR] 중복 없는 6개 번호를 입력해주세요."}
-        return winningNumber
+    fun setWinningNumber(): List<Int> {
+        while (true) {
+            try {
+                println("당첨 번호를 입력해 주세요.")
+                val winningNumber = Console.readLine().split(",").map { it.trim().toInt() }
+                require(winningNumber.size == 6) { "[ERROR] 당첨 번호는 6개여야 합니다." }
+                require(winningNumber.all { it in LOTTO_MIN..LOTTO_MAX }) { "[ERROR] 1에서 45 사이의 숫자만 입력 가능합니다." }
+                require(winningNumber.toSet().size == 6) { "[ERROR] 중복 없는 6개 번호를 입력해주세요." }
+                return winningNumber
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
     }
 
     // [*] 보너스 번호 입력 기능
-    fun setBonusNumber() : Int {
-        println("\n보너스 번호를 입력해 주세요.")
-        val input = Console.readLine()
-            ?.takeIf { it.isNotBlank() }
-            ?: throw IllegalArgumentException("[ERROR] 입력 값을 다시 확인해주세요.")
-        val bonusNumber = input.toInt()
-        require(bonusNumber in LOTTO_MIN..LOTTO_MAX) { "[ERROR] 보너스 번호 값을 다시 확인해주세요." }
-        return bonusNumber
+    fun setBonusNumber(): Int {
+        while (true) {
+            try {
+                println("\n보너스 번호를 입력해 주세요.")
+                val input = Console.readLine()
+                    ?.takeIf { it.isNotBlank() }
+                    ?: throw IllegalArgumentException("[ERROR] 입력 값을 다시 확인해주세요.")
+                val bonusNumber = input.toInt()
+                require(bonusNumber in LOTTO_MIN..LOTTO_MAX) { "[ERROR] 보너스 번호 값을 다시 확인해주세요." }
+                return bonusNumber
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
     }
 }
