c114391593f8752b2b68b8a524fb56d60dc8a888
yoon <cbj0010@naver.com>
2024-11-04 21:36:02 +0900

    refact : enum 수정
    enum class
    우승자 enum class 수정

--- src/main/kotlin/lotto/MatchedCount.kt
+++ src/main/kotlin/lotto/MatchedCount.kt
@@ -1,21 +1,22 @@
 package lotto
 
-enum class MatchedCount(val prize: Int) {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    NONE(0);
+enum class MatchedCount(val title: String, val prize: Long, val statement: String) {
+    FIFTH("5등", 5_000, "3개 일치"),
+    FOURTH("4등", 50_000, "4개 일치"),
+    THIRD("3등", 1_500_000, "5개 일치"),
+    SECOND("2등", 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST("1등", 2_000_000_000, "6개 일치"),
+
+    NONE("꽝", 0, "2개 이하 일치");
 
     companion object {
         fun fromMatchedNumbers(matchedNumbers: Int, isBonusNumberMatched: Boolean): MatchedCount {
             return when (matchedNumbers) {
-                6 -> MatchedCount.FIRST
-                5 -> if (isBonusNumberMatched) MatchedCount.SECOND else MatchedCount.THIRD
-                4 -> MatchedCount.FOURTH
-                3 -> MatchedCount.FIFTH
-                else -> MatchedCount.NONE
+                6 -> FIRST
+                5 -> if (isBonusNumberMatched) SECOND else THIRD
+                4 -> FOURTH
+                3 -> FIFTH
+                else -> NONE
             }
         }
     }
