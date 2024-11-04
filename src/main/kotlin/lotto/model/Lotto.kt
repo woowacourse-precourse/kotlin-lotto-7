853cf538e9e2ef5f83e5c525381c823a30fae47b
cbdhoho <81568368+cbdhoho@users.noreply.github.com>
2024-11-04 15:23:49 +0900

    feat: 로또 랜덤 번호 추첨

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,14 @@
 package lotto.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    fun randomNums(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1,45,6)
+    }
+
 }
