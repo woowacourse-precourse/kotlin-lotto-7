d56a0382efeb5e63bfa6f8fda9171fdcf7c8844a
giovannijunseokim <kingjunseo@gmail.com>
2024-11-04 19:21:27 +0900

    feat: implement LottoResult toString

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -1,7 +1,19 @@
 package lotto.domain
 
-enum class LottoResult(val prize: Long) {
-    FIRST(2_000_000_000), SECOND(30_000_000), THIRD(1_500_000), FOURTH(50_000), FIFTH(5_000), LOSE(0);
+import java.text.NumberFormat
+import java.util.*
+
+enum class LottoResult(val prize: Long, private val condition: String) {
+    LOSE(0, "낙첨"),
+    FIFTH(5_000, "3개 일치"),
+    FOURTH(50_000, "4개 일치"),
+    THIRD(1_500_000, "5개 일치"),
+    SECOND(30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(2_000_000_000, "6개 일치");
+
+    override fun toString(): String {
+        return "$condition (${NumberFormat.getNumberInstance(Locale.KOREA).format(prize)}원)"
+    }
 
     companion object {
         fun of(matchedNumbersCount: Int, bonusMatched: Boolean): LottoResult {
