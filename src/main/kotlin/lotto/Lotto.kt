7b8da1d565e3b51a2cfc67594f5f9122619eeb35
gixxla <gigixquisite@gmail.com>
2024-11-04 22:32:43 +0900

    fix: Lotto 클래스 예외 처리 수정
    - Lotto 클래스가 생성될 때 입력된 숫자 리스트 중 중복된 숫자가 있을 시 예외 발생 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다. 로또 번호를 다시 입력해주세요." }
+        require(numbers.distinct().count() == 6) { "[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다. 로또 번호를 다시 입력해주세요." }
     }
 
     fun getNumbers() = this.numbers
