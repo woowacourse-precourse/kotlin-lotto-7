c2a54106527380023b20bf9a66ef5ebcfbe0b8e7
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2024-11-04 22:20:29 +0900

    feat(Lotto.kt) : 중복된 숫자에 대한 예외 처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,7 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == LOTTO_COUNT) { "[ERROR] 로또 번호는 ${LOTTO_COUNT}개여야 합니다." }
+        require(numbers.size == LOTTO_COUNT) {throw IllegalArgumentException ("[ERROR] 로또 번호는 ${LOTTO_COUNT}개여야 합니다.") }
+        require(numbers.groupingBy { it }.eachCount().all { 1==it.value }) {throw IllegalArgumentException (exceptionMessageDuplicate) }
         showNumbers()
     }
 
@@ -13,9 +14,16 @@ class Lotto(private val numbers: List<Int>) {
     private fun showNumbers(){
         println(numbers.sorted().joinToString(separator = ",", prefix = "[", postfix = "]"))
     }
+
+    fun getNumbers() : List<Int>{
+        return numbers
+    }
+
     companion object {
         const val LOTTO_COUNT = 6
         const val MIN_LOTTO_NUMBER = 1
         const val MAX_LOTTO_NUMBER = 45
+
+        val exceptionMessageDuplicate = "[ERROR] 중복된 숫자가 존재하면 안 됩니다."
     }
 }
