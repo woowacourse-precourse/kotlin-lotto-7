669c861beb059474c66365f448723d6d0ec0b0ad
jooiss <jusong513@pusan.ac.kr>
2024-11-04 22:51:36 +0900

    refactor: 구입 급액 재입력하도록 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -57,13 +57,19 @@ fun main() {
 }
 
 fun getLottoBudget(): Int {
-    println("구입금액을 입력해 주세요.")
-    val userInput = Console.readLine()
-    val lottoBudget = userInput.toInt()
-    if (lottoBudget % LOTTO_TICKET_VALUE != 0) {
-        throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해 주세요.")
+    try {
+        println("구입금액을 입력해 주세요.")
+        val userInput = Console.readLine()
+        val lottoBudget = userInput.toInt()
+        if (lottoBudget % LOTTO_TICKET_VALUE != 0) {
+            throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해 주세요.")
+        }
+        return lottoBudget
+    } catch(e: java.lang.IllegalArgumentException) {
+        println("[ERROR] ${e.message}")
     }
-    return lottoBudget
+
+    return getLottoBudget()
 }
 
 fun getLottoTicketCount(lottoBudget: Int): Int {
