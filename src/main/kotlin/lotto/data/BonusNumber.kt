250e98f95e4808b93d64beb9408c74cc4244b6e4
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-11-04 20:52:34 +0900

    refactor: 불필요한 프로퍼티를 매개변수화

--- src/main/kotlin/lotto/data/BonusNumber.kt
+++ src/main/kotlin/lotto/data/BonusNumber.kt
@@ -4,7 +4,7 @@ import lotto.constants.Error.ERROR_BONUS_NUMBER_DUPLICATE
 import lotto.constants.Error.ERROR_BONUS_NUMBER_NOT_INTEGER
 import lotto.constants.Error.ERROR_BONUS_NUMBER_OUT_OF_RANGE
 
-class BonusNumber(private val bonusNumber: String, private val winningNumbers: List<Int>) {
+class BonusNumber(private val bonusNumber: String, winningNumbers: List<Int>) {
     init {
         require(bonusNumber.toIntOrNull() != null) { ERROR_BONUS_NUMBER_NOT_INTEGER }
         require(bonusNumber.toInt() in 1..45) { ERROR_BONUS_NUMBER_OUT_OF_RANGE }
