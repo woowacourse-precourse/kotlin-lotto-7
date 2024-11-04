1597842c4a941105f89560256ee8699cbd07dfbf
minseok0416 <day5274567@gmail.com>
2024-11-04 22:45:17 +0900

    feat(Lotto): 당첨내역을 산정하는 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,12 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class Lotto(private val numbers: List<Int>) {
+    var firstRank = 0
+    var secondRank = 0
+    var thirdRank = 0
+    var fourthRank = 0
+    var fifthRank = 0
+
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.toSet().size == 6) { "[ERROR] 로또 번호는 중복일 수 없습니다." }
@@ -12,7 +18,20 @@ class Lotto(private val numbers: List<Int>) {
     fun issueLotto(number: Int): List<List<Int>> {
         return (1..number).map {
             Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    fun getLottoRank(myLotto: List<List<Int>>, bonusNumber: Int) {
+        myLotto.forEach { lottoNumbers ->
+            val matchCount = countMatchingNumbers(lottoNumbers, numbers)
+            when {
+                matchCount == 6 -> firstRank++
+                matchCount == 5 && lottoNumbers.contains(bonusNumber) -> secondRank++
+                matchCount == 5 -> thirdRank++
+                matchCount == 4 -> fourthRank++
+                matchCount == 3 -> fifthRank++
+            }
         }
     }
 
+    private fun countMatchingNumbers(list1: List<Int>, list2: List<Int>): Int {
+        return list1.intersect(list2.toSet()).size
+    }
 }
