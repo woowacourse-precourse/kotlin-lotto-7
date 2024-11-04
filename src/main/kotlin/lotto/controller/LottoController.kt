4641e64954d4228f708a5340c98028ff2e2f91fb
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-11-04 19:31:57 +0900

    feat(LottoController): Add createRandomLotto() function for creating Lotto
    - Implemented createRandomLotto() to create random numbers lotto

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,9 +1,15 @@
 package lotto.controller
 
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
+import lotto.Lotto
 import lotto.view.Input
 
 class LottoController(val input: Input) {
     private fun getLottoTicketCount(): Int {
         return input.getPurchaseAmount() / 1000
+    }
+
+    private fun createRandomLotto(): Lotto {
+        return Lotto(pickUniqueNumbersInRange(1, 45, 6).sorted())
     }
 }
