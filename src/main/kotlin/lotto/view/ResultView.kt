4745f771341798f96290b068f98eae7d5c66b600
Jieun <lje2000lje@naver.com>
2024-11-04 22:20:09 +0900

    feat(ResultView.kt): 당첨 통계 내용 출력

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,6 +1,10 @@
 package lotto.view
 
 import lotto.model.Lotto
+import lotto.model.LottoResult
+import lotto.model.Prize
+import java.text.NumberFormat
+import java.util.Locale
 
 class ResultView {
 
@@ -16,5 +20,19 @@ class ResultView {
     private fun displayTitle() {
         println("당첨 통계")
         println("---")
+    }
+
+    private fun displayPrizes(lottoResult: LottoResult) {
+        Prize.entries
+            .filter { it != Prize.NONE }
+            .sortedWith(compareBy({ it.matchCount }, { it.bonusMatch }))
+            .forEach { prize ->
+                val count = lottoResult.prizeCounts.getOrDefault(prize, 0)
+                val bonusText = if (prize.bonusMatch) ", 보너스 볼 일치" else ""
+                val formattedPrizeMoney =
+                    NumberFormat.getNumberInstance(Locale.KOREA).format(prize.prizeMoney)
+
+                println("${prize.matchCount}개 일치${bonusText} (${formattedPrizeMoney}원) - ${count}개")
+            }
     }
 }
