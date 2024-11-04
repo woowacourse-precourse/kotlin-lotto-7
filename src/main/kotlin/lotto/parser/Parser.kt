7ab7f9ea2ef54b35b0f76fe99e2ef5d5418b4881
Jieun <lje2000lje@naver.com>
2024-11-04 21:34:24 +0900

    feat(Parser.kt): 입력받은 당첨 번호를 쉼표(,)로 구분하여 분리

--- src/main/kotlin/lotto/parser/Parser.kt
+++ src/main/kotlin/lotto/parser/Parser.kt
@@ -4,4 +4,8 @@ class Parser {
     fun parsePurchaseAmount(input: String): Int {
         return input.toInt()
     }
+
+    fun parseWinningNumbers(input: String): List<Int> {
+        return input.split(",").map { it.trim().toInt() }
+    }
 }
