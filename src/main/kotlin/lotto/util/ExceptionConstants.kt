c8da03c6493f6a3091182e998b867adcd1a2e418
Jieun <lje2000lje@naver.com>
2024-11-04 21:10:42 +0900

    feat(ExceptionConstants.kt): 로또 번호 입력 유효성 메시지 추가

--- src/main/kotlin/lotto/util/ExceptionConstants.kt
+++ src/main/kotlin/lotto/util/ExceptionConstants.kt
@@ -1,5 +1,9 @@
 package lotto.util
 
 class ExceptionConstants {
-
+    companion object {
+        const val ERROR_INVALID_WINNING_NUMBERS_COUNT = "[ERROR] 로또 번호는 6개여야 합니다."
+        const val ERROR_INVALID_WINNING_NUMBERS_RANGE = "[ERROR] 로또 번호는 1부터 45 사이여야 합니다."
+        const val ERROR_DUPLICATE_WINNING_NUMBERS = "[ERROR] 로또 번호는 중복될 수 없습니다."
+    }
 }
