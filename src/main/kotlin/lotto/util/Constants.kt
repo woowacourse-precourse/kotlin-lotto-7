4e47b39ecc5958c9f9d6454334b80ef4209b1b88
Jieun <lje2000lje@naver.com>
2024-11-04 22:08:23 +0900

    feat(Constants.kt): 상금 금액 상수 추가

--- src/main/kotlin/lotto/util/Constants.kt
+++ src/main/kotlin/lotto/util/Constants.kt
@@ -6,5 +6,11 @@ class Constants {
         const val MIN_NUMBER = 1
         const val MAX_NUMBER = 45
         const val WINNING_NUMBERS_COUNT = 6
+
+        const val FIRST_PRIZE_MONEY = 2_000_000_000
+        const val SECOND_PRIZE_MONEY = 30_000_000
+        const val THIRD_PRIZE_MONEY = 1_500_000
+        const val FOURTH_PRIZE_MONEY = 50_000
+        const val FIFTH_PRIZE_MONEY = 5_000
     }
 }
