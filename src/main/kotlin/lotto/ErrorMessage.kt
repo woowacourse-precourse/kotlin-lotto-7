90a35dd7eacd27c84d603a836e761d6a95400798
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-04 18:03:37 +0900

    feat: 로또 번호를 중복입력하면 재입력 받는다

--- src/main/kotlin/lotto/ErrorMessage.kt
+++ src/main/kotlin/lotto/ErrorMessage.kt
@@ -1,3 +1,4 @@
 package lotto
 
 const val NOT_NUMBER = "[ERROR] 구입금액 입력이 숫자가 아닙니다"
+const val DUPLICATION_NUMBER = "[ERROR] 로또 번호가 중복입니다"
--- src/main/kotlin/lotto/Input.kt
+++ src/main/kotlin/lotto/Input.kt
@@ -5,23 +5,61 @@ import camp.nextstep.edu.missionutils.Console
 class Input {
     private fun input(): String = Console.readLine()
 
+    private fun checkChangeInt(input  : String) : Boolean {
+        try {
+            input.toInt()
+            return true
+        }catch (e: NumberFormatException){
+            throw IllegalArgumentException(NOT_NUMBER)
+        }
+    }
+
     fun getAmount(): Int {
         println(REQUEST_AMOUNT_MESSAGE);
-        return input().toInt()
+
+        while (true) {
+            val input = input()
+
+            try {
+                checkChangeInt(input)
+                return input.toInt()
+
+            }catch (e : IllegalArgumentException){
+                println(NOT_NUMBER)
+            }
+        }
+    }
+
+    private fun checkDuplicationLottoNumbers(lottoNumbers: List<Int>){
+        val duplicationNumbers = lottoNumbers.filterIndexed { idx, it -> lottoNumbers.indexOf(it) != idx }
+        if(duplicationNumbers.isNotEmpty()){
+           throw IllegalArgumentException(DUPLICATION_NUMBER)
+        }
     }
 
     fun getLottoNumbers(): List<Int> {
         println(REQUEST_NUMBERS_MESSAGE)
-        return input().split(',').map { it.toInt() }
+
+        while (true){
+            val lottoNumbers = input().split(',').map { it.toInt() }
+
+            try{
+                checkDuplicationLottoNumbers(lottoNumbers)
+                return lottoNumbers
+            }catch (e : IllegalArgumentException){
+                println(DUPLICATION_NUMBER)
+            }
+        }
     }
 
-    fun getLottoBonusNumbers() : Int {
+    fun getLottoBonusNumbers(): Int {
         println(REQUEST_BONUS_NUMBERS_MESSAGE)
         return input().toInt()
     }
 
     companion object {
         private const val REQUEST_AMOUNT_MESSAGE = "구입금액을 입력해 주세요."
+        private const val REQUEST_AMOUNT_MESSAGE_AGAIN = "구입금액을 형식에 맞춰 재입력해 주세요."
         private const val REQUEST_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요."
         private const val REQUEST_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요."
     }
