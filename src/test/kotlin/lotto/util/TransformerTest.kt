cc34d0c75544b6ca989bf570f14d8f5136a43e0c
조형석 <chs9902@naver.com>
2024-11-04 17:59:46 +0900

    :white_check_mark: test: 변환 클래스 단위 테스트 추가

--- src/test/kotlin/lotto/util/TransformerTest.kt
+++ src/test/kotlin/lotto/util/TransformerTest.kt
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Assertions.*
 class TransformerTest {
 
     @Test
-    fun stringToIntInt() {
+    fun `문자열 목록이 올바르게 정수 목록으로 변환된다`() {
+        val stringList = listOf("1", "2", "3", "4", "5", "6")
+        val result = Transformer().stringToIntInt(stringList)
+
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), result, "변환 결과가 예상과 다릅니다.")
     }
 }
