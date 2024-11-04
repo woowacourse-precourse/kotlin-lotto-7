68ea0cc06214b4cfa02198d87efe29caad24708b
kangyein9892 <inwon9892@naver.com>
2024-11-04 16:48:20 +0900

    feat(Lotto.kt): Lotto 생성 기능 및 Lotto 번호 저장 프로퍼티 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,8 +1,20 @@
 package lotto.model
+
+import camp.nextstep.edu.missionutils.Randoms
 import lotto.util.ValidatorUtil.validateLottoSize
 
 class Lotto(private val numbers: List<Int>) {
     init {
         validateLottoSize(numbers.size)
     }
+
+    companion object {
+        fun generate(): Lotto {
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+            return Lotto(numbers)
+        }
+    }
+
+    fun getNumbers(): List<Int> = numbers
+
 }
