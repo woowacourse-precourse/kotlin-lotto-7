ab28fa7f650ccaf2c5474791a0a56132bb108d84
kangyein9892 <inwon9892@naver.com>
2024-11-04 17:51:27 +0900

    feat(LottoViewImpl.kt): 당첨 계산된 티켓을 보여주는 함수 구현

--- src/main/kotlin/lotto/util/ConstantsUtil.kt
+++ src/main/kotlin/lotto/util/ConstantsUtil.kt
@@ -2,5 +2,8 @@ package lotto.util
 
 object ConstantsUtil {
     const val MESSAGE_NUMBER_SIZE_ERROR = "[ERROR] 로또 번호는 6개여야 합니다."
+    const val MESSAGE_TICKETS_COUNT = "%d개를 구매했습니다."
+    const val MESSAGE_SHOW_CALCULATED_TICKETS = "당첨 통계\n ---"
+    const val MESSAGE_CALCULATED_TICKETS = "%d개 일치 %d원 - %d개"
     const val TICKET_PRICE = 1000
 }
--- src/main/kotlin/lotto/view/LottoViewImpl.kt
+++ src/main/kotlin/lotto/view/LottoViewImpl.kt
@@ -4,7 +4,21 @@ class LottoViewImpl: LottoView {
         println(formatShowTickets(tickets.size))
         tickets.forEach { println(it.getNumbers()) }
     }
+
+    override fun showCalculatedTickets(calculatedTickets: Map<LottoRank, Int>) {
+        println(MESSAGE_SHOW_CALCULATED_TICKETS)
+        LottoRank.entries.forEach { rank ->
+            if(rank != LottoRank.NONE){
+                println(formatCalculatedTickets(rank.matchCount, rank.reward, calculatedTickets[rank] ?: 0))
+            }
+        }
+    }
     private fun formatShowTickets(ticketsSize: Int): String{
         return MESSAGE_TICKETS_COUNT.format(ticketsSize)
+    }
+
+    private fun formatCalculatedTickets(matchCount: Int, reward: Int, ticketCount: Int): String{
+        val formatReward = NumberFormat.getNumberInstance().format(reward)
+        return MESSAGE_CALCULATED_TICKETS.format(matchCount, formatReward, ticketCount)
     }
 }
