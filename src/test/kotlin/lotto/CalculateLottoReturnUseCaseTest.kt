b1cd2c9103e4443f5f4db19b740c977b04c83b00
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-04 22:26:35 +0900

    refactor(CalculateLottoReturnUseCaseTest): 테스트 수정
    - 1~5등까지 및 당첨 되지 않은 상금까지 단일 상금 테스트를 통합
    - 소수점 두 번째 자리 반올림 테스트의 DisplayName을 상세하게 수정

--- src/test/kotlin/lotto/CalculateLottoReturnUseCaseTest.kt
+++ src/test/kotlin/lotto/CalculateLottoReturnUseCaseTest.kt
@@ -4,71 +4,49 @@ import lotto.domain.usecase.CalculateLottoReturnUseCase
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class CalculateLottoReturnUseCaseTest {
     private val calculateLottoReturnUseCase = CalculateLottoReturnUseCase()
 
-    @Test
-    fun `1등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(2_000_000_000), 1_000)
-        val expect = "200000000.0%"
-        assertEquals(result, expect)
-    }
-
-    @Test
-    fun `2등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(30_000_000), 1_000)
-        val expect = "3000000.0%"
-        assertEquals(result, expect)
-    }
-
-    @Test
-    fun `3등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(1_500_000), 1_000)
-        val expect = "150000.0%"
-        assertEquals(result, expect)
-    }
-
-    @Test
-    fun `4등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(50_000), 1_000)
-        val expect = "5000.0%"
-        assertEquals(result, expect)
-    }
-
-    @Test
-    fun `5등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(5_000), 1_000)
-        val expect = "500.0%"
-        assertEquals(result, expect)
-    }
-
-    @Test
-    fun `6등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(0), 1_000)
-        val expect = "0.0%"
-        assertEquals(result, expect)
+    @ParameterizedTest
+    @CsvSource(
+        "2000000000, 200000000.0%",
+        "30000000, 3000000.0%",
+        "1500000, 150000.0%",
+        "50000, 5000.0%",
+        "5000, 500.0%",
+        "0, 0.0%"
+    )
+    fun `1등에서 5등 그리고 당첨 되지 않은 상금까지 단일 상금 수익률 계산 테스트`(
+        winningPrize: Int,
+        expectedRate: String
+    ) {
+        val result = calculateLottoReturnUseCase.execute(listOf(winningPrize), 1_000)
+        assertEquals(result, expectedRate)
     }
 
     @Test
     fun `1등과 4등 당첨 수익률 계산 테스트`() {
-        val result = calculateLottoReturnUseCase.execute(listOf(2_000_000_000,50_000), 2_000)
+        val result = calculateLottoReturnUseCase.execute(listOf(2_000_000_000, 50_000), 2_000)
         val expect = "100002500.0%"
         assertEquals(result, expect)
     }
 
-    @DisplayName("56.5555... 반올림 테스트")
+    @DisplayName("소수점 두 번째 자리에서 반올림하여 올림되는 경우 (5,000원 당첨, 9,000원 지출 -> 55.555..% -> 55.6%)")
     @Test
-    fun `소수점 두번째 반올림으로 올림하는 계산 테스트`(){
-        val result = calculateLottoReturnUseCase.execute(listOf(5000,0,0,0,0,0,0,0,0), 9000)
+    fun `소수점 두번째 반올림으로 올림하는 계산 테스트`() {
+        val result = calculateLottoReturnUseCase.execute(listOf(5000, 0, 0, 0, 0, 0, 0, 0, 0), 9000)
         val expect = "55.6%"
         assertEquals(result, expect)
     }
 
-    @DisplayName("83.333... 반올림 테스트")
+    @DisplayName("소수점 두 번째 자리에서 반올림하여 내림되는 경우 (5,000원 당첨, 6,000원 지출 -> 83.333..% -> 83.3)")
     @Test
-    fun `소수점 두번째 반올림으로 내림하는 계산 테스트`(){
-        val result = calculateLottoReturnUseCase.execute(listOf(5000,0,0,0,0,0), 6000)
+    fun `소수점 두번째 반올림으로 내림하는 계산 테스트`() {
+        val result = calculateLottoReturnUseCase.execute(listOf(5000, 0, 0, 0, 0, 0), 6000)
         val expect = "83.3%"
         assertEquals(result, expect)
     }
