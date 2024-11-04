4846a409709109cc503060a419c22c76da33eb35
Min Seon <cbdhoho1@gmail.com>
2024-11-04 20:09:50 +0900

    fix: prize enum class 수정

--- src/main/kotlin/lotto/model/Prize.kt
+++ src/main/kotlin/lotto/model/Prize.kt
@@ -1,9 +1,21 @@
 package lotto.model
 
-enum class Prize(count: Int, money: Int) {
-    hitThree(3, 5000),
-    hitFour(4, 50000),
-    hitFive(5, 1500000),
-    hitFiveBonus(5, 30000000),
-    hitSix(6, 2000000000);
+enum class Prize(count: Int, money: Int, val description: String) {
+    hitThree(3, 5000, "3개 일치"),
+    hitFour(4, 50000, "4개 일치"),
+    hitFive(5, 1500000, "5개 일치"),
+    hitFiveBonus(5, 30000000, "5개 일치, 보너스 볼 일치"),
+    hitSix(6, 2000000000, "6개 일치");
+
+    companion object {
+        fun getPrize(matchCount: Int, bonusMatch: Boolean): Prize? {
+            return when (matchCount) {
+                6 -> hitSix
+                5 -> if (bonusMatch) hitFiveBonus else hitFive
+                4 -> hitFour
+                3 -> hitThree
+                else -> null
+            }
+        }
+    }
 }
