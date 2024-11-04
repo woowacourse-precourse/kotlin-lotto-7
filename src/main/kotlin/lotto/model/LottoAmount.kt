33e1cb1e2593dcc629bdc5ad3416342b658e5a58
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-11-04 17:11:01 +0900

    test: 구입 금액에 대한 테스트 추가
    - LottoAmountTest.kt: 로또 구입 금액이 0원 이하일 경우의 테스트를 추가합니다.

--- src/main/kotlin/lotto/model/LottoAmount.kt
+++ src/main/kotlin/lotto/model/LottoAmount.kt
@@ -5,7 +5,7 @@ import lotto.model.Lotto.Companion.LOTTO_PRICE
 class LottoAmount(val money: Int) {
 
     init {
-        require(money > ZERO) { "[ERROR] 로또 구매 금액은 0원 이상이어야 합니다." }
+        require(money > ZERO) { "[ERROR] 로또 구매 금액은 0원 초과여야 합니다." }
         require(money % LOTTO_PRICE == ZERO) { "[ERROR] 로또 구매 금액은 천 원 단위로 입력 가능합니다." }
     }
 
--- src/test/kotlin/lotto/LottoAmountTest.kt
+++ src/test/kotlin/lotto/LottoAmountTest.kt
@@ -14,4 +14,22 @@ class LottoAmountTest {
             LottoAmount(lottoAmount)
         }
     }
+
+    @Test
+    fun `구입 금액이 음수일 경우 예외가 발생한다`() {
+        val lottoAmount = -1_500
+
+        assertThrows<IllegalArgumentException> {
+            LottoAmount(lottoAmount)
+        }
+    }
+
+    @Test
+    fun `구입 금액이 0원일 경우 예외가 발생한다`() {
+        val lottoAmount = 0
+
+        assertThrows<IllegalArgumentException> {
+            LottoAmount(lottoAmount)
+        }
+    }
 }
