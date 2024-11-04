026d0b8f87a58d8d4dc4bdd47c0ec10739b53d35
jhseo12 <jhseo1778@gmail.com>
2024-11-04 21:15:33 +0900

    test: 수익률 테스트 케이스 추가

--- src/test/kotlin/lotto/EarningRateTest.kt
+++ src/test/kotlin/lotto/EarningRateTest.kt
@@ -17,21 +17,27 @@ class EarningRateTest {
         listOf(21, 3, 14, 25, 37, 12), // 1
         listOf(42, 10, 19, 14, 13, 3) // 5 + 1
     )
-    private val prizeNumber = listOf(42, 13, 10, 14, 32, 19)
+    private val prizeNumberOne = listOf(1, 2, 3, 4, 5, 6)
+    private val prizeNumberTwo = listOf(42, 13, 10, 14, 32, 19)
     private val bonusNumber = 3
 
     @Test
     fun `수익률이 올바르게 구해지는지 확인 1`() {
-        assertThat(verifyPrize.prizeResult(ticketOne, prizeNumber, bonusNumber)).isEqualTo(1500.0)
+        assertThat(verifyPrize.prizeResult(ticketOne, prizeNumberOne, bonusNumber)).isEqualTo(0.0)
     }
 
     @Test
     fun `수익률이 올바르게 구해지는지 확인 2`() {
-        assertThat(verifyPrize.prizeResult(ticketTwo, prizeNumber, bonusNumber)).isEqualTo(1000166.7)
+        assertThat(verifyPrize.prizeResult(ticketOne, prizeNumberTwo, bonusNumber)).isEqualTo(1500.0)
     }
 
     @Test
     fun `수익률이 올바르게 구해지는지 확인 3`() {
-        assertThat(verifyPrize.prizeResult(ticketOne + ticketTwo, prizeNumber, bonusNumber)).isEqualTo(429500.0)
+        assertThat(verifyPrize.prizeResult(ticketTwo, prizeNumberTwo, bonusNumber)).isEqualTo(1000166.7)
+    }
+
+    @Test
+    fun `수익률이 올바르게 구해지는지 확인 4`() {
+        assertThat(verifyPrize.prizeResult(ticketOne + ticketTwo, prizeNumberTwo, bonusNumber)).isEqualTo(429500.0)
     }
 }
