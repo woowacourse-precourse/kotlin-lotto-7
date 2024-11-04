88c2719f41945b455d55139c5eda52872ad88660
joo <yjy010222gmail.com>
2024-11-04 17:22:25 +0900

    feat: 구입 금액에 따라 로또 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,9 +2,13 @@ package lotto
 
 fun main() {
     val purchase_money = Purchase_money()
+    var lotto_number = MutableList<List<Int>>(purchase_money){ emptyList() }
+
+    lotto_number=LottoNumberPrint(purchase_money)
 
     return
 }
+
 
 
 
---
+++ src/main/kotlin/lotto/OutputView.kt
@@ -0,0 +1,15 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Randoms
+
+fun LottoNumberPrint(purchase_money: Int): MutableList<List<Int>> {
+    var lotto_num = MutableList<List<Int>>(purchase_money){ emptyList() }
+    val PURCHASE_NUM_MESSAGE = "개를 구입했습니다."
+
+    println("$purchase_money$PURCHASE_NUM_MESSAGE")
+    for (i: Int in 0..<(purchase_money)) {
+        lotto_num[i]= Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        println(lotto_num[i])
+    }
+    return lotto_num
+}
