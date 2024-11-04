de145bc93330a6367305e47c589a46a11ab586ed
SANDY <nnal0256@naver.com>
2024-11-04 19:13:05 +0900

    test: 로또 당첨 등수 단위 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.utils.LottoGenerator
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -19,5 +21,63 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `기능 테스트-6개의 번호가 모두 일치하면 PLACE_1st를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+        val rank = lotto.getWinningRank(winningNumber, bonusNumber)
+        assertEquals(WinningRank.PLACE_1st, rank)
+    }
+
+    @Test
+    fun `기능 테스트-5개의 번호가 일치하고 보너스 번호도 일치하면 PLACE_2nd를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+        val rank = lotto.getWinningRank(winningNumber, bonusNumber)
+        assertEquals(WinningRank.PLACE_2nd, rank)
+    }
+
+    @Test
+    fun `기능 테스트-5개의 번호가 일치하고 보너스 번호가 일치하지 않으면 PLACE_3rd를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 10))
+        val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+        val rank = lotto.getWinningRank(winningNumber, bonusNumber)
+        assertEquals(WinningRank.PLACE_3rd, rank)
+    }
+
+    @Test
+    fun `기능 테스트-4개의 번호가 일치하면 PLACE_4th를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 7, 10))
+        val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+        val rank = lotto.getWinningRank(winningNumber, bonusNumber)
+        assertEquals(WinningRank.PLACE_4th, rank)
+    }
+
+    @Test
+    fun `기능 테스트-3개의 번호가 일치하면 PLACE_5th를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 7, 8, 10))
+        val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+        val rank = lotto.getWinningRank(winningNumber, bonusNumber)
+        assertEquals(WinningRank.PLACE_5th, rank)
+    }
+
+    @Test
+    fun `기능 테스트-일치하는 번호가 3개 미만이면 PLACE_Fail를 반환한다`() {
+        //val lotto = Lotto(listOf(1, 2, 9, 10, 11, 12))
+        //val lotto = Lotto(listOf(1, 9, 10, 11, 12, 15))
+        val lotto = Lotto(listOf(9, 10, 11, 12, 15, 18))
+        val winningNumber = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+        val rank = lotto.getWinningRank(winningNumber, bonusNumber)
+        assertEquals(WinningRank.PLACE_Fail, rank)
+    }
+
+
+
+
 }
