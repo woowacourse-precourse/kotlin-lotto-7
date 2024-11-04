b3a388b3d45db8e754d6bdeb09f7e0026aa4a4d4
조형석 <chs9902@naver.com>
2024-11-04 17:32:35 +0900

    :white_check_mark: test: 로또 생서 클래스 단위 테스트 추가

--- src/test/kotlin/lotto/model/LotteryGeneratorTest.kt
+++ src/test/kotlin/lotto/model/LotteryGeneratorTest.kt
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.Assertions.*
 class LotteryGeneratorTest {
 
     @Test
-    fun makeLotto() {
+    fun `금액에 따라 올바른 수의 로또가 생성된다`() {
+        val money = 3000
+        val expectedLottoCount = money / 1000
+        val lotteryGenerator = LotteryGenerator()
+        lotteryGenerator.makeLotto(money)
+        val lotteries = lotteryGenerator.getLotteries()
+
+        assertEquals(expectedLottoCount, lotteries.size, "로또의 개수가 일치하지 않습니다.")
     }
 
-    @Test
-    fun getLotteries() {
-    }
 }
