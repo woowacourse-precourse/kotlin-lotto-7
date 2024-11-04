5cb92553d5a18abba2fc2a252763acee496b3079
chaemina <chaemina82@naver.com>
2024-11-04 17:11:44 +0900

    feat(LottoConstants) : 상수 관리 클래스 작성
    에러메세지, 로또 가격, 숫자 범위 등의 고정값을 미리 정의
    - 미리 작성해 고정값을 다른 클래스에서 참조하기 위함

---
+++ src/main/kotlin/lotto/constants/LottoConstans.kt
@@ -0,0 +1,11 @@
+package lotto.constants
+
+object LottoConstants {
+  const val LOTTO_NUMBER_MIN = 1
+  const val LOTTO_NUMBER_MAX = 45
+  const val LOTTO_PRICE = 1000
+  const val ERROR_INVALID_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+  const val ERROR_INVALID_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다."
+  const val ERROR_DUPLICATE_NUMBER = "[ERROR] 로또 번호는 중복될 수 없습니다."
+  const val ERROR_LOTTO_SIZE = "[ERROR] 로또 번호는 6개여야 합니다."
+}
