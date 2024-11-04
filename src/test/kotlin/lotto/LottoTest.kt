a01a6135dde05fe41607b38ea787893587f0ef95
DDangDin <Kcorelec7123@gmail.com>
2024-11-04 17:49:56 +0900

    test: LottoTest 테스트 코드 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -21,7 +20,14 @@ class LottoTest {
     }
 
     @Test
-    fun `로또 번호 오름차순으로 정렬`() {
+    fun `로또 번호가 최대 번호인 45를 초과하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 오름차순으로 올바르게 정렬 됐는지 테스트`() {
         val testList = listOf(6,5,4,3,2,1)
         val lotto = Lotto(testList)
         val expectedList = testList.sorted()
