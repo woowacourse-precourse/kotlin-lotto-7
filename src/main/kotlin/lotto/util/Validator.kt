b5a248d3ef603b67f35b6b2279ab358daf1a846a
조형석 <chs9902@naver.com>
2024-11-04 14:05:22 +0900

    :sparkles: feat: 보너스 번호 범위 검증 기능 추가

--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -4,51 +4,56 @@ import java.lang.NumberFormatException
 
 class Validator {
 
-    fun validateMoneyInput(money: String){
+    fun validateMoneyInput(money: String) {
         isNumber(money)
         isThousandUnit(money)
         isZero(money)
     }
 
-    private fun isNumber(money:String){
+    private fun isNumber(money: String) {
         try {
             money.toInt()
-        }catch (e: NumberFormatException){
-            println("[ERROR] 돈은 숫자여야 합니다.")
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 입력이 숫자여야 합니다.")
             throw IllegalArgumentException("[ERROR] 돈은 숫자여야 합니다.")
         }
     }
 
-    private fun isZero(money:String){
-
+    private fun isZero(money: String) {
         if (money.toInt() == 0) {
             println("[ERROR] 돈은 주셔야 합니다.")
             throw IllegalArgumentException("[ERROR] 돈은 주셔야 합니다.")
         }
     }
 
-    private fun isThousandUnit(money:String){
+    private fun isThousandUnit(money: String) {
         if (money.toInt() % 1000 != 0) {
             println("[ERROR] 돈은 천 단위 숫자여야 합니다.")
             throw IllegalArgumentException("[ERROR] 돈은 천 단위 숫자여야 합니다.")
         }
     }
 
-    fun validateWinningNumberInput(winningNumber: String){
+    fun validateWinningNumberInput(winningNumber: String) {
         validateLottoFormat(winningNumber)
     }
 
-    private fun validateLottoFormat(winningNumber: String){
+    private fun validateLottoFormat(winningNumber: String) {
         val reg = Regex("[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+,[0-9]+")
-        if (!reg.matches(winningNumber)){
+        if (!reg.matches(winningNumber)) {
             println("[ERROR] 로또 입력 형식이 맞지 않습니다.")
             throw IllegalArgumentException("[ERROR] 로또 입력 형식이 맞지 않습니다.")
         }
     }
 
+    fun validateBonusNumberInput(bonusNumber: String) {
+        isNumber(bonusNumber)
+        validateBonusNumberrange(bonusNumber)
+    }
 
-
-    fun validateBonusNumberInput(winningNumber: String){
-
+    private fun validateBonusNumberrange(bonusNumber: String) {
+        if (bonusNumber.toInt() > 45 || bonusNumber.toInt() < 1) {
+            println("[ERROR] 로또 번호의 범위는 1 ~ 45 입니다.")
+            throw IllegalArgumentException("[ERROR] 로또 번호의 범위는 1 ~ 45 입니다.")
+        }
     }
 }
