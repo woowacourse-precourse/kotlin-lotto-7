3324b0cfe13d8cdfbadfdf4056060f75a3abd854
hwsg0912@naver.com <hwsg0912@naver.com>
2024-11-04 17:34:59 +0900

    chore: matchesBonusNumber 파라미터를 사용하지 않아 제거

--- src/main/kotlin/lotto/model/LottoRank.kt
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -4,20 +4,18 @@ enum class LottoRank(
     val message: String,
     val price: Int,
     val matchingNumbers: Int,
-    val matchesBonusNumber: Boolean,
 ) {
-    NONE_RANK("꽝",0,0,false),
-    FIFTH_RANK("3개 일치",5_000,3,false),
-    FOURTH_RANK("4개 일치",50_000,4,false),
-    THIRD_RANK("5개 일치",1_500_000,5,false),
-    SECOND_RANK("5개 일치, 보너스 볼 일치", 30_000_000,5,true),
-    FIRST_RANK("6개 일치", 2_000_000_000,6,false);
+    NONE_RANK("꽝",0,0),
+    FIFTH_RANK("3개 일치",5_000,3),
+    FOURTH_RANK("4개 일치",50_000,4),
+    THIRD_RANK("5개 일치",1_500_000,5),
+    SECOND_RANK("5개 일치, 보너스 볼 일치", 30_000_000,5),
+    FIRST_RANK("6개 일치", 2_000_000_000,6);
 
     companion object {
         fun matchingLottoNumber(winningNumbers: Lotto, bonusNumber: Int, purchaseLotto: Lotto): Pair<Int, Boolean> {
             val winningLotto = winningNumbers.getLotto()
             val userLotto = purchaseLotto.getLotto()
-
             var matchingCount = 0
             val isBonusNumber = containsBonusNumber(bonusNumber, purchaseLotto)
 
@@ -26,7 +24,6 @@ enum class LottoRank(
                     matchingCount++
                 }
             }
-
             return Pair(matchingCount, isBonusNumber)
         }
 
