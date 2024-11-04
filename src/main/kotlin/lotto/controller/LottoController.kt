7d2249da55731edce43bddd16ab4bf091b7bfea3
Jieun <lje2000lje@naver.com>
2024-11-04 22:25:51 +0900

    feat(LottoController.kt): 구매금액 입력 받기

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,5 +1,16 @@
 package lotto.controller
 
-class LottoController {
+import lotto.parser.Parser
+import lotto.validator.Validator
+import lotto.view.InputView
 
+class LottoController {
+    private val validator = Validator()
+    private val parser = Parser()
+    private val inputView = InputView(validator, parser)
+
+    fun start() {
+        val purchaseAmount = inputView.inputPurchaseAmount()
+
+    }
 }
