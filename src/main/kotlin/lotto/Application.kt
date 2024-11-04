5444a5b1cbe4d95837190d662fb60185b69ebc8a
eeazzy <khs2498@gmail.com>
2024-11-04 20:58:09 +0900

    feat: 로또 발매 기능 추가
    구입 금액을 입력받아 로또 개수 계산
    각 로또는 1~45 사이의 중복 없는 6개의 숫자로 구성
    구입한 로또 티켓을 출력하도록 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,27 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("구입금액을 입력해 주세요.")
+    val amount = readLine()?.toIntOrNull() ?: 0
+    val lottoCount = amount / 1000
+
+    println("\n${lottoCount}개를 구매했습니다.")
+    val lottoTickets = generateLottoTickets(lottoCount)
+    lottoTickets.forEach { println(it.sorted()) }
+}
+
+// 개수만큼 로또 발행
+fun generateLottoTickets(count: Int): List<List<Int>> {
+    val tickets = mutableListOf<List<Int>>()
+    repeat(count) {
+        tickets.add(generateLottoTicket())
+    }
+    return tickets
+}
+
+// 랜덤 로또 번호 생성
+fun generateLottoTicket(): List<Int> {
+    return Randoms.pickUniqueNumbersInRange(1, 45, 6).toList()
 }
