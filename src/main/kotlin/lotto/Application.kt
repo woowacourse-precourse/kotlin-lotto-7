c22fe55d21701d72520bb7e572121c6d661c570c
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2024-11-04 20:15:06 +0900

    feat(Application.kt) : 당첨 번호 입력 및 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,7 +5,49 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val money = inputHowMuch()
     val myLottos = makeMyLotto(money)
-    println()
+    val hitNumbers = inputHitNumbers()
+}
+
+fun inputHitNumbers(): List<Int> {
+    val inputMessage = "당첨 번호를 입력해 주세요."
+    println(inputMessage)
+    val inputHit = Console.readLine()
+    Console.close()
+    return validateHitNumbers(inputHit)
+}
+
+fun validateHitNumbers(input: String) : List<Int> {
+    validateEmpty(input)
+    validateBlank(input)
+    validatePattern(input)
+    val parsedInput = parseByComma(input)
+    validateBetween1And45(parsedInput)
+    validateDuplicate(parsedInput)
+    return parsedInput
+}
+
+fun validateDuplicate(parsedInput: List<Int>) {
+    val exceptionMessage = "중복된 숫자가 존재하면 안 됩니다."
+    require(parsedInput.groupingBy { it }.eachCount().all { 1 == it.value }){throw IllegalArgumentException(exceptionMessage)}
+}
+
+fun validateBetween1And45(parsedInput: List<Int>) {
+    val exceptionMessage = "1이상 45이하의 숫자여야 합니다."
+    require(parsedInput.all { it in Lotto.MIN_LOTTO_NUMBER..Lotto.MAX_LOTTO_NUMBER }) {
+        throw IllegalArgumentException(
+            exceptionMessage
+        )
+    }
+}
+
+fun parseByComma(input: String): List<Int> {
+    return input.split(",").map { it.toInt() }.toList()
+}
+
+fun validatePattern(input: String) {
+    val exceptionMessage = "숫자,숫자,숫자,숫자,숫자,숫자 패턴이 아닙니다."
+    val pattern = Regex("^(\\d+,){5}\\d+$") // 숫콤숫콤숫콤숫콤숫콤숫(숫 : 숫자, 콤 : 콤마)
+    require(pattern.matches(input)) { throw IllegalArgumentException(exceptionMessage) }
 }
 
 fun makeMyLotto(money: Int): List<Lotto> {
