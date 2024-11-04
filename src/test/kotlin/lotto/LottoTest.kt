73d1dc40cea57680061bd4ad989fa0924d92d998
yoon <cbj0010@naver.com>
2024-11-04 18:50:17 +0900

    test : lotto 개수 유효성 체크
    lotto가 6개 뽑히지 않았을 경우 에러를 전달

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -3,6 +3,7 @@ package lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import validator.LottoGenerator
+import validator.RangeValidator
 
 class LottoTest {
     @Test
@@ -39,15 +40,28 @@ class LottoTest {
 
     @Test
     fun `1~45 사이의 숫자를 뽑은 경우`() {
-        val duplicateValidate = LottoGenerator()
+        val rangeValidate = LottoGenerator()
         val numbers = listOf(1, 2, 3, 45, 5, 6)
-        duplicateValidate.validate(numbers)
+        rangeValidate.validate(numbers)
     }
 
     @Test
     fun `1~45 사이의 숫자를 뽑지 않은 경우`() {
-        val duplicateValidate = LottoGenerator()
+        val rangeValidate = RangeValidator()
         val numbers = listOf(0, 2, 3, 49, 5, 6)
-        duplicateValidate.validate(numbers)
+        assertThrows<IllegalArgumentException>("로또 번호는 1~45 사이의 값이여야 합니다.") {
+            rangeValidate.validate(numbers)
+        }
     }
+
+    @Test
+    fun `6개의 숫자를 선택하지 않은 경우`() {
+        val rangeValidate = RangeValidator()
+        val numbers = listOf(2, 3, 49, 5, 6)
+        assertThrows<IllegalArgumentException>("로또는 6개를 선택해야 됩니다.") {
+            rangeValidate.validate(numbers)
+        }
+    }
+
+
 }
