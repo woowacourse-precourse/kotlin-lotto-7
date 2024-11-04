a37b4c84200c8a32477de07ff76f07dd3fe680a3
Yujin Choe <dev.yujinchoe@gmail.com>
2024-11-04 23:50:23 +0900

    fix(lotto): 로또 발행 내역 출력 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -10,6 +10,7 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     companion object {
+        // 로또 번호 생성
         fun generateNumbers(): List<Int> {
             return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
         }
@@ -19,23 +20,15 @@ class Lotto(private val numbers: List<Int>) {
             require(amount > 0 && amount % 1000 == 0) { "[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다." }
         }
 
-        // 로또 번호 유효성 검증
-        fun validateLottoNumbers(numbers: List<Int>) {
-            require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
-            require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
-            require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1부터 45 사이여야 합니다." }
+        // 당첨 번호와 보너스 번호 검증
+        fun validateWinningNumbers(winningNumbers: List<Int>, bonusNumber: Int) {
+            require(winningNumbers.size == 6) { "[ERROR] 당첨 번호는 6개여야 합니다." }
+            require(winningNumbers.distinct().size == 6) { "[ERROR] 당첨 번호는 중복되지 않아야 합니다." }
+            require(winningNumbers.all { it in 1..45 }) { "[ERROR] 당첨 번호는 1부터 45 사이여야 합니다." }
+            require(!winningNumbers.contains(bonusNumber)) { "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다." }
+            require(bonusNumber in 1..45) { "[ERROR] 보너스 번호는 1부터 45 사이여야 합니다." }
         }
     }
-
-    // 당첨 번호와 보너스 번호 검증
-    fun validateWinningNumbers(winningNumbers: List<Int>, bonusNumber: Int) {
-        require(winningNumbers.size == 6) { "[ERROR] 당첨 번호는 6개여야 합니다." }
-        require(winningNumbers.distinct().size == 6) { "[ERROR] 당첨 번호는 중복되지 않아야 합니다." }
-        require(winningNumbers.all { it in 1..45 }) { "[ERROR] 당첨 번호는 1부터 45 사이여야 합니다." }
-        require(!winningNumbers.contains(bonusNumber)) { "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다." }
-        require(bonusNumber in 1..45) { "[ERROR] 보너스 번호는 1부터 45 사이여야 합니다." }
-    }
-
 
     // 당첨 내역 계산
     fun calculateWinningResults(winningNumbers: List<Int>, bonusNumber: Int): WinningResults {
@@ -45,18 +38,7 @@ class Lotto(private val numbers: List<Int>) {
         return WinningResults(matchCount, isBonusMatched)
     }
 
-    // 수익률 계산
-    fun calculateYield(totalInvestment: Int, winningResults: WinningResults): Double {
-        val prize = when (winningResults.matchCount) {
-            6 -> 2000000000 // 1등
-            5 -> if (winningResults.isBonusMatched) 30000000 else 1500000 // 2등, 3등
-            4 -> 50000 // 4등
-            3 -> 5000 // 5등
-            else -> 0 // 꽝
-        }
-        return if (totalInvestment == 0) 0.0 else prize.toDouble() / totalInvestment
-    }
-
+    // 로또 번호 출력
     override fun toString(): String {
         return numbers.toString()
     }
