ec29a6d84c0448d91130165c8e2240b508f77685
Yujin Choe <dev.yujinchoe@gmail.com>
2024-11-04 23:22:07 +0900

    feat(lotto): 로또 번호 유효성 검증 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,6 +6,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
         require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1부터 45 사이여야 합니다." }
     }
 
     companion object {
