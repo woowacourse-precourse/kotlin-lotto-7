36ed12969ecf0517fc9b3a196c3698313a2dd31d
minseok0416 <day5274567@gmail.com>
2024-11-04 23:31:59 +0900

    feat(Application): 메인 메서드 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,22 @@
 package lotto
 
 fun main() {
-    // TODO: 프로그램 구현
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    val purchaseAmount = inputView.getPurchaseAmount()
+    val lottoCount = purchaseAmount / 1000
+
+    val myLotto = Lotto.issueLotto(lottoCount)
+    outputView.printLottoCount(lottoCount, myLotto)
+
+    val winningNumbers = inputView.getLottoNumbers()
+    val bonusNumber = inputView.getBonusNumber()
+
+    val lotto = Lotto(winningNumbers)
+    lotto.getLottoRank(myLotto, bonusNumber)
+    outputView.printLottoRank(lotto)
+
+    val profitRate = lotto.calculateProfitRate(purchaseAmount)
+    outputView.printProfitRate(profitRate)
 }
