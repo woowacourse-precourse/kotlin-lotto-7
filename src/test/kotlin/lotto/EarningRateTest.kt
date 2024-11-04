b9f3c315776a539527e4b10ea54cbc9fb97e59f5
jhseo12 <jhseo1778@gmail.com>
2024-11-04 15:30:35 +0900

    test: 수익률 계산 테스트 코드 추가

--- src/test/kotlin/lotto/EarningRateTest.kt
+++ src/test/kotlin/lotto/EarningRateTest.kt
@@ -7,15 +7,15 @@ import org.junit.jupiter.api.Test
 class EarningRateTest {
     private val verifyPrize = VerifyPrize()
     private val ticketOne = listOf(
-        listOf(8, 32, 14, 5, 19, 30),
-        listOf(11, 31, 22, 31, 5, 9),
-        listOf(11, 31, 10, 13, 42, 9),
-        listOf(42, 1, 19, 14, 13, 33)
+        listOf(8, 32, 14, 5, 19, 30), // 3
+        listOf(11, 31, 22, 31, 5, 9), // 0
+        listOf(11, 31, 10, 13, 42, 9), // 3
+        listOf(42, 1, 19, 14, 13, 33) // 4
     )
     private val ticketTwo = listOf(
-        listOf(3, 32, 2, 14, 42, 44),
-        listOf(21, 3, 14, 25, 37, 12),
-        listOf(42, 10, 19, 14, 13, 3)
+        listOf(3, 32, 2, 14, 42, 44), // 3
+        listOf(21, 3, 14, 25, 37, 12), // 1
+        listOf(42, 10, 19, 14, 13, 3) // 5 + 1
     )
     private val prizeNumber = listOf(42, 13, 10, 14, 32, 19)
     private val bonusNumber = 3
@@ -28,5 +28,10 @@ class EarningRateTest {
     @Test
     fun `수익률이 올바르게 구해지는지 확인 2`() {
         assertThat(verifyPrize.prizeResult(ticketTwo, prizeNumber, bonusNumber)).isEqualTo(1000166.7)
+    }
+
+    @Test
+    fun `수익률이 올바르게 구해지는지 확인 3`() {
+        assertThat(verifyPrize.prizeResult(ticketOne + ticketTwo, prizeNumber, bonusNumber)).isEqualTo(429500.0)
     }
 }
