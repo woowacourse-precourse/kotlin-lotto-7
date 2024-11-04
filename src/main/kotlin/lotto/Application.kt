d13e5a448d3d21c8e04ad3bdd8579f1a725792ae
jusungwon <tjddnjspower@naver.com>
2024-11-04 18:58:54 +0900

    feat(purchasedLotto): 발행한 로또 수량 출력하는 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,6 +5,12 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("구입금액을 입력해 주세요.")
     val inputPrice = getUserInput()
+    val numberOfLotto = purchasedLotto(inputPrice)
+    println("${numberOfLotto}개를 구매했습니다.")
+
+
 }
 
 private fun getUserInput(): String = Console.readLine()
+
+private fun purchasedLotto(inputPrice: String): Int = inputPrice.toInt() / 1000
