b2c5ef3b481dc0c0df0f914ff6f9857e29968677
Grove1212 <stella223@korea.ac.kr>
2024-11-04 21:46:36 +0900

    refactor: Enum 클래스 적용

--- src/main/kotlin/lotto/controller/LottoResultChecker.kt
+++ src/main/kotlin/lotto/controller/LottoResultChecker.kt
@@ -3,30 +3,43 @@ package lotto.controller
 import lotto.model.Lotto
 
 object LottoResultChecker {
-    fun checkWinningStatus(lottos: List<Lotto>, myLotto: Lotto, myBonus: Int)
-            : Pair<List<Int>, Int> {
-        val winningCounts = MutableList(7) { 0 }
-        var bonusWin = 0;
+
+    fun checkWinningStatus(lottos: List<Lotto>, myLotto: Lotto, myBonus: Int): List<Int> {
+        val ranking = MutableList(6) { 0 }
 
         for (lotto in lottos) {
-            val sameValueOfLotto = lotto.getNumbers().intersect(myLotto.getNumbers().toSet())
-            val matchingCount = sameValueOfLotto.size
-            val containsBonus = myBonus in lotto.getNumbers()
-
-            if (matchingCount >= 3)
-                winningCounts[matchingCount]++
-            if (matchingCount == 5 && containsBonus) {
-                bonusWin++
-                winningCounts[matchingCount]--
+            val (duplicateCount, isBonus) = compareLotto(lotto, myLotto, myBonus)
+            when (duplicateCount) {
+                3 -> ranking[5]++
+                4 -> ranking[4]++
+                5 -> if (isBonus) ranking[2]++ else ranking[3]++
+                6 -> ranking[1]++
             }
         }
-        return Pair(winningCounts, bonusWin)
+        return ranking
     }
 
-    fun calculateProfitRate(money: Int, winningCounts: List<Int>, bonusWin: Int): Double {
-        val profit =
-            winningCounts[3] * 5000 + winningCounts[4] * 50000 + winningCounts[5] * 1500000 + winningCounts[6] * 2000000000 + bonusWin * 30000000
+    private fun compareLotto(purchasedLotto: Lotto, myLotto: Lotto, myBonus: Int): Pair<Int, Boolean> {
+        val sameValueOfLotto = purchasedLotto.getNumbers().intersect(myLotto.getNumbers().toSet())
+        val duplicateCount = sameValueOfLotto.size
+        val isBonus = myBonus in purchasedLotto.getNumbers()
+
+        return Pair(duplicateCount, isBonus)
+    }
+
+    fun calculateProfitRate(money: Int, ranking: List<Int>): Double {
+        val profit = RankPerPrice.entries.toTypedArray().sumOf { rank ->
+            (ranking.getOrNull(rank.ranked) ?: 0) * rank.price
+        }
         val profitRate = profit.toDouble() / money
         return profitRate * 100
+    }
+
+    enum class RankPerPrice(val ranked: Int, val price: Int) {
+        FIRST(1, 2000000000),
+        SECOND(2, 30000000),
+        THIRD(3, 1500000),
+        FORTH(4, 50000),
+        FIFTH(5, 5000);
     }
 }
