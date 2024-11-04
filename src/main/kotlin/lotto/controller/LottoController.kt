b14642221f6aa73ce67db025f87bb2f9fa1d7edb
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-11-04 19:33:50 +0900

    feat(LottoController): Add createResult() function for creating lotto number result
    - Implemented createResult() to create lotto result

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -2,9 +2,12 @@ package lotto.controller
 
 import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
 import lotto.Lotto
+import lotto.validation.InputValidation
 import lotto.view.Input
 
 class LottoController(val input: Input) {
+    private val validator = InputValidation()
+
     private fun getLottoTicketCount(): Int {
         return input.getPurchaseAmount() / 1000
     }
@@ -12,4 +15,18 @@ class LottoController(val input: Input) {
     private fun createRandomLotto(): Lotto {
         return Lotto(pickUniqueNumbersInRange(1, 45, 6).sorted())
     }
+
+    private fun createResult(): List<Int> {
+        val numbers = input.getLottoNumbers()
+        val bonusNumbers = input.getLottoBonusNumber()
+        val resultNumbers: MutableList<Int> = mutableListOf()
+
+        repeat(numbers.size) {
+            resultNumbers.add(numbers[it])
+        }
+        resultNumbers.add(bonusNumbers)
+
+        return validator.lottoNumbersNotDuplicate(resultNumbers.toList())
+    }
+
 }
