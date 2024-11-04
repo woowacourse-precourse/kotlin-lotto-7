7d98efa022b1569c5febc7f9d8ef01d5ad0e28d1
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2024-11-04 19:25:37 +0900

    feat(Application.kt) : 로또 자동 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,17 +3,33 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    inputHowMuch()
+    val money = inputHowMuch()
+    val myLottos = makeMyLotto(money)
+    println()
 }
 
-fun inputHowMuch() {
+fun makeMyLotto(money: Int): List<Lotto> {
+    val amount = howManyBuy(money)
+    return List(amount) { Lotto() }
+}
+
+fun howManyBuy(money: Int): Int {
+    val amount = money / 1000
+    val amountMessage = "개를 구매했습니다."
+    println("$amount$amountMessage")
+    return amount
+}
+
+fun inputHowMuch(): Int {
     val inputMessage = "구입금액을 입력해 주세요."
     println(inputMessage)
     val money = Console.readLine()
+    Console.close()
     validateMoney(money)
+    return money.toInt()
 }
 
-fun validateMoney(input : String) {
+fun validateMoney(input: String) {
     validateEmpty(input)
     validateBlank(input)
     validateBecomeNumber(input)
@@ -21,27 +37,27 @@ fun validateMoney(input : String) {
     validate1000won(input)
 }
 
-fun validateEmpty(input : String){
+fun validateEmpty(input: String) {
     val exceptionMessage = "[ERROR] 입력이 들어오지 않았습니다."
-    require(input.isNotEmpty()){throw IllegalArgumentException(exceptionMessage)}
+    require(input.isNotEmpty()) { throw IllegalArgumentException(exceptionMessage) }
 }
 
-fun validateBlank(input : String){
+fun validateBlank(input: String) {
     val exceptionMessage = "[ERROR] 입력에 공백만이 있습니다."
-    require(input.isNotBlank()){throw IllegalArgumentException(exceptionMessage)}
+    require(input.isNotBlank()) { throw IllegalArgumentException(exceptionMessage) }
 }
 
-fun validateBecomeNumber(input : String){
+fun validateBecomeNumber(input: String) {
     val exceptionMessage = "[ERROR] 정수로 변환할수 없습니다."
-    require(input.toIntOrNull() is Int){throw NumberFormatException(exceptionMessage)}
+    require(input.toIntOrNull() is Int) { throw NumberFormatException(exceptionMessage) }
 }
 
-fun validateNaturalNumber(input: String){
+fun validateNaturalNumber(input: String) {
     val exceptionMessage = "[ERROR] 자연수가 아닙니다."
-    require(0 < input.toInt()){throw IllegalArgumentException(exceptionMessage)}
+    require(0 < input.toInt()) { throw IllegalArgumentException(exceptionMessage) }
 }
 
-fun validate1000won(input: String){
+fun validate1000won(input: String) {
     val exceptionMessage = "[ERROR] 1000원 단위가 아닙니다."
-    require(input.toInt()%1000 == 0){throw IllegalArgumentException(exceptionMessage)}
+    require(input.toInt() % 1000 == 0) { throw IllegalArgumentException(exceptionMessage) }
 }
