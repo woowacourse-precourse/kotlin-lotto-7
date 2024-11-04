b205a85db14a499741379336ad9ddefd402cc423
hwannow <vnfdlvgid061@naver.com>
2024-11-04 16:17:24 +0900

    feat: 출력 구현

--- src/main/kotlin/lotto/constants/ViewConstant.kt
+++ src/main/kotlin/lotto/constants/ViewConstant.kt
@@ -2,6 +2,15 @@ package lotto.constants
 
 object ViewConstant {
     const val INPUT_GUIDE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
-    const val INPUT_GUIDE_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
-    const val INPUT_GUIDE_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val INPUT_GUIDE_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요."
+    const val INPUT_GUIDE_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요."
+
+    const val OUTPUT_PRINT_LOTTO = "\n%d개를 구매했습니다."
+    const val OUTPUT_PRINT_WINNING_STATISTICS = "\n당첨 통계\n---"
+    const val OUTPUT_PRINT_MATCH_3_COUNT = "3개 일치 (5,000원) - %d개"
+    const val OUTPUT_PRINT_MATCH_4_COUNT = "4개 일치 (50,000원) - %d개"
+    const val OUTPUT_PRINT_MATCH_5_COUNT = "5개 일치 (1,500,000원) - %d개"
+    const val OUTPUT_PRINT_MATCH_5_COUNT_AND_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"
+    const val OUTPUT_PRINT_MATCH_6_COUNT = "6개 일치 (2,000,000,000원) - %d개"
+    const val OUTPUT_PRINT_TOTAL_PROFIT = "총 수익률은 %.1f%%입니다."
 }
