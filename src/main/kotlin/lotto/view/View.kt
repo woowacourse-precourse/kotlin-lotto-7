ee322168eb2f1bf01ce52a7fefda3457a0df9785
giovannijunseokim <kingjunseo@gmail.com>
2024-11-04 19:20:49 +0900

    feat: implement View

---
+++ src/main/kotlin/lotto/view/View.kt
@@ -0,0 +1,10 @@
+package lotto.view
+
+interface View {
+    fun buyLottos()
+    fun displayWinningLottos()
+    fun readUserLottoNumbers()
+    fun readUserBonusNumbers()
+    fun displayLottoResult()
+    fun displayReturnRate()
+}
