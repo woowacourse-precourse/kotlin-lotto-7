d9d7da7e06a21e6a05b614037f30a7c8306c7734
yoon <cbj0010@naver.com>
2024-11-04 18:50:11 +0900

    feat : lotto 개수 유효성 체크
    lotto가 6개 뽑히지 않았을 경우 에러를 전달

--- src/main/kotlin/validator/RangeValidator.kt
+++ src/main/kotlin/validator/RangeValidator.kt
@@ -1,6 +1,7 @@
 package validator
 
 import lotto.LottoConstants.LOTTO_NUMBER_RANGE
+import lotto.LottoConstants.LOTTO_NUMBER_SIZE
 
 class RangeValidator(private val range: IntRange = LOTTO_NUMBER_RANGE) : NumbersValidator {
 
@@ -8,9 +9,13 @@ class RangeValidator(private val range: IntRange = LOTTO_NUMBER_RANGE) : Numbers
         require(value.all { it in range }) {
             RANGE_ERROR_FORMAT.format(value)
         }
+        require(value.size == LOTTO_NUMBER_SIZE) {
+            LOTTO_COUNT_ERROR
+        }
     }
 
     companion object {
         private val RANGE_ERROR_FORMAT = "로또 번호는 1~45 사이의 값이여야 합니다."
+        private val LOTTO_COUNT_ERROR = "로또는 6개를 선택해야 됩니다."
     }
 }
