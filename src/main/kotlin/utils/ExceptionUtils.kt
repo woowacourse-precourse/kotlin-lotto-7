ab6ba3f26dbfef655cd430df167b3c335237f796
nuyhhyun <sarahs19991115@gmail.com>
2024-11-04 20:22:32 +0900

    refactor(Exception): 예외 문구 수정

---
+++ src/main/kotlin/utils/ExceptionUtils.kt
@@ -0,0 +1,6 @@
+package utils
+
+object ExceptionUtils {
+    const val WRONG_UNIT_OF_MONEY_MESSAGE: String = "[ERROR] 구입 금액은 1,000원 단위이어야 합니다."
+    const val WRONG_MONEY_INPUT_MESSAGE: String = "[ERROR] 올바른 금액을 입력해 주세요."
+}
