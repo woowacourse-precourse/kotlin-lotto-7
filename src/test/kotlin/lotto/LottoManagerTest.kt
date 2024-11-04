4fb5b3dcea55c93c99d6697e7f5a9cd3d6c89663
ezhoon <david0275@naver.com>
2024-11-04 20:30:19 +0900

    feat: LottoManagerTest 파일 setup 테스트 함수 추가

--- src/test/kotlin/lotto/LottoManagerTest.kt
+++ src/test/kotlin/lotto/LottoManagerTest.kt
@@ -1,4 +1,17 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
 class LottoManagerTest {
+
+    private lateinit var lottoManager: LottoManager
+
+    @BeforeEach
+    fun `8개의 로또 티켓 발행 매니저`() {
+        lottoManager = LottoManager(8)
+    }
+
+
 }
