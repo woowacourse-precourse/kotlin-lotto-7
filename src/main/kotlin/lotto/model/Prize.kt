41bdd6f26112f140602e80fea445fb49c716bf33
Jieun <lje2000lje@naver.com>
2024-11-04 22:09:09 +0900

    feat(Prize.kt): 당첨 갯수에 따라 등수 반환

--- src/main/kotlin/lotto/model/Prize.kt
+++ src/main/kotlin/lotto/model/Prize.kt
@@ -9,4 +9,17 @@ enum class Prize(val matchCount: Int, val prizeMoney: Int, val bonusMatch: Boole
     FOURTH(4, Constants.FOURTH_PRIZE_MONEY),
     FIFTH(3, Constants.FIFTH_PRIZE_MONEY),
     NONE(0, 0);
+
+    companion object {
+        fun getPrize(matchCount: Int, bonusMatch: Boolean): Prize {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && bonusMatch -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> NONE
+            }
+        }
+    }
 }
