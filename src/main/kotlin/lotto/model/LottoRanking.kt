5b4a7c2b727f46d51ce6873896c386543328aa77
eden <devfeijoa@gmail.com>
2024-11-04 22:23:59 +0900

    refactor: 초기화 로직, 등수 카운트 증가 및 상금 계산 로직, 수익률 계산 로직을 각각 분리

--- src/main/kotlin/lotto/model/LottoRanking.kt
+++ src/main/kotlin/lotto/model/LottoRanking.kt
@@ -3,28 +3,42 @@ package lotto.model
 import lotto.model.LottoStore
 import lotto.util.round
 
-class LottoRanking(var countByRanking: MutableMap<LottoResult, Int>, var totalRevenue: Double){
+class LottoRanking(var countByRanking: MutableMap<LottoResult, Int>, var totalRevenue: Double) {
     companion object {
         fun of(lottoResults: List<LottoResult>): LottoRanking {
+            val lottoResultBundle = initializeResultBundle()  // 1. 초기화
+            val prize = calculateTotalPrize(lottoResults, lottoResultBundle)  // 2. 등수에 해당하는 count 증가 및 상금 계산
+            val totalRevenue = calculateROI(lottoResults.size, prize)  // 3. 수익률 계산 및 반올림
+            return LottoRanking(lottoResultBundle, totalRevenue)
+        }
+
+        private fun initializeResultBundle(): MutableMap<LottoResult, Int> {
             val lottoResultBundle = mutableMapOf<LottoResult, Int>()
-            var prize = 0.0
-
             LottoResult.entries.forEach { result -> lottoResultBundle[result] = 0 }
+            return lottoResultBundle
+        }
 
+        private fun calculateTotalPrize(
+            lottoResults: List<LottoResult>,
+            lottoResultBundle: MutableMap<LottoResult, Int>
+        ): Double {
+            var prize = 0.0
             for (result in lottoResults) {
                 lottoResultBundle[result] = (lottoResultBundle[result] ?: 0) + 1
                 prize += result.prize
             }
-
-            prize = calculateROI((lottoResults.size * LottoStore.LOTTO_TICKET_PRICE), prize).round(PRECISION)
-            return LottoRanking(lottoResultBundle, prize)
+            return prize
         }
 
-        private fun calculateROI(initialInvestment: Int, profit: Double): Double {
-            return (profit / initialInvestment) * PERCENT_FACTOR
+        private fun calculateROI(lottoCount: Int, profit: Double): Double {
+            val initialInvestment = lottoCount * LottoStore.LOTTO_TICKET_PRICE
+            val roi = (profit / initialInvestment) * PERCENT_FACTOR
+            return roi.round(PRECISION)
         }
 
         private const val PERCENT_FACTOR = 100
         private const val PRECISION = 2
     }
 }
+
+
