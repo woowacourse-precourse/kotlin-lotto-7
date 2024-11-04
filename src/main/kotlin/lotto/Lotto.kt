baff899f31d338910d22b9afdeefa74b48f6b9cb
PIYUJIN <yjyj020201@gmail.com>
2024-11-04 23:08:14 +0900

    feat($Lotto): 사용자가 로또 당첨 번호를 잘못 입력할 경우 예외처리 기능 구현
    - 로또 당첨 번호가 중복되는 경우
    - 로또 당첨 번호가 1~45까지의 숫자에 포함되지 않는 경우
    - 로또 당첨 번호가 공백인 경우

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,13 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == numbers.distinct().size) { "[ERROR] 로또 당첨 번호는 중복이 아닌 6개의 숫자여야 합니다." }
+        require(numbers.all { it in 1..45}) {"[ERROR] 로또 당첨 번호는 1~45까지의 숫자로만 입력해야 합니다."}
+        require(numbers.isNotEmpty()) {"[ERROR] 로또 당첨 번호는 중복이 아닌 6개의 숫자여야 합니다."}
+    }
     }
 
     // TODO: 추가 기능 구현
