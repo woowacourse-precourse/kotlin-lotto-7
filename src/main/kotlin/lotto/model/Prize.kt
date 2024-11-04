9869b697137c6cb7006a5c37af46af34f5448a5f
cbdhoho <81568368+cbdhoho@users.noreply.github.com>
2024-11-04 15:28:29 +0900

    feat: 상금 enum class 생성

--- src/main/kotlin/lotto/model/Prize.kt
+++ src/main/kotlin/lotto/model/Prize.kt
@@ -1,4 +1,9 @@
 package lotto.model
 
-enum class Prize {
+enum class Prize(count: Int, money: Int) {
+    hitThree(3, 5000),
+    hitFour(4, 50000),
+    hitFive(5, 1500000),
+    hitFiveBonus(5, 30000000),
+    hitSix(6, 2000000000);
 }
