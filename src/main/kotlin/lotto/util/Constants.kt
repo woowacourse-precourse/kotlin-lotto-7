6ead5e9db9a7d0adbe52009e5d7d6515ab64cd9b
Jieun <lje2000lje@naver.com>
2024-11-04 21:29:38 +0900

    feat(Validator.kt): 구입금액 유효성 검사 함수 구현
    빈값이 들어왔는지, 모두 숫자인지,
    0보다 큰 값인지, 1000원 단위인지 검사

---
+++ src/main/kotlin/lotto/util/Constants.kt
@@ -0,0 +1,7 @@
+package lotto.util
+
+class Constants {
+    companion object {
+        const val LOTTO_UNIT_PRICE = 1000
+    }
+}
--- src/main/kotlin/lotto/validator/Validator.kt
+++ src/main/kotlin/lotto/validator/Validator.kt
@@ -1,5 +1,18 @@
 package lotto.validator
 
+import lotto.util.Constants
+import lotto.util.ExceptionConstants
+
 class Validator {
 
+    fun validatePurchaseAmount(amountString: String) {
+        require(amountString.isNotBlank()) { ExceptionConstants.ERROR_EMPTY_PURCHASE_AMOUNT }
+        require(amountString.all { it.isDigit() }) { ExceptionConstants.ERROR_NOT_A_NUMBER }
+        require(amountString.toBigInteger() <= Int.MAX_VALUE.toBigInteger()) { ExceptionConstants.ERROR_OUT_OF_RANGE }
+
+        val amount = amountString.toInt()
+
+        require(amount > 0) { ExceptionConstants.ERROR_INVALID_PURCHASE_AMOUNT }
+        require(amount % Constants.LOTTO_UNIT_PRICE == 0) { ExceptionConstants.ERROR_INVALID_PURCHASE_AMOUNT }
+    }
 }
