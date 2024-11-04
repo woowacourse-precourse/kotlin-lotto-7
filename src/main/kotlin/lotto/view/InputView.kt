b7211283f5cab73894bc673736ee33623f6a05dc
Jieun <lje2000lje@naver.com>
2024-11-04 21:31:36 +0900

    feat(InputView.kt): 구입 금액을 유효성 검사 후 정수로 변환

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,13 +1,21 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.parser.Parser
+import lotto.validator.Validator
 
-class InputView() {
+class InputView(private val validator: Validator, private val parser: Parser) {
 
     fun inputPurchaseAmount(): Int {
         while (true) {
             println("구입금액을 입력해 주세요.")
             val input = Console.readLine()
+            validator.validatePurchaseAmount(input)
+
+            val amount = parser.parsePurchaseAmount(input)
+            println()
+
+            return amount
         }
     }
 }
