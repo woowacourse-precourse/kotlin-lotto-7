2694aab4dcb391f3b653d211bdc691e9667c2ea4
unifolio0 <boho82ha@gmail.com>
2024-11-04 16:29:46 +0900

    test: 로또 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -19,5 +19,24 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun When_6MatchNumbers_Expect_6() {
+        val input = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val expected = 6
+        assert(input.countMatchNumber(Lotto(listOf(1, 2, 3, 4, 5, 6))) == expected)
+    }
+
+    @Test
+    fun When_3MatchNumbers_Expect_3() {
+        val input = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val expected = 3
+        assert(input.countMatchNumber(Lotto(listOf(1, 2, 3, 7, 8, 9))) == expected)
+    }
+
+    @Test
+    fun When_0MatchNumber_Expect_0() {
+        val input = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val expected = 0
+        assert(input.countMatchNumber(Lotto(listOf(7, 8, 9, 10, 11, 12))) == expected)
+    }
 }
