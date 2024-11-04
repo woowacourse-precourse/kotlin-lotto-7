ee9ffa325f7c250690289b3ad64176c3d5255176
ezhoon <david0275@naver.com>
2024-11-04 20:48:46 +0900

    feat: Lotto 테스트 통과를 위한 중복 확인 코드 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == 6){"[ERROR] 중복된 숫자는 발행 불가합니다."}
         initLotto()
     }
 
@@ -10,7 +11,7 @@ class Lotto(private val numbers: List<Int>) {
 
     // [*] 초기화
     fun initLotto() {
-
+        // 추후 필요 시 업데이트
     }
 
     // [*] Getter
