88bcfb872ecdff3bfda1b0c356f108a1928509ee
이지윤 <02leejiyun@naver.com>
2024-11-04 16:03:01 +0900

    feat: 로또 번호가 중복된 경우 예외 처리

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,12 +3,14 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == LOTTO_SIZE) { ERROR_INVALID_SIZE_MESSAGE }
+        require(numbers.toSet().size == LOTTO_SIZE) { ERROR_DUPLICATE_NUMBER_MESSAGE }
     }
 
     fun getNumbers(): List<Int> = numbers
 
     companion object {
         private const val ERROR_INVALID_SIZE_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
+        private const val ERROR_DUPLICATE_NUMBER_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다."
 
         private const val LOTTO_SIZE = 6
     }
