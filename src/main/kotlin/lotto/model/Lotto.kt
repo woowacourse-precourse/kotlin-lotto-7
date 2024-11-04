ab20982df7c26f8f20f112b96fede3ea2247b5ee
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-11-04 23:08:52 +0900

    fix(Lotto): Change condition in calculateMatchResult() function
    - Changed condition from (it in numbers) to (resultNumbers[idx] in numbers) as 'it' represents the index, not the value.

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -17,8 +17,8 @@ class Lotto(private val numbers: List<Int>) {
         val numbersForCheck = numbers.toSet()
         val lottoResult = LottoMatchResult()
 
-        repeat(resultNumbers.size - 1) {
-            if (it in numbersForCheck) lottoResult.matchNumbersCount += 1
+        repeat(resultNumbers.size - 1) { idx ->
+            if (resultNumbers[idx] in numbersForCheck) lottoResult.matchNumbersCount += 1
         }
         if (resultNumbers.last() in numbersForCheck) lottoResult.isMatchBonus = true
 
