16f5d2e81b20fba72f82d2d6ced06beeca9d2a69
ezhoon <david0275@naver.com>
2024-11-04 20:44:40 +0900

    feat: LottoManagerTest 파일 - 당첨 결과가 올바른지 확인하는 Test 함수 추가

--- src/test/kotlin/lotto/LottoManagerTest.kt
+++ src/test/kotlin/lotto/LottoManagerTest.kt
@@ -32,4 +32,25 @@ class LottoManagerTest {
             assertThat(ticket.getLottoNumbers().toSet().size).isEqualTo(6)
         }
     }
+
+    @Test
+    fun `당첨 번호와 비교하여 당첨 결과가 올바른지 확인`() {
+
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+
+        val tickets = listOf(
+            Lotto(listOf(1, 2, 3, 4, 5, 6)), // 6개 일치 -> 1등
+            Lotto(listOf(1, 2, 3, 4, 5, 7)), // 5개 + 보너스 일치 -> 2등
+            Lotto(listOf(1, 2, 3, 4, 5, 8)), // 5개 일치 -> 3등
+            Lotto(listOf(1, 2, 3, 4, 10, 11)), // 4개 일치 -> 4등
+            Lotto(listOf(1, 2, 3, 7, 8, 9)) // 3개 일치 -> 5등
+        )
+
+        val lottoTicketsField = LottoManager::class.java.getDeclaredField("lottoTickets")
+        lottoTicketsField.isAccessible = true
+        lottoTicketsField.set(lottoManager, tickets)
+
+        lottoManager.compareResults(winningNumbers, bonusNumber)
+    }
 }
