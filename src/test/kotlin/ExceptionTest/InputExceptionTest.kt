4b352bc440e610cd076ce7292cc8b6b19d30958d
nuyhhyun <sarahs19991115@gmail.com>
2024-11-04 19:25:45 +0900

    test(Input): 올바른 구입 금액 입력 확인 테스트 작성

--- src/test/kotlin/ExceptionTest/InputExceptionTest.kt
+++ src/test/kotlin/ExceptionTest/InputExceptionTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class InputExceptionTest {
     private val input = InputValidation()
+
     @ParameterizedTest
     @CsvSource("1000, true", "365, false")
     fun `로또 구입 금액은 1,000원 단위이어야 한다`(money: Int, expected: Boolean) {
@@ -15,10 +16,25 @@ class InputExceptionTest {
             // given
 
             // when
-            val result = input.isValidUnit(money)
+            val valid = input.isValidUnit(money)
 
             // then
-            assertThat(result).isEqualTo(expected)
+            assertThat(valid).isEqualTo(expected)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource("-1", "abc", "' '", "\\n", "''", "0")
+    fun `로또 구입 금액은 양의 정수이어야 한다`(moneyInput: String) {
+        assertSimpleTest {
+            // given
+            val money = moneyInput.toIntOrNull()?: 0
+
+            // when
+            val valid = input.isValidMoney(money)
+
+            // then
+            assertThat(valid).isEqualTo(false)
         }
     }
 }
