d9e8a84fbc9b4c7d94751230441ba36b4624ecaf
giovannijunseokim <kingjunseo@gmail.com>
2024-11-04 19:24:22 +0900

    feat: make ReturnRateCalculator singleton

--- src/test/kotlin/lotto/ReturnRateCalculatorTest.kt
+++ src/test/kotlin/lotto/ReturnRateCalculatorTest.kt
@@ -4,13 +4,12 @@ import lotto.domain.ReturnRateCalculator
 import org.junit.jupiter.api.Test
 
 class ReturnRateCalculatorTest {
-    private val returnRateCalculator = ReturnRateCalculator()
 
     @Test
     fun `수익률은 소수점 둘째 자리에서 반올림한다`() {
         val cost = 1_000L
         val profit = 3_567L
-        val returnRate = returnRateCalculator.calculate(cost, profit)
+        val returnRate = ReturnRateCalculator.calculate(cost, profit)
         assert(returnRate.toString().substringAfter(".").length == 1)
     }
 }
