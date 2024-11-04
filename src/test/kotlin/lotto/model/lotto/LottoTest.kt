55af591b38c818d007fd4a7e8b19b7af185e99a4
jerry8282 <ka06196@naver.com>
2024-11-04 21:50:37 +0900

    style: 불필요한 공백 및 import 문 삭제

--- src/test/kotlin/lotto/model/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/model/lotto/LottoTest.kt
@@ -20,13 +20,10 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 1~45사이 이외에 다른 숫자가 있으면 예외가 발생한다 `() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 50))
         }
     }
-
-
 }
--- src/test/kotlin/lotto/model/lotto/LottoTicketTest.kt
+++ src/test/kotlin/lotto/model/lotto/LottoTicketTest.kt
@@ -1,8 +1,8 @@
 package lotto.model.lotto
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class LottoTicketTest {
 
