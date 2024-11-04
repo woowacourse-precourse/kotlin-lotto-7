9bcc88f7bdbd7c13914d60a33e83a26ff149591d
DoableTuple <doabletuple@gmail.com>
2024-11-04 20:56:24 +0900

    refactor: 당첨 통계를 집계하는 기능 분리

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,5 +1,6 @@
 package lotto.controller
 
+import lotto.model.Lotto
 import lotto.model.LottoHandler
 import lotto.model.LottoRank
 import lotto.util.Constant
@@ -7,11 +8,11 @@ import lotto.view.InputView
 import lotto.view.OutputView
 
 class LottoController {
-    fun run() {
-        val inputView = InputView()
-        val outputView = OutputView()
-        val lottoHandler = LottoHandler()
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val lottoHandler = LottoHandler()
 
+    fun run() {
         val lottoMoney = inputView.readLottoMoney()
         val lottoCount = lottoMoney / Constant.LOTTO_PRICE
         outputView.printLottoCount(lottoCount)
@@ -22,6 +23,14 @@ class LottoController {
         val winningNumbers = inputView.readWinningNumbers()
         val bonusNumber = inputView.readBonusNumber()
 
+        val winCounts = getWinCounts(lottos, winningNumbers, bonusNumber)
+        outputView.printWinCounts(winCounts)
+
+        val revenue = getRevenue(winCounts)
+        outputView.printRevenueToCostRatio(revenue, lottoMoney)
+    }
+
+    private fun getWinCounts(lottos: Set<Lotto>, winningNumbers: List<Int>, bonusNumber: Int): Map<LottoRank, Int> {
         val winCounts = mutableMapOf<LottoRank, Int>().withDefault { 0 }
         lottos.forEach {
             val matchCount = it.countMatches(winningNumbers)
@@ -29,10 +38,12 @@ class LottoController {
             val rank = LottoRank.getRank(matchCount, hasBonus)
             winCounts[rank] = winCounts.getValue(rank) + 1
         }
+        return winCounts
+    }
 
-        outputView.printWinCounts(winCounts)
+    private fun getRevenue(winCounts: Map<LottoRank, Int>): Int {
         var revenue = 0
         winCounts.forEach { revenue += it.value * it.key.prize }
-        outputView.printRevenueToCostRatio(revenue, lottoMoney)
+        return revenue
     }
 }
