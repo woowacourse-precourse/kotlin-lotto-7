bbd76767f1bd4e42799b76f730d2a6cae109e262
kangyein9892 <inwon9892@naver.com>
2024-11-04 16:50:54 +0900

    feat(LottoRank.kt): 로또 당첨 등수 계산 및 관리 enum class 추가

---
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -0,0 +1,23 @@
+package lotto.model
+
+enum class LottoRank(val matchCount: Int, val reward: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    companion object {
+        fun fromMatchCount(matchCount: Int, bonusMatch: Boolean): LottoRank {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && bonusMatch -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> NONE
+            }
+        }
+    }
+}
