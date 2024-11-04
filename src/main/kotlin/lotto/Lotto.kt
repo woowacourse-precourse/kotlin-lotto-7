08bf9a5195a9a82c6c0ee10cc06bc134da73c65b
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2024-11-04 19:26:28 +0900

    feat(Lotto.kt) : 자동 응모를 위한 생성자 및 번호 보여주기

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,21 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_COUNT) { "[ERROR] 로또 번호는 ${LOTTO_COUNT}개여야 합니다." }
+        showNumbers()
     }
 
-    // TODO: 추가 기능 구현
+    constructor() : this(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,MAX_LOTTO_NUMBER,LOTTO_COUNT))
+
+    private fun showNumbers(){
+        println(numbers.sorted().joinToString(separator = ",", prefix = "[", postfix = "]"))
+    }
+    companion object {
+        const val LOTTO_COUNT = 6
+        const val MIN_LOTTO_NUMBER = 1
+        const val MAX_LOTTO_NUMBER = 45
+    }
 }
