9b13fe30da0625c2e76978cbce72803b232b8db5
Jinwoo Choi <cjw1234577@gmail.com>
2024-11-04 23:26:18 +0900

    refactor(Message): 구입 금액 입력 이전에 개행 제거 및 구매 결과 출력 시 개행 추가

--- src/main/kotlin/lotto/utils/OutputMessage.kt
+++ src/main/kotlin/lotto/utils/OutputMessage.kt
@@ -1,7 +1,7 @@
 package lotto.utils
 
 enum class OutputMessage(private val message: String) {
-    PURCHASED_LOTTOS("%d개를 구매했습니다."),
+    PURCHASED_LOTTOS("\n%d개를 구매했습니다."),
     MATCH_COUNT("%d개 일치 (%,d원) - %d개"),
     MATCH_COUNT_WITH_BONUS("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
     PROFIT_RATE("총 수익률은 %,.1f%%입니다."),
