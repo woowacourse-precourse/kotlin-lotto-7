121e8d29e7db8f586860204925933529efcf2c6f
이지윤 <02leejiyun@naver.com>
2024-11-04 15:42:20 +0900

    feat: 로또 구입 금액이 1000원 단위가 아닌 경우 예외 처리

--- src/main/kotlin/lotto/domain/PurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -3,6 +3,7 @@ package lotto.domain
 class PurchaseAmount(private val money: Int) {
     init {
         require(money > 0) { ERROR_NEGATIVE_NUMBER_MESSAGE }
+        require(money % LOTTO_PRICE == 0) { ERROR_INVALID_UNIT_MESSAGE }
     }
 
     fun getMoney(): Int = money
@@ -12,6 +13,7 @@ class PurchaseAmount(private val money: Int) {
     companion object {
         private const val LOTTO_PRICE = 1000
 
-        private const val ERROR_NEGATIVE_NUMBER_MESSAGE = "[ERROR] 0보다 큰 값을 입력해야 합니다."
+        private const val ERROR_NEGATIVE_NUMBER_MESSAGE = "[ERROR] 금액은 0보다 큰 값이어야 합니다."
+        private const val ERROR_INVALID_UNIT_MESSAGE = "[ERROR] 금액은 1,000원 단위여야 합니다."
     }
 }
