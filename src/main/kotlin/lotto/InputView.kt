e9a1188f95f63a9cb5b254879cf2674ac63b485b
minseok0416 <day5274567@gmail.com>
2024-11-04 17:57:01 +0900

    feat(InputView): 로또 구입 금액을 입력 받는 함수에 출력문 추가

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun inputLottoPurchaseAmount(): String {
+        println("구입금액을 입력해 주세요.")
         return Console.readLine()
     }
 }
