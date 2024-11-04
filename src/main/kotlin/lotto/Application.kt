a1dd160f6c16fa626268ce1c5c058204a5029703
jusungwon <tjddnjspower@naver.com>
2024-11-04 23:33:20 +0900

    feat(checkWinningNumber): 당첨번호를 입력받아 생성된 랜덤번호와 얼마나 일치하는 지 비교하는 기능 및 README.md 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,3 +1,4 @@
+// Main.kt 파일 수정
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
@@ -6,21 +7,53 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     println("구입금액을 입력해 주세요.")
     val inputPrice = getUserInput()
-    val numberOfLotto = purchasedLotto(inputPrice)
-    println("${numberOfLotto}개를 구매했습니다.")
-    // 로또 번호 출력하기
-    lottoNumbersResult(numberOfLotto)
+    val lottoCount = purchasedLotto(inputPrice)
+    println()
+    println("${lottoCount}개를 구매했습니다.")
+
+    val randomNumbers = randomNumberResult(lottoCount)
+
+    println()
+    println("당첨 번호를 입력해 주세요.")
+    val winningNumber = getWinningNumbers()
+    println()
+    println("보너스 번호를 입력해 주세요.")
+    val bonusNumber = getBonusNumber()
+
+    val matchNumbers = checkWinningNumber(randomNumbers, winningNumber)
+
+
 }
 
 private fun getUserInput(): String = Console.readLine()
 
 private fun purchasedLotto(inputPrice: String): Int = inputPrice.toInt() / 1000
 
-private fun lottoNumbers(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-
-private fun lottoNumbersResult(numberOfLotto: Int) {
-    for (i in 1..numberOfLotto) {
-        val lottoNumber = lottoNumbers()
+private fun randomNumberResult(lottoCount: Int): List<List<Int>> {
+    val lottoNumbers = mutableListOf<List<Int>>()
+    repeat(lottoCount) {
+        val lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6)
         println(lottoNumber)
+        lottoNumbers.add(lottoNumber)
     }
+    return lottoNumbers
 }
+
+private fun getWinningNumbers(): List<Int> {
+    val winningNumber = getUserInput()
+    return winningNumber.split(",").map { it.trim().toInt() }
+}
+
+private fun checkWinningNumber(randomNumbers: List<List<Int>>, winningNumbers: List<Int>): List<Int>{
+    val resultNumber = mutableListOf<Int>()
+    randomNumbers.forEach{ randomNumber ->
+        val matchCount = Lotto(randomNumber).matchCount(winningNumbers)
+        if(matchCount >= 5){
+
+        }
+    }
+    println(resultNumber)
+    return resultNumber
+}
+
+private fun getBonusNumber(): Int = getUserInput().toInt()
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,16 @@
 package lotto
 
+// numbers : 입력받은 로또 번호
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    // 번호 일치 개수 반환
+    fun matchCount(winningNumbers: List<Int>): Int {
+        return numbers.count { it in winningNumbers }
+    }
+
 }
