fcc079ce375a195bdf442ffb17e46912165f54c0
eden <devfeijoa@gmail.com>
2024-11-04 18:44:43 +0900

    feat: 로또 결과에 따른 수익률 계산

--- src/main/kotlin/lotto/model/LottoRanking.kt
+++ src/main/kotlin/lotto/model/LottoRanking.kt
@@ -1,4 +1,30 @@
 package lotto.model
 
+import lotto.model.LottoStore
+import lotto.util.round
+
 class LottoRanking(var countByRanking: MutableMap<LottoResult, Int>, var totalReturn: Double){
+    companion object {
+        fun of(lottoResults: List<LottoResult>): LottoRanking {
+            val lottoResultBundle = mutableMapOf<LottoResult, Int>()
+            var prize = 0.0
+
+            LottoResult.entries.forEach { result -> lottoResultBundle[result] = 0 }
+
+            for (result in lottoResults) {
+                lottoResultBundle[result] = (lottoResultBundle[result] ?: 0) + 1
+                prize += result.prize
+            }
+
+            prize = calculateROI((lottoResults.size * LottoStore.LOTTO_TICKET_PRICE), prize).round(PRECISION)
+            return LottoRanking(lottoResultBundle, prize)
+        }
+
+        private fun calculateROI(initialInvestment: Int, profit: Double): Double {
+            return (profit / initialInvestment) * PERCENT_FACTOR
+        }
+
+        private const val PERCENT_FACTOR = 100
+        private const val PRECISION = 2
+    }
 }
