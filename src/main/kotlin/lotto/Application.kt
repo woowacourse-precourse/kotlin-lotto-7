ab6b18d95d11767c0cf337527f57fd0bd2de2bb8
gixxla <gigixquisite@gmail.com>
2024-11-04 16:16:23 +0900

    feat: 로또 당첨 번호 입력 기능 추가
    - 안내 문구 출력
    - 입력한 번호가 형식에 맞지 않을 경우 IllegalArgumentException 발생
    - 올바른 형식의 번호가 입력될 때까지 위 과정 무한 루프

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,17 +4,28 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    println("구입 금액을 입력해 주세요.")
-    var lottos: ArrayList<Lotto>
+    println("구입 금액을 입력해주세요.")
+    var lottoCount: Int
     do {
         try {
-            val number = getAmount()
+            lottoCount = getAmount()
             println()
-            println("${number}개를 구매했습니다.")
+            println("${lottoCount}개를 구매했습니다.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            continue
+        }
+        break
+    } while (true)
 
-            lottos = createLottos(number)
-            printLottos(lottos)
+    val lottos = createLottos(lottoCount)
+    printLottos(lottos)
 
+    println("당첨 번호를 입력해주세요.")
+    var prizeLotto: Lotto
+    do {
+        try {
+            prizeLotto = getPrizeLotto()
         } catch (e: IllegalArgumentException) {
             println(e.message)
             continue
@@ -48,4 +59,21 @@ fun printLottos(lottos: ArrayList<Lotto>) {
         println(it.getNumbers())
     }
     println()
+}
+
+fun getPrizeLotto(): Lotto {
+    val prizeNumbers = Console.readLine().split(",")
+    return Lotto(checkLotto(prizeNumbers))
+}
+
+fun checkLotto(lottoNumbers: List<String>): List<Int> {
+    try {
+        val mappedLottoNumbers = lottoNumbers.map { it.toInt() }
+        mappedLottoNumbers.forEach {
+            require(it in 1..45) {"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다. 당첨 번호를 다시 입력해주세요."}
+        }
+        return mappedLottoNumbers
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("[ERROR] 쉼표(,) 이외의 구분자를 사용할 수 없습니다. 당첨 번호를 다시 입력해주세요.")
+    }
 }
