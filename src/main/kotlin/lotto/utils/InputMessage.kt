be848c9eda43821108948c97315353ab3ae9e9e4
Jinwoo Choi <cjw1234577@gmail.com>
2024-11-04 23:24:44 +0900

    refactor(InputMessage): 구입 금액 입력 이전에 개행 추가

--- src/main/kotlin/lotto/utils/InputMessage.kt
+++ src/main/kotlin/lotto/utils/InputMessage.kt
@@ -1,7 +1,7 @@
 package lotto.utils
 
 enum class InputMessage(private val message: String) {
-    INPUT_PRICE("구입금액을 입력해 주세요."),
+    INPUT_PRICE("\n구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요"),
     INPUT_BONUS_NUMBER("\n보너스 볼을 입력해 주세요.");
 
