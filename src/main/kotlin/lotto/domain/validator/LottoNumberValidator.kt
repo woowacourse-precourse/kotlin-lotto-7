03ce50f0b1a8b6c870b9374ace4ea0efc65aa79d
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-04 21:41:28 +0900

    feat(LottoNumberValidator): 로또 숫자 범위 에러 메세지 수정
    45보다 클 경우 45보다 같거나 작아야 한다는 정보만 받기 때문에
    에러 유형에 따라 정보를 한정적으로 받아서 한번에 받도록 수정

--- src/main/kotlin/lotto/domain/validator/LottoNumberValidator.kt
+++ src/main/kotlin/lotto/domain/validator/LottoNumberValidator.kt
@@ -5,12 +5,11 @@ import lotto.common.MIN_LOTTO_NUMBER
 
 open class LottoNumberValidator {
     protected fun validateNumberRange(number: Int) {
-        require(number <= MAX_LOTTO_NUMBER) { OVER_LOTTO_NUMBER_ERROR_MESSAGE }
-        require(number >= MIN_LOTTO_NUMBER) { UNDER_LOTTO_NUMBER_ERROR_MESSAGE }
+        require(number <= MAX_LOTTO_NUMBER) { LOTTO_NUMBER_RANGE_ERROR_MESSAGE }
+        require(number >= MIN_LOTTO_NUMBER) { LOTTO_NUMBER_RANGE_ERROR_MESSAGE }
     }
 
     companion object {
-        private const val OVER_LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호는 45보다 작거나 같아야 합니다."
-        private const val UNDER_LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1보다 크거나 같아야 합니다."
+        private const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
     }
 }
