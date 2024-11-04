01fce1ca4853d1ca4a9903a6756d9b3b0111c81f
mcdmaisd <tkim0156@gmail.com>
2024-11-04 22:42:53 +0900

    feat: Lotto 클래스 작성
    입력값의 검증기능 담당

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,37 @@
 package lotto
-
+/**
+ * 로또 번호를 관리하는 클래스
+ */
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        validate()
     }
 
-    // TODO: 추가 기능 구현
+    private fun validate() {
+        validateSize()
+        validateRange()
+        validateDuplicate()
+    }
+
+    private fun validateSize() {
+        require(numbers.size == LottoConfig.LOTTO_NUMBER_COUNT) {
+            Message.ERROR_INVALID_WINNING_NUMBERS
+        }
+    }
+
+    private fun validateRange() {
+        require(numbers.all { it in LottoConfig.MIN_NUMBER..LottoConfig.MAX_NUMBER }) {
+            Message.ERROR_INVALID_NUMBER_RANGE
+        }
+    }
+
+    private fun validateDuplicate() {
+        require(numbers.size == numbers.toSet().size) {
+            Message.ERROR_INVALID_WINNING_NUMBERS
+        }
+    }
+
+    fun getNumbers(): List<Int> {
+        return numbers.sorted()
+    }
 }
