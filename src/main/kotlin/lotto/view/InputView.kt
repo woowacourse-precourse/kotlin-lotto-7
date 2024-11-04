2d00d741137ec28264346a601a6bdf3cc38c8e1e
Jieun <lje2000lje@naver.com>
2024-11-04 21:22:29 +0900

    feat(InputView.kt): 구입금액 입력 함수 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,5 +1,13 @@
 package lotto.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView() {
 
+    fun inputPurchaseAmount(): Int {
+        while (true) {
+            println("구입금액을 입력해 주세요.")
+            val input = Console.readLine()
+        }
+    }
 }
