dbc4e21f9e79cfe2db80511e08e4e67e528b82b9
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-11-04 13:37:42 +0900

    fix: 검증 함수 분리 및 메서드 추가
    1. 검증 함수를 object로 Validator로 옮겨서 사용
    2. 검증된 입력을 사용하므로 init 제거
    3. purchaseAmount 제공하는 메서드 추가

--- src/main/kotlin/lotto/model/LottoFactory.kt
+++ src/main/kotlin/lotto/model/LottoFactory.kt
@@ -1,18 +1,15 @@
 package lotto.model
 
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.util.PurchaseAmountValidator
 
 // 로또를 발행하는 역할(로또 생성)
 // 구입금액을 통해 로또를 발행 -> 발행된 로또
 // 구입금액 제공
-class LottoFactory(private val purchaseAmountInput: String) {
-    val purchaseAmount: Int
-    val lottoQuantity: Int
+class LottoFactory(private val purchaseAmount: Int) {
+    val lottoQuantity: Int = purchaseAmount / 1000
 
-    init {
-        purchaseAmount = getValidatePurchaseAmount()
-        lottoQuantity = purchaseAmount / 1000
-    }
+    fun getPurchaseAmount(): Int = purchaseAmount
 
     private fun createNumber(): List<Int> {
         val randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
@@ -27,20 +24,5 @@ class LottoFactory(private val purchaseAmountInput: String) {
             lottoList.add(lotto)
         }
         return lottoList
-    }
-
-    private fun getValidatePurchaseAmount(): Int {
-        validatePurchaseAmount()
-        val validatedPurchaseAmount = purchaseAmountInput.trim().toInt()
-        return validatedPurchaseAmount
-    }
-
-    private fun validatePurchaseAmount() {
-        require(purchaseAmountInput.isNotEmpty()) { "[ERROR] 구입 금액은 빈 값이 될 수 없습니다." }
-
-        val amount = purchaseAmountInput.trim().toIntOrNull()
-        require(amount != null) { "[ERROR] 구입 금액은 숫자여야 합니다." }
-        require(amount % 1000 == 0) { "[ERROR] 구입 금액은 1,000원 단위여야 합니다." }
-        require(amount <= 1000000000) { "[ERROR] 구입 금액은 1억원 이하여야 합니다." }
     }
 }
