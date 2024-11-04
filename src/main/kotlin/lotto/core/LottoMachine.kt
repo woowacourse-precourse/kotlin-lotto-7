2ea60c33f4313e2ce47e0444a66c6b9f2ba5288c
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-11-04 16:44:35 +0900

    feat: 당첨 번호 요청 기능 추가
    `Lotto` 클래스의 `init`블록에 예외 처리

--- src/main/kotlin/lotto/core/LottoMachine.kt
+++ src/main/kotlin/lotto/core/LottoMachine.kt
@@ -2,6 +2,7 @@ package lotto.core
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.data.Lotto
 import lotto.data.PurchaseAmount
 
 const val TICKET_PRICE = 1000
@@ -9,10 +10,14 @@ const val TICKET_PRICE = 1000
 class LottoMachine {
     private var purchaseAmount: Int = 0
     private val lottoTickets = mutableListOf<List<Int>>()
+    private var winningNumber = listOf<Int>()
+
 
     fun run() {
         requestPurchaseAmount()
         printPurchasedLottoTickets()
+
+        requestWinningNumber()
     }
 
     private fun requestPurchaseAmount() {
@@ -40,6 +45,21 @@ class LottoMachine {
             println("[${userLottoTicket.joinToString(", ")}]")
         }
         println()
+    }
+
+    private fun requestWinningNumber() {
+        while (true) {
+            try {
+                println("당첨 번호를 입력해 주세요.")
+
+                winningNumber = Lotto(Console.readLine()).getWinningNumber()
+                break
+            } catch (e: IllegalArgumentException) {
+                println("잘못된 입력입니다. 다시 입력해 주세요. ${e.message}")
+            } catch (e: NumberFormatException) {
+                println("숫자를 입력해 주세요.")
+            }
+        }
     }
 
 }
--- src/main/kotlin/lotto/data/Lotto.kt
+++ src/main/kotlin/lotto/data/Lotto.kt
@@ -1,9 +1,14 @@
 package lotto.data
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(private val numbers: String) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.split(",").all { it.toIntOrNull() != null }) { "[ERROR] 로또 번호는 정수여야 합니다." }
+        require(numbers.split(",").map { it.toInt() }.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.split(",").all { it.toInt() in 1..45 }) { "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다." }
+        require(numbers.split(",").distinct().size == 6) { "[ERROR] 당첨 번호는 중복될 수 없습니다." }
     }
 
-    // TODO: 추가 기능 구현
+    constructor(numbers: List<Int>) : this(numbers.joinToString(","))   // 단위 테스트 시 인자가 `List<Int>`인 경우
+
+    fun getWinningNumber(): List<Int> = numbers.split(",").map { it.toInt() }
 }
