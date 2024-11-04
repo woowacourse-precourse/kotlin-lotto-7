a998ccfe39cd298e55cd9d1a2d5e784977741461
minseok0416 <day5274567@gmail.com>
2024-11-04 23:00:43 +0900

    feat(OutputView): 당첨 내역을 출력하는 기능 추가

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -7,4 +7,14 @@ class OutputView {
             println(lottoNumbers.joinToString(prefix = "[", postfix = "]", separator = ", "))
         }
     }
+
+    fun printLottoRank(lotto: Lotto) {
+        println("\n당첨 통계")
+        println("---")
+        println("3개 일치 (5,000원) - ${lotto.fifthRank}개")
+        println("4개 일치 (50,000원) - ${lotto.fourthRank}개")
+        println("5개 일치 (1,500,000원) - ${lotto.thirdRank}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${lotto.secondRank}개")
+        println("6개 일치 (2,000,000,000원) - ${lotto.firstRank}개")
+    }
 }
