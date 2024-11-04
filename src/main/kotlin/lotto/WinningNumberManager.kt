a6e9e0d5cee28b8c4ad637944553dd5725c736f7
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-04 15:45:27 +0900

    fix(WinningNumberManager): ERROR 메시지 수정

--- src/main/kotlin/lotto/WinningNumberManager.kt
+++ src/main/kotlin/lotto/WinningNumberManager.kt
@@ -36,7 +36,7 @@ class WinningNumberManager {
     companion object {
         private const val REQUEST_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요."
         private const val REGEX_WINNING_NUMBERS_PATTERN = "^[0-9,]+\$"
-        private const val ERROR_WINNING_NUMBERS_INPUT_MESSAGE = "[ERROR] 당첨 번호 입력은 숫자와 쉼표(,)만 입력 가능합니다."
+        private const val ERROR_WINNING_NUMBERS_INPUT_MESSAGE = "[ERROR] 당첨 번호 입력은 부호가 없는 숫자와 쉼표(,)만 입력 가능합니다."
         private const val ERROR_WINNING_NUMBERS_BLANK_MESSAGE = "[ERROR] 당첨 번호는 빈 값이 올 수 없습니다."
     }
 }
