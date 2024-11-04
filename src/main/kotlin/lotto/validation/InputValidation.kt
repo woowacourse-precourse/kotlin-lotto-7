bd0dc2e3d2c9c2a0f7dbd4330324048b22c7060b
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-11-04 16:58:35 +0900

    feat(Input): Create getLotteryNumbers() function for user input
    - Implemented getLotteryNumbers() to receive result of lotto numbers

--- src/main/kotlin/lotto/validation/InputValidation.kt
+++ src/main/kotlin/lotto/validation/InputValidation.kt
@@ -2,12 +2,18 @@ package lotto.validation
 
 class InputValidation {
     fun typeInt(input: String): Int {
-        return input.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
+        return input.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요. ${input}은 숫자가 아닙니다.")
     }
 
-    fun unitOfNumber (number:Int, unit: Int): Int {
+    fun unitOfNumber(number: Int, unit: Int): Int {
         if (number % unit == 0)
             return number
         throw IllegalArgumentException("${unit}원 단위로 입력해주세요.")
+    }
+
+    fun lotteryNumbersDelimiter(input: String): List<Int> {
+        return input.split(",").map {
+            typeInt(it)
+        }
     }
 }
--- src/main/kotlin/lotto/view/Input.kt
+++ src/main/kotlin/lotto/view/Input.kt
@@ -9,9 +9,17 @@ class Input {
     fun getPurchaseAmount(): Int {
         println("구입금액을 입력해 주세요.")
         return readLine().let {
-            inputValidation.typeInt(it). also { amount ->
-                inputValidation.unitOfNumber(amount, 1000) }
+            inputValidation.typeInt(it).also { amount ->
+                inputValidation.unitOfNumber(amount, 1000)
             }
+        }
+    }
+
+    fun getLotteryNumbers(): List<Int> {
+        println("당첨 번호를 입력해 주세요.")
+        return readLine().let {
+            inputValidation.lotteryNumbersDelimiter(it)
+        }
     }
 
     private fun readLine(): String = Console.readLine()
