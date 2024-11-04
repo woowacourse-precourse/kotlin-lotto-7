ab4731db5c9029511fa90b8c3ae2ba33adda2f9e
Jieun <lje2000lje@naver.com>
2024-11-04 21:12:04 +0900

    feat(Lotto.kt): 로또 번호 반환 함수 구현

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -8,4 +8,8 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.all { it in 1..45 }) { ExceptionConstants.ERROR_INVALID_WINNING_NUMBERS_RANGE }
         require(numbers.distinct().size == 6) { ExceptionConstants.ERROR_DUPLICATE_WINNING_NUMBERS }
     }
+
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
 }
