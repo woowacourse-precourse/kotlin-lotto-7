7c96a235ceeda9f8bbcc22e4ce1e75c91b13d543
wjdrjs00 <kimjk3409@gmail.com>
2024-11-04 17:42:52 +0900

    refactor(domain): WinningNumbers 클래스 메시지 상수화
    예외 처리 메시지 상수화 진행

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -16,7 +16,7 @@ class WinningNumbers(
     }
 
     private fun validateInputNumberNotEmpty(inputNumber: String) =
-        require(inputNumber.isNotEmpty()) { "당첨 번호는 비어있을 수 없습니다." }
+        require(inputNumber.isNotEmpty()) { NUMBER_EMPTY_ERROR_MESSAGE }
 
     private fun splitInputNumber(inputNumber: String): List<String> {
         val rawWinningNumbers = inputNumber.split(",")
@@ -25,7 +25,7 @@ class WinningNumbers(
 
     private fun parseRawWinningNumbers(rawWinningNumbers: List<String>) =
         rawWinningNumbers.map { number ->
-            number.toIntOrNull() ?: throw IllegalArgumentException("당첨 번호는 숫자를 입력해야 합니다.")
+            number.toIntOrNull() ?: throw IllegalArgumentException(NUMBER_ERROR_MESSAGE)
         }
 
     private fun validateWinningNumbers(numbers: List<Int>) {
@@ -35,11 +35,22 @@ class WinningNumbers(
     }
 
     private fun validateSize(numbers: List<Int>) =
-        require(numbers.size == 6) { "당첨 번호는 6개여야 합니다." }
+        require(numbers.size == NUMBER_SIZE) { NUMBER_SIZE_ERROR_MESSAGE }
 
     private fun validateUnique(numbers: List<Int>) =
-        require(numbers.distinct().size == numbers.size) { "당첨 번호는 중복될 수 없습니다." }
+        require(numbers.distinct().size == numbers.size) { NUMBER_DISTINCT_ERROR_MESSAGE }
 
     private fun validateNumberRange(numbers: List<Int>) =
-        require(numbers.all { it in 1..45 }) { "당첨 번호는 1 ~ 45 중 하나여야 합니다." }
+        require(numbers.all { it in START_RANGE..END_RANGE }) { NUMBER_RANGE_ERROR_MESSAGE }
+
+    companion object {
+        private const val START_RANGE = 1
+        private const val END_RANGE = 45
+        private const val NUMBER_SIZE = 6
+        private const val NUMBER_EMPTY_ERROR_MESSAGE = "당첨 번호는 비어있을 수 없습니다."
+        private const val NUMBER_ERROR_MESSAGE = "당첨 번호는 숫자를 입력해야 합니다."
+        private const val NUMBER_SIZE_ERROR_MESSAGE = "당첨 번호는 6개여야 합니다."
+        private const val NUMBER_DISTINCT_ERROR_MESSAGE = "당첨 번호는 중복될 수 없습니다."
+        private const val NUMBER_RANGE_ERROR_MESSAGE = "당첨 번호는 $START_RANGE ~ $END_RANGE 중 하나여야 합니다."
+    }
 }
