a6068655f6f73c7fec313a7dc3549364e602880e
gixxla <gigixquisite@gmail.com>
2024-11-04 15:33:07 +0900

    feat: 로또 발매 기능 추가
    - 구입 개수 안내 문구 출력
    - 입력한 금액만큼의 랜덤 Lotto 객체를 생성
    - 생성한 Lotto 객체들의 numbers 변수를 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,13 +1,20 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("구입 금액을 입력해 주세요.")
+    var lottos: ArrayList<Lotto>
     do {
         try {
             val number = getAmount()
-            println(number)
+            println()
+            println("${number}개를 구매했습니다.")
+
+            lottos = createLottos(number)
+            printLottos(lottos)
+
         } catch (e: IllegalArgumentException) {
             println(e.message)
             continue
@@ -26,4 +33,19 @@ fun checkAmount(amount: Int): Int {
         throw IllegalArgumentException("[ERROR] 1,000원 단위로만 구매 가능합니다. 구입 금액을 다시 입력해주세요.")
     }
     return amount / 1000
+}
+
+fun createLottos(number: Int): ArrayList<Lotto> {
+    val lottos = ArrayList<Lotto>()
+    for (i in 0 until number) {
+        lottos.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
+    }
+    return lottos
+}
+
+fun printLottos(lottos: ArrayList<Lotto>) {
+    lottos.forEach {
+        println(it.getNumbers())
+    }
+    println()
 }
