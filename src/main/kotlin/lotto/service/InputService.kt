c6cf5fc9c483ae7bd01ccdee6f97126cd733ae89
hwsg0912@naver.com <hwsg0912@naver.com>
2024-11-04 17:21:41 +0900

    refactor: InputService 코드 일관성을 위해 상수명 뒤 ERROR 추가

--- src/main/kotlin/lotto/service/InputService.kt
+++ src/main/kotlin/lotto/service/InputService.kt
@@ -6,26 +6,26 @@ class InputService {
     companion object {
         private const val DELIMITER = ","
         private const val ERROR_MESSAGE = "[ERROR]"
-        private const val INPUT_EMPTY_STRING = "$ERROR_MESSAGE 빈 문자열은 입력할 수 없습니다."
-        private const val INVALID_NUMBER = "$ERROR_MESSAGE 유효하지 않은 입력입니다."
+        private const val INPUT_EMPTY_STRING_ERROR = "$ERROR_MESSAGE 빈 문자열은 입력할 수 없습니다."
+        private const val INVALID_NUMBER_ERROR = "$ERROR_MESSAGE 유효하지 않은 입력입니다."
 
         private val inputView = InputView()
 
         fun readLineNumberList(): List<Int> {
             val userInput = inputView.readLine()
-            require(userInput.isNotBlank()) { INPUT_EMPTY_STRING }
+            require(userInput.isNotBlank()) { INPUT_EMPTY_STRING_ERROR }
 
             val numberList = userInput.split(DELIMITER)
                 .map { it.trim() }
-                .onEach { require(it.isNotEmpty()) { INPUT_EMPTY_STRING } }
-                .map { it.toIntOrNull() ?: throw IllegalArgumentException(INVALID_NUMBER) }
+                .onEach { require(it.isNotEmpty()) { INPUT_EMPTY_STRING_ERROR } }
+                .map { it.toIntOrNull() ?: throw IllegalArgumentException(INVALID_NUMBER_ERROR) }
             return numberList
         }
 
         fun readLineNumber(): Int {
             val userInput = inputView.readLine()
-            require(userInput.isNotBlank()) { INPUT_EMPTY_STRING }
-            return userInput.toIntOrNull() ?: throw IllegalArgumentException(INVALID_NUMBER)
+            require(userInput.isNotBlank()) { INPUT_EMPTY_STRING_ERROR }
+            return userInput.toIntOrNull() ?: throw IllegalArgumentException(INVALID_NUMBER_ERROR)
         }
     }
 
