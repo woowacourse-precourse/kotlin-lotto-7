0ee970b6dec6cfe28030d12adce9d77ee183e91e
wjdrjs00 <kimjk3409@gmail.com>
2024-11-04 17:37:52 +0900

    test(domain): LottoTest 테스트 추가
    로또번호가 주어진 범위를 벗어난 경우 예외 테스트
    올바른 값이 주어진 경우 올바른 문자열을 반환하는지 확인 검증
    에외 메시지 상수화

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -18,7 +19,7 @@ class LottoTest {
         }
 
         // Then
-        assertEquals("[ERROR] 로또 번호는 비어있을 수 없습니다.", exception.message)
+        assertEquals(NUMBER_EMPTY_ERROR_MESSAGE, exception.message)
     }
 
     @Test
@@ -32,7 +33,7 @@ class LottoTest {
         }
 
         // Then
-        assertEquals("[ERROR] 로또 번호는 6개여야 합니다.", exception.message)
+        assertEquals(NUMBER_SIZE_ERROR_MESSAGE, exception.message)
     }
 
     @Test
@@ -46,7 +47,7 @@ class LottoTest {
         }
 
         // Then
-        assertEquals("[ERROR] 로또 번호는 6개여야 합니다.", exception.message)
+        assertEquals(NUMBER_SIZE_ERROR_MESSAGE, exception.message)
     }
 
     @Test
@@ -60,7 +61,21 @@ class LottoTest {
         }
 
         // Then
-        assertEquals("[ERROR] 로또 번호는 중복될 수 없습니다.", exception.message)
+        assertEquals(NUMBER_DISTINCT_ERROR_MESSAGE, exception.message)
+    }
+
+    @Test
+    fun `주어진 범위를 벗어난 로또번호가 있는 경우 예외를 발생시킨다`() {
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 60)
+
+        // When
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+
+        // Then
+        assertEquals(NUMBER_RANGE_ERROR_MESSAGE, exception.message)
     }
 
     @Test
@@ -69,10 +84,32 @@ class LottoTest {
         val numbers = listOf(1, 2, 3, 4, 5, 6)
 
         // When
-        val lotto = Lotto(numbers).getLotto()
+        val lotto = Lotto(numbers).getNumbers()
 
         // Then
         assertThat(lotto).isEqualTo(numbers)
+    }
+
+    @Test
+    @DisplayName("올바른 숫자 리스트가 주어진 경우, 올바른 값을 문자열로 반환한다.")
+    fun toStringTest() {
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+
+        // When
+        val lotto = Lotto(numbers).toString()
+
+        // Then
+        assertThat("[1, 2, 3, 4, 5, 6]").isEqualTo(lotto)
+    }
+
+    companion object {
+        private const val START_RANGE = 1
+        private const val END_RANGE = 45
+        private const val NUMBER_EMPTY_ERROR_MESSAGE = "로또 번호는 비어있을 수 없습니다."
+        private const val NUMBER_SIZE_ERROR_MESSAGE = "로또 번호는 6개여야 합니다."
+        private const val NUMBER_DISTINCT_ERROR_MESSAGE = "로또 번호는 중복될 수 없습니다."
+        private const val NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 $START_RANGE ~ $END_RANGE 중 하나여야 합니다."
     }
 
 }
