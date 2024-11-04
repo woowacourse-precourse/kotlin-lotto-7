1a8ef04e480eb344e8beb812dee799a575ea76de
jooiss <jusong513@pusan.ac.kr>
2024-11-04 18:58:35 +0900

    feat: 구입 금액 입력 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val lottoBudget = getLottoBudget()
+}
+
+fun getLottoBudget(): Int {
+    println("구입금액을 입력해 주세요.")
+    val userInput = Console.readLine()
+    val lottoBudget = userInput.toInt()
+    if (lottoBudget % 1000 != 0) {
+        throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해 주세요.")
+    }
+    return lottoBudget
 }
