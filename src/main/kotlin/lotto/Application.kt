0231da77d500325d776435b0fea98f7f24268f8c
jusungwon <tjddnjspower@naver.com>
2024-11-04 18:57:30 +0900

    feat(getUserInput): 로또 구입 금액을 입력받는 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("구입금액을 입력해 주세요.")
+    val inputPrice = getUserInput()
 }
+
+private fun getUserInput(): String = Console.readLine()
