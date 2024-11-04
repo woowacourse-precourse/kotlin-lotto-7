97e86da2f8a14b86ce6cd653b96c69755de85508
minseok0416 <day5274567@gmail.com>
2024-11-04 22:42:40 +0900

    refactor(inputView): 입력받는 함수와 변환하는 함수 합침

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -4,44 +4,29 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
 
-    fun inputPurchaseAmount(): String {
-        val guideMessage = GuideMessage.PurchaseAmount
-        guideMessage.printGuideMessage()
-        return Console.readLine()
-    }
-
     fun getPurchaseAmount(): Int {
-        return inputPurchaseAmount().toInt()
-    }
-
-    fun inputLottoNumbers(): String {
-        val guideMessage = GuideMessage.LottoNumbers
-        guideMessage.printGuideMessage()
-        return Console.readLine()
+        printGuideMessage(GuideMessage.PurchaseAmount)
+        return Console.readLine().toInt()
     }
 
     fun getLottoNumbers(): List<Int> {
-        val lottoNumbers = inputLottoNumbers().split(",")
+        printGuideMessage(GuideMessage.LottoNumbers)
+        val lottoNumbers = Console.readLine().split(",")
         return lottoNumbers.map { it.trim().toInt() }.sorted()
     }
 
-    fun inputBonusNumber(): String {
-        val guideMessage = GuideMessage.BonusNumber
-        guideMessage.printGuideMessage()
-        return Console.readLine()
-    }
-
     fun getBonusNumber(): Int {
-        return inputBonusNumber().toInt()
+        printGuideMessage(GuideMessage.BonusNumber)
+        return Console.readLine().toInt()
     }
 
-    enum class GuideMessage(private val message: String) {
-        PurchaseAmount("구입금액을 입력해 주세요."),
-        LottoNumbers("당첨 번호를 입력해 주세요."),
-        BonusNumber("보너스 번호를 입력해 주세요.");
+    private fun printGuideMessage(guideMessage: GuideMessage) {
+        println(guideMessage.message)
+    }
 
-        fun printGuideMessage() {
-            println(message)
-        }
+    enum class GuideMessage(val message: String) {
+        PurchaseAmount("구입금액을 입력해 주세요."),
+        LottoNumbers("\n당첨 번호를 입력해 주세요."),
+        BonusNumber("\n보너스 번호를 입력해 주세요.");
     }
 }
