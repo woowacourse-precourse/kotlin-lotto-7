da4a5eca48e923aa12dbd4982c3c52672b11dea0
park-hwaan <parkhwan1129@gmail.com>
2024-11-04 15:13:30 +0900

    feat: 로또 결과를 확인하는 기능 추가

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -2,6 +2,8 @@ package lotto.controller
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.model.lotto.Lotto
+import lotto.model.lotto.Prize
+import lotto.model.lotto.WinningNumbers
 import view.InputView
 import view.OutputView
 
@@ -14,4 +16,32 @@ class LottoController(private val inputView: InputView, private val outputView: 
         return List(purchasedAmount) { Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)) }
     }
 
+    private fun countMatchingLotto(lotto: Lotto, winningNumbers: WinningNumbers): Prize? {
+        val matchCount = lotto.countMatchingNumbers(winningNumbers.winningNumbers)
+        val isBonusMatch = lotto.matchBonusNumber(winningNumbers.bonusNumber)
+
+        return when {
+            matchCount == 6 -> Prize.SIX_MATCH
+            matchCount == 5 && isBonusMatch -> Prize.FIVE_MATCH_BONUS
+            matchCount == 5 -> Prize.FIVE_MATCH
+            matchCount == 4 -> Prize.FOUR_MATCH
+            matchCount == 3 -> Prize.THREE_MATCH
+            else -> null
+        }
+    }
+
+    private fun checkLottoResults(purchasedLotto: List<Lotto>, winningNumbers: WinningNumbers): Map<Prize, Int> {
+        val results = mutableMapOf<Prize, Int>().apply {
+            Prize.values().forEach { this[it] = 0 }
+        }
+
+        purchasedLotto.forEach { lotto ->
+            val prize = countMatchingLotto(lotto, winningNumbers)
+            prize?.let {
+                results[it] = results.getValue(it) + 1
+            }
+        }
+
+        return results
+    }
 }
