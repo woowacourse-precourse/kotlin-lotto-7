c4a80417fa4b0cb37d457e6f08420f902c08b95d
jbrunoo <6pizz@naver.com6pizz@naver.comv6pizz@naver.com6pizz@naver.com6pizz@naver.com6pizz@naver.com>
2024-11-04 23:19:02 +0900

    feat: 출력 뷰 구현

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,4 +1,28 @@
 package lotto.view
 
+import lotto.model.Lotto
+import lotto.model.Statistics
+
 class OutputView {
+    fun printPurchaseHistory(lottoList: List<Lotto>) {
+        val sb = StringBuilder()
+        sb.appendLine()
+        sb.appendLine("${lottoList.size}개를 구매했습니다.")
+
+        lottoList.forEach { lotto ->
+            val userLotto = lotto.getNumbers().joinToString(separator = ", ", prefix = "[", postfix = "]")
+            sb.appendLine(userLotto)
+        }
+
+        println(sb)
+    }
+
+    fun printStatistics(statistics: Statistics) {
+        println("\n당첨 통계\n---")
+        val winningHistory = statistics.getWinningHistory()
+        val lottoROI = statistics.getLottoROI()
+
+        println(winningHistory)
+        print(lottoROI)
+    }
 }
