978366ef53c8c007a7975748d46fc97bdef8b8e3
wjdrjs00 <kimjk3409@gmail.com>
2024-11-04 18:25:48 +0900

    feat(app): 로또 게임 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,14 @@
 package lotto
 
+import lotto.controller.LottoController
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    LottoController(
+        inputView = InputView(),
+        outputView = OutputView(),
+    ).run()
+
 }
