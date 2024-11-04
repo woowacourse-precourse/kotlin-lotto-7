fc8e46f0b8a7b750de5189494d7e6fbe67fa1514
yoon <cbj0010@naver.com>
2024-11-04 21:28:47 +0900

    feat : 사용자 입력 뷰 생성

---
+++ src/main/kotlin/lotto/User.kt
@@ -0,0 +1,26 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.LottoConstants.DELIMITER
+
+class User {
+    private fun getUserInput() = Console.readLine()
+    fun inputBonusNum(): Int = Console.readLine().toInt()
+
+    fun inputMoney(): String = Console.readLine()
+    
+    private fun isValidLottoNumbers(): List<Int> {
+        val lottoNumbers = getUserInput()
+        CheckInputValidation.validateNull(lottoNumbers)
+        return lottoNumbers.split(DELIMITER).map { it.toInt() }
+    }
+
+    fun getLottoNumber(): List<Int> {
+        return try {
+            isValidLottoNumbers()
+        } catch (e: IllegalArgumentException) {
+            print(e.message)
+            getLottoNumber()
+        }
+    }
+}
