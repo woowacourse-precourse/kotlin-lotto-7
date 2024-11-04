a73efcef717bcfe923951cb612bd2c2b16ec5027
jooiss <jusong513@pusan.ac.kr>
2024-11-04 23:03:05 +0900

    feat: 로또 번호 중복되지 않도록 예외 상황 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복되지 않아야 합니다." }
     }
 
     override fun toString(): String {
